8a8a06879fedbf0224f8840d527dcb4787b66e39
Jonathan Toppins <jtoppins@redhat.com>
2018-07-24 19:17:40 -0400

    [netdrv] ice: mark driver as tech-preview
    Message-id: <f087de8dbe4e8eac9f0992cc34def0a07aba206d.1532459145.git.jtoppins@redhat.com>
    Patchwork-id: 8526
    O-Subject: [kernel team] [PATCH RHEL8 1/3] [redhat] ice: mark driver as tech-preview
    Bugzilla: 1495347
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Upstream Status: RHEL-only
    Bugzilla: 1495347
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=17334539
    Tested: booted and verified taint flags were set when the driver was loaded.
    
    Signed-off-by: Jonathan Toppins <jtoppins@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -3486,6 +3486,7 @@ static int __init ice_module_init(void)
 
 	pr_info("%s - version %s\n", ice_driver_string, ice_drv_ver);
 	pr_info("%s\n", ice_copyright);
+	mark_tech_preview(DRV_SUMMARY, THIS_MODULE);
 
 	ice_wq = alloc_ordered_workqueue("%s", WQ_MEM_RECLAIM, KBUILD_MODNAME);
 	if (!ice_wq) {
