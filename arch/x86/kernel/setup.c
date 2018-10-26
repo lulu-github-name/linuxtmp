a233b148eac9fc0ab68883142c43f512dd6e1119
David Arcari <darcari@redhat.com>
2018-10-26 19:13:41 -0400

    [x86] mark coffeelake-s/h 8+2 as supported
    Message-id: <1540581221-16907-1-git-send-email-darcari@redhat.com>
    Patchwork-id: 231949
    O-Subject: [RHEL8.0 BZ 1575460] mark coffeelake-s/h 8+2 as supported
    Bugzilla: 1575461 1575460
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1575460
    Bugzilla: http://bugzilla.redhat.com/1575461
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=18925129
    Tested: Verified that the rh_check_supported mesage is not output
    Upstream Status: RHEL Only
    
    CoffeeLake-S (CFL-S) and CoffeeLake-H (CFL-H) 8+2 are cpu model 158
    with stepping 12.
    
    Cc: Prarit Bhargava <prarit@redhat.com>
    Cc: David Arcari <darcari@redhat.com>
    Cc: Steve Best <sbest@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -842,7 +842,7 @@ static bool valid_intel_processor(__u8 family, __u8 model, __u8 stepping)
 		break;
 
 	case INTEL_FAM6_KABYLAKE_DESKTOP:
-		valid = (stepping <= 10);
+		valid = (stepping <= 10 || stepping == 12);
 		break;
 
 	case INTEL_FAM6_KABYLAKE_MOBILE:
