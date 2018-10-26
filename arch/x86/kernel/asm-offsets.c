ff115984abfcb7ad4081caf3da3963d9e8e684d7
Waiman Long <longman@redhat.com>
2018-10-25 21:32:15 -0400

    [x86] x86/asm-offsets: Move TSS_sp0 and TSS_sp1 to asm-offsets.c
    Message-id: <1540503144-25869-5-git-send-email-longman@redhat.com>
    Patchwork-id: 231872
    O-Subject: [RHEL8 PATCH 04/13] x86/asm-offsets: Move TSS_sp0 and TSS_sp1 to asm-offsets.c
    Bugzilla: 1643233
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Christoph von Recklinghausen <crecklin@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1643233
    Conflicts: There is a merge conflict in arch/x86/kernel/asm-offsets.c
    	   because of the RHEL specific IBRS code.
    
    commit 9e97b73fdb235345a826519862a52a7398c89eb8
    Author: Joerg Roedel <jroedel@suse.de>
    Date:   Wed Jul 18 11:40:38 2018 +0200
    
        x86/asm-offsets: Move TSS_sp0 and TSS_sp1 to asm-offsets.c
    
        These offsets will be used in 32 bit assembly code as well, so make them
        available for all of x86 code.
    
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Tested-by: Pavel Machek <pavel@ucw.cz>
        Reviewed-by: Andy Lutomirski <luto@kernel.org>
        Cc: "H . Peter Anvin" <hpa@zytor.com>
        Cc: linux-mm@kvack.org
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Dave Hansen <dave.hansen@intel.com>
        Cc: Josh Poimboeuf <jpoimboe@redhat.com>
        Cc: Juergen Gross <jgross@suse.com>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Borislav Petkov <bp@alien8.de>
        Cc: Jiri Kosina <jkosina@suse.cz>
        Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
        Cc: Brian Gerst <brgerst@gmail.com>
        Cc: David Laight <David.Laight@aculab.com>
        Cc: Denys Vlasenko <dvlasenk@redhat.com>
        Cc: Eduardo Valentin <eduval@amazon.com>
        Cc: Greg KH <gregkh@linuxfoundation.org>
        Cc: Will Deacon <will.deacon@arm.com>
        Cc: aliguori@amazon.com
        Cc: daniel.gruss@iaik.tugraz.at
        Cc: hughd@google.com
        Cc: keescook@google.com
        Cc: Andrea Arcangeli <aarcange@redhat.com>
        Cc: Waiman Long <llong@redhat.com>
        Cc: "David H . Gutteridge" <dhgutteridge@sympatico.ca>
        Cc: joro@8bytes.org
        Link: https://lkml.kernel.org/r/1531906876-13451-2-git-send-email-joro@8bytes.org
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- arch/x86/kernel/asm-offsets.c
+++ arch/x86/kernel/asm-offsets.c
@@ -105,6 +105,10 @@ void common(void) {
 	OFFSET(CPU_ENTRY_AREA_entry_stack, cpu_entry_area, entry_stack_page);
 	DEFINE(SIZEOF_entry_stack, sizeof(struct entry_stack));
 
+	/* Offset for sp0 and sp1 into the tss_struct */
+	OFFSET(TSS_sp0, tss_struct, x86_tss.sp0);
+	OFFSET(TSS_sp1, tss_struct, x86_tss.sp1);
+
 	/* Kernel IBRS speculation control structure */
 	OFFSET(KERNEL_IBRS_SPEC_CTRL_entry, kernel_ibrs_spec_ctrl, entry);
 	OFFSET(KERNEL_IBRS_SPEC_CTRL_exit, kernel_ibrs_spec_ctrl, exit);
