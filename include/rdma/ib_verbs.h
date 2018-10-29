14580cb5b159146f8f760b67a490c4499c5e927b
Alaa Hleihel <ahleihel@redhat.com>
2018-10-29 15:32:06 -0400

    [infiniband] RDMA/netdev: Hoist alloc_netdev_mqs out of the driver
    Message-id: <1540827132-9597-17-git-send-email-ahleihel@redhat.com>
    Patchwork-id: 231995
    O-Subject: [RHEL8.0 BZ 1643103 v2 16/22] RDMA/netdev: Hoist alloc_netdev_mqs out of the driver
    Bugzilla: 1643103
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Honggang Li <honli@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1643103
    Upstream: net-next/master
    Backport notes:
     - Small context conflict in include/rdma/ib_verbs.h due to missing commit
       528922afd41c ("IB: Enable uverbs_destroy_def_handler to be used by drivers")
    
    commit f6a8a19bb11b46d60250ddc4e3e1ba6aa166f488
    Author: Denis Drozdov <denisd@mellanox.com>
    Date:   Tue Aug 14 14:08:51 2018 +0300
    
        RDMA/netdev: Hoist alloc_netdev_mqs out of the driver
    
        netdev has several interfaces that expect to call alloc_netdev_mqs from
        the core code, with the driver only providing the arguments.  This is
        incompatible with the rdma_netdev interface that returns the netdev
        directly.
    
        Thus re-organize the API used by ipoib so that the verbs core code calls
        alloc_netdev_mqs for the driver. This is done by allowing the drivers to
        provide the allocation parameters via a 'get_params' callback and then
        initializing an allocated netdev as a second step.
    
        Fixes: cd565b4b51e5 ("IB/IPoIB: Support acceleration options callbacks")
        Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
        Signed-off-by: Denis Drozdov <denisd@mellanox.com>
        Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>
    
    Cc: Jonathan Toppins <jtoppins@redhat.com>
    Cc: Don Dutile <ddutile@redhat.com>
    Cc: Alaa Hleihel <ahleihel@redhat.com>
    Cc: Doug Ledford <dledford@redhat.com>
    Cc: Honggang Li <honli@redhat.com>
    Cc: Erez Alfasi <ealfasi@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -2209,6 +2209,16 @@ struct rdma_netdev {
 			    union ib_gid *gid, u16 mlid);
 };
 
+struct rdma_netdev_alloc_params {
+	size_t sizeof_priv;
+	unsigned int txqs;
+	unsigned int rxqs;
+	void *param;
+
+	int (*initialize_rdma_netdev)(struct ib_device *device, u8 port_num,
+				      struct net_device *netdev, void *param);
+};
+
 struct ib_port_pkey_list {
 	/* Lock to hold while modifying the list. */
 	spinlock_t                    list_lock;
@@ -2515,8 +2525,8 @@ struct ib_device {
 	/**
 	 * rdma netdev operation
 	 *
-	 * Driver implementing alloc_rdma_netdev must return -EOPNOTSUPP if it
-	 * doesn't support the specified rdma netdev type.
+	 * Driver implementing alloc_rdma_netdev or rdma_netdev_get_params
+	 * must return -EOPNOTSUPP if it doesn't support the specified type.
 	 */
 	struct net_device *(*alloc_rdma_netdev)(
 					struct ib_device *device,
@@ -2525,6 +2535,10 @@ struct ib_device {
 					const char *name,
 					unsigned char name_assign_type,
 					void (*setup)(struct net_device *));
+
+	int (*rdma_netdev_get_params)(struct ib_device *device, u8 port_num,
+				      enum rdma_netdev_t type,
+				      struct rdma_netdev_alloc_params *params);
 
 	struct module               *owner;
 	struct device                dev;
@@ -4090,4 +4104,8 @@ ib_get_vector_affinity(struct ib_device *device, int comp_vector)
  */
 void rdma_roce_rescan_device(struct ib_device *ibdev);
 
+struct net_device *rdma_alloc_netdev(struct ib_device *device, u8 port_num,
+				     enum rdma_netdev_t type, const char *name,
+				     unsigned char name_assign_type,
+				     void (*setup)(struct net_device *));
 #endif /* IB_VERBS_H */
