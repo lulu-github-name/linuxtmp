df6dbc0c79ba9693496ac5a856129612d8b5f134
Herton R. Krzesinski <herton@redhat.com>
2019-02-15 17:12:16 -0500

    [redhat] add support for early y+1 versioning
    Message-id: <20190215171218.9607-3-herton@redhat.com>
    Patchwork-id: 241984
    O-Subject: [RHEL8.0 BZ1677444 PATCH 2/4] [kernel] add support for early y+1 versioning
    Bugzilla: 1677444
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1677444
    Upstream Status: RHEL only
    Tested: rebuild of 'rpmbuild -bp', in-tree, out-of-tree modules
            systemtap (which builds modules too against kernel-devel)
            usage and merge scenarios of early y stream
    
    Early branching/development for the next y+1 stream changes nvr slightly.
    Since streams run in parallel, we can't keep incrementing RHEL_RELEASE
    in both. y+1 stream adds one more number to RHEL_RELEASE. This number
    keeps increasing with each build and resets back to 1 after merge
    from y stream.
    
    This achieves:
    1) unique nvr for both streams that run in parallel
    2) y+1 nvr being higher than y (with frequent merges)
    3) y+1 nvr reflects merge status
    
    Example:
    
     rhel-8.0 branch                rhel-8.1 branch
                                    <8.1 in early y+1 process>
       kernel-4.18.0-58.el8   -->   kernel-4.18.0-58.1.el8
                                    kernel-4.18.0-58.2.el8
                                    kernel-4.18.0-58.3.el8
       kernel-4.18.0-59.el8         kernel-4.18.0-59.1.el8
       kernel-4.18.0-60.el8
       kernel-4.18.0-61.el8   -->   kernel-4.18.0-61.1.el8
       GA
       <zstream>                    <8.1 back to normal y stream process>
                                    kernel-4.18.0-62.el8
       kernel-4.18.0-61.1.1.el8     kernel-4.18.0-62.el8
       kernel-4.18.0-61.1.2.el8     kernel-4.18.0-64.el8
    
    [jstancek, fhrbata: updates/improvements to patch changelog, suggestions
     to early y stream logic in makefiles]
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- redhat/scripts/new_release.sh
+++ redhat/scripts/new_release.sh
@@ -5,7 +5,8 @@ if [ -z "$1" -o -z "$2" ]; then
 fi
 
 RHPATH="$1";
-ZSTREAM_FLAG="$2";
+YSTREAM_FLAG="$2";
+ZSTREAM_FLAG="$3";
 
 if [ -s "$RHPATH/linux-kernel-test.patch" ]; then
 	echo "linux-kernel-test.patch is not empty, aborting" >&2;
@@ -14,15 +15,28 @@ fi
 
 RELEASE=$(sed -n -e 's/^RHEL_RELEASE\ =\ \(.*\)/\1/p' $RHPATH/../Makefile.rhelver)
 
-YVER=$(echo $RELEASE | sed -n 's/^\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)$/\1/p')
+YVER=$(echo $RELEASE | cut -d "." -f 1)
 YVER=${YVER:=$RELEASE}
-ZMAJ=$(echo $RELEASE | sed -n 's/^\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)$/\2/p')
+ZMAJ=$(echo $RELEASE | cut -s -d "." -f 2)
 ZMAJ=${ZMAJ:=0}
-ZMIN=$(echo $RELEASE | sed -n 's/^\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)$/\3/p')
+ZMIN=$(echo $RELEASE | cut -s -d "." -f 3)
 ZMIN=${ZMIN:=0}
 
 if [ "$ZSTREAM_FLAG" == "no" ]; then
-	NEW_RELEASE="$[RELEASE + 1]";
+	if [ "$YSTREAM_FLAG" == "yes" ]; then
+		NEW_RELEASE="$[RELEASE + 1]";
+	else
+		EARLY_YBUILD=$(sed -n -e 's/^EARLY_YBUILD:=\(.*\)/\1/p' $RHPATH/../Makefile.rhelver);
+		EARLY_YRELEASE=$(sed -n -e 's/^EARLY_YRELEASE:=\(.*\)/\1/p' $RHPATH/../Makefile.rhelver);
+		if [ "$EARLY_YBUILD" != "$RELEASE" ]; then
+			NEW_EARLY_YRELEASE=1;
+		else
+			NEW_EARLY_YRELEASE="$[EARLY_YRELEASE + 1]";
+		fi
+		sed -i -e "s/^EARLY_YBUILD:=$EARLY_YBUILD/EARLY_YBUILD:=$RELEASE/" $RHPATH/../Makefile.rhelver;
+		sed -i -e "s/^EARLY_YRELEASE:=$EARLY_YRELEASE/EARLY_YRELEASE:=$NEW_EARLY_YRELEASE/" $RHPATH/../Makefile.rhelver;
+		NEW_RELEASE=$RELEASE;
+	fi
 elif [ "$ZSTREAM_FLAG" == "yes" ]; then
 	NEW_RELEASE=$YVER.$((ZMAJ+1)).1;
 elif [ "$ZSTREAM_FLAG" == "branch" ]; then
