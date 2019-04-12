cd871d5e7478a7ce5a37de5f760845ca58b67e9e
Robert Richter <rrichter@redhat.com>
2019-04-12 08:39:04 -0400

    [include] redhat: Reuse other arch's cpuhp_state to protect kABI
    Message-id: <20190412083905.6688-4-rrichter@redhat.com>
    Patchwork-id: 249723
    O-Subject: [RHEL-8.1 BZ 1658476 v3 3/4] redhat: Reuse other arch's cpuhp_state to protect kABI
    Bugzilla: 1658476
    RH-Acked-by: Jeremy Linton <jlinton@redhat.com>
    RH-Acked-by: Bhupesh Sharma <bhsharma@redhat.com>
    RH-Acked-by: Mark Salter <msalter@redhat.com>
    
    Upstream Status: RHEL only
    
    RHEL only since this fixes a kABI breakage.
    
    Adding enum values in cpuhotplug.h will cause a kABI breakage:
    
    ---
    *** ERROR - ABI BREAKAGE WAS DETECTED ***
    The following symbols have been changed (this will cause an ABI
    breakage):
    __cpuhp_remove_state
    __cpuhp_setup_state
    ---
    
    Avoid this by reusing other arch's enum values.
    
    Signed-off-by: Robert Richter <rrichter@redhat.com>
    
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- include/linux/cpuhotplug.h
+++ include/linux/cpuhotplug.h
@@ -162,9 +162,7 @@ enum cpuhp_state {
 	CPUHP_AP_PERF_ARM_QCOM_L2_ONLINE,
 	CPUHP_AP_PERF_ARM_QCOM_L3_ONLINE,
 	CPUHP_AP_PERF_ARM_APM_XGENE_ONLINE,
-#ifdef CONFIG_THUNDERX2_PMU
-	CPUHP_AP_PERF_ARM_CAVIUM_TX2_UNCORE_ONLINE,
-#endif
+	/* kABI: CPUHP_AP_PERF_ARM_CAVIUM_TX2_UNCORE_ONLINE, */
 	CPUHP_AP_PERF_POWERPC_NEST_IMC_ONLINE,
 	CPUHP_AP_PERF_POWERPC_CORE_IMC_ONLINE,
 	CPUHP_AP_PERF_POWERPC_THREAD_IMC_ONLINE,
@@ -177,6 +175,10 @@ enum cpuhp_state {
 	CPUHP_AP_ACTIVE,
 	CPUHP_ONLINE,
 };
+
+/* Reuse other arch's entries to avoid kABI breakage */
+#define CPUHP_AP_PERF_ARM_CAVIUM_TX2_UNCORE_ONLINE	\
+		CPUHP_AP_PERF_POWERPC_NEST_IMC_ONLINE
 
 int __cpuhp_setup_state(enum cpuhp_state state,	const char *name, bool invoke,
 			int (*startup)(unsigned int cpu),
