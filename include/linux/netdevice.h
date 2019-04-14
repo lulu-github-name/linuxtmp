b59e54fb2d11d78491966e749b1e3b94e3690c63
Ivan Vecera <ivecera@redhat.com>
2019-04-13 21:48:00 -0400

    [net] ndo_bridge_setlink: Add extack
    Message-id: <20190413214805.6159-14-ivecera@redhat.com>
    Patchwork-id: 249808
    O-Subject: [RHEL8.1 net PATCH 13/18] net: ndo_bridge_setlink: Add extack
    Bugzilla: 1690125
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1690125
    
    Conflicts: We need to use RH_KABI_REPLACE macro to add an additional
               extack argument to .ndo_bridge_setlink() callback. This change
               is safe because old binary drivers won't see this new argument
               and semantic of this callback was not changed.
    
    Upstream commit(s):
    commit 2fd527b72bb6f95dfe8a1902e998cb76390c431e
    Author: Petr Machata <petrm@mellanox.com>
    Date:   Wed Dec 12 17:02:48 2018 +0000
    
        net: ndo_bridge_setlink: Add extack
    
        Drivers may not be able to implement a VLAN addition or reconfiguration.
        In those cases it's desirable to explain to the user that it was
        rejected (and why).
    
        To that end, add extack argument to ndo_bridge_setlink. Adapt all users
        to that change.
    
        Following patches will use the new argument in the bridge driver.
    
        Signed-off-by: Petr Machata <petrm@mellanox.com>
        Acked-by: Jiri Pirko <jiri@mellanox.com>
        Reviewed-by: Ido Schimmel <idosch@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1219,7 +1219,7 @@ struct net_device_ops_extended_rh {
  *	entries to skb and update idx with the number of entries.
  *
  * int (*ndo_bridge_setlink)(struct net_device *dev, struct nlmsghdr *nlh,
- *			     u16 flags)
+ *			     u16 flags, struct netlink_ext_ack *extack)
  * int (*ndo_bridge_getlink)(struct sk_buff *skb, u32 pid, u32 seq,
  *			     struct net_device *dev, u32 filter_mask,
  *			     int nlflags)
@@ -1442,9 +1442,13 @@ struct net_device_ops {
 						struct net_device *filter_dev,
 						int *idx);
 
-	int			(*ndo_bridge_setlink)(struct net_device *dev,
+	RH_KABI_REPLACE(int	(*ndo_bridge_setlink)(struct net_device *dev,
 						      struct nlmsghdr *nlh,
-						      u16 flags);
+						      u16 flags),
+			int	(*ndo_bridge_setlink)(struct net_device *dev,
+						      struct nlmsghdr *nlh,
+						      u16 flags,
+						      struct netlink_ext_ack *extack))
 	int			(*ndo_bridge_getlink)(struct sk_buff *skb,
 						      u32 pid, u32 seq,
 						      struct net_device *dev,
