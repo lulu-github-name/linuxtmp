aa172bf6f0ce6ba90a162e8339414ea5a77918ab
Ivan Vecera <ivecera@redhat.com>
2019-04-30 19:56:38 -0400

    [net] skbuff: Rename 'offload_mr_fwd_mark' to 'offload_l3_fwd_mark'
    Message-id: <20190430195639.7023-2-ivecera@redhat.com>
    Patchwork-id: 252115
    O-Subject: [RHEL-8.1 net PATCH 1/2] skbuff: Rename 'offload_mr_fwd_mark' to 'offload_l3_fwd_mark'
    Bugzilla: 1704923
    RH-Acked-by: Hangbin Liu <haliu@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1704923
    
    Conflicts: - Due to missing commit b02597d513a9 ("mlxsw: spectrum:
                 Add NVE packet traps")
               - We need to use RH_KABI_RENAME to rename the field
    
    Upstream commit(s):
    commit 875e8939953483d856de226b72d14c6a000f9457
    Author: Ido Schimmel <idosch@mellanox.com>
    Date:   Tue Dec 4 08:15:10 2018 +0000
    
        skbuff: Rename 'offload_mr_fwd_mark' to 'offload_l3_fwd_mark'
    
        Commit abf4bb6b63d0 ("skbuff: Add the offload_mr_fwd_mark field") added
        the 'offload_mr_fwd_mark' field to indicate that a packet has already
        undergone L3 multicast routing by a capable device. The field is used to
        prevent the kernel from forwarding a packet through a netdev through
        which the device has already forwarded the packet.
    
        Currently, no unicast packet is routed by both the device and the
        kernel, but this is about to change by subsequent patches and we need to
        be able to mark such packets, so that they will no be forwarded twice.
    
        Instead of adding yet another field to 'struct sk_buff', we can just
        rename 'offload_mr_fwd_mark' to 'offload_l3_fwd_mark', as a packet
        either has a multicast or a unicast destination IP.
    
        While at it, add a comment about both 'offload_fwd_mark' and
        'offload_l3_fwd_mark'.
    
        Signed-off-by: Ido Schimmel <idosch@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum.c
@@ -3256,10 +3256,10 @@ static void mlxsw_sp_rx_listener_mark_func(struct sk_buff *skb, u8 local_port,
 	return mlxsw_sp_rx_listener_no_mark_func(skb, local_port, priv);
 }
 
-static void mlxsw_sp_rx_listener_mr_mark_func(struct sk_buff *skb,
+static void mlxsw_sp_rx_listener_l3_mark_func(struct sk_buff *skb,
 					      u8 local_port, void *priv)
 {
-	skb->offload_mr_fwd_mark = 1;
+	skb->offload_l3_fwd_mark = 1;
 	skb->offload_fwd_mark = 1;
 	return mlxsw_sp_rx_listener_no_mark_func(skb, local_port, priv);
 }
@@ -3307,8 +3307,8 @@ out:
 	MLXSW_RXL(mlxsw_sp_rx_listener_mark_func, _trap_id, _action,	\
 		_is_ctrl, SP_##_trap_group, DISCARD)
 
-#define MLXSW_SP_RXL_MR_MARK(_trap_id, _action, _trap_group, _is_ctrl)	\
-	MLXSW_RXL(mlxsw_sp_rx_listener_mr_mark_func, _trap_id, _action,	\
+#define MLXSW_SP_RXL_L3_MARK(_trap_id, _action, _trap_group, _is_ctrl)	\
+	MLXSW_RXL(mlxsw_sp_rx_listener_l3_mark_func, _trap_id, _action,	\
 		_is_ctrl, SP_##_trap_group, DISCARD)
 
 #define MLXSW_SP_EVENTL(_func, _trap_id)		\
@@ -3382,7 +3382,7 @@ static const struct mlxsw_listener mlxsw_sp_listener[] = {
 	MLXSW_SP_RXL_MARK(IPV6_PIM, TRAP_TO_CPU, PIM, false),
 	MLXSW_SP_RXL_MARK(RPF, TRAP_TO_CPU, RPF, false),
 	MLXSW_SP_RXL_MARK(ACL1, TRAP_TO_CPU, MULTICAST, false),
-	MLXSW_SP_RXL_MR_MARK(ACL2, TRAP_TO_CPU, MULTICAST, false),
+	MLXSW_SP_RXL_L3_MARK(ACL2, TRAP_TO_CPU, MULTICAST, false),
 };
 
 static int mlxsw_sp_cpu_policers_set(struct mlxsw_core *mlxsw_core)
