841e35036015a89d4058f9e921e15cb2bc062707
Steve Best <sbest@redhat.com>
2019-05-01 12:07:50 -0400

    [x86] x86/cpu: Add Atom Tremont (Jacobsville)
    Message-id: <20190501120707.3147.27356.sendpatchset@stevebest>
    Patchwork-id: 252211
    O-Subject: [PATCH RHEL8.1 BZ1482401 1/2] x86/cpu: Add Atom Tremont (Jacobsville)
    Bugzilla: 1482401
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    
    RHBZ#:
    ------
    https://bugzilla.redhat.com/show_bug.cgi?id=1482401
    
    Description:
    ------------
    Add the Atom Tremont model number to the Intel family list.
    
    [ Tony: Also update comment at head of file to say "_X" suffix is
      also used for microserver parts. ]
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Cc: Aristeu Rozanski <aris@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: linux-edac <linux-edac@vger.kernel.org>
    Cc: Mauro Carvalho Chehab <mchehab@s-opensource.com>
    Cc: Megha Dey <megha.dey@linux.intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Cc: Rajneesh Bhardwaj <rajneesh.bhardwaj@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: x86-ml <x86@kernel.org>
    Link: https://lkml.kernel.org/r/20190125195902.17109-4-tony.luck@intel.com
    
    Upstream Status:
    ----------------
    commit 00ae831dfe4474ef6029558f5eb3ef0332d80043
    
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=00ae831dfe4474ef6029558f5eb3ef0332d80043
    
    ----------------
    Steve Best
    
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- arch/x86/include/asm/intel-family.h
+++ arch/x86/include/asm/intel-family.h
@@ -6,7 +6,7 @@
  * "Big Core" Processors (Branded as Core, Xeon, etc...)
  *
  * The "_X" parts are generally the EP and EX Xeons, or the
- * "Extreme" ones, like Broadwell-E.
+ * "Extreme" ones, like Broadwell-E, or Atom microserver.
  *
  * Things ending in "2" are usually because we have no better
  * name for them.  There's no processor called "SILVERMONT2".
@@ -72,6 +72,7 @@
 #define INTEL_FAM6_ATOM_GOLDMONT	0x5C
 #define INTEL_FAM6_ATOM_DENVERTON	0x5F /* Goldmont Microserver */
 #define INTEL_FAM6_ATOM_GEMINI_LAKE	0x7A
+#define INTEL_FAM6_ATOM_TREMONT_X	0x86 /* Jacobsville */
 
 /* Xeon Phi */
 
