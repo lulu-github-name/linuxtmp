f18ae3dcf58fbfce7b7a2e113fc2de9e810e7097
Herbert Xu <herbert.xu@redhat.com>
2019-05-06 06:26:40 -0400

    [net] xfrm: Allow Set Mark to be Updated Using UPDSA
    Message-id: <E1hNX5E-0001WV-49@gondobar>
    Patchwork-id: 253683
    O-Subject: [RHEL8.1 v3 PATCH 5/16] xfrm: Allow Set Mark to be Updated Using UPDSA
    Bugzilla: 1702198
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    BZ: 1702198
    Brew https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=21527872
    
    Conflicts: Use output_mark/output_mark_mask instead of smark due to kABI.
    
    commit 6d8e85ffe17895d7bc632dfbaa9e2e33b22fe873
    
    Allow UPDSA to change "set mark" to permit
    policy separation of packet routing decisions from
    SA keying in systems that use mark-based routing.
    
    The set mark, used as a routing and firewall mark
    for outbound packets, is made update-able which
    allows routing decisions to be handled independently
    of keying/SA creation. To maintain consistency with
    other optional attributes, the set mark is only
    updated if sent with a non-zero value.
    
    The per-SA lock and the xfrm_state_lock are taken in
    that order to avoid a deadlock with
    xfrm_timer_handler(), which also takes the locks in
    that order.
    
    Signed-off-by: Nathan Harold <nharold@google.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Herbert Xu <herbert.xu@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -1563,6 +1563,16 @@ out:
 		if (x1->curlft.use_time)
 			xfrm_state_check_expire(x1);
 
+		if (x->output_mark_mask || x->props.output_mark) {
+			spin_lock_bh(&net->xfrm.xfrm_state_lock);
+
+			x1->props.output_mark = x->props.output_mark;
+			x1->output_mark_mask = x->output_mark_mask;
+
+			__xfrm_state_bump_genids(x1);
+			spin_unlock_bh(&net->xfrm.xfrm_state_lock);
+		}
+
 		err = 0;
 		x->km.state = XFRM_STATE_DEAD;
 		__xfrm_state_put(x);
