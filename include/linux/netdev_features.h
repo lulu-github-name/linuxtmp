658e724eca6b68543d305de355148a5469e5014a
Ivan Vecera <ivecera@redhat.com>
2019-05-16 13:46:26 -0400

    [net] Add TLS RX offload feature
    Message-id: <20190516134642.8186-4-ivecera@redhat.com>
    Patchwork-id: 256313
    O-Subject: [RHEL-8.1 net PATCH 03/19] net: Add TLS RX offload feature
    Bugzilla: 1710366
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1710366
    
    Upstream commit(s):
    commit 14136564c8ee94566945e85014019cbdb1716dca
    Author: Ilya Lesokhin <ilyal@mellanox.com>
    Date:   Fri Jul 13 14:33:36 2018 +0300
    
        net: Add TLS RX offload feature
    
        This patch adds a netdev feature to configure TLS RX inline crypto offload.
    
        Signed-off-by: Ilya Lesokhin <ilyal@mellanox.com>
        Signed-off-by: Boris Pismenny <borisp@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- include/linux/netdev_features.h
+++ include/linux/netdev_features.h
@@ -74,7 +74,7 @@ enum {
 	__NETIF_F_RH_KABI_PLACEHOLDER_6,
 	__NETIF_F_RH_KABI_PLACEHOLDER_7,
 	__NETIF_F_RH_KABI_PLACEHOLDER_8,
-	__NETIF_F_RH_KABI_PLACEHOLDER_9,
+	NETIF_F_HW_TLS_RX_BIT,		/* Hardware TLS RX offload */
 
 	NETIF_F_FCOE_CRC_BIT,		/* FCoE CRC32 */
 	NETIF_F_SCTP_CRC_BIT,		/* SCTP checksum offload */
@@ -168,6 +168,7 @@ enum {
 #define NETIF_F_HW_TLS_RECORD	__NETIF_F(HW_TLS_RECORD)
 #define NETIF_F_GSO_UDP_L4	__NETIF_F(GSO_UDP_L4)
 #define NETIF_F_HW_TLS_TX	__NETIF_F(HW_TLS_TX)
+#define NETIF_F_HW_TLS_RX	__NETIF_F(HW_TLS_RX)
 
 #define for_each_netdev_feature(mask_addr, bit)	\
 	for_each_set_bit(bit, (unsigned long *)mask_addr, NETDEV_FEATURE_COUNT)
