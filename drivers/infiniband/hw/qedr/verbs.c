bcc3c2f7428992b81a0c6f39559cdb0219018276
Manish Chopra <mchopra@redhat.com>
2019-05-20 06:45:03 -0400

    [infiniband] RDMA: Cleanup undesired pd->uobject usage
    Message-id: <20190520064507.24882-106-mchopra@redhat.com>
    Patchwork-id: 259014
    O-Subject: [PATCH v2 RHEL8.1 105/109] RDMA: Cleanup undesired pd->uobject usage
    Bugzilla: 1663274
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Honggang Li <honli@redhat.com>
    
    From: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1663274
    Upstream: linux.git
    
    commit e00b64f7c54c4cbd88143bbd43e7c3d61a090e5c
    Author: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
    Date:   Mon Dec 17 17:15:18 2018 +0200
    
        RDMA: Cleanup undesired pd->uobject usage
    
        Drivers should be using udata to determine if a method is invoked from
        user space or kernel space. A pd does not necessarily say a different
        objects is kernel or user.
    
        Transforming the tests to use udata eliminates a large number of uobject
        references from the drivers.
    
        Signed-off-by: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
        Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    
    Backport note:
    ==============
    Differences as this patch was partially backported in qedr by jtoppins
    due to qedr lacking other srq patches support, take remaining qedr hunk
    
    Signed-off-by: Manish Chopra <mchopra@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- drivers/infiniband/hw/qedr/verbs.c
+++ drivers/infiniband/hw/qedr/verbs.c
@@ -1541,7 +1541,7 @@ int qedr_destroy_srq(struct ib_srq *ibsrq)
 	in_params.srq_id = srq->srq_id;
 	dev->ops->rdma_destroy_srq(dev->rdma_ctx, &in_params);
 
-	if (ibsrq->pd->uobject)
+	if (ibsrq->uobject)
 		qedr_free_srq_user_params(srq);
 	else
 		qedr_free_srq_kernel_params(srq);
