9d23f76757c71af220dc2614eacfb1ff89550109
Lyude Paul <lyude@redhat.com>
2019-05-30 01:31:35 -0400

    [drm] drm/i915/icl: Add missing combo PHY lane power setup
    Message-id: <20190530013147.11438-106-lyude@redhat.com>
    Patchwork-id: 261290
    O-Subject: [RHEL8 drm 105/117] drm/i915/icl: Add missing combo PHY lane power setup
    Bugzilla: 1707312 1688524 1685552 1685427 1665631 1665088 1664615 1664609 1664189 1664177 1664175 1664104 1662017 1660302 1643842 1640840 1639990 1639436 1637606 1633845 1633843 1633842 1504286
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Ben Skeggs <bskeggs@redhat.com>
    RH-Acked-by: Jerome Glisse <jglisse@redhat.com>
    RH-Acked-by: Dave Airlie <airlied@redhat.com>
    
    Bugzilla: 1665631,1639436,1633843,1665088,1660302,1504286,1664609,1633842,1707312,1664189,1639990,1633845,1664175,1664615,1685552,1662017,1640840,1688524,1664104,1685427,1637606,1643842,1664177
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=21914589
    Upstream: since next-20190521
    
    commit cfda08cdac3401db03849a62206b539bb4e4632e
    Author:     Imre Deak <imre.deak@intel.com>
    AuthorDate: Thu Apr 25 21:52:53 2019 +0300
    Commit:     Imre Deak <imre.deak@intel.com>
    CommitDate: Thu May 2 17:15:09 2019 +0300
    
        drm/i915/icl: Add missing combo PHY lane power setup
    
        This step of the BSpec combo PHY port enabling is missing, so add it
        now.
    
        v2:
        - Rebased on the new fixed v2 version of the helper.
        v3:
        - Use intel_ instead of icl_ prefix. (Jani)
    
        Reported-by: Ville Syrjala <ville.syrjala@linux.intel.com>
        Cc: Jani Nikula <jani.nikula@intel.com>
        Cc: Madhav Chauhan <madhav.chauhan@intel.com>
        Cc: Ville Syrjala <ville.syrjala@linux.intel.com>
        Signed-off-by: Imre Deak <imre.deak@intel.com>
        Reviewed-by: Jani Nikula <jani.nikula@intel.com>
        Link: https://patchwork.freedesktop.org/patch/msgid/20190425185253.3197-2-imre.deak@intel.com
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- drivers/gpu/drm/i915/intel_ddi.c
+++ drivers/gpu/drm/i915/intel_ddi.c
@@ -3133,6 +3133,15 @@ static void intel_ddi_pre_enable_dp(struct intel_encoder *encoder,
 	else
 		intel_prepare_dp_ddi_buffers(encoder, crtc_state);
 
+	if (intel_port_is_combophy(dev_priv, port)) {
+		bool lane_reversal =
+			dig_port->saved_port_bits & DDI_BUF_PORT_REVERSAL;
+
+		intel_combo_phy_power_up_lanes(dev_priv, port, false,
+					       crtc_state->lane_count,
+					       lane_reversal);
+	}
+
 	intel_ddi_init_dp_buf_reg(encoder);
 	if (!is_mst)
 		intel_dp_sink_dpms(intel_dp, DRM_MODE_DPMS_ON);
