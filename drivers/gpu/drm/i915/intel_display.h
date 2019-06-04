e984cb486c86238180a1e4b68ec0754305b3be1c
Lyude Paul <lyude@redhat.com>
2019-06-03 20:50:21 -0400

    [drm] drm/i915: Overcome display engine stride limits via GTT remapping
    Message-id: <20190603205024.19255-6-lyude@redhat.com>
    Patchwork-id: 261979
    O-Subject: [RHEL8.1 drm PATCH 5/8] drm/i915: Overcome display engine stride limits via GTT remapping
    Bugzilla: 1664969
    RH-Acked-by: Ben Skeggs <bskeggs@redhat.com>
    RH-Acked-by: Dave Airlie <airlied@redhat.com>
    
    Bugzilla: 1664969
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=21965373
    Upstream: since next-20190521
    
    commit 54d4d719fa119c1738f97adc242c05fff224c845
    Author:     Ville Syrjälä <ville.syrjala@linux.intel.com>
    AuthorDate: Thu May 9 15:21:56 2019 +0300
    Commit:     Ville Syrjälä <ville.syrjala@linux.intel.com>
    CommitDate: Mon May 20 18:04:47 2019 +0300
    
        drm/i915: Overcome display engine stride limits via GTT remapping
    
        The display engine stride limits are getting in our way. On SKL+
        we are limited to 8k pixels, which is easily exceeded with three
        4k displays. To overcome this limitation we can remap the pages
        in the GTT to provide the display engine with a view of memory
        with a smaller stride.
    
        The code is mostly already there as We already play tricks with
        the plane surface address and x/y offsets.
    
        A few caveats apply:
        * linear buffers need the fb stride to be page aligned, as
          otherwise the remapped lines wouldn't start at the same
          spot
        * compressed buffers can't be remapped due to the new
          ccs hash mode causing the virtual address of the pages
          to affect the interpretation of the compressed data. IIRC
          the old hash was limited to the low 12 bits so if we were
          using that mode we could remap. As it stands we just refuse
          to remapp with compressed fbs.
        * no remapping gen2/3 as we'd need a fence for the remapped
          vma, which we currently don't have. Need to deal with the
          fence POT requirements, and do something about the gen2
          gtt page size vs tile size difference
    
        v2: Rebase due to is_ccs_modifier()
            Fix up the skl+ stride_mult mess
            memset() the gtt_view because otherwise we could leave
            junk in plane[1] when going from 2 plane to 1 plane format
        v3: intel_check_plane_stride() was split out
        v4: Drop the aligned viewport stuff, it was meant for ccs which
            can't be remapped anyway
        v5: Introduce intel_plane_can_remap()
            Reorder the code so that plane_state->view gets filled
            even for invisible planes, otherwise we'd keep using
            stale values and could explode during remapping. The new
            logic never remaps invisible planes since we don't have
            a viewport, and instead pins the full fb instead
        v6: Fix plane src coord checks after remapping by moving
            plane_state->base.src to the final plane x/y offsets.
            Allow intel_plane_check_stride() to fail even with
            remapping (can happen at least with a linear 64bpp
            fb with a 4k plane and a suitably inconvenient src
            coordinates).
            Improve aux plane FIXME (Daniel)
            Move some code shuffling into a separate patch (Daniel)
    
        Testcase: igt/kms_big_fb
        Cc: Daniel Vetter <daniel@ffwll.ch>
        Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
        Link: https://patchwork.freedesktop.org/patch/msgid/20190509122159.24376-6-ville.syrjala@linux.intel.com
        Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- drivers/gpu/drm/i915/intel_display.h
+++ drivers/gpu/drm/i915/intel_display.h
@@ -28,6 +28,8 @@
 #include <drm/drm_util.h>
 #include <drm/i915_drm.h>
 
+struct intel_plane_state;
+
 enum i915_gpio {
 	GPIOA,
 	GPIOB,
@@ -432,4 +434,6 @@ void intel_link_compute_m_n(u16 bpp, int nlanes,
 			    struct intel_link_m_n *m_n,
 			    bool constant_n);
 bool is_ccs_modifier(u64 modifier);
+bool intel_plane_can_remap(const struct intel_plane_state *plane_state);
+
 #endif
