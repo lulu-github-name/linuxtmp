8d22cf86c203119a9795e8bbfe22e09573487426
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2019-06-04 08:44:27 -0400

    [include] flow_dissector: implements flow dissector BPF hook
    Message-id: <20190604085256.3304-118-yauheni.kaliuta@redhat.com>
    Patchwork-id: 262117
    O-Subject: [RHEL8.1 PATCH v2 118/627] flow_dissector: implements flow dissector BPF hook
    Bugzilla: 1700846
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1700846
    
    Conflicts: with previous KABI workaround
    KABI:
      extend enum bpf_prog_type
      extend bpf_attach_type. It breaks KABI with RHEL8.0 extending size
        of several aarays in cgroup_bpf, see commit
        'bpf: deal with kABI breakage in cgroups'
    
    commit d58e468b1112dcd1d5193c0a89ff9f98b5a3e8b9
    Author: Petar Penkov <ppenkov@google.com>
    Date:   Fri Sep 14 07:46:18 2018 -0700
    
        flow_dissector: implements flow dissector BPF hook
    
        Adds a hook for programs of type BPF_PROG_TYPE_FLOW_DISSECTOR and
        attach type BPF_FLOW_DISSECTOR that is executed in the flow dissector
        path. The BPF program is per-network namespace.
    
        Signed-off-by: Petar Penkov <ppenkov@google.com>
        Signed-off-by: Willem de Bruijn <willemb@google.com>
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/net/net_namespace.h
+++ include/net/net_namespace.h
@@ -42,6 +42,7 @@ struct ctl_table_header;
 struct net_generic;
 struct uevent_sock;
 struct netns_ipvs;
+struct bpf_prog;
 
 
 #define NETDEV_HASHBITS    8
@@ -161,6 +162,7 @@ struct net {
 	atomic_t		fnhe_genid;
 
 	RH_KABI_EXTEND(int	ipv4_sysctl_ip_fwd_update_priority)
+	RH_KABI_EXTEND(struct bpf_prog __rcu	*flow_dissector_prog)
 } __randomize_layout;
 
 #include <linux/seq_file_net.h>
