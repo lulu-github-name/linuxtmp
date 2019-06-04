ccc1841352ef083d398622b4d9ca88e0c0291256
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2019-06-04 08:47:13 -0400

    [netdrv] bpf: call verifier_prep from its callback in struct bpf_offload_dev
    Message-id: <20190604085256.3304-284-yauheni.kaliuta@redhat.com>
    Patchwork-id: 262281
    O-Subject: [RHEL8.1 PATCH v2 284/627] bpf: call verifier_prep from its callback in struct bpf_offload_dev
    Bugzilla: 1700846
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1700846
    
    Conflicts: drivers/net/ethernet/netronome/nfp/bpf/offload.c due to
      skipped nfp updates. Coming with a separate patchset.
    
      Leave BPF_OFFLOAD_VERIFIER_PREP in enum bpf_netdev_command to keep
      nfp driver buildable.
    
      Leave struct netdev_bpf::verifier to keep nfp driver buildable.
    
      Leave struct bpf_prog_offload::dev_ops to keep nfp driver
      buidable.
    
    KABI: structure bpf_prog_offload_ops is not protected by KABI, safe
      to extend.
    
    commit 00db12c3d141356a4d1e6b6f688e0d5ed3b1f757
    Author: Quentin Monnet <quentin.monnet@netronome.com>
    Date:   Fri Nov 9 13:03:28 2018 +0000
    
        bpf: call verifier_prep from its callback in struct bpf_offload_dev
    
        In a way similar to the change previously brought to the verify_insn
        hook and to the finalize callback, switch to the newly added ops in
        struct bpf_prog_offload for calling the functions used to prepare driver
        verifiers.
    
        Since the dev_ops pointer in struct bpf_prog_offload is no longer used
        by any callback, we can now remove it from struct bpf_prog_offload.
    
        Signed-off-by: Quentin Monnet <quentin.monnet@netronome.com>
        Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -272,6 +272,7 @@ struct bpf_prog_offload_ops {
 	int (*insn_hook)(struct bpf_verifier_env *env,
 			 int insn_idx, int prev_insn_idx);
 	RH_KABI_EXTEND(int (*finalize)(struct bpf_verifier_env *env))
+	RH_KABI_EXTEND(int (*prepare)(struct net_device *netdev, struct bpf_verifier_env *env))
 };
 
 struct bpf_prog_offload {
