38e5439de5a9acb437093e346bf8c51d6d4ca370
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2019-06-04 08:46:28 -0400

    [net] bpf, verifier: fix register type dump in xadd and st
    Message-id: <20190604085256.3304-239-yauheni.kaliuta@redhat.com>
    Patchwork-id: 262236
    O-Subject: [RHEL8.1 PATCH v2 239/627] bpf, verifier: fix register type dump in xadd and st
    Bugzilla: 1700846
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1700846
    
    commit 2a159c6f82381a458bc56e7e202b6bee57a2ccb7
    Author: Daniel Borkmann <daniel@iogearbox.net>
    Date:   Sun Oct 21 02:09:24 2018 +0200
    
        bpf, verifier: fix register type dump in xadd and st
    
        Using reg_type_str[insn->dst_reg] is incorrect since insn->dst_reg
        contains the register number but not the actual register type. Add
        a small reg_state() helper and use it to get to the type. Also fix
        up the test_verifier test cases that have an incorrect errstr.
    
        Fixes: 9d2be44a7f33 ("bpf: Reuse canonical string formatter for ctx errs")
        Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -1531,14 +1531,19 @@ static bool __is_pointer_value(bool allow_ptr_leaks,
 	return reg->type != SCALAR_VALUE;
 }
 
+static struct bpf_reg_state *reg_state(struct bpf_verifier_env *env, int regno)
+{
+	return cur_regs(env) + regno;
+}
+
 static bool is_pointer_value(struct bpf_verifier_env *env, int regno)
 {
-	return __is_pointer_value(env->allow_ptr_leaks, cur_regs(env) + regno);
+	return __is_pointer_value(env->allow_ptr_leaks, reg_state(env, regno));
 }
 
 static bool is_ctx_reg(struct bpf_verifier_env *env, int regno)
 {
-	const struct bpf_reg_state *reg = cur_regs(env) + regno;
+	const struct bpf_reg_state *reg = reg_state(env, regno);
 
 	return reg->type == PTR_TO_CTX ||
 	       reg->type == PTR_TO_SOCKET;
@@ -1546,7 +1551,7 @@ static bool is_ctx_reg(struct bpf_verifier_env *env, int regno)
 
 static bool is_pkt_reg(struct bpf_verifier_env *env, int regno)
 {
-	const struct bpf_reg_state *reg = cur_regs(env) + regno;
+	const struct bpf_reg_state *reg = reg_state(env, regno);
 
 	return type_is_pkt_pointer(reg->type);
 }
@@ -1961,7 +1966,8 @@ static int check_xadd(struct bpf_verifier_env *env, int insn_idx, struct bpf_ins
 	if (is_ctx_reg(env, insn->dst_reg) ||
 	    is_pkt_reg(env, insn->dst_reg)) {
 		verbose(env, "BPF_XADD stores into R%d %s is not allowed\n",
-			insn->dst_reg, reg_type_str[insn->dst_reg]);
+			insn->dst_reg,
+			reg_type_str[reg_state(env, insn->dst_reg)->type]);
 		return -EACCES;
 	}
 
@@ -1986,7 +1992,7 @@ static int check_stack_boundary(struct bpf_verifier_env *env, int regno,
 				int access_size, bool zero_size_allowed,
 				struct bpf_call_arg_meta *meta)
 {
-	struct bpf_reg_state *reg = cur_regs(env) + regno;
+	struct bpf_reg_state *reg = reg_state(env, regno);
 	struct bpf_func_state *state = func(env, reg);
 	int off, i, slot, spi;
 
@@ -5429,7 +5435,8 @@ static int do_check(struct bpf_verifier_env *env)
 
 			if (is_ctx_reg(env, insn->dst_reg)) {
 				verbose(env, "BPF_ST stores into R%d %s is not allowed\n",
-					insn->dst_reg, reg_type_str[insn->dst_reg]);
+					insn->dst_reg,
+					reg_type_str[reg_state(env, insn->dst_reg)->type]);
 				return -EACCES;
 			}
 
