b9f03b010c3b77e772e02e4bbe677db7a6040e87
Ivan Vecera <ivecera@redhat.com>
2019-06-07 19:19:58 -0400

    [netdrv] mlxsw: spectrum_switchdev: Process learned VxLAN FDB entries
    Message-id: <20190607192438.30003-207-ivecera@redhat.com>
    Patchwork-id: 263394
    O-Subject: [RHEL-8.1 PATCH 206/486] mlxsw: spectrum_switchdev: Process learned VxLAN FDB entries
    Bugzilla: 1680241
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1680241
    
    Conflicts: Added several hunks from commit 6685987c2958 ("switchdev:
               Add extack argument to call_switchdev_notifiers()") that is
    	   backported but these hunks were N/A.
    
    Upstream commit(s):
    commit 981f084b36754b4e5179e8a3fbba554ba0183f0a
    Author: Ido Schimmel <idosch@mellanox.com>
    Date:   Wed Nov 21 08:02:47 2018 +0000
    
        mlxsw: spectrum_switchdev: Process learned VxLAN FDB entries
    
        Start processing two new entry types in addition to current ones:
        * Learned unicast tunnel entry
        * Aged-out unicast tunnel entry
    
        In both cases the device reports on a new {MAC, FID, IP address} tuple
        that was learned / aged-out. Based on this notification, the driver
        instructs the device to add / delete the entry to / from its database.
    
        The driver also makes sure to notify the bridge and VxLAN drivers about
        the new entry.
    
        Signed-off-by: Ido Schimmel <idosch@mellanox.com>
        Reviewed-by: Petr Machata <petrm@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
@@ -7197,7 +7197,8 @@ static void mlxsw_sp_rif_vlan_fdb_del(struct mlxsw_sp_rif *rif, const char *mac)
 
 	info.addr = mac;
 	info.vid = vid;
-	call_switchdev_notifiers(SWITCHDEV_FDB_DEL_TO_BRIDGE, dev, &info.info);
+	call_switchdev_notifiers(SWITCHDEV_FDB_DEL_TO_BRIDGE, dev, &info.info,
+				 NULL);
 }
 
 static const struct mlxsw_sp_rif_ops mlxsw_sp_rif_vlan_ops = {
@@ -7284,7 +7285,8 @@ static void mlxsw_sp_rif_fid_fdb_del(struct mlxsw_sp_rif *rif, const char *mac)
 
 	info.addr = mac;
 	info.vid = 0;
-	call_switchdev_notifiers(SWITCHDEV_FDB_DEL_TO_BRIDGE, dev, &info.info);
+	call_switchdev_notifiers(SWITCHDEV_FDB_DEL_TO_BRIDGE, dev, &info.info,
+				 NULL);
 }
 
 static const struct mlxsw_sp_rif_ops mlxsw_sp_rif_fid_ops = {
