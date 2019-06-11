70f562ac5a077b8fd2d5570770950916ea5e3a1b
Alaa Hleihel <ahleihel@redhat.com>
2019-06-11 12:22:07 -0400

    [infiniband] RDMA/mlx5: Add a new flow action verb - modify header
    Message-id: <20190611122401.30768-14-ahleihel@redhat.com>
    Patchwork-id: 264580
    O-Subject: [RHEL8.1 BZ 1691234 v2 013/127] RDMA/mlx5: Add a new flow action verb - modify header
    Bugzilla: 1691234 1663215
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1691234
    Bugzilla: http://bugzilla.redhat.com/1663215
    Upstream: v4.20-rc1
    Conflicts:
     - Adapted the code to API changes made by backported commit by RDMA maintainer:
       0cbf432db405 ("RDMA/uverbs: Use a linear list to describe the compiled-in uapi")
       15a1b4becba8 ("RDMA/uverbs: Do not pass ib_uverbs_file to ioctl methods")
    
    commit b4749bf25652689d8e33827460266b78bb2ec42c
    Author: Mark Bloch <markb@mellanox.com>
    Date:   Tue Aug 28 14:18:51 2018 +0300
    
        RDMA/mlx5: Add a new flow action verb - modify header
    
        Expose the ability to create a flow action which changes packet
        headers. The data passed from userspace should be modify header actions as
        defined by HW specification.
    
        Signed-off-by: Mark Bloch <markb@mellanox.com>
        Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
        Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/infiniband/hw/mlx5/mlx5_ib.h
+++ drivers/infiniband/hw/mlx5/mlx5_ib.h
@@ -153,6 +153,10 @@ struct mlx5_ib_pd {
 	u32			pdn;
 };
 
+enum {
+	MLX5_IB_FLOW_ACTION_MODIFY_HEADER,
+};
+
 #define MLX5_IB_FLOW_MCAST_PRIO		(MLX5_BY_PASS_NUM_PRIOS - 1)
 #define MLX5_IB_FLOW_LAST_PRIO		(MLX5_BY_PASS_NUM_REGULAR_PRIOS - 1)
 #if (MLX5_IB_FLOW_LAST_PRIO <= 0)
@@ -816,6 +820,11 @@ struct mlx5_ib_flow_action {
 			u64			    ib_flags;
 			struct mlx5_accel_esp_xfrm *ctx;
 		} esp_aes_gcm;
+		struct {
+			struct mlx5_ib_dev *dev;
+			u32 sub_type;
+			u32 action_id;
+		} flow_action_raw;
 	};
 };
 
@@ -1239,6 +1248,7 @@ struct mlx5_ib_flow_handler *mlx5_ib_raw_fs_rule_add(
 	struct mlx5_ib_dev *dev, struct mlx5_ib_flow_matcher *fs_matcher,
 	void *cmd_in, int inlen, int dest_id, int dest_type);
 bool mlx5_ib_devx_is_flow_dest(void *obj, int *dest_id, int *dest_type);
+void mlx5_ib_destroy_flow_action_raw(struct mlx5_ib_flow_action *maction);
 #else
 static inline int
 mlx5_ib_devx_create(struct mlx5_ib_dev *dev,
@@ -1250,6 +1260,11 @@ static inline bool mlx5_ib_devx_is_flow_dest(void *obj, int *dest_id,
 {
 	return false;
 }
+static inline void
+mlx5_ib_destroy_flow_action_raw(struct mlx5_ib_flow_action *maction)
+{
+	return;
+};
 #endif
 static inline void init_query_mad(struct ib_smp *mad)
 {
