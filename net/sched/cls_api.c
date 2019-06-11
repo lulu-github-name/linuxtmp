f16b718610f701cbf01192a648cf3e2883fdf5ea
Ivan Vecera <ivecera@redhat.com>
2019-06-10 21:17:38 -0400

    [net] net_sched: refetch skb protocol for each filter
    Message-id: <20190610212005.12299-48-ivecera@redhat.com>
    Patchwork-id: 264032
    O-Subject: [RHEL-8.1 net PATCH 047/194] net_sched: refetch skb protocol for each filter
    Bugzilla: 1688236 1687456
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Petr Oros <poros@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1688236
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1687456
    
    Upstream commit(s):
    commit cd0c4e70fc0ccfa705cdf55efb27519ce9337a26
    Author: Cong Wang <xiyou.wangcong@gmail.com>
    Date:   Fri Jan 11 18:55:42 2019 -0800
    
        net_sched: refetch skb protocol for each filter
    
        Martin reported a set of filters don't work after changing
        from reclassify to continue. Looking into the code, it
        looks like skb protocol is not always fetched for each
        iteration of the filters. But, as demonstrated by Martin,
        TC actions could modify skb->protocol, for example act_vlan,
        this means we have to refetch skb protocol in each iteration,
        rather than using the one we fetch in the beginning of the loop.
    
        This bug is _not_ introduced by commit 3b3ae880266d
        ("net: sched: consolidate tc_classify{,_compat}"), technically,
        if act_vlan is the only action that modifies skb protocol, then
        it is commit c7e2b9689ef8 ("sched: introduce vlan action") which
        introduced this bug.
    
        Reported-by: Martin Olsson <martin.olsson+netdev@sentorsecurity.com>
        Cc: Jamal Hadi Salim <jhs@mojatatu.com>
        Cc: Jiri Pirko <jiri@resnulli.us>
        Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
        Acked-by: Jamal Hadi Salim <jhs@mojatatu.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- net/sched/cls_api.c
+++ net/sched/cls_api.c
@@ -1285,7 +1285,6 @@ EXPORT_SYMBOL(tcf_block_cb_unregister);
 int tcf_classify(struct sk_buff *skb, const struct tcf_proto *tp,
 		 struct tcf_result *res, bool compat_mode)
 {
-	__be16 protocol = tc_skb_protocol(skb);
 #ifdef CONFIG_NET_CLS_ACT
 	const int max_reclassify_loop = 4;
 	const struct tcf_proto *orig_tp = tp;
@@ -1295,6 +1294,7 @@ int tcf_classify(struct sk_buff *skb, const struct tcf_proto *tp,
 reclassify:
 #endif
 	for (; tp; tp = rcu_dereference_bh(tp->next)) {
+		__be16 protocol = tc_skb_protocol(skb);
 		int err;
 
 		if (tp->protocol != protocol &&
@@ -1327,7 +1327,6 @@ reset:
 	}
 
 	tp = first_tp;
-	protocol = tc_skb_protocol(skb);
 	goto reclassify;
 #endif
 }
