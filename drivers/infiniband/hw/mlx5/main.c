f67337a5f3eea9aa61b4f6587e6303f752cc291a
Alaa Hleihel <ahleihel@redhat.com>
2019-06-11 12:54:37 -0400

    [infiniband] RDMA/mlx5: Use the uapi disablement APIs instead of code
    Message-id: <20190611125818.20321-18-ahleihel@redhat.com>
    Patchwork-id: 264680
    O-Subject: [RHEL8.1 BZ 1691236 v2 017/238] RDMA/mlx5: Use the uapi disablement APIs instead of code
    Bugzilla: 1691236
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1691236
    Upstream: v5.0-rc1
    
    commit 36e235c8829935a59d4652c878cffb08229205c2
    Author: Jason Gunthorpe <jgg@mellanox.com>
    Date:   Mon Nov 12 22:59:53 2018 +0200
    
        RDMA/mlx5: Use the uapi disablement APIs instead of code
    
        Rely on UAPI_DEF_IS_OBJ_SUPPORTED instead of manipulating the contents of
        the driver's definition list.
    
        Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
        Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/infiniband/hw/mlx5/main.c
+++ drivers/infiniband/hw/mlx5/main.c
@@ -5548,6 +5548,7 @@ ADD_UVERBS_ATTRIBUTES_SIMPLE(
 
 static const struct uapi_definition mlx5_ib_defs[] = {
 #if IS_ENABLED(CONFIG_INFINIBAND_USER_ACCESS)
+	UAPI_DEF_CHAIN(mlx5_ib_devx_defs),
 	UAPI_DEF_CHAIN(mlx5_ib_flow_defs),
 #endif
 
@@ -5556,27 +5557,6 @@ static const struct uapi_definition mlx5_ib_defs[] = {
 	UAPI_DEF_CHAIN_OBJ_TREE(UVERBS_OBJECT_DM, &mlx5_ib_dm),
 	{}
 };
-
-static int populate_specs_root(struct mlx5_ib_dev *dev)
-{
-	struct uapi_definition *defs = dev->driver_defs;
-
-#if IS_ENABLED(CONFIG_INFINIBAND_USER_ACCESS)
-	if (MLX5_CAP_GEN_64(dev->mdev, general_obj_types) &
-	    MLX5_GENERAL_OBJ_TYPES_CAP_UCTX)
-		*defs++ = (struct uapi_definition)UAPI_DEF_CHAIN(
-			mlx5_ib_devx_defs);
-#endif
-
-	*defs++ = (struct uapi_definition)UAPI_DEF_CHAIN(mlx5_ib_defs);
-	*defs++ = (struct uapi_definition){};
-	WARN_ON(defs - dev->driver_defs >= ARRAY_SIZE(dev->driver_defs));
-
-	if (IS_ENABLED(CONFIG_INFINIBAND_USER_ACCESS))
-		dev->ib_dev.driver_def = dev->driver_defs;
-
-	return 0;
-}
 
 static int mlx5_ib_read_counters(struct ib_counters *counters,
 				 struct ib_counters_read_attr *read_attr,
@@ -5916,6 +5896,9 @@ int mlx5_ib_stage_caps_init(struct mlx5_ib_dev *dev)
 	dev->ib_dev.driver_id = RDMA_DRIVER_MLX5;
 	ib_set_device_ops(&dev->ib_dev, &mlx5_ib_dev_ops);
 
+	if (IS_ENABLED(CONFIG_INFINIBAND_USER_ACCESS))
+		dev->ib_dev.driver_def = mlx5_ib_defs;
+
 	err = init_node_data(dev);
 	if (err)
 		return err;
@@ -6141,11 +6124,6 @@ void mlx5_ib_stage_bfrag_cleanup(struct mlx5_ib_dev *dev)
 	mlx5_free_bfreg(dev->mdev, &dev->bfreg);
 }
 
-static int mlx5_ib_stage_populate_specs(struct mlx5_ib_dev *dev)
-{
-	return populate_specs_root(dev);
-}
-
 int mlx5_ib_stage_ib_reg_init(struct mlx5_ib_dev *dev)
 {
 	const char *name;
@@ -6280,9 +6258,6 @@ static const struct mlx5_ib_profile pf_profile = {
 	STAGE_CREATE(MLX5_IB_STAGE_PRE_IB_REG_UMR,
 		     NULL,
 		     mlx5_ib_stage_pre_ib_reg_umr_cleanup),
-	STAGE_CREATE(MLX5_IB_STAGE_SPECS,
-		     mlx5_ib_stage_populate_specs,
-		     NULL),
 	STAGE_CREATE(MLX5_IB_STAGE_IB_REG,
 		     mlx5_ib_stage_ib_reg_init,
 		     mlx5_ib_stage_ib_reg_cleanup),
@@ -6325,9 +6300,6 @@ static const struct mlx5_ib_profile nic_rep_profile = {
 	STAGE_CREATE(MLX5_IB_STAGE_PRE_IB_REG_UMR,
 		     NULL,
 		     mlx5_ib_stage_pre_ib_reg_umr_cleanup),
-	STAGE_CREATE(MLX5_IB_STAGE_SPECS,
-		     mlx5_ib_stage_populate_specs,
-		     NULL),
 	STAGE_CREATE(MLX5_IB_STAGE_IB_REG,
 		     mlx5_ib_stage_ib_reg_init,
 		     mlx5_ib_stage_ib_reg_cleanup),
