1b156b9185abe807747b96c39c25dbc4677553dd
Alaa Hleihel <ahleihel@redhat.com>
2019-06-11 12:22:08 -0400

    [infiniband] RDMA/uverbs: Add generic function to fill in flow action object
    Message-id: <20190611122401.30768-15-ahleihel@redhat.com>
    Patchwork-id: 264534
    O-Subject: [RHEL8.1 BZ 1691234 v2 014/127] RDMA/uverbs: Add generic function to fill in flow action object
    Bugzilla: 1691234 1663215
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1691234
    Bugzilla: http://bugzilla.redhat.com/1663215
    Upstream: v4.20-rc1
    Conflicts:
     - This completes the original patch done by the InfiniBand subsystem
       maintainer by adding the hunks that he dropped.
    
    commit 841eefc5cb57030ad05a0c4bc285f93ffa668ad9
    Author: Mark Bloch <markb@mellanox.com>
    Date:   Tue Aug 28 14:18:52 2018 +0300
    
        RDMA/uverbs: Add generic function to fill in flow action object
    
        Refactor the initialization of a flow action object to a common function.
    
        Signed-off-by: Mark Bloch <markb@mellanox.com>
        Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
        Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/infiniband/hw/mlx5/flow.c
+++ drivers/infiniband/hw/mlx5/flow.c
@@ -279,11 +279,8 @@ static int UVERBS_HANDLER(MLX5_IB_METHOD_FLOW_ACTION_CREATE_MODIFY_HEADER)(
 	if (IS_ERR(action))
 		return PTR_ERR(action);
 
-	atomic_set(&action->usecnt, 0);
-	action->device = uobj->context->device;
-	action->type = IB_FLOW_ACTION_UNSPECIFIED;
-	action->uobject = uobj;
-	uobj->object = action;
+	uverbs_flow_action_fill_action(action, uobj, uobj->context->device,
+				       IB_FLOW_ACTION_UNSPECIFIED);
 
 	return 0;
 }
