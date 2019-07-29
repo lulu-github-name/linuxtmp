7a3e90009eb423e807c2862248b624b8f7dd7f06
Ivan Vecera <ivecera@redhat.com>
2019-07-29 15:52:03 -0400

    [netdrv] Add IANA_VXLAN_UDP_PORT definition to vxlan header file
    Message-id: <20190729155203.7539-3-ivecera@redhat.com>
    Patchwork-id: 267288
    O-Subject: [RHEL-8.2 net PATCH 2/2] net: Add IANA_VXLAN_UDP_PORT definition to vxlan header file
    Bugzilla: 1731347
    RH-Acked-by: Ken Cox <jkc@redhat.com>
    RH-Acked-by: Hangbin Liu <haliu@redhat.com>
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1731347
    
    Conflicts: Small context conflict due to missing commit 1a6e552df33c ("net:
               hns3: reuse the definition of l3 and l4 header info union")
    
    Upstream commit(s):
    commit bea964107fa78ffe484ef8659ecc26f9ae2bcd2f
    Author: Moshe Shemesh <moshe@mellanox.com>
    Date:   Thu Mar 21 15:51:39 2019 -0700
    
        net: Add IANA_VXLAN_UDP_PORT definition to vxlan header file
    
        Added IANA_VXLAN_UDP_PORT (4789) definition to vxlan header file so it
        can be used by drivers instead of local definition.
        Updated drivers which locally defined it as 4789 to use it.
    
        Signed-off-by: Moshe Shemesh <moshe@mellanox.com>
        Reviewed-by: Or Gerlitz <ogerlitz@mellanox.com>
        Cc: John Hurley <john.hurley@netronome.com>
        Cc: Jakub Kicinski <jakub.kicinski@netronome.com>
        Cc: Yunsheng Lin <linyunsheng@huawei.com>
        Cc: Peng Li <lipeng321@huawei.com>
        Reviewed-by: Tariq Toukan <tariqt@mellanox.com>
        Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>
        Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Phillip Lougher <plougher@redhat.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3_enet.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3_enet.c
@@ -753,7 +753,6 @@ static void hns3_set_l2l3l4_len(struct sk_buff *skb, u8 ol4_proto,
  */
 static bool hns3_tunnel_csum_bug(struct sk_buff *skb)
 {
-#define IANA_VXLAN_PORT	4789
 	union {
 		struct tcphdr *tcp;
 		struct udphdr *udp;
@@ -763,7 +762,8 @@ static bool hns3_tunnel_csum_bug(struct sk_buff *skb)
 
 	l4.hdr = skb_transport_header(skb);
 
-	if (!(!skb->encapsulation && l4.udp->dest == htons(IANA_VXLAN_PORT)))
+	if (!(!skb->encapsulation &&
+	      l4.udp->dest == htons(IANA_VXLAN_UDP_PORT)))
 		return false;
 
 	skb_checksum_help(skb);
