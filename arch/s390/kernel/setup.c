de5d740ce93ac0defd706882e561fa2955a95a67
Philipp Rudo <prudo@redhat.com>
2019-09-03 12:42:03 -0400

    [s390] s390/setup: Fix kernel lock down for s390
    Message-id: <20190903124204.22653-2-prudo@redhat.com>
    Patchwork-id: 270291
    O-Subject: [RHEL8.2 PATCH 1/2] [s390] s390/setup: Fix kernel lock down for s390
    Bugzilla: 1748343
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Cornelia Huck <cohuck@redhat.com>
    
    From: Philipp Rudo <prudo@linux.ibm.com>
    
    Bugzilla: 1748343
    Upstream Status: RHEL-only
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=23295707
    Tested: by IBM
    Conflicts: None
    
    Kernel lock down is initialized in arch not common code. So s390 has to
    call init_lockdown itself within setup_arch for lock down to work.
    
    Fixes: a162c8aeaac6 ("[s390] Disable kexec_load via kernel_lockdown for s390")
    Signed-off-by: Philipp Rudo <prudo@linux.ibm.com>
    Signed-off-by: Phillip Lougher <plougher@redhat.com>

--- arch/s390/kernel/setup.c
+++ arch/s390/kernel/setup.c
@@ -50,6 +50,7 @@
 #include <linux/memory.h>
 #include <linux/compat.h>
 #include <linux/version.h>
+#include <linux/security.h>
 
 #include <asm/boot_data.h>
 #include <asm/ipl.h>
@@ -1019,6 +1020,7 @@ void __init setup_arch(char **cmdline_p)
 		pr_info("Linux is running natively in 64-bit mode\n");
 
 	log_component_list();
+	init_lockdown();
 
 	/* Have one command line that is parsed and saved in /proc/cmdline */
 	/* boot_command_line has been already set up in early.c */
