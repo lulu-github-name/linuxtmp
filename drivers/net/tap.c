c332b96184887fc2fdcab18f4376d534124dfac2
Davide Caratti <dcaratti@redhat.com>
2019-09-17 22:18:19 -0400

    [netdrv] drivers: tap.c: fix wrong backport causing WARN_ON_ONCE(1) in skb_flow_dissect()
    Message-id: <57a4ac37d016bf6411ef27aafc6f89d448f3c166.1568758524.git.dcaratti@redhat.com>
    Patchwork-id: 272330
    O-Subject: [RHEL8.1 PATCH] drivers: tap.c: fix wrong backport causing WARN_ON_ONCE(1) in skb_flow_dissect()
    Bugzilla: 1750711
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Jason Wang <jasowang@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1750711
    Upstream Status: RHEL-only, but it produces the same result as
     net.git commit 8c76e77f9069 ("tap: call skb_probe_transport_header
     after setting skb->dev")
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=23542981
    Tested: boot-test of a guest VM that uses virtio_net / macvtap, verified that
     the reported WARN_ON() does not appear anymore in the host.
    
    This reverts commit ef059c51793350e22429820f739c542e075ef3e3, and applies
    upstream commit 8c76e77f9069 ("tap: call skb_probe_transport_header after
    setting skb->dev") correctly, i.e. the call to skb_probe_transport_header()
    is delayed in tap_get_user_xdp(), not in tap_get_user().
    
    Fixes: ef059c517933 ("[netdrv] tap: call skb_probe_transport_header after setting skb->dev")
    Signed-off-by: Davide Caratti <dcaratti@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- drivers/net/tap.c
+++ drivers/net/tap.c
@@ -712,6 +712,8 @@ static ssize_t tap_get_user(struct tap_queue *q, void *msg_control,
 			goto err_kfree;
 	}
 
+	skb_probe_transport_header(skb, ETH_HLEN);
+
 	/* Move network header to the right position for VLAN tagged packets */
 	if ((skb->protocol == htons(ETH_P_8021Q) ||
 	     skb->protocol == htons(ETH_P_8021AD)) &&
@@ -732,7 +734,6 @@ static ssize_t tap_get_user(struct tap_queue *q, void *msg_control,
 
 	if (tap) {
 		skb->dev = tap->dev;
-		skb_probe_transport_header(skb, ETH_HLEN);
 		dev_queue_xmit(skb);
 	} else {
 		kfree_skb(skb);
@@ -1177,8 +1178,6 @@ static int tap_get_user_xdp(struct tap_queue *q, struct xdp_buff *xdp)
 			goto err_kfree;
 	}
 
-	skb_probe_transport_header(skb, ETH_HLEN);
-
 	/* Move network header to the right position for VLAN tagged packets */
 	if ((skb->protocol == htons(ETH_P_8021Q) ||
 	     skb->protocol == htons(ETH_P_8021AD)) &&
@@ -1189,6 +1188,7 @@ static int tap_get_user_xdp(struct tap_queue *q, struct xdp_buff *xdp)
 	tap = rcu_dereference(q->tap);
 	if (tap) {
 		skb->dev = tap->dev;
+		skb_probe_transport_header(skb, ETH_HLEN);
 		dev_queue_xmit(skb);
 	} else {
 		kfree_skb(skb);
