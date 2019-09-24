b81bd1871e4b987c2241c9cf93d00697b8c785e7
Jaroslav Kysela <jkysela@redhat.com>
2019-09-24 07:35:55 -0400

    [sound] ALSA: ASoC: soc-core: initialize list at one place
    Message-id: <15693081207948942-782-patchset-jkysela@redhat.com>
    Patchwork-id: 273905
    O-Subject: [RHEL 8.2 PATCH 0781/1298] ALSA: ASoC: soc-core: initialize list at one place
    Bugzilla: 1738610
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Initialize component related list at random place is very difficult
    to read. This patch initialize it at snd_soc_component_initialize().
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Link: https://lore.kernel.org/r/87y2zozazp.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>
    (cherry picked from commit 495efdb01f89a5fc53f9b2e61f5726d804d4a15d)
    Bugzilla: 1738610
    Signed-off-by: Phillip Lougher <plougher@redhat.com>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1299,7 +1299,6 @@ static int soc_probe_component(struct snd_soc_card *card,
 
 	component->card = card;
 	dapm->card = card;
-	INIT_LIST_HEAD(&component->card_list);
 	INIT_LIST_HEAD(&dapm->list);
 	soc_set_name_prefix(card, component);
 
@@ -2675,6 +2674,8 @@ static int snd_soc_component_initialize(struct snd_soc_component *component,
 	struct snd_soc_dapm_context *dapm;
 
 	INIT_LIST_HEAD(&component->dai_list);
+	INIT_LIST_HEAD(&component->dobj_list);
+	INIT_LIST_HEAD(&component->card_list);
 	mutex_init(&component->io_mutex);
 
 	component->name = fmt_single_name(dev, &component->id);
@@ -2760,7 +2761,6 @@ static void snd_soc_component_add(struct snd_soc_component *component)
 
 	/* see for_each_component */
 	list_add(&component->list, &component_list);
-	INIT_LIST_HEAD(&component->dobj_list);
 
 	mutex_unlock(&client_mutex);
 }
