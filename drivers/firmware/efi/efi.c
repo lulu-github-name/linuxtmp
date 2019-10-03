899894b9ac165c3e03c639b93ca3d0bfb65e2173
Al Stone <ahs3@redhat.com>
2019-10-02 20:38:54 -0400

    [firmware] efi: Export Runtime Configuration Interface table to sysfs
    Message-id: <20191002203855.9785-2-ahs3@redhat.com>
    Patchwork-id: 276510
    O-Subject: [RHEL8.2 BZ1665518 PATCH 1/2] efi: Export Runtime Configuration Interface table to sysfs
    Bugzilla: 1665518
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1665518
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=23783425
    Upstream Status: https://git.kernel.org/pub/scm/linux/kernel/git/efi/efi.git/
    Tested: general boot tests to check for regressions
    	beaker test to check for RCI2 table (dell-per74* systems)
    	pending partner QA (though beaker test does the same thing)
    
    commit 1c5fecb61255aa12a16c4c06335ab68979865914
    Author: Narendra K <Narendra.K@dell.com>
    Date: Wed, 10 Jul 2019 18:59:15 +0000
    
        System firmware advertises the address of the 'Runtime
        Configuration Interface table version 2 (RCI2)' via
        an EFI Configuration Table entry. This code retrieves the RCI2
        table from the address and exports it to sysfs as a binary
        attribute 'rci2' under /sys/firmware/efi/tables directory.
        The approach adopted is similar to the attribute 'DMI' under
        /sys/firmware/dmi/tables.
    
        RCI2 table contains BIOS HII in XML format and is used to populate
        BIOS setup page in Dell EMC OpenManage Server Administrator tool.
        The BIOS setup page contains BIOS tokens which can be configured.
    
        Signed-off-by: Narendra K <Narendra.K@dell.com>
        Reviewed-by: Mario Limonciello <mario.limonciello@dell.com>
        Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    
    Conflicts:
    	arch/x86/platform/efi/efi.c
    		This file does not contain the efi_tables struct in RHEL;
    		it has been moved to drivers/firmware/efi/efi.c just as in
    		upstream.  Changed the file name.
    
    Cc: Lenny Szubowicz <lszubowi@redhat.com>
    Cc: Prarit Bhargava <prarit@redhat.com>
    Signed-off-by: Al Stone <ahs3@redhat.com>
    Signed-off-by: Phillip Lougher <plougher@redhat.com>

--- drivers/firmware/efi/efi.c
+++ drivers/firmware/efi/efi.c
@@ -74,6 +74,9 @@ static unsigned long *efi_tables[] = {
 	&efi.esrt,
 	&efi.properties_table,
 	&efi.mem_attr_table,
+#ifdef CONFIG_EFI_RCI2_TABLE
+	&rci2_table_phys,
+#endif
 };
 
 struct mm_struct efi_mm = {
@@ -492,6 +495,9 @@ static __initdata efi_config_table_type_t common_tables[] = {
 	{LINUX_EFI_RANDOM_SEED_TABLE_GUID, "RNG", &efi.rng_seed},
 	{LINUX_EFI_TPM_EVENT_LOG_GUID, "TPMEventLog", &efi.tpm_log},
 	{LINUX_EFI_MEMRESERVE_TABLE_GUID, "MEMRESERVE", &efi.mem_reserve},
+#ifdef CONFIG_EFI_RCI2_TABLE
+	{DELLEMC_EFI_RCI2_TABLE_GUID, NULL, &rci2_table_phys},
+#endif
 	{NULL_GUID, NULL, NULL},
 };
 
--- include/linux/efi.h
+++ include/linux/efi.h
@@ -678,6 +678,9 @@ void efi_native_runtime_setup(void);
 #define LINUX_EFI_TPM_EVENT_LOG_GUID		EFI_GUID(0xb7799cb0, 0xeca2, 0x4943,  0x96, 0x67, 0x1f, 0xae, 0x07, 0xb7, 0x47, 0xfa)
 #define LINUX_EFI_MEMRESERVE_TABLE_GUID		EFI_GUID(0x888eb0c6, 0x8ede, 0x4ff5,  0xa8, 0xf0, 0x9a, 0xee, 0x5c, 0xb9, 0x77, 0xc2)
 
+/* OEM GUIDs */
+#define DELLEMC_EFI_RCI2_TABLE_GUID		EFI_GUID(0x2d9f28a2, 0xa886, 0x456a,  0x97, 0xa8, 0xf1, 0x1e, 0xf2, 0x4f, 0xf4, 0x55)
+
 typedef struct {
 	efi_guid_t guid;
 	u64 table;
@@ -1690,6 +1693,8 @@ struct linux_efi_tpm_eventlog {
 };
 
 extern int efi_tpm_eventlog_init(void);
+
+extern unsigned long rci2_table_phys;
 
 /*
  * efi_runtime_service() function identifiers.
