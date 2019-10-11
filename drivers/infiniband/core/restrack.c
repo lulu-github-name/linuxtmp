ee6ba0d21ae2d44fe6ef9d1a0ab2161cfd0fde4b
Kamal Heib <kheib@redhat.com>
2019-10-11 07:05:27 -0400

    [infiniband] RDMA/nldev: Connect QP number to .doit callback
    Message-id: <20191011070548.14360-86-kheib@redhat.com>
    Patchwork-id: 278660
    O-Subject: [RHEL-8.2 PATCH 085/106] RDMA/nldev: Connect QP number to .doit callback
    Bugzilla: 1720888
    RH-Acked-by: Honggang Li <honli@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1720888
    Bugzilla: http://bugzilla.redhat.com/1722277
    Conflicts:
    Include merge hunks from ea295481b6e3 ("Merge tag 'xarray-5.1-rc1' of
    git://git.infradead.org/users/willy/linux-dax") to syc the code with
    upstream kernel.
    
    commit 1b8b778864b338b2e295687733d785ae1a65a9ea
    Author: Leon Romanovsky <leonro@mellanox.com>
    Date:   Mon Feb 18 22:25:51 2019 +0200
    
        RDMA/nldev: Connect QP number to .doit callback
    
        This patch adds ability to query specific QP based on its LQPN (local
        QPN), which is assigned by HW and needs special treatment while inserting
        into restrack DB.
    
        Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
        Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    
    Signed-off-by: Kamal Heib <kheib@redhat.com>
    Signed-off-by: Phillip Lougher <plougher@redhat.com>

--- drivers/infiniband/core/restrack.c
+++ drivers/infiniband/core/restrack.c
@@ -13,28 +13,6 @@
 #include "cma_priv.h"
 #include "restrack.h"
 
-static int rt_xa_alloc_cyclic(struct xarray *xa, u32 *id, void *entry,
-			      u32 *next)
-{
-	int err;
-
-	*id = *next;
-	if (*next == U32_MAX)
-		*id = 0;
-
-	xa_lock(xa);
-	err = __xa_alloc(xa, id, entry, xa_limit_32b, GFP_KERNEL);
-	if (err && *next != U32_MAX) {
-		*id = 0;
-		err = __xa_alloc(xa, id, entry, xa_limit_32b, GFP_KERNEL);
-	}
-
-	if (!err)
-		*next = *id + 1;
-	xa_unlock(xa);
-	return err;
-}
-
 /**
  * rdma_restrack_init() - initialize and allocate resource tracking
  * @dev:  IB device
@@ -225,7 +203,17 @@ static void rdma_restrack_add(struct rdma_restrack_entry *res)
 
 	kref_init(&res->kref);
 	init_completion(&res->comp);
-	ret = rt_xa_alloc_cyclic(&rt->xa, &res->id, res, &rt->next_id);
+	if (res->type != RDMA_RESTRACK_QP)
+		ret = xa_alloc_cyclic(&rt->xa, &res->id, res, xa_limit_32b,
+				&rt->next_id, GFP_KERNEL);
+	else {
+		/* Special case to ensure that LQPN points to right QP */
+		struct ib_qp *qp = container_of(res, struct ib_qp, res);
+
+		ret = xa_insert(&rt->xa, qp->qp_num, res, GFP_KERNEL);
+		res->id = ret ? 0 : qp->qp_num;
+	}
+
 	if (!ret)
 		res->valid = true;
 }
