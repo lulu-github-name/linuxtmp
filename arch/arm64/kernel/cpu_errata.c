106f6a4013a65233d447ece1aadab51cfbe225a8
Mark Salter <msalter@redhat.com>
2019-10-11 17:47:40 -0400

    [arm64] arm64: cpu: Move errata and feature enable callbacks closer to callers
    Message-id: <20191011174745.15717-3-msalter@redhat.com>
    Patchwork-id: 278718
    O-Subject: [RHEL8.2 BZ1742821 PATCH 2/7] arm64: cpu: Move errata and feature enable callbacks closer to callers
    Bugzilla: 1742821
    RH-Acked-by: Bhupesh Sharma <bhsharma@redhat.com>
    RH-Acked-by: Mark Langsdorf <mlangsdo@redhat.com>
    
    commit b8925ee2e12d1cb9a11d6f28b5814f2bfa59dce1
    From: Will Deacon <will.deacon@arm.com>
    
        The cpu errata and feature enable callbacks are only called via their
        respective arm64_cpu_capabilities structure and therefore shouldn't
        exist in the global namespace.
    
        Move the PAN, RAS and cache maintenance emulation enable callbacks into
        the same files as their corresponding arm64_cpu_capabilities structures,
        making them static in the process.
    
        Signed-off-by: Will Deacon <will.deacon@arm.com>
        Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Conflicts:
    	arch/arm64/kernel/cpufeature.c
    	Context conflict with unexpected cpu_show_meltdown()
    	from previous backport.
    
    Signed-off-by: Mark Salter <msalter@redhat.com>
    Signed-off-by: Phillip Lougher <plougher@redhat.com>

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -502,6 +502,12 @@ static const struct midr_range arm64_ssb_cpus[] = {
 	{},
 };
 
+static void __maybe_unused
+cpu_enable_cache_maint_trap(const struct arm64_cpu_capabilities *__unused)
+{
+	sysreg_clear_set(sctlr_el1, SCTLR_EL1_UCI, 0);
+}
+
 #define CAP_MIDR_RANGE(model, v_min, r_min, v_max, r_max)	\
 	.matches = is_affected_midr_range,			\
 	.midr_range = MIDR_RANGE(model, v_min, r_min, v_max, r_max)
