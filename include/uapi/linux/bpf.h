7cc43a9e7a665411ef6dfb8c71dfbb8a4ad6eacb
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2019-10-18 13:51:17 -0400

    [net] bpf: support input __sk_buff context in BPF_PROG_TEST_RUN
    Message-id: <20191018135423.24316-82-yauheni.kaliuta@redhat.com>
    Patchwork-id: 280218
    O-Subject: [RHEL8.2 082/268] bpf: support input __sk_buff context in BPF_PROG_TEST_RUN
    Bugzilla: 1747615
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1747615
    
    Conflicts: KABI extend union bpf_attr. Safe since BPF is
      not protected with KABI
    
    commit b0b9395d865e3060d97658fbc9ba3f77fecc8da1
    Author: Stanislav Fomichev <sdf@google.com>
    Date:   Tue Apr 9 11:49:09 2019 -0700
    
        bpf: support input __sk_buff context in BPF_PROG_TEST_RUN
    
        Add new set of arguments to bpf_attr for BPF_PROG_TEST_RUN:
        * ctx_in/ctx_size_in - input context
        * ctx_out/ctx_size_out - output context
    
        The intended use case is to pass some meta data to the test runs that
        operate on skb (this has being brought up on recent LPC).
    
        For programs that use bpf_prog_test_run_skb, support __sk_buff input and
        output. Initially, from input __sk_buff, copy _only_ cb and priority into
        skb, all other non-zero fields are prohibited (with EINVAL).
        If the user has set ctx_out/ctx_size_out, copy the potentially modified
        __sk_buff back to the userspace.
    
        We require all fields of input __sk_buff except the ones we explicitly
        support to be set to zero. The expectation is that in the future we might
        add support for more fields and we want to fail explicitly if the user
        runs the program on the kernel where we don't yet support them.
    
        The API is intentionally vague (i.e. we don't explicitly add __sk_buff
        to bpf_attr, but ctx_in) to potentially let other test_run types use
        this interface in the future (this can be xdp_md for xdp types for
        example).
    
        v4:
          * don't copy more than allowed in bpf_ctx_init [Martin]
    
        v3:
          * handle case where ctx_in is NULL, but ctx_out is not [Martin]
          * convert size==0 checks to ptr==NULL checks and add some extra ptr
            checks [Martin]
    
        v2:
          * Addressed comments from Martin Lau
    
        Signed-off-by: Stanislav Fomichev <sdf@google.com>
        Acked-by: Martin KaFai Lau <kafai@fb.com>
        Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -421,6 +421,15 @@ union bpf_attr {
 		__aligned_u64	data_out;
 		__u32		repeat;
 		__u32		duration;
+#ifndef __GENKSYMS__
+		__u32		ctx_size_in;	/* input: len of ctx_in */
+		__u32		ctx_size_out;	/* input/output: len of ctx_out
+						 *   returns ENOSPC if ctx_out
+						 *   is too small.
+						 */
+		__aligned_u64	ctx_in;
+		__aligned_u64	ctx_out;
+#endif /* __GENKSYMS__ */
 	} test;
 
 	struct { /* anonymous struct used by BPF_*_GET_*_ID */
