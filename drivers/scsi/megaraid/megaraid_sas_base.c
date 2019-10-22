645bf27c869a0743e59f1f7e55eebbf7c656d21f
Tomas Henzl <thenzl@redhat.com>
2019-10-22 15:18:49 -0400

    [scsi] scsi: megaraid_sas: remove set but not used variables 'buff_addr' and 'ci_h'
    Message-id: <20191022151923.12065-25-thenzl@redhat.com>
    Patchwork-id: 280693
    O-Subject: [RHEL8.2 e-stor 24/58] scsi: megaraid_sas: remove set but not used variables 'buff_addr' and 'ci_h'
    Bugzilla: 1726251
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Fixes gcc '-Wunused-but-set-variable' warnings:
    
    drivers/scsi/megaraid/megaraid_sas_base.c: In function megasas_fw_crash_buffer_show:
    drivers/scsi/megaraid/megaraid_sas_base.c:3138:16: warning: variable buff_addr set but not used [-Wunused-but-set-variable]
    drivers/scsi/megaraid/megaraid_sas_base.c: In function megasas_get_pd_list:
    drivers/scsi/megaraid/megaraid_sas_base.c:4426:13: warning: variable ci_h set but not used [-Wunused-but-set-variable]
    
    'buff_addr' is never used since inroduction in commit fc62b3fc9021
    ("megaraid_sas : Firmware crash dump feature support")
    
    'ci_h' is not used since commit 9b3d028f3468 ("scsi: megaraid_sas:
    Pre-allocate frequently used DMA buffers")
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Acked-by: Sumit Saxena <sumit.saxena@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit cdf79db476c44c580045e9e0937288502e00eaad)
    Signed-off-by: Tomas Henzl <thenzl@redhat.com>
    Signed-off-by: Phillip Lougher <plougher@redhat.com>

--- drivers/scsi/megaraid/megaraid_sas_base.c
+++ drivers/scsi/megaraid/megaraid_sas_base.c
@@ -3129,7 +3129,6 @@ megasas_fw_crash_buffer_show(struct device *cdev,
 	struct megasas_instance *instance =
 		(struct megasas_instance *) shost->hostdata;
 	u32 size;
-	unsigned long buff_addr;
 	unsigned long dmachunk = CRASH_DMA_BUF_SIZE;
 	unsigned long src_addr;
 	unsigned long flags;
@@ -3145,8 +3144,6 @@ megasas_fw_crash_buffer_show(struct device *cdev,
 		spin_unlock_irqrestore(&instance->crashdump_lock, flags);
 		return -EINVAL;
 	}
-
-	buff_addr = (unsigned long) buf;
 
 	if (buff_offset > (instance->fw_crash_buffer_size * dmachunk)) {
 		dev_err(&instance->pdev->dev,
@@ -4396,7 +4393,6 @@ megasas_get_pd_list(struct megasas_instance *instance)
 	struct megasas_dcmd_frame *dcmd;
 	struct MR_PD_LIST *ci;
 	struct MR_PD_ADDRESS *pd_addr;
-	dma_addr_t ci_h = 0;
 
 	if (instance->pd_list_not_supported) {
 		dev_info(&instance->pdev->dev, "MR_DCMD_PD_LIST_QUERY "
@@ -4405,7 +4401,6 @@ megasas_get_pd_list(struct megasas_instance *instance)
 	}
 
 	ci = instance->pd_list_buf;
-	ci_h = instance->pd_list_buf_h;
 
 	cmd = megasas_get_cmd(instance);
 
