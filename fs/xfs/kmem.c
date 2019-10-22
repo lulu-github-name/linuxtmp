a6875392d17f20ae87421b69a437690945b4e151
Bill O'Donnell <billodo@redhat.com>
2019-10-21 20:00:43 -0400

    [fs] xfs: add kmem_alloc_io()
    Message-id: <20191021200046.31650-2-billodo@redhat.com>
    Patchwork-id: 280615
    O-Subject: [RHEL8.2 PATCH v3 1/4] xfs: add kmem allocation trace points
    Bugzilla: 1583556
    RH-Acked-by: Brian Foster <bfoster@redhat.com>
    RH-Acked-by: Eric Sandeen <esandeen@redhat.com>
    
    From: Dave Chinner <dchinner@redhat.com>
    
    commit 0ad95687c3adb91e762d0f6ea50a6b1137300e19
    Author: Dave Chinner <dchinner@redhat.com>
    Date:   Mon Aug 26 12:08:10 2019 -0700
    
        xfs: add kmem allocation trace points
    
        When trying to correlate XFS kernel allocations to memory reclaim
        behaviour, it is useful to know what allocations XFS is actually
        attempting. This information is not directly available from
        tracepoints in the generic memory allocation and reclaim
        tracepoints, so these new trace points provide a high level
        indication of what the XFS memory demand actually is.
    
        There is no per-filesystem context in this code, so we just trace
        the type of allocation, the size and the allocation constraints.
        The kmem code also doesn't include much of the common XFS headers,
        so there are a few definitions that need to be added to the trace
        headers and a couple of types that need to be made common to avoid
        needing to include the whole world in the kmem code.
    
        Signed-off-by: Dave Chinner <dchinner@redhat.com>
        Reviewed-by: Brian Foster <bfoster@redhat.com>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
        Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    
    Signed-off-by: Bill O'Donnell <billodo@redhat.com>
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- fs/xfs/kmem.c
+++ fs/xfs/kmem.c
@@ -4,14 +4,14 @@
  * All Rights Reserved.
  */
 #include <linux/mm.h>
-#include <linux/sched/mm.h>
 #include <linux/highmem.h>
 #include <linux/slab.h>
 #include <linux/swap.h>
 #include <linux/blkdev.h>
+#include "xfs.h"
 #include <linux/backing-dev.h>
-#include "kmem.h"
 #include "xfs_message.h"
+#include "xfs_trace.h"
 
 void *
 kmem_alloc(size_t size, xfs_km_flags_t flags)
@@ -19,6 +19,8 @@ kmem_alloc(size_t size, xfs_km_flags_t flags)
 	int	retries = 0;
 	gfp_t	lflags = kmem_flags_convert(flags);
 	void	*ptr;
+
+	trace_kmem_alloc(size, flags, _RET_IP_);
 
 	do {
 		ptr = kmalloc(size, lflags);
@@ -39,6 +41,8 @@ kmem_alloc_large(size_t size, xfs_km_flags_t flags)
 	unsigned nofs_flag = 0;
 	void	*ptr;
 	gfp_t	lflags;
+
+	trace_kmem_alloc_large(size, flags, _RET_IP_);
 
 	ptr = kmem_alloc(size, flags | KM_MAYFAIL);
 	if (ptr)
@@ -70,6 +74,8 @@ kmem_realloc(const void *old, size_t newsize, xfs_km_flags_t flags)
 	gfp_t	lflags = kmem_flags_convert(flags);
 	void	*ptr;
 
+	trace_kmem_realloc(newsize, flags, _RET_IP_);
+
 	do {
 		ptr = krealloc(old, newsize, lflags);
 		if (ptr || (flags & (KM_MAYFAIL|KM_NOSLEEP)))
@@ -90,6 +96,7 @@ kmem_zone_alloc(kmem_zone_t *zone, xfs_km_flags_t flags)
 	gfp_t	lflags = kmem_flags_convert(flags);
 	void	*ptr;
 
+	trace_kmem_zone_alloc(kmem_cache_size(zone), flags, _RET_IP_);
 	do {
 		ptr = kmem_cache_alloc(zone, lflags);
 		if (ptr || (flags & (KM_MAYFAIL|KM_NOSLEEP)))
