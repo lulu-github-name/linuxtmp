223197d43bdd065dbe4b0e9af41827fd3222f80c
Michael Petlan <mpetlan@redhat.com>
2019-10-24 07:30:03 -0400

    [x86] x86/cpu: Add Comet Lake to the Intel CPU models header
    Message-id: <20191024073011.27755-2-mpetlan@redhat.com>
    Patchwork-id: 281035
    O-Subject: [RHEL8.2 BZ1763290 1/9] x86/cpu: Add Comet Lake to the Intel CPU models header
    Bugzilla: 1763290
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: bz1763290
    https://bugzilla.redhat.com/show_bug.cgi?id=1763290
    
    upstream
    ========
    commit 8d7c6ac3b2371eb1cbc9925a88f4d10efff374de
    Author: Kan Liang <kan.liang@linux.intel.com>
    Date:   Tue Oct 8 08:50:02 2019 -0700
    
    description
    ===========
    Comet Lake is the new 10th Gen Intel processor. Add two new CPU model
    numbers to the Intel family list.
    
    The CPU model numbers are not published in the SDM yet but they come
    from an authoritative internal source.
    
     [ bp: Touch up commit message. ]
    
    Signed-off-by: Phillip Lougher <plougher@redhat.com>

--- arch/x86/include/asm/intel-family.h
+++ arch/x86/include/asm/intel-family.h
@@ -58,6 +58,9 @@
 #define INTEL_FAM6_ICELAKE_L	0x7E
 #define INTEL_FAM6_ICELAKE_NNPI		0x9D
 
+#define INTEL_FAM6_COMETLAKE		0xA5
+#define INTEL_FAM6_COMETLAKE_L		0xA6
+
 /* "Small Core" Processors (Atom) */
 
 #define INTEL_FAM6_ATOM_BONNELL		0x1C /* Diamondville, Pineview */
