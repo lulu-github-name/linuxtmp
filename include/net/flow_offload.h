06db60fc559ce8494f91da6bfe37b7ebf7fcef12
Ivan Vecera <ivecera@redhat.com>
2019-10-25 18:59:04 -0400

    [net] flow_offload: add flow_block_cb_is_busy() and use it
    Message-id: <20191025185915.405-12-ivecera@redhat.com>
    Patchwork-id: 281184
    O-Subject: [RHEL-8.2 net PATCH 11/22] net: flow_offload: add flow_block_cb_is_busy() and use it
    Bugzilla: 1737890
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1737890
    
    Conflicts: \
    - Hunks for ocelot driver were removed as it does not exist in RHEL
    - Small context conflict due to RHEL specific comment
    
    Upstream commit(s):
    commit 0d4fd02e7199fbf57c0d175dd1890c82cd4a6f4f
    Author: Pablo Neira Ayuso <pablo@netfilter.org>
    Date:   Tue Jul 9 22:55:48 2019 +0200
    
        net: flow_offload: add flow_block_cb_is_busy() and use it
    
        This patch adds a function to check if flow block callback is already in
        use.  Call this new function from flow_block_cb_setup_simple() and from
        drivers.
    
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- include/net/flow_offload.h
+++ include/net/flow_offload.h
@@ -314,6 +314,9 @@ static inline void flow_block_cb_remove(struct flow_block_cb *block_cb,
 	list_move(&block_cb->list, &offload->cb_list);
 }
 
+bool flow_block_cb_is_busy(tc_setup_cb_t *cb, void *cb_ident,
+			   struct list_head *driver_block_list);
+
 /* RHEL: Increase the version of flow_block_cb_setup_simple() when TC subsystem
  * is changed in a kABI incompatible way. This includes changes to ndo_setup_tc,
  * inline function changes and TC structs changes.
