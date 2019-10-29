27bc231213f7fa06339edc5310ff7962a061acee
Vivek Goyal <vgoyal@redhat.com>
2019-10-29 19:12:28 -0400

    [fs] fuse: use kvmalloc to allocate array of pipe_buffer structs.
    Message-id: <20191029191438.6931-5-vgoyal@redhat.com>
    Patchwork-id: 281669
    O-Subject: [RHEL8.2 PATCH v2 004/134] fuse: use kvmalloc to allocate array of pipe_buffer structs.
    Bugzilla: 1694161
    RH-Acked-by: Miklos Szeredi <mszeredi@redhat.com>
    RH-Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    
    BZ:1694161
    
    commit d6d931adce1193ef54d06fa9bbf58e5780ca516c
    Author: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Date: Tue, 17 Jul 2018 19:00:34 +0300
    
    The amount of pipe->buffers is basically controlled by userspace by
    fcntl(... F_SETPIPE_SZ ...) so it could be large. High order allocations
    could be slow (if memory is heavily fragmented) or may fail if the order
    is larger than PAGE_ALLOC_COSTLY_ORDER.
    
    Since the 'bufs' doesn't need to be physically contiguous, use
    the kvmalloc_array() to allocate memory. If high order
    page isn't available, the kvamalloc*() will fallback to 0-order.
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Phillip Lougher <plougher@redhat.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -1386,8 +1386,8 @@ static ssize_t fuse_dev_splice_read(struct file *in, loff_t *ppos,
 	if (!fud)
 		return -EPERM;
 
-	bufs = kmalloc_array(pipe->buffers, sizeof(struct pipe_buffer),
-			     GFP_KERNEL);
+	bufs = kvmalloc_array(pipe->buffers, sizeof(struct pipe_buffer),
+			      GFP_KERNEL);
 	if (!bufs)
 		return -ENOMEM;
 
@@ -1420,7 +1420,7 @@ out:
 	for (; page_nr < cs.nr_segs; page_nr++)
 		put_page(bufs[page_nr].page);
 
-	kfree(bufs);
+	kvfree(bufs);
 	return ret;
 }
 
@@ -1976,8 +1976,8 @@ static ssize_t fuse_dev_splice_write(struct pipe_inode_info *pipe,
 
 	pipe_lock(pipe);
 
-	bufs = kmalloc_array(pipe->buffers, sizeof(struct pipe_buffer),
-			     GFP_KERNEL);
+	bufs = kvmalloc_array(pipe->buffers, sizeof(struct pipe_buffer),
+			      GFP_KERNEL);
 	if (!bufs) {
 		pipe_unlock(pipe);
 		return -ENOMEM;
@@ -2038,7 +2038,7 @@ out_free:
 		pipe_buf_release(pipe, &bufs[idx]);
 	pipe_unlock(pipe);
 
-	kfree(bufs);
+	kvfree(bufs);
 	return ret;
 }
 
