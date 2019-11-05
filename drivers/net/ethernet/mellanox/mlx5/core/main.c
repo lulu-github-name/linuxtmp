c52529c55330df3694e12d7dcc7d8ea5f583333c
Alaa Hleihel <ahleihel@redhat.com>
2019-11-05 09:48:00 -0500

    [netdrv] mlx5: Move pci status reg access mutex to mlx5_pci_init
    Message-id: <20191105095158.18958-98-ahleihel@redhat.com>
    Patchwork-id: 283122
    O-Subject: [RHEL8.2 BZ 1724336 097/335] net/mlx5: Move pci status reg access mutex to mlx5_pci_init
    Bugzilla: 1724336
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1724336
    Upstream: v5.3-rc1
    
    commit d22663edacdfb292e7132989de93bf2828cac493
    Author: Parav Pandit <parav@mellanox.com>
    Date:   Fri Jun 28 22:36:00 2019 +0000
    
        net/mlx5: Move pci status reg access mutex to mlx5_pci_init
    
        mlx5_pci_init() performs pci specific initialization of the
        mlx5_core_dev struct.
        Hence move pci_status_mutex to pci initialization routine
        mlx5_pci_init().
        This allows reusing mlx5_mdev_init() to non PCI devices.
    
        Signed-off-by: Parav Pandit <parav@mellanox.com>
        Reviewed-by: Vu Pham <vuhuong@mellanox.com>
        Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- drivers/net/ethernet/mellanox/mlx5/core/main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -764,6 +764,7 @@ static int mlx5_pci_init(struct mlx5_core_dev *dev, struct pci_dev *pdev,
 	struct mlx5_priv *priv = &dev->priv;
 	int err = 0;
 
+	mutex_init(&dev->pci_status_mutex);
 	pci_set_drvdata(dev->pdev, dev);
 
 	dev->bar_addr = pci_resource_start(pdev, 0);
@@ -1284,7 +1285,6 @@ static int mlx5_mdev_init(struct mlx5_core_dev *dev, int profile_idx)
 
 	INIT_LIST_HEAD(&priv->ctx_list);
 	spin_lock_init(&priv->ctx_lock);
-	mutex_init(&dev->pci_status_mutex);
 	mutex_init(&dev->intf_state_mutex);
 
 	mutex_init(&priv->bfregs.reg_head.lock);
