188ce95b2d3b6637e3bc272bd305cc67884b76cc
Waiman Long <longman@redhat.com>
2019-11-07 14:45:47 -0500

    [kernel] stacktrace: Constify 'entries' arguments
    Message-id: <20191107144549.13258-10-longman@redhat.com>
    Patchwork-id: 283913
    O-Subject: [RHEL8.2 PATCH v2 53/55] stacktrace: Constify 'entries' arguments
    Bugzilla: 1766681
    RH-Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1766681
    
    commit a2970421640bd9b6a78f2685d7750a791abdfd4e
    Author: Bart Van Assche <bvanassche@acm.org>
    Date:   Mon Jul 22 11:24:41 2019 -0700
    
        stacktrace: Constify 'entries' arguments
    
        Make it clear to humans and to the compiler that the stack trace
        ('entries') arguments are not modified.
    
        Signed-off-by: Bart Van Assche <bvanassche@acm.org>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: Waiman Long <longman@redhat.com>
        Cc: Will Deacon <will.deacon@arm.com>
        Link: https://lkml.kernel.org/r/20190722182443.216015-3-bvanassche@acm.org
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Phillip Lougher <plougher@redhat.com>

--- kernel/stacktrace.c
+++ kernel/stacktrace.c
@@ -19,7 +19,7 @@
  * @nr_entries:	Number of entries in the storage array
  * @spaces:	Number of leading spaces to print
  */
-void stack_trace_print(unsigned long *entries, unsigned int nr_entries,
+void stack_trace_print(const unsigned long *entries, unsigned int nr_entries,
 		       int spaces)
 {
 	unsigned int i;
@@ -48,7 +48,7 @@ EXPORT_SYMBOL_GPL(print_stack_trace);
  *
  * Return: Number of bytes printed.
  */
-int stack_trace_snprint(char *buf, size_t size, unsigned long *entries,
+int stack_trace_snprint(char *buf, size_t size, const unsigned long *entries,
 			unsigned int nr_entries, int spaces)
 {
 	unsigned int generated, i, total = 0;
