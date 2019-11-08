f1e3c63b9334f6d58bdba5d9ed90f96e64afe60e
Ivan Vecera <ivecera@redhat.com>
2019-11-08 14:13:24 -0500

    [net] sched: add API for registering unlocked offload block callbacks
    Message-id: <20191108141427.425-71-ivecera@redhat.com>
    Patchwork-id: 284546
    O-Subject: [RHEL-8.2 net PATCH 070/133] net: sched: add API for registering unlocked offload block callbacks
    Bugzilla: 1739606
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Davide Caratti <dcaratti@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1739606
    
    Upstream commit(s):
    commit c9f14470d04830de217f9d28fcd0deffd7e8c0b1
    Author: Vlad Buslov <vladbu@mellanox.com>
    Date:   Mon Aug 26 16:45:01 2019 +0300
    
        net: sched: add API for registering unlocked offload block callbacks
    
        Extend struct flow_block_offload with "unlocked_driver_cb" flag to allow
        registering and unregistering block hardware offload callbacks that do not
        require caller to hold rtnl lock. Extend tcf_block with additional
        lockeddevcnt counter that is incremented for each non-unlocked driver
        callback attached to device. This counter is necessary to conditionally
        obtain rtnl lock before calling hardware callbacks in following patches.
    
        Register mlx5 tc block offload callbacks as "unlocked".
    
        Signed-off-by: Vlad Buslov <vladbu@mellanox.com>
        Acked-by: Jiri Pirko <jiri@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- net/sched/cls_api.c
+++ net/sched/cls_api.c
@@ -1423,6 +1423,8 @@ static int tcf_block_bind(struct tcf_block *block,
 						  bo->extack);
 		if (err)
 			goto err_unroll;
+		if (!bo->unlocked_driver_cb)
+			block->lockeddevcnt++;
 
 		i++;
 	}
@@ -1438,6 +1440,8 @@ err_unroll:
 						    block_cb->cb_priv, false,
 						    tcf_block_offload_in_use(block),
 						    NULL);
+			if (!bo->unlocked_driver_cb)
+				block->lockeddevcnt--;
 		}
 		flow_block_cb_free(block_cb);
 	}
@@ -1459,6 +1463,8 @@ static void tcf_block_unbind(struct tcf_block *block,
 					    NULL);
 		list_del(&block_cb->list);
 		flow_block_cb_free(block_cb);
+		if (!bo->unlocked_driver_cb)
+			block->lockeddevcnt--;
 	}
 }
 
