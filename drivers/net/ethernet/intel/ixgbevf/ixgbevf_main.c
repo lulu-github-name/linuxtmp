eb1f3edc96ec594b35d31117461ac9468fc4710c
Ken Cox <jkc@redhat.com>
2019-11-12 13:18:07 -0500

    [netdrv] ixgbevf: Bump version for RHEL 8.2.0
    Message-id: <20191112131807.18164-1-jkc@redhat.com>
    Patchwork-id: 285648
    O-Subject: [RHEL8.2 BZ 1721701 9/9 V2] ixgbevf: Bump version for RHEL 8.2.0
    Bugzilla: 1721701
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1721701
    Upstream Status: RHEL only
    
    Update the driver version for RHEL 8.2.0
    V2: fixup the Upstream Status
    
    Cc: Ken Cox <jkc@redhat.com>
    Signed-off-by: Ken Cox <jkc@redhat.com>
    
    Cc: Ken Cox <jkc@redhat.com>
    Signed-off-by: Phillip Lougher <plougher@redhat.com>

--- drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
+++ drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
@@ -38,7 +38,7 @@ const char ixgbevf_driver_name[] = "ixgbevf";
 static const char ixgbevf_driver_string[] =
 	"Intel(R) 10 Gigabit PCI Express Virtual Function Network Driver";
 
-#define DRV_VERSION "4.1.0-k-rh8.1.0"
+#define DRV_VERSION "4.1.0-k-rh8.2.0"
 const char ixgbevf_driver_version[] = DRV_VERSION;
 static char ixgbevf_copyright[] =
 	"Copyright (c) 2009 - 2018 Intel Corporation.";
