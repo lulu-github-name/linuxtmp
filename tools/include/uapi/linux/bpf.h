5f57d443cb7eef0d7dac7a17812965c12ae55501
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2019-11-12 15:08:38 -0500

    [tools] bpf/tools: sync bpf.h
    Message-id: <20191112151107.11979-99-yauheni.kaliuta@redhat.com>
    Patchwork-id: 285754
    O-Subject: [RHEL8.2 099/248] bpf/tools: sync bpf.h
    Bugzilla: 1747617
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1747617
    
    commit cd17d77705780e2270937fb3cbd2b985adab3edc
    Author: Stanislav Fomichev <sdf@google.com>
    Date:   Wed Jun 12 10:30:39 2019 -0700
    
        bpf/tools: sync bpf.h
    
        Add sk to struct bpf_sock_addr and struct bpf_sock_ops.
    
        Cc: Martin Lau <kafai@fb.com>
        Signed-off-by: Stanislav Fomichev <sdf@google.com>
        Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -3249,6 +3249,7 @@ struct bpf_sock_addr {
 	__u32 msg_src_ip6[4];	/* Allows 1,2,4-byte read an 4-byte write.
 				 * Stored in network byte order.
 				 */
+	__bpf_md_ptr(struct bpf_sock *, sk);
 };
 
 /* User bpf_sock_ops struct to access socket values and specify request ops
@@ -3300,6 +3301,7 @@ struct bpf_sock_ops {
 	__u32 sk_txhash;
 	__u64 bytes_received;
 	__u64 bytes_acked;
+	__bpf_md_ptr(struct bpf_sock *, sk);
 };
 
 /* Definitions for bpf_sock_ops_cb_flags */
