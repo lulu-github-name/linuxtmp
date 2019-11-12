892206b33c36ad23a9054f46e7538d406edec99f
Baoquan He <bhe@redhat.com>
2019-11-12 09:35:33 -0500

    [mm] memory_hotplug: move and simplify walk_memory_blocks()
    Message-id: <20191112093553.27965-52-bhe@redhat.com>
    Patchwork-id: 285626
    O-Subject: [RHEL8.2 PATCH v3 51/71] mm/memory_hotplug: move and simplify walk_memory_blocks()
    Bugzilla: 1724969
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: David Hildenbrand <david@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/1724969
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=24591000
    Test: Passed on intel-purley-aep-02.khw1.lab.eng.bos.redhat.com
    
    This is back ported from upstream. There's conflict in
    include/linux/memory_hotplug.h
    
    commit ea8846411ad686ff626e00bb2c3821b3db2ab56a
    Author: David Hildenbrand <david@redhat.com>
    Date:   Thu Jul 18 15:57:50 2019 -0700
    
        mm/memory_hotplug: move and simplify walk_memory_blocks()
    
        Let's move walk_memory_blocks() to the place where memory block logic
        resides and simplify it.  While at it, add a type for the callback
        function.
    
        Link: http://lkml.kernel.org/r/20190614100114.311-6-david@redhat.com
        Signed-off-by: David Hildenbrand <david@redhat.com>
        Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
        Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        Cc: "Rafael J. Wysocki" <rafael@kernel.org>
        Cc: David Hildenbrand <david@redhat.com>
        Cc: Stephen Rothwell <sfr@canb.auug.org.au>
        Cc: Pavel Tatashin <pasha.tatashin@soleen.com>
        Cc: Andrew Banman <andrew.banman@hpe.com>
        Cc: Mike Travis <mike.travis@hpe.com>
        Cc: Oscar Salvador <osalvador@suse.com>
        Cc: Michal Hocko <mhocko@suse.com>
        Cc: Wei Yang <richard.weiyang@gmail.com>
        Cc: Arun KS <arunks@codeaurora.org>
        Cc: Qian Cai <cai@lca.pw>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Baoquan He <bhe@redhat.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- include/linux/memory_hotplug.h
+++ include/linux/memory_hotplug.h
@@ -336,8 +336,6 @@ static inline int remove_memory(int nid, u64 start, u64 size)
 static inline void __remove_memory(int nid, u64 start, u64 size) {}
 #endif /* CONFIG_MEMORY_HOTREMOVE */
 
-extern int walk_memory_blocks(unsigned long start, unsigned long size,
-		void *arg, int (*func)(struct memory_block *, void *));
 extern int __add_memory(int nid, u64 start, u64 size);
 extern int add_memory(int nid, u64 start, u64 size);
 extern int add_memory_resource(int nid, struct resource *resource);
