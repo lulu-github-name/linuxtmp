04ac843fef5f0120d1f0ebb4743931183479d6b1
Sabrina Dubroca <sdubroca@redhat.com>
2019-11-12 15:43:19 -0500

    [net] tls: don't clear TX resync flag on error
    Message-id: <5416f7eecce4f69ad68c5c857983a952c61a04a0.1573554426.git.sdubroca@redhat.com>
    Patchwork-id: 285922
    O-Subject: [PATCH RHEL8.2 net 25/62] net/tls: don't clear TX resync flag on error
    Bugzilla: 1760375
    RH-Acked-by: Davide Caratti <dcaratti@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1760375
    Upstream Status: b5d9a834f4fd in torvalds/linux
    Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=24594655
    Tested: tls selftest
    
    Conflicts: \
     - struct tlsdev_ops: apply changes to include/linux/netdevice.h
       (missing commit da68b4ad0234), and tls_dev_resync uses RH_KABI_USE
     - drop changes to the nfp driver
    
    commit b5d9a834f4fd1b6abfa527ec351c871084dd23a3
    Author: Dirk van der Merwe <dirk.vandermerwe@netronome.com>
    Date:   Mon Jul 8 19:53:13 2019 -0700
    
        net/tls: don't clear TX resync flag on error
    
        Introduce a return code for the tls_dev_resync callback.
    
        When the driver TX resync fails, kernel can retry the resync again
        until it succeeds.  This prevents drivers from attempting to offload
        TLS packets if the connection is known to be out of sync.
    
        We don't worry about the RX resync since they will be retried naturally
        as more encrypted records get received.
    
        Signed-off-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>
        Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Sabrina Dubroca <sdubroca@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -966,10 +966,10 @@ struct tlsdev_ops {
 			    struct tls_context *ctx,
 			    enum tls_offload_ctx_dir direction);
 
-	RH_KABI_USE(1, void (*tls_dev_resync)(struct net_device *netdev,
-					      struct sock *sk, u32 seq,
-					      u8 *rcd_sn,
-					      enum tls_offload_ctx_dir direction))
+	RH_KABI_USE(1, int (*tls_dev_resync)(struct net_device *netdev,
+					     struct sock *sk, u32 seq,
+					     u8 *rcd_sn,
+					     enum tls_offload_ctx_dir direction))
 	RH_KABI_RESERVE(2)
 	RH_KABI_RESERVE(3)
 	RH_KABI_RESERVE(4)
