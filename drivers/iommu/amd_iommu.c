5a3e0b08bc81bf64355fa0cf32c187ac4943a519
Jerry Snitselaar <jsnitsel@redhat.com>
2019-11-25 17:51:45 -0500

    [iommu] amd: Lock dev_data in attach/detach code paths
    Message-id: <20191125175220.10229-232-jsnitsel@redhat.com>
    Patchwork-id: 288704
    O-Subject: [RHEL8.2 PATCH v3 231/266] iommu/amd: Lock dev_data in attach/detach code paths
    Bugzilla: 1742234
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    RH-Acked-by: Bhupesh Sharma <bhsharma@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1742234
    Upstream Status: ab7b2577f0d119052b98b8d913bad369ac2760eb
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=24919229
    
    commit ab7b2577f0d119052b98b8d913bad369ac2760eb
    Author: Joerg Roedel <jroedel@suse.de>
    Date:   Wed Sep 25 15:22:59 2019 +0200
    
        iommu/amd: Lock dev_data in attach/detach code paths
    
        Make sure that attaching a detaching a device can't race against each
        other and protect the iommu_dev_data with a spin_lock in these code
        paths.
    
        Fixes: 92d420ec028d ("iommu/amd: Relax locking in dma_ops path")
        Reviewed-by: Filippo Sironi <sironi@amazon.de>
        Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    (imported from commit ab7b2577f0d119052b98b8d913bad369ac2760eb)
    
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- drivers/iommu/amd_iommu.c
+++ drivers/iommu/amd_iommu.c
@@ -213,6 +213,7 @@ static struct iommu_dev_data *alloc_dev_data(u16 devid)
 	if (!dev_data)
 		return NULL;
 
+	spin_lock_init(&dev_data->lock);
 	dev_data->devid = devid;
 	ratelimit_default_init(&dev_data->rs);
 
@@ -2169,6 +2170,8 @@ static int attach_device(struct device *dev,
 
 	dev_data = get_dev_data(dev);
 
+	spin_lock(&dev_data->lock);
+
 	ret = -EBUSY;
 	if (dev_data->domain != NULL)
 		goto out;
@@ -2211,6 +2214,8 @@ skip_ats_check:
 	domain_flush_complete(domain);
 
 out:
+	spin_unlock(&dev_data->lock);
+
 	spin_unlock_irqrestore(&domain->lock, flags);
 
 	return ret;
@@ -2229,6 +2234,8 @@ static void detach_device(struct device *dev)
 	domain   = dev_data->domain;
 
 	spin_lock_irqsave(&domain->lock, flags);
+
+	spin_lock(&dev_data->lock);
 
 	/*
 	 * First check if the device is still attached. It might already
@@ -2252,6 +2259,8 @@ static void detach_device(struct device *dev)
 	dev_data->ats.enabled = false;
 
 out:
+	spin_unlock(&dev_data->lock);
+
 	spin_unlock_irqrestore(&domain->lock, flags);
 }
 
