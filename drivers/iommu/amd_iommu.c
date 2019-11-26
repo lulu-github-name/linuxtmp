ec4a06fa8f937067e99f98515a226e2e9bcc1adb
Jerry Snitselaar <jsnitsel@redhat.com>
2019-11-26 05:23:42 -0500

    [iommu] Pass struct iommu_iotlb_gather to ->unmap() and ->iotlb_sync()
    Message-id: <20191126052402.14821-9-jsnitsel@redhat.com>
    Patchwork-id: 288818
    O-Subject: [RHEL8.2 PATCH BZ1729845 08/28] iommu: Pass struct iommu_iotlb_gather to ->unmap() and ->iotlb_sync()
    Bugzilla: 1729845
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Auger Eric <eric.auger@redhat.com>
    RH-Acked-by: Mark Salter <msalter@redhat.com>
    
    Bugzilla: 1729845
    Upstream Status: 56f8af5e9d38f120cba2c2adb0786fa2dbc901a4
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=24947401
    Conflicts: virtio-iommu.c hasn't been backported yet. We don't compile tegra-gart
    	   and the sync functions aren't there, so not backporting unmap change
    	   to avoid touching the file for now. That way if we do decide to compile
    	   it later, when I'm backporting I won't run into issue of thinking this
    	   has been backporting for tegra-gart already by seeing in the log.
    	   Use RH_KABI_REPLACE to modify iommu_ops due to s390 false kabi warnings
    	   from asm/pci.h including linux/iommu.h.
    
    commit 56f8af5e9d38f120cba2c2adb0786fa2dbc901a4
    Author: Will Deacon <will@kernel.org>
    Date:   Tue Jul 2 16:44:06 2019 +0100
    
        iommu: Pass struct iommu_iotlb_gather to ->unmap() and ->iotlb_sync()
    
        To allow IOMMU drivers to batch up TLB flushing operations and postpone
        them until ->iotlb_sync() is called, extend the prototypes for the
        ->unmap() and ->iotlb_sync() IOMMU ops callbacks to take a pointer to
        the current iommu_iotlb_gather structure.
    
        All affected IOMMU drivers are updated, but there should be no
        functional change since the extra parameter is ignored for now.
    
        Signed-off-by: Will Deacon <will@kernel.org>
    
    (imported from commit 56f8af5e9d38f120cba2c2adb0786fa2dbc901a4)
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- drivers/iommu/amd_iommu.c
+++ drivers/iommu/amd_iommu.c
@@ -3141,7 +3141,8 @@ static int amd_iommu_map(struct iommu_domain *dom, unsigned long iova,
 }
 
 static size_t amd_iommu_unmap(struct iommu_domain *dom, unsigned long iova,
-			   size_t page_size)
+			      size_t page_size,
+			      struct iommu_iotlb_gather *gather)
 {
 	struct protection_domain *domain = to_pdomain(dom);
 	size_t unmap_size;
@@ -3285,6 +3286,12 @@ static void amd_iommu_flush_iotlb_all(struct iommu_domain *domain)
 	spin_unlock_irqrestore(&dom->lock, flags);
 }
 
+static void amd_iommu_iotlb_sync(struct iommu_domain *domain,
+				 struct iommu_iotlb_gather *gather)
+{
+	amd_iommu_flush_iotlb_all(domain);
+}
+
 const struct iommu_ops amd_iommu_ops = {
 	.capable = amd_iommu_capable,
 	.domain_alloc = amd_iommu_domain_alloc,
@@ -3303,7 +3310,7 @@ const struct iommu_ops amd_iommu_ops = {
 	.is_attach_deferred = amd_iommu_is_attach_deferred,
 	.pgsize_bitmap	= AMD_IOMMU_PGSIZES,
 	.flush_iotlb_all = amd_iommu_flush_iotlb_all,
-	.iotlb_sync = amd_iommu_flush_iotlb_all,
+	.iotlb_sync = amd_iommu_iotlb_sync,
 };
 
 /*****************************************************************************
