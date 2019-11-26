8b2bf5cb9859769cd9b7949e789ffc323d86819d
Mark Salter <msalter@redhat.com>
2019-11-26 16:27:18 -0500

    arm64: cpufeature: Rework ptr auth hwcaps using multi_entry_cap_matches
    Message-id: <20191126162722.423848-16-msalter@redhat.com>
    Patchwork-id: 289487
    O-Subject: [RHEL8.2 BZ1756103 PATCH v2 15/19] arm64: cpufeature: Rework ptr auth hwcaps using multi_entry_cap_matches
    Bugzilla: 1756103
    RH-Acked-by: Al Stone <ahs3@redhat.com>
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    
    commit 1e013d06120cbf67e771848fc5d98174c33b078a
    From: Will Deacon <will.deacon@arm.com>
    
        Open-coding the pointer-auth HWCAPs is a mess and can be avoided by
        reusing the multi-cap logic from the CPU errata framework.
    
        Move the multi_entry_cap_matches code to cpufeature.h and reuse it for
        the pointer auth HWCAPs.
    
        Reviewed-by: Suzuki Poulose <suzuki.poulose@arm.com>
        Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    Conflicts:
    	arch/arm64/kernel/cpu_errata.c
    	Contextual conflict cause by previous backports:
    	rhel commits 88908eccdc26f and be5b299830c63
    
    Signed-off-by: Mark Salter <msalter@redhat.com>
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -561,38 +561,6 @@ int get_spectre_v2_workaround_state(void)
 }
 
 /*
- * Generic helper for handling capabilties with multiple (match,enable) pairs
- * of call backs, sharing the same capability bit.
- * Iterate over each entry to see if at least one matches.
- */
-static bool __maybe_unused
-multi_entry_cap_matches(const struct arm64_cpu_capabilities *entry, int scope)
-{
-	const struct arm64_cpu_capabilities *caps;
-
-	for (caps = entry->match_list; caps->matches; caps++)
-		if (caps->matches(caps, scope))
-			return true;
-
-	return false;
-}
-
-/*
- * Take appropriate action for all matching entries in the shared capability
- * entry.
- */
-static void __maybe_unused
-multi_entry_cap_cpu_enable(const struct arm64_cpu_capabilities *entry)
-{
-	const struct arm64_cpu_capabilities *caps;
-
-	for (caps = entry->match_list; caps->matches; caps++)
-		if (caps->matches(caps, SCOPE_LOCAL_CPU) &&
-		    caps->cpu_enable)
-			caps->cpu_enable(caps);
-}
-
-/*
  * List of CPUs that do not need any Spectre-v2 mitigation at all.
  */
 static const struct midr_range spectre_v2_safe_list[] = {
@@ -790,7 +758,7 @@ const struct arm64_cpu_capabilities arm64_errata[] = {
 	{
 		.desc = "Qualcomm Technologies Falkor/Kryo erratum 1003",
 		.capability = ARM64_WORKAROUND_QCOM_FALKOR_E1003,
-		.matches = multi_entry_cap_matches,
+		.matches = cpucap_multi_entry_cap_matches,
 		.match_list = qcom_erratum_1003_list,
 	},
 #endif
