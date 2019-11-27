torvalds/linux

commit 409afa44f9ba1443ef165a9e6d6a9cb5ad6ad176

iommu/amd: Introduce free_sub_pt() function
The function is a more generic version of free_pagetable()
and will be used to free only specific sub-trees of a
page-table.

Signed-off-by: Joerg Roedel <jroedel@suse.de>

drivers/iommu/amd_iommu.c
@@ -1367,12 +1367,10 @@ DEFINE_FREE_PT_FN(l4, free_pt_l3)
 DEFINE_FREE_PT_FN(l5, free_pt_l4)
 DEFINE_FREE_PT_FN(l6, free_pt_l5)
 
-static void free_pagetable(struct protection_domain *domain)
+static struct page *free_sub_pt(unsigned long root, int mode,
+				struct page *freelist)
 {
-	unsigned long root = (unsigned long)domain->pt_root;
-	struct page *freelist = NULL;
-
-	switch (domain->mode) {
+	switch (mode) {
 	case PAGE_MODE_NONE:
 		break;
 	case PAGE_MODE_1_LEVEL:
@@ -1396,6 +1394,16 @@ static void free_pagetable(struct protection_domain *domain)
 	default:
 		BUG();
 	}
+
+	return freelist;
+}
+
+static void free_pagetable(struct protection_domain *domain)
+{
+	unsigned long root = (unsigned long)domain->pt_root;
+	struct page *freelist = NULL;
+
+	free_sub_pt(root, domain->mode, freelist);
 
 	free_page_list(freelist);
 }

lulu-github-name/linuxtmp

commit bddd654495e5b4bcd3b22d9bda806f43e3682f96

[iommu] amd: Fix pages leak in free_pagetable()
Message-id: <20191125175220.10229-220-jsnitsel@redhat.com>
Patchwork-id: 288627
O-Subject: [RHEL8.2 PATCH v3 219/266] iommu/amd: Fix pages leak in free_pagetable()
Bugzilla: 1742234
RH-Acked-by: David Milburn <dmilburn@redhat.com>
RH-Acked-by: Bhupesh Sharma <bhsharma@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: 1742234
Upstream Status: 34c0989c05314b0288b058a4d1f6e58e2d320929
Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=24919229

commit 34c0989c05314b0288b058a4d1f6e58e2d320929
Author: Andrei Dulea <adulea@amazon.de>
Date:   Fri Sep 13 16:42:28 2019 +0200

    iommu/amd: Fix pages leak in free_pagetable()

    Take into account the gathered freelist in free_sub_pt(), otherwise we
    end up leaking all that pages.

    Fixes: 409afa44f9ba ("iommu/amd: Introduce free_sub_pt() function")
    Signed-off-by: Andrei Dulea <adulea@amazon.de>

(imported from commit 34c0989c05314b0288b058a4d1f6e58e2d320929)

Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>
Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

drivers/iommu/amd_iommu.c
@@ -1437,7 +1437,7 @@ static void free_pagetable(struct protection_domain *domain)
 	BUG_ON(domain->mode < PAGE_MODE_NONE ||
 	       domain->mode > PAGE_MODE_6_LEVEL);
 
-	free_sub_pt(root, domain->mode, freelist);
+	freelist = free_sub_pt(root, domain->mode, freelist);
 
 	free_page_list(freelist);
 }