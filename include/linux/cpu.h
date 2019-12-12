a251b7e151928ce25e6da38a0a91b4063facdeb8
Paolo Bonzini <pbonzini@redhat.com>
2019-12-12 17:49:16 -0500

    [kernel] cpu/speculation: Uninline and export CPU mitigations helpers
    Message-id: <20191212174922.31339-5-pbonzini@redhat.com>
    Patchwork-id: 291976
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.2 virt PATCH v2 04/10] cpu/speculation: Uninline and export CPU mitigations helpers
    Bugzilla: 1690344
    CVE: CVE-2018-12207
    RH-Acked-by: Waiman Long <longman@redhat.com>
    RH-Acked-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    
    From: Tyler Hicks <tyhicks@canonical.com>
    
    A kernel module may need to check the value of the "mitigations=" kernel
    command line parameter as part of its setup when the module needs
    to perform software mitigations for a CPU flaw.
    
    Uninline and export the helper functions surrounding the cpu_mitigations
    enum to allow for their usage from a module.
    
    Lastly, privatize the enum and cpu_mitigations variable since the value of
    cpu_mitigations can be checked with the exported helper functions.
    
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    (cherry picked from commit 731dc9df975a5da21237a18c3384f811a7a41cc6)
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -213,28 +213,7 @@ static inline void cpu_smt_check_topology(void) { }
 static inline bool cpu_smt_possible(void) { return false; }
 #endif
 
-/*
- * These are used for a global "mitigations=" cmdline option for toggling
- * optional CPU mitigations.
- */
-enum cpu_mitigations {
-	CPU_MITIGATIONS_OFF,
-	CPU_MITIGATIONS_AUTO,
-	CPU_MITIGATIONS_AUTO_NOSMT,
-};
-
-extern enum cpu_mitigations cpu_mitigations;
-
-/* mitigations=off */
-static inline bool cpu_mitigations_off(void)
-{
-	return cpu_mitigations == CPU_MITIGATIONS_OFF;
-}
-
-/* mitigations=auto,nosmt */
-static inline bool cpu_mitigations_auto_nosmt(void)
-{
-	return cpu_mitigations == CPU_MITIGATIONS_AUTO_NOSMT;
-}
+extern bool cpu_mitigations_off(void);
+extern bool cpu_mitigations_auto_nosmt(void);
 
 #endif /* _LINUX_CPU_H_ */
