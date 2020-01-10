lulu-github-name/linuxtmp

commit 8248fa6aaa7553b1da7bc4c7c492e228b82e1a2e

[net] netlink: make nla_nest_start() add NLA_F_NESTED flag
Message-id: <a9118e125f66d28d548b5dda2f691c6ef4d4e612.1571818860.git.poros@redhat.com>
Patchwork-id: 280837
O-Subject: [RHEL8.2 net PATCH 16/27] netlink: make nla_nest_start() add NLA_F_NESTED flag
Bugzilla: 1751273
RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
RH-Acked-by: John Linville <linville@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
RH-Acked-by: Stanislaw Gruszka <sgruszka@redhat.com>
RH-Acked-by: Kamal Heib <kheib@redhat.com>

Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1751273

Conflicts: \
drivers/block/drbd/drbd_nl.c
- Chunk skipped due to missing 4ef2a4f43f5e52 ("drbd: fix confusing
  error message during attach")

net/bridge/br_mdb.c
- Adjusted context conflict due to missing 19e3a9c90c5347 ("net: bridge:
  convert multicast to generic rhashtable")

net/core/lwtunnel.c
- Adjusted due to missing ffa8ce54be3aaf ("lwtunnel: Pass encap and
  encap type attributes to lwtunnel_fill_encap")

net/ipv4/fib_semantics.c
- Chunk skipped due to missing series b571bc623eb969
  ("Merge branch 'net-More-movement-to-fib_nh_common'")

net/netfilter/nfnetlink_cttimeout.c
- Adjusted due to missing 4430b897a2063c ("netfilter: cttimeout: remove
  superfluous check on layer 4 netlink functions")

net/netfilter/nft_ct.c
- Chunk skipped due to missing 7e0b2b57f01d18 ("netfilter: nft_ct:
  add ct timeout support")

net/openvswitch/conntrack.c
- Adjusted context conflict due to already applied ca96534630e2ed
  ("openvswitch: check for null pointer return from
  nla_nest_start_noflag")

net/openvswitch/flow_netlink.c
- Chunk skipped due to missing 4d5ec89fc8d14d ("net: openvswitch: Add
  a new action check_pkt_len")

net/sched/sch_taprio.c
- Adjusted due to already applied a3d43c0d56f1b9 ("taprio: Add support
  adding an admin schedule")

- RHEL specific defines was intorduced in 48e79044f70217 ("[net]

net/bridge/br_mdb.c
@@ -26,14 +26,14 @@ static int br_rports_fill_info(struct sk_buff *skb, struct netlink_callback *cb,
 	if (!br->multicast_router || hlist_empty(&br->router_list))
 		return 0;
 
-	nest = nla_nest_start(skb, MDBA_ROUTER);
+	nest = nla_nest_start_noflag(skb, MDBA_ROUTER);
 	if (nest == NULL)
 		return -EMSGSIZE;
 
 	hlist_for_each_entry_rcu(p, &br->router_list, rlist) {
 		if (!p)
 			continue;
-		port_nest = nla_nest_start(skb, MDBA_ROUTER_PORT);
+		port_nest = nla_nest_start_noflag(skb, MDBA_ROUTER_PORT);
 		if (!port_nest)
 			goto fail;
 		if (nla_put_nohdr(skb, sizeof(u32), &p->dev->ifindex) ||
@@ -91,7 +91,7 @@ static int br_mdb_fill_info(struct sk_buff *skb, struct netlink_callback *cb,
 	if (!mdb)
 		return 0;
 
-	nest = nla_nest_start(skb, MDBA_MDB);
+	nest = nla_nest_start_noflag(skb, MDBA_MDB);
 	if (nest == NULL)
 		return -EMSGSIZE;
 
@@ -105,7 +105,7 @@ static int br_mdb_fill_info(struct sk_buff *skb, struct netlink_callback *cb,
 			if (idx < s_idx)
 				goto skip;
 
-			nest2 = nla_nest_start(skb, MDBA_MDB_ENTRY);
+			nest2 = nla_nest_start_noflag(skb, MDBA_MDB_ENTRY);
 			if (nest2 == NULL) {
 				err = -EMSGSIZE;
 				goto out;
@@ -259,10 +259,10 @@ static int nlmsg_populate_mdb_fill(struct sk_buff *skb,
 	memset(bpm, 0, sizeof(*bpm));
 	bpm->family  = AF_BRIDGE;
 	bpm->ifindex = dev->ifindex;
-	nest = nla_nest_start(skb, MDBA_MDB);
+	nest = nla_nest_start_noflag(skb, MDBA_MDB);
 	if (nest == NULL)
 		goto cancel;
-	nest2 = nla_nest_start(skb, MDBA_MDB_ENTRY);
+	nest2 = nla_nest_start_noflag(skb, MDBA_MDB_ENTRY);
 	if (nest2 == NULL)
 		goto end;
 
@@ -457,7 +457,7 @@ static int nlmsg_populate_rtr_fill(struct sk_buff *skb,
 	memset(bpm, 0, sizeof(*bpm));
 	bpm->family = AF_BRIDGE;
 	bpm->ifindex = dev->ifindex;
-	nest = nla_nest_start(skb, MDBA_ROUTER);
+	nest = nla_nest_start_noflag(skb, MDBA_ROUTER);
 	if (!nest)
 		goto cancel;
 

commit 029fb07c2196ccdcb30bfe0229054ccc2c1849c7

[net] bridge: fix regression in br_mdb_fill_info()
Message-id: <20191216113940.42613-1-poros@redhat.com>
Patchwork-id: 292005
Patchwork-instance: patchwork
O-Subject: [RHEL8.2 PATCH] bridge: fix regression in br_mdb_fill_info()
Bugzilla: 1783888
RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1783888
Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=25391998
Tested: bridge mdb add/del/show
Upstream: RHEL only

Description:
Due to backport upstream patch ae0be8de9a53cd ("netlink: make
nla_nest_start() add NLA_F_NESTED flag") was introduced possible
regression into br_mdb_fill_info(). Upstream commit 19e3a9c90c5347
("net: bridge: convert multicast to generic rhashtable") missing in rhel and
i omited convert one nla_nest_start() to nla_nest_start_noflag()
It can couse different behavior in userspace with rhel/upstream kernel.
This patch convert remaining nla_nest_start() to nla_nest_start_noflag()

Fixes: 8248fa6aaa7553 ("[net] netlink: make nla_nest_start() add NLA_F_NESTED flag")
Signed-off-by: Petr Oros <poros@redhat.com>
Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

net/bridge/br_mdb.c
@@ -132,7 +132,7 @@ static int br_mdb_fill_info(struct sk_buff *skb, struct netlink_callback *cb,
 					e.addr.u.ip6 = p->addr.u.ip6;
 #endif
 				e.addr.proto = p->addr.proto;
-				nest_ent = nla_nest_start(skb,
+				nest_ent = nla_nest_start_noflag(skb,
 							  MDBA_MDB_ENTRY_INFO);
 				if (!nest_ent) {
 					nla_nest_cancel(skb, nest2);