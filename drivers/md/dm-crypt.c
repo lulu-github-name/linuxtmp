bac1cfd643c1e644183b4a7ce147afeec1d2f0b8
Mike Snitzer <snitzer@redhat.com>
2020-03-05 20:36:58 -0500

    [md] dm crypt and integrity: shash - remove shash_desc::flags
    Message-id: <20200305203809.31164-3-snitzer@redhat.com>
    Patchwork-id: 295545
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 02/73] dm crypt and integrity: shash - remove shash_desc::flags
    Bugzilla: 1810746
    RH-Acked-by: Mikulas Patocka <mpatocka@redhat.com>
    RH-Acked-by: Heinz Mauelshagen <heinzm@redhat.com>
    
    BZ: 1810746
    
    Following the lead of rhel8 commit 3dc60adee6085010a75d and only
    cherry-pick'ing the dm-crypt and dm-integrity hunks.
    
    commit 877b5691f27a1aec0d9b53095a323e45c30069e2
    Author: Eric Biggers <ebiggers@google.com>
    Date:   Sun Apr 14 17:37:09 2019 -0700
    
        crypto: shash - remove shash_desc::flags
    
        The flags field in 'struct shash_desc' never actually does anything.
        The only ostensibly supported flag is CRYPTO_TFM_REQ_MAY_SLEEP.
        However, no shash algorithm ever sleeps, making this flag a no-op.
    
        With this being the case, inevitably some users who can't sleep wrongly
        pass MAY_SLEEP.  These would all need to be fixed if any shash algorithm
        actually started sleeping.  For example, the shash_ahash_*() functions,
        which wrap a shash algorithm with the ahash API, pass through MAY_SLEEP
        from the ahash API to the shash API.  However, the shash functions are
        called under kmap_atomic(), so actually they're assumed to never sleep.
    
        Even if it turns out that some users do need preemption points while
        hashing large buffers, we could easily provide a helper function
        crypto_shash_update_large() which divides the data into smaller chunks
        and calls crypto_shash_update() and cond_resched() for each chunk.  It's
        not necessary to have a flag in 'struct shash_desc', nor is it necessary
        to make individual shash algorithms aware of this at all.
    
        Therefore, remove shash_desc::flags, and document that the
        crypto_shash_*() functions can be called from any context.
    
        Signed-off-by: Eric Biggers <ebiggers@google.com>
        Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -329,7 +329,6 @@ static int crypt_iv_essiv_init(struct crypt_config *cc)
 	int err;
 
 	desc->tfm = essiv->hash_tfm;
-	desc->flags = 0;
 
 	err = crypto_shash_digest(desc, cc->key, cc->key_size, essiv->salt);
 	shash_desc_zero(desc);
@@ -603,7 +602,6 @@ static int crypt_iv_lmk_one(struct crypt_config *cc, u8 *iv,
 	int i, r;
 
 	desc->tfm = lmk->hash_tfm;
-	desc->flags = 0;
 
 	r = crypto_shash_init(desc);
 	if (r)
@@ -765,7 +763,6 @@ static int crypt_iv_tcw_whitening(struct crypt_config *cc,
 
 	/* calculate crc32 for every 32bit part and xor it */
 	desc->tfm = tcw->crc32_tfm;
-	desc->flags = 0;
 	for (i = 0; i < 4; i++) {
 		r = crypto_shash_init(desc);
 		if (r)
