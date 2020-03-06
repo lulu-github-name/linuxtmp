f51a5d7c1aa7e8bd583b3e6c3bef9c53e85bc90a
Prarit Bhargava <prarit@redhat.com>
2020-03-06 13:30:51 -0500

    [kernel] workqueue: Make alloc/apply/free_workqueue_attrs() static
    Message-id: <20200306133101.31890-7-prarit@redhat.com>
    Patchwork-id: 295628
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 BZ 1810243 06/16] workqueue: Make alloc/apply/free_workqueue_attrs() static
    Bugzilla: 1810243
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Waiman Long <longman@redhat.com>
    RH-Acked-by: Luis Goncalves <lgoncalv@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1810243
    
    commit 2c9858ecbeb1e68224290043445990e29337d4c0
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date:   Wed Jun 26 16:52:37 2019 +0200
    
        workqueue: Make alloc/apply/free_workqueue_attrs() static
    
        None of those functions have any users outside of workqueue.c. Confine
        them.
    
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
        Signed-off-by: Tejun Heo <tj@kernel.org>
    
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -3299,7 +3299,7 @@ EXPORT_SYMBOL_GPL(execute_in_process_context);
  *
  * Undo alloc_workqueue_attrs().
  */
-void free_workqueue_attrs(struct workqueue_attrs *attrs)
+static void free_workqueue_attrs(struct workqueue_attrs *attrs)
 {
 	if (attrs) {
 		free_cpumask_var(attrs->cpumask);
@@ -3316,7 +3316,7 @@ void free_workqueue_attrs(struct workqueue_attrs *attrs)
  *
  * Return: The allocated new workqueue_attr on success. %NULL on failure.
  */
-struct workqueue_attrs *alloc_workqueue_attrs(gfp_t gfp_mask)
+static struct workqueue_attrs *alloc_workqueue_attrs(gfp_t gfp_mask)
 {
 	struct workqueue_attrs *attrs;
 
@@ -4003,7 +4003,7 @@ static int apply_workqueue_attrs_locked(struct workqueue_struct *wq,
  *
  * Return: 0 on success and -errno on failure.
  */
-int apply_workqueue_attrs(struct workqueue_struct *wq,
+static int apply_workqueue_attrs(struct workqueue_struct *wq,
 			  const struct workqueue_attrs *attrs)
 {
 	int ret;
@@ -4014,7 +4014,6 @@ int apply_workqueue_attrs(struct workqueue_struct *wq,
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(apply_workqueue_attrs);
 
 /**
  * wq_update_unbound_numa - update NUMA affinity of a wq for CPU hot[un]plug
