a0b201166180813bf8dfb656138f9f37fc74ce51
Maurizio Lombardi <mlombard@redhat.com>
2020-03-09 17:11:37 -0400

    [scsi] scsi: be2iscsi: remove unused variable dmsg
    Message-id: <20200309171141.28439-3-mlombard@redhat.com>
    Patchwork-id: 295766
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 e-stor PATCH 2/6] scsi: be2iscsi: remove unused variable dmsg
    Bugzilla: 1796740
    RH-Acked-by: Tomas Henzl <thenzl@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    From: Colin Ian King <colin.king@canonical.com>
    
    Variable dmsg is not being used and is redundant and hence can be
    removed.
    
    Cleans up clang warning:
    warning: variable 'dmsg' set but not used [-Wunused-but-set-variable]
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 45b7af985d546df2cc5e142c9c108d482811126c)
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/scsi/be2iscsi/be_main.c
+++ drivers/scsi/be2iscsi/be_main.c
@@ -1861,7 +1861,6 @@ unsigned int beiscsi_process_cq(struct be_eq_obj *pbe_eq, int budget)
 {
 	struct be_queue_info *cq;
 	struct sol_cqe *sol;
-	struct dmsg_cqe *dmsg;
 	unsigned int total = 0;
 	unsigned int num_processed = 0;
 	unsigned short code = 0, cid = 0;
@@ -1934,7 +1933,6 @@ unsigned int beiscsi_process_cq(struct be_eq_obj *pbe_eq, int budget)
 				    "BM_%d : Received %s[%d] on CID : %d\n",
 				    cqe_desc[code], code, cid);
 
-			dmsg = (struct dmsg_cqe *)sol;
 			hwi_complete_drvr_msgs(beiscsi_conn, phba, sol);
 			break;
 		case UNSOL_HDR_NOTIFY:
