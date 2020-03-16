0995bb4495e0c5faaebc922cd1f492dd296fec80
Kamal Heib <kheib@redhat.com>
2020-03-16 13:37:54 -0400

    [infiniband] RDMA/core: Make invalidate_range a device operation
    Message-id: <20200316133811.105286-48-kheib@redhat.com>
    Patchwork-id: 296614
    Patchwork-instance: patchwork
    O-Subject: [RHEL-8.3 PATCH 47/64] RDMA/core: Make invalidate_range a device operation
    Bugzilla: 1786601
    RH-Acked-by: Honggang Li <honli@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1786601
    
    commit ce51346feede2ea41de0ad58af2b514223e11dad
    Author: Moni Shoua <monis@mellanox.com>
    Date:   Mon Aug 19 14:17:08 2019 +0300
    
        RDMA/core: Make invalidate_range a device operation
    
        The callback function 'invalidate_range' is implemented in a driver so the
        place for it is in the ib_device_ops structure and not in ib_ucontext.
    
        Signed-off-by: Moni Shoua <monis@mellanox.com>
        Reviewed-by: Guy Levi <guyle@mellanox.com>
        Reviewed-by: Jason Gunthorpe <jgg@mellanox.com>
        Link: https://lore.kernel.org/r/20190819111710.18440-11-leon@kernel.org
        Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
        Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    
    Signed-off-by: Kamal Heib <kheib@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -1466,8 +1466,6 @@ struct ib_ucontext {
 
 	bool cleanup_retryable;
 
-	void (*invalidate_range)(struct ib_umem_odp *umem_odp,
-				 unsigned long start, unsigned long end);
 	struct mutex per_mm_list_lock;
 	struct list_head per_mm_list;
 
@@ -2426,6 +2424,8 @@ struct ib_device_ops {
 			    u64 iova);
 	int (*unmap_fmr)(struct list_head *fmr_list);
 	int (*dealloc_fmr)(struct ib_fmr *fmr);
+	void (*invalidate_range)(struct ib_umem_odp *umem_odp,
+				 unsigned long start, unsigned long end);
 	int (*attach_mcast)(struct ib_qp *qp, union ib_gid *gid, u16 lid);
 	int (*detach_mcast)(struct ib_qp *qp, union ib_gid *gid, u16 lid);
 	struct ib_xrcd *(*alloc_xrcd)(struct ib_device *device,
