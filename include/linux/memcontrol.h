ac8d9525db14299c901ce2f83fb7b3097dcd09ba
Waiman Long <longman@redhat.com>
2020-03-20 18:11:13 -0400

    [mm] mm, memcg: rename ambiguously named memory.stat counters and functions
    Message-id: <20200320181138.5897-18-longman@redhat.com>
    Patchwork-id: 297188
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 17/42] mm, memcg: rename ambiguously named memory.stat counters and functions
    Bugzilla: 1793534
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1793534
    Conflicts: The RH_KABI_RENAME() macro is used to do renaming to avoid
    	   kABI problem.
    
    commit 871789d4af807d1e91a6299f12a67e06177ed420
    Author: Chris Down <chris@chrisdown.name>
    Date:   Tue, 14 May 2019 15:46:57 -0700
    
        mm, memcg: rename ambiguously named memory.stat counters and functions
    
        I spent literally an hour trying to work out why an earlier version of
        my memory.events aggregation code doesn't work properly, only to find
        out I was calling memcg->events instead of memcg->memory_events, which
        is fairly confusing.
    
        This naming seems in need of reworking, so make it harder to do the
        wrong thing by using vmevents instead of events, which makes it more
        clear that these are vm counters rather than memcg-specific counters.
    
        There are also a few other inconsistent names in both the percpu and
        aggregated structs, so these are all cleaned up to be more coherent and
        easy to understand.
    
        This commit contains code cleanup only: there are no logic changes.
    
        [akpm@linux-foundation.org: fix it for preceding changes]
        Link: http://lkml.kernel.org/r/20190208224319.GA23801@chrisdown.name
        Signed-off-by: Chris Down <chris@chrisdown.name>
        Acked-by: Johannes Weiner <hannes@cmpxchg.org>
        Cc: Michal Hocko <mhocko@kernel.org>
        Cc: Tejun Heo <tj@kernel.org>
        Cc: Roman Gushchin <guro@fb.com>
        Cc: Dennis Zhou <dennis@kernel.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -94,8 +94,8 @@ enum mem_cgroup_events_target {
 	MEM_CGROUP_NTARGETS,
 };
 
-struct mem_cgroup_stat_cpu {
-	long count[MEMCG_NR_STAT];
+struct RH_KABI_RENAME(mem_cgroup_stat_cpu, memcg_vmstats_percpu) {
+	long RH_KABI_RENAME(count, stat)[MEMCG_NR_STAT];
 	unsigned long events[NR_VM_EVENT_ITEMS];
 	unsigned long nr_page_events;
 	unsigned long targets[MEM_CGROUP_NTARGETS];
@@ -280,12 +280,13 @@ struct mem_cgroup {
 	struct task_struct	*move_lock_task;
 
 	/* memory.stat */
-	struct mem_cgroup_stat_cpu __percpu *stat_cpu;
+	struct RH_KABI_RENAME(mem_cgroup_stat_cpu, memcg_vmstats_percpu)
+		__percpu *RH_KABI_RENAME(stat_cpu, vmstats_percpu);
 
 	MEMCG_PADDING(_pad2_);
 
-	atomic_long_t		stat[MEMCG_NR_STAT];
-	atomic_long_t		events[NR_VM_EVENT_ITEMS];
+	atomic_long_t		RH_KABI_RENAME(stat, vmstats)[MEMCG_NR_STAT];
+	atomic_long_t		RH_KABI_RENAME(events, vmevents)[NR_VM_EVENT_ITEMS];
 	atomic_long_t memory_events[MEMCG_NR_MEMORY_EVENTS];
 
 	unsigned long		socket_pressure;
@@ -563,7 +564,7 @@ void unlock_page_memcg(struct page *page);
 static inline unsigned long memcg_page_state(struct mem_cgroup *memcg,
 					     int idx)
 {
-	long x = atomic_long_read(&memcg->stat[idx]);
+	long x = atomic_long_read(&memcg->vmstats[idx]);
 #ifdef CONFIG_SMP
 	if (x < 0)
 		x = 0;
@@ -580,12 +581,12 @@ static inline void __mod_memcg_state(struct mem_cgroup *memcg,
 	if (mem_cgroup_disabled())
 		return;
 
-	x = val + __this_cpu_read(memcg->stat_cpu->count[idx]);
+	x = val + __this_cpu_read(memcg->vmstats_percpu->stat[idx]);
 	if (unlikely(abs(x) > MEMCG_CHARGE_BATCH)) {
-		atomic_long_add(x, &memcg->stat[idx]);
+		atomic_long_add(x, &memcg->vmstats[idx]);
 		x = 0;
 	}
-	__this_cpu_write(memcg->stat_cpu->count[idx], x);
+	__this_cpu_write(memcg->vmstats_percpu->stat[idx], x);
 }
 
 /* idx can be of type enum memcg_stat_item or node_stat_item */
@@ -723,12 +724,12 @@ static inline void __count_memcg_events(struct mem_cgroup *memcg,
 	if (mem_cgroup_disabled())
 		return;
 
-	x = count + __this_cpu_read(memcg->stat_cpu->events[idx]);
+	x = count + __this_cpu_read(memcg->vmstats_percpu->events[idx]);
 	if (unlikely(x > MEMCG_CHARGE_BATCH)) {
-		atomic_long_add(x, &memcg->events[idx]);
+		atomic_long_add(x, &memcg->vmevents[idx]);
 		x = 0;
 	}
-	__this_cpu_write(memcg->stat_cpu->events[idx], x);
+	__this_cpu_write(memcg->vmstats_percpu->events[idx], x);
 }
 
 static inline void count_memcg_events(struct mem_cgroup *memcg,
