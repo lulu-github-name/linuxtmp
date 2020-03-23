a5553d978d9400bb88d62eef52b46ce3879082da
Philipp Rudo <prudo@redhat.com>
2020-03-23 19:35:31 -0400

    [virtio] virtio_ring: fix return code on DMA mapping fails
    Message-id: <20200323193532.153369-2-prudo@redhat.com>
    Patchwork-id: 297409
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 1/2] [virtio] virtio_ring: fix return code on DMA mapping fails
    Bugzilla: 1797289
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Cornelia Huck <cohuck@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    
    Bugzilla: 1797289
    Upstream Status: kernel/git/torvalds/linux.git
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=27420236
    Tested: by IBM
    Conflicts: None
    
    commit f7728002c1c7bfa787b276a31c3ef458739b8e7c
    Author: Halil Pasic <pasic@linux.ibm.com>
    Date:   Thu Nov 14 13:46:46 2019 +0100
    
        virtio_ring: fix return code on DMA mapping fails
    
        Commit 780bc7903a32 ("virtio_ring: Support DMA APIs")  makes
        virtqueue_add() return -EIO when we fail to map our I/O buffers. This is
        a very realistic scenario for guests with encrypted memory, as swiotlb
        may run out of space, depending on it's size and the I/O load.
    
        The virtio-blk driver interprets -EIO form virtqueue_add() as an IO
        error, despite the fact that swiotlb full is in absence of bugs a
        recoverable condition.
    
        Let us change the return code to -ENOMEM, and make the block layer
        recover form these failures when virtio-blk encounters the condition
        described above.
    
        Cc: stable@vger.kernel.org
        Fixes: 780bc7903a32 ("virtio_ring: Support DMA APIs")
        Signed-off-by: Halil Pasic <pasic@linux.ibm.com>
        Tested-by: Michael Mueller <mimu@linux.ibm.com>
        Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    Signed-off-by: Philipp Rudo <prudo@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/virtio/virtio_ring.c
+++ drivers/virtio/virtio_ring.c
@@ -592,7 +592,7 @@ unmap_release:
 		kfree(desc);
 
 	END_USE(vq);
-	return -EIO;
+	return -ENOMEM;
 }
 
 static bool virtqueue_kick_prepare_split(struct virtqueue *_vq)
@@ -1094,7 +1094,7 @@ unmap_release:
 	kfree(desc);
 
 	END_USE(vq);
-	return -EIO;
+	return -ENOMEM;
 }
 
 static inline int virtqueue_add_packed(struct virtqueue *_vq,
