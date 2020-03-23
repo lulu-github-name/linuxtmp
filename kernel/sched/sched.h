9121761011314b491a332f86954da3ba9ccb42bb
Phil Auld <pauld@redhat.com>
2020-03-23 15:47:11 -0400

    [kernel] sched/core: Simplify sched_class::pick_next_task()
    Message-id: <20200323154713.13061-70-pauld@redhat.com>
    Patchwork-id: 297321
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 69/71] sched/core: Simplify sched_class::pick_next_task()
    Bugzilla: 1810399
    RH-Acked-by: Luis Goncalves <lgoncalv@redhat.com>
    RH-Acked-by: Juri Lelli <juri.lelli@redhat.com>
    RH-Acked-by: Waiman Long <longman@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1810399
    Upstream Status: linux
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=27369616
    Tested:  With cki.kt1, stress/ltp-lite, and manual scheduler tests. Kernel perf
    team ran a set of tests as noted in the bug.
    
    Conflicts: Modified for kABI.
    
    commit 98c2f700edb413e4baa4a0368c5861d96211a775
    Author: Peter Zijlstra <peterz@infradead.org>
    Date:   Fri Nov 8 14:15:58 2019 +0100
    
        sched/core: Simplify sched_class::pick_next_task()
    
        Now that the indirect class call never uses the last two arguments of
        pick_next_task(), remove them.
    
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: bsegall@google.com
        Cc: dietmar.eggemann@arm.com
        Cc: juri.lelli@redhat.com
        Cc: ktkhai@virtuozzo.com
        Cc: mgorman@suse.de
        Cc: qais.yousef@arm.com
        Cc: qperret@google.com
        Cc: rostedt@goodmis.org
        Cc: valentin.schneider@arm.com
        Cc: vincent.guittot@linaro.org
        Link: https://lkml.kernel.org/r/20191108131909.660595546@infradead.org
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Phil Auld <pauld@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1682,20 +1682,10 @@ struct sched_class {
 
 	void (*check_preempt_curr)(struct rq *rq, struct task_struct *p, int flags);
 
-	/*
-	 * Both @prev and @rf are optional and may be NULL, in which case the
-	 * caller must already have invoked put_prev_task(rq, prev, rf).
-	 *
-	 * Otherwise it is the responsibility of the pick_next_task() to call
-	 * put_prev_task() on the @prev task or something equivalent, IFF it
-	 * returns a next task.
-	 *
-	 * In that case (@rf != NULL) it may return RETRY_TASK when it finds a
-	 * higher prio class has runnable tasks.
-	 */
-	struct task_struct * (*pick_next_task)(struct rq *rq,
-					       struct task_struct *prev,
-					       struct rq_flags *rf);
+	RH_KABI_REPLACE(struct task_struct * (*pick_next_task)(struct rq *rq,
+							       struct task_struct *prev,
+						               struct rq_flags *rf),
+			struct task_struct *(*pick_next_task)(struct rq *rq))
 	void (*put_prev_task)(struct rq *rq, struct task_struct *p);
 
 #ifdef CONFIG_SMP
@@ -1796,7 +1786,7 @@ static inline bool sched_fair_runnable(struct rq *rq)
 }
 
 extern struct task_struct *pick_next_task_fair(struct rq *rq, struct task_struct *prev, struct rq_flags *rf);
-extern struct task_struct *pick_next_task_idle(struct rq *rq, struct task_struct *prev, struct rq_flags *rf);
+extern struct task_struct *pick_next_task_idle(struct rq *rq);
 
 #ifdef CONFIG_SMP
 
