016dc1cb4c5055a2ce59898c2ae2a73ed44f6ba9
Phil Auld <pauld@redhat.com>
2020-03-23 15:46:39 -0400

    [kernel] sched: Add task_struct pointer to sched_class::set_curr_task
    Message-id: <20200323154713.13061-38-pauld@redhat.com>
    Patchwork-id: 297288
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 37/71] sched: Add task_struct pointer to sched_class::set_curr_task
    Bugzilla: 1810399
    RH-Acked-by: Luis Goncalves <lgoncalv@redhat.com>
    RH-Acked-by: Juri Lelli <juri.lelli@redhat.com>
    RH-Acked-by: Waiman Long <longman@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1810399
    Upstream Status: linux
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=27369616
    Tested:  With cki.kt1, stress/ltp-lite, and manual scheduler tests. Kernel perf
    team ran a set of tests as noted in the bug.
    
    Conflicts: Modified for kABI. We need to use REPLACE instead of removing
    the one field and replacing it in a different spot.
    
    commit 03b7fad167efca3b7abbbb39733933f9df56e79c
    Author: Peter Zijlstra <peterz@infradead.org>
    Date:   Wed May 29 20:36:41 2019 +0000
    
        sched: Add task_struct pointer to sched_class::set_curr_task
    
        In preparation of further separating pick_next_task() and
        set_curr_task() we have to pass the actual task into it, while there,
        rename the thing to better pair with put_prev_task().
    
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Cc: Aaron Lu <aaron.lwe@gmail.com>
        Cc: Valentin Schneider <valentin.schneider@arm.com>
        Cc: mingo@kernel.org
        Cc: Phil Auld <pauld@redhat.com>
        Cc: Julien Desfossez <jdesfossez@digitalocean.com>
        Cc: Nishanth Aravamudan <naravamudan@digitalocean.com>
        Link: https://lkml.kernel.org/r/a96d1bcdd716db4a4c5da2fece647a1456c0ed78.1559129225.git.vpillai@digitalocean.com
    
    Signed-off-by: Phil Auld <pauld@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1701,7 +1701,8 @@ struct sched_class {
 	void (*rq_offline)(struct rq *rq);
 #endif
 
-	void (*set_curr_task)(struct rq *rq);
+	RH_KABI_REPLACE(void (*set_curr_task)(struct rq *rq),
+			void (*set_next_task)(struct rq *rq, struct task_struct *p))
 	void (*task_tick)(struct rq *rq, struct task_struct *p, int queued);
 	void (*task_fork)(struct task_struct *p);
 	void (*task_dead)(struct task_struct *p);
@@ -1739,9 +1740,10 @@ static inline void put_prev_task(struct rq *rq, struct task_struct *prev)
 	prev->sched_class->put_prev_task(rq, prev);
 }
 
-static inline void set_curr_task(struct rq *rq, struct task_struct *curr)
+static inline void set_next_task(struct rq *rq, struct task_struct *next)
 {
-	curr->sched_class->set_curr_task(rq);
+	WARN_ON_ONCE(rq->curr != next);
+	next->sched_class->set_next_task(rq, next);
 }
 
 #ifdef CONFIG_SMP
