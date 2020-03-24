463c538957d83f90bcff544786ecdf4dffbc4abb
Phil Auld <pauld@redhat.com>
2020-03-24 17:20:11 -0400

    [kernel] sched/membarrier: Fix p->mm->membarrier_state racy load
    Message-id: <20200324172011.GE23487@lorien.usersys.redhat.com>
    Patchwork-id: 297813
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 v2 53/71] sched/membarrier: Fix p->mm->membarrier_state racy load
    Bugzilla: 1810399
    RH-Acked-by: Luis Goncalves <lgoncalv@redhat.com>
    RH-Acked-by: Juri Lelli <juri.lelli@redhat.com>
    RH-Acked-by: Waiman Long <longman@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1810399
    Upstream Status: Linux
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=27446383
    Tested: With cki.kt1, stress/ltp-lite, with manual scheduler tests. Kernel perf
    team ran a set of tests as noted in the bug.
    
    Conflicts: One hunk in sched/sched.h applied by hand due to context differences.
    In mm_struct, not moving membarrier_state because it breaks kABI. Filling a 4-byte
    hole in struct rq.
    Note: This won't build upstream - breaks bisectability until c172e0a3e8.
    
    V2: Add missing conflicts section and fix indentation whitespace in sched/sched.h
    added code.
    
    commit 227a4aadc75ba22fcb6c4e1c078817b8cbaae4ce
    Author: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Date:   Thu Sep 19 13:37:02 2019 -0400
    
        sched/membarrier: Fix p->mm->membarrier_state racy load
    
        The membarrier_state field is located within the mm_struct, which
        is not guaranteed to exist when used from runqueue-lock-free iteration
        on runqueues by the membarrier system call.
    
        Copy the membarrier_state from the mm_struct into the scheduler runqueue
        when the scheduler switches between mm.
    
        When registering membarrier for mm, after setting the registration bit
        in the mm membarrier state, issue a synchronize_rcu() to ensure the
        scheduler observes the change. In order to take care of the case
        where a runqueue keeps executing the target mm without swapping to
        other mm, iterate over each runqueue and issue an IPI to copy the
        membarrier_state from the mm_struct into each runqueue which have the
        same mm which state has just been modified.
    
        Move the mm membarrier_state field closer to pgd in mm_struct to use
        a cache line already touched by the scheduler switch_mm.
    
        The membarrier_execve() (now membarrier_exec_mmap) hook now needs to
        clear the runqueue's membarrier state in addition to clear the mm
        membarrier state, so move its implementation into the scheduler
        membarrier code so it can access the runqueue structure.
    
        Add memory barrier in membarrier_exec_mmap() prior to clearing
        the membarrier state, ensuring memory accesses executed prior to exec
        are not reordered with the stores clearing the membarrier state.
    
        As suggested by Linus, move all membarrier.c RCU read-side locks outside
        of the for each cpu loops.
    
        Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
        Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Cc: Chris Metcalf <cmetcalf@ezchip.com>
        Cc: Christoph Lameter <cl@linux.com>
        Cc: Eric W. Biederman <ebiederm@xmission.com>
        Cc: Kirill Tkhai <tkhai@yandex.ru>
        Cc: Mike Galbraith <efault@gmx.de>
        Cc: Oleg Nesterov <oleg@redhat.com>
        Cc: Paul E. McKenney <paulmck@linux.ibm.com>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Russell King - ARM Linux admin <linux@armlinux.org.uk>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Link: https://lkml.kernel.org/r/20190919173705.2181-5-mathieu.desnoyers@efficios.com
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Phil Auld <pauld@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -871,6 +871,10 @@ struct rq {
 
 	atomic_t		nr_iowait;
 
+#ifdef CONFIG_MEMBARRIER
+	RH_KABI_FILL_HOLE(int membarrier_state) /* KABI use 4-byte hole */
+#endif
+
 #ifdef CONFIG_SMP
 	struct root_domain	*rd;
 	struct sched_domain	*sd;
@@ -2365,4 +2369,34 @@ unsigned long scale_irq_capacity(unsigned long util, unsigned long irq, unsigned
 
 #ifdef CONFIG_SMP
 extern struct static_key_false sched_energy_present;
+#endif
+
+#ifdef CONFIG_MEMBARRIER
+/*
+ * The scheduler provides memory barriers required by membarrier between:
+ * - prior user-space memory accesses and store to rq->membarrier_state,
+ * - store to rq->membarrier_state and following user-space memory accesses.
+ * In the same way it provides those guarantees around store to rq->curr.
+ */
+static inline void membarrier_switch_mm(struct rq *rq,
+					struct mm_struct *prev_mm,
+					struct mm_struct *next_mm)
+{
+	int membarrier_state;
+
+	if (prev_mm == next_mm)
+		return;
+
+	membarrier_state = atomic_read(&next_mm->membarrier_state);
+	if (READ_ONCE(rq->membarrier_state) == membarrier_state)
+		return;
+
+	WRITE_ONCE(rq->membarrier_state, membarrier_state);
+}
+#else
+static inline void membarrier_switch_mm(struct rq *rq,
+					struct mm_struct *prev_mm,
+					struct mm_struct *next_mm)
+{
+}
 #endif
