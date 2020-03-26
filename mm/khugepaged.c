552af3a3c9803974745ba9c1505186424e2938e0
Rafael Aquini <aquini@redhat.com>
2020-03-26 14:43:54 -0400

    [mm] mm/khugepaged: fix crashes due to misaccounted holes
    Message-id: <fa92ca0a6069d895cdc9e7b86444035cac572af7.1585232883.git.aquini@redhat.com>
    Patchwork-id: 297881
    Patchwork-instance: patchwork
    O-Subject: [RHEL8 BZ#1809698 PATCH 04/36] mm/khugepaged: fix crashes due to misaccounted holes
    Bugzilla: 1809698
    RH-Acked-by: Waiman Long <longman@redhat.com>
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    
    This patch is a backport of the following upstream commit:
    commit aaa52e340073b7f4593b3c4ddafcafa70cf838b5
    Author: Hugh Dickins <hughd@google.com>
    Date:   Fri Nov 30 14:10:29 2018 -0800
    
        mm/khugepaged: fix crashes due to misaccounted holes
    
        Huge tmpfs testing on a shortish file mapped into a pmd-rounded extent
        hit shmem_evict_inode()'s WARN_ON(inode->i_blocks) followed by
        clear_inode()'s BUG_ON(inode->i_data.nrpages) when the file was later
        closed and unlinked.
    
        khugepaged's collapse_shmem() was forgetting to update mapping->nrpages
        on the rollback path, after it had added but then needs to undo some
        holes.
    
        There is indeed an irritating asymmetry between shmem_charge(), whose
        callers want it to increment nrpages after successfully accounting
        blocks, and shmem_uncharge(), when __delete_from_page_cache() already
        decremented nrpages itself: oh well, just add a comment on that to them
        both.
    
        And shmem_recalc_inode() is supposed to be called when the accounting is
        expected to be in balance (so it can deduce from imbalance that reclaim
        discarded some pages): so change shmem_charge() to update nrpages
        earlier (though it's rare for the difference to matter at all).
    
        Link: http://lkml.kernel.org/r/alpine.LSU.2.11.1811261523450.2275@eggly.anvils
        Fixes: 800d8c63b2e98 ("shmem: add huge pages support")
        Fixes: f3f0e1d2150b2 ("khugepaged: add support of collapse for tmpfs/shmem pages")
        Signed-off-by: Hugh Dickins <hughd@google.com>
        Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
        Cc: Jerome Glisse <jglisse@redhat.com>
        Cc: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
        Cc: Matthew Wilcox <willy@infradead.org>
        Cc: <stable@vger.kernel.org>    [4.8+]
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1525,8 +1525,9 @@ tree_unlocked:
 		*hpage = NULL;
 	} else {
 		/* Something went wrong: rollback changes to the radix-tree */
-		shmem_uncharge(mapping->host, nr_none);
 		xa_lock_irq(&mapping->i_pages);
+		mapping->nrpages -= nr_none;
+		shmem_uncharge(mapping->host, nr_none);
 		radix_tree_for_each_slot(slot, &mapping->i_pages, &iter, start) {
 			if (iter.index >= end)
 				break;
