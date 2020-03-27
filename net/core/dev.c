36938efe2fdbf5d6dfcf0065c1b79555ac413aa7
Ivan Vecera <ivecera@redhat.com>
2020-03-27 19:44:19 -0400

    [net] export netdev_next_lower_dev_rcu()
    Message-id: <20200327194424.1643094-15-ivecera@redhat.com>
    Patchwork-id: 298085
    Patchwork-instance: patchwork
    O-Subject: [RHEL-8.3 net PATCH 14/19] net: export netdev_next_lower_dev_rcu()
    Bugzilla: 1818074
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Petr Oros <poros@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1818074
    
    Upstream commit(s):
    commit 7151affeef8d527f50b4b68a871fd28bd660023f
    Author: Taehee Yoo <ap420073@gmail.com>
    Date:   Sat Feb 15 10:50:21 2020 +0000
    
        net: export netdev_next_lower_dev_rcu()
    
        netdev_next_lower_dev_rcu() will be used to implement a function,
        which is to walk all lower interfaces.
        There are already functions that they walk their lower interface.
        (netdev_walk_all_lower_dev_rcu, netdev_walk_all_lower_dev()).
        But, there would be cases that couldn't be covered by given
        netdev_walk_all_lower_dev_{rcu}() function.
        So, some modules would want to implement own function,
        which is to walk all lower interfaces.
    
        In the next patch, netdev_next_lower_dev_rcu() will be used.
        In addition, this patch removes two unused prototypes in netdevice.h.
    
        Signed-off-by: Taehee Yoo <ap420073@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -154,7 +154,6 @@
 
 /* Instead of increasing this, you should create a hash table. */
 #define MAX_GRO_SKBS 8
-#define MAX_NEST_DEV 8
 
 /* This should be increased if a protocol with a bigger head is added. */
 #define GRO_MAX_HEAD (MAX_HEADER + 128)
@@ -6605,8 +6604,8 @@ static int __netdev_walk_all_lower_dev(struct net_device *dev,
 	return 0;
 }
 
-static struct net_device *netdev_next_lower_dev_rcu(struct net_device *dev,
-						    struct list_head **iter)
+struct net_device *netdev_next_lower_dev_rcu(struct net_device *dev,
+					     struct list_head **iter)
 {
 	struct netdev_adjacent *lower;
 
@@ -6618,6 +6617,7 @@ static struct net_device *netdev_next_lower_dev_rcu(struct net_device *dev,
 
 	return lower->dev;
 }
+EXPORT_SYMBOL(netdev_next_lower_dev_rcu);
 
 static u8 __netdev_upper_depth(struct net_device *dev)
 {
