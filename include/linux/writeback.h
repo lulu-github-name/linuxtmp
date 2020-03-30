b10fc245b1349b452057c28da54c092d7f3454f1
Ming Lei <ming.lei@redhat.com>
2020-03-30 01:02:54 -0400

    [include] blkcg, writeback: Implement wbc_blkcg_css()
    Message-id: <20200330010628.30014-12-ming.lei@redhat.com>
    Patchwork-id: 298109
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 V4 011/225] blkcg, writeback: Implement wbc_blkcg_css()
    Bugzilla: 1798919
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ: 1798919
    Conflicts: use RH_KABI_HIDE_INCLUDE to avoid include header for avoid kabi warning
    
    commit 653c45c6b90c9659facbef10546d1f3a8e37d0cf
    Author: Tejun Heo <tj@kernel.org>
    Date:   Thu Jun 27 13:39:51 2019 -0700
    
        blkcg, writeback: Implement wbc_blkcg_css()
    
        Add a helper to determine the target blkcg from wbc.
    
        Reviewed-by: Josef Bacik <josef@toxicpanda.com>
        Reviewed-by: Jan Kara <jack@suse.cz>
        Signed-off-by: Tejun Heo <tj@kernel.org>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/linux/writeback.h
+++ include/linux/writeback.h
@@ -12,6 +12,7 @@
 #include <linux/backing-dev-defs.h>
 #include <linux/blk_types.h>
 #include <linux/rh_kabi.h>
+#include RH_KABI_HIDE_INCLUDE(<linux/blk-cgroup.h>)
 
 struct bio;
 
@@ -103,6 +104,16 @@ static inline int wbc_to_write_flags(struct writeback_control *wbc)
 		return REQ_BACKGROUND;
 
 	return 0;
+}
+
+static inline struct cgroup_subsys_state *
+wbc_blkcg_css(struct writeback_control *wbc)
+{
+#ifdef CONFIG_CGROUP_WRITEBACK
+	if (wbc->wb)
+		return wbc->wb->blkcg_css;
+#endif
+	return blkcg_root_css;
 }
 
 /*
