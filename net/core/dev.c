3a4b88cf9be710aaaec30d1c065e4271a8c1f596
Ivan Vecera <ivecera@redhat.com>
2020-04-04 15:31:17 -0400

    [net] Add and use skb_mark_not_on_list().
    Message-id: <20200404153119.917762-6-ivecera@redhat.com>
    Patchwork-id: 299191
    Patchwork-instance: patchwork
    O-Subject: [RHEL-8.3 net PATCH 5/7] net: Add and use skb_mark_not_on_list().
    Bugzilla: 1805302
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1805302
    
    Conflicts: \
    - The commit was already backported into RHEL and skb_mark_not_on_list()
      exists but it is not used where it should be.
    - Hunks for the following files were removed due to existing
      backport of commits from upstream series dda6a7a3685b
      ("Merge branch 'ipv6-defrag-rbtree'"): net/ipv4/ip_fragment.c
      net/ipv6/netfilter/nf_conntrack_reasm.c net/ipv6/reassembly.c
    - Hunk for net/ipv4/ip_input.c was removed as it is not applicable due to
      absence of commit a16bb99784d1 ("net: ipv4: fix listify ip_rcv_finish
      in case of forwarding")
    
    Upstream commit(s):
    commit a8305bff685252e80b7c60f4f5e7dd2e63e38218
    Author: David S. Miller <davem@davemloft.net>
    Date:   Sun Jul 29 20:42:53 2018 -0700
    
        net: Add and use skb_mark_not_on_list().
    
        An SKB is not on a list if skb->next is NULL.
    
        Codify this convention into a helper function and use it
        where we are dequeueing an SKB and need to mark it as such.
    
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3191,7 +3191,7 @@ struct sk_buff *dev_hard_start_xmit(struct sk_buff *first, struct net_device *de
 	while (skb) {
 		struct sk_buff *next = skb->next;
 
-		skb->next = NULL;
+		skb_mark_not_on_list(skb);
 		rc = xmit_one(skb, dev, txq, next != NULL);
 		if (unlikely(!dev_xmit_complete(rc))) {
 			skb->next = next;
@@ -3291,7 +3291,7 @@ struct sk_buff *validate_xmit_skb_list(struct sk_buff *skb, struct net_device *d
 
 	for (; skb != NULL; skb = next) {
 		next = skb->next;
-		skb->next = NULL;
+		skb_mark_not_on_list(skb);
 
 		/* in case skb wont be segmented, point to itself */
 		skb->prev = skb;
@@ -5107,7 +5107,7 @@ void napi_gro_flush(struct napi_struct *napi, bool flush_old)
 		if (flush_old && NAPI_GRO_CB(skb)->age == jiffies)
 			return;
 		list_del(&skb->list);
-		skb->next = NULL;
+		skb_mark_not_on_list(skb);
 		napi_gro_complete(skb);
 		napi->gro_count--;
 	}
@@ -5258,7 +5258,7 @@ static enum gro_result dev_gro_receive(struct napi_struct *napi, struct sk_buff 
 
 	if (pp) {
 		list_del(&pp->list);
-		pp->next = NULL;
+		skb_mark_not_on_list(pp);
 		napi_gro_complete(pp);
 		napi->gro_count--;
 	}
