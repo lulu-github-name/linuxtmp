199d3a80b3395eee7ce5d242ffdfe541381ae985
Myron Stowe <myron.stowe@redhat.com>
2020-04-13 17:13:32 -0400

    [pci] PCI/MSI: Remove unused pci_irq_get_node()
    Message-id: <20200413171332.195133.64336.stgit@tak.stowe>
    Patchwork-id: 301354
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 32/69] PCI/MSI: Remove unused pci_irq_get_node()
    Bugzilla: 1820933
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1820933
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=27894501
    Upstream Status: d41b0e64d206f8948212b4d0f30c330db95c9636
    
    commit d41b0e64d206f8948212b4d0f30c330db95c9636
    Author: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Date:   Mon Oct 14 12:04:52 2019 +0200
    
        PCI/MSI: Remove unused pci_irq_get_node()
    
        The function pci_irq_get_node() is not used by anyone in the tree, so just
        delete it.
    
        Link: https://lore.kernel.org/r/20191014100452.GA6699@kroah.com
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
        Reviewed-by: Andrew Murray <andrew.murray@arm.com>
    
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -1501,7 +1501,6 @@ int pci_alloc_irq_vectors_affinity(struct pci_dev *dev, unsigned int min_vecs,
 void pci_free_irq_vectors(struct pci_dev *dev);
 int pci_irq_vector(struct pci_dev *dev, unsigned int nr);
 const struct cpumask *pci_irq_get_affinity(struct pci_dev *pdev, int vec);
-int pci_irq_get_node(struct pci_dev *pdev, int vec);
 
 #else
 static inline int pci_msi_vec_count(struct pci_dev *dev) { return -ENOSYS; }
@@ -1543,11 +1542,6 @@ static inline const struct cpumask *pci_irq_get_affinity(struct pci_dev *pdev,
 		int vec)
 {
 	return cpu_possible_mask;
-}
-
-static inline int pci_irq_get_node(struct pci_dev *pdev, int vec)
-{
-	return first_online_node;
 }
 #endif
 
