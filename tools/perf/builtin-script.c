0e7acb8462a66c8fffadb9aa6f60df11e927bc09
Michael Petlan <mpetlan@redhat.com>
2020-04-15 14:28:18 -0400

    [tools] perf script: Allow showing the --switch-on event
    Message-id: <20200415143309.16001-178-mpetlan@redhat.com>
    Patchwork-id: 302418
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 178/469] perf script: Allow showing the --switch-on event
    Bugzilla: 1818835
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    
    Bugzilla: 1818835
    https://bugzilla.redhat.com/show_bug.cgi?id=1818835
    
    upstream
    ========
    commit 6469eb6dffeb44edfa3d4ca496b044b4a9c643b9
    Author: Arnaldo Carvalho de Melo <acme@redhat.com>
    Date: Wed Aug 14 16:40:58 2019 -0300
    
    description
    ===========
    One may want to see the --switch-on event as well, allow for that, using
    the previous cset example:
    
      # perf script --switch-on syscalls:sys_enter_nanosleep --show-on-off
            sleep 13638 [001] 108237.582286: syscalls:sys_enter_nanosleep: rqtp: 0x7fff1948ac40, rmtp: 0x00000000
            sleep 13638 [001] 108237.582289:     sched:sched_stat_runtime: comm=sleep pid=13638 runtime=578104 [ns] vruntime=202889459556 [ns]
            sleep 13638 [001] 108237.582291:           sched:sched_switch: sleep:13638 [120] S ==> swapper/1:0 [120]
          swapper     0 [001] 108238.582428:           sched:sched_waking: comm=sleep pid=13638 prio=120 target_cpu=001
          swapper     0 [001] 108238.582458:           sched:sched_wakeup: sleep:13638 [120] success=1 CPU:001
            sleep 13638 [001] 108238.582698:     sched:sched_stat_runtime: comm=sleep pid=13638 runtime=173915 [ns] vruntime=202889633471 [ns]
            sleep 13638 [001] 108238.582782:     sched:sched_process_exit: comm=sleep pid=13638 prio=120
      #
      # perf script --switch-on syscalls:sys_enter_nanosleep
            sleep 13638 [001] 108237.582289:     sched:sched_stat_runtime: comm=sleep pid=13638 runtime=578104 [ns] vruntime=202889459556 [ns]
            sleep 13638 [001] 108237.582291:           sched:sched_switch: sleep:13638 [120] S ==> swapper/1:0 [120]
          swapper     0 [001] 108238.582428:           sched:sched_waking: comm=sleep pid=13638 prio=120 target_cpu=001
          swapper     0 [001] 108238.582458:           sched:sched_wakeup: sleep:13638 [120] success=1 CPU:001
            sleep 13638 [001] 108238.582698:     sched:sched_stat_runtime: comm=sleep pid=13638 runtime=173915 [ns] vruntime=202889633471 [ns]
            sleep 13638 [001] 108238.582782:     sched:sched_process_exit: comm=sleep pid=13638 prio=120
      #
    
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- tools/perf/builtin-script.c
+++ tools/perf/builtin-script.c
@@ -1620,6 +1620,7 @@ static int perf_sample__fprintf_synth(struct perf_sample *sample,
 struct evswitch {
 	struct evsel *on;
 	bool	     discarding;
+	bool	     show_on_off_events;
 };
 
 struct perf_script {
@@ -1817,6 +1818,9 @@ static void process_event(struct perf_script *script,
 			return;
 
 		script->evswitch.discarding = false;
+
+		if (!script->evswitch.show_on_off_events)
+			return;
 	}
 
 	++es->samples;
@@ -3555,6 +3559,8 @@ int cmd_script(int argc, const char **argv)
 		   "file", "file saving guest os /proc/modules"),
 	OPT_STRING(0, "switch-on", &event_switch_on,
 		   "event", "Consider events from the first ocurrence of this event"),
+	OPT_BOOLEAN(0, "show-on-off-events", &script.evswitch.show_on_off_events,
+		    "Show the on/off switch events, used with --switch-on"),
 	OPT_END()
 	};
 	const char * const script_subcommands[] = { "record", "report", NULL };
