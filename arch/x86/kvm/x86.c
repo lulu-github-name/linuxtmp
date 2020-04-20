37ff7a5e3339f7617b929063582d090219747e2a
Vitaly Kuznetsov <vkuznets@redhat.com>
2020-04-20 15:16:06 -0400

    [virt] x86/kvm: Cache gfn to pfn translation
    Message-id: <20200420151606.931902-1-vkuznets@redhat.com>
    Patchwork-id: 304002
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 virt PATCH v2 288/614] x86/kvm: Cache gfn to pfn translation
    Bugzilla: 1813987
    RH-Acked-by: Andrew Jones <drjones@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    
    __kvm_map_gfn()'s call to gfn_to_pfn_memslot() is
    * relatively expensive
    * in certain cases (such as when done from atomic context) cannot be called
    
    Stashing gfn-to-pfn mapping should help with both cases.
    
    This is part of CVE-2019-3016.
    
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 917248144db5d7320655dbb41d3af0b8a0f3d589)
    
    Conflicts:
    	arch/x86/kvm/x86.c (see upstream merge conflict 4cbc418a44d50)
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -9327,7 +9327,10 @@ void kvm_arch_vcpu_postcreate(struct kvm_vcpu *vcpu)
 
 void kvm_arch_vcpu_destroy(struct kvm_vcpu *vcpu)
 {
+	struct gfn_to_pfn_cache *cache = &vcpu->arch.st.cache;
 	int idx;
+
+	kvm_release_pfn(cache->pfn, cache->dirty, cache);
 
 	kvmclock_reset(vcpu);
 
@@ -9857,11 +9860,18 @@ out_free:
 
 void kvm_arch_memslots_updated(struct kvm *kvm, u64 gen)
 {
+	struct kvm_vcpu *vcpu;
+	int i;
+
 	/*
 	 * memslots->generation has been incremented.
 	 * mmio generation may have reached its maximum value.
 	 */
 	kvm_mmu_invalidate_mmio_sptes(kvm, gen);
+
+	/* Force re-initialization of steal_time cache */
+	kvm_for_each_vcpu(i, vcpu, kvm)
+		kvm_vcpu_kick(vcpu);
 }
 
 int kvm_arch_prepare_memory_region(struct kvm *kvm,
