876c93c9f6bb421bcedc7d5f74c16fe940485204
Florian Westphal <fwestpha@redhat.com>
2020-04-24 10:02:11 -0400

    [net] netfilter: conntrack: pass nf_hook_state to packet and error handlers
    Message-id: <20200424100218.26317-3-fwestpha@redhat.com>
    Patchwork-id: 304752
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 net 22/40] netfilter: conntrack: pass nf_hook_state to packet and error handlers
    Bugzilla: 1822085
    RH-Acked-by: Guillaume Nault <gnault@redhat.com>
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1822085
    Upstream Status: commit 93e66024b024
    
    commit 93e66024b0249cec81e91328c55a754efd3192e0
    Author: Florian Westphal <fw@strlen.de>
    Date:   Wed Sep 12 15:19:07 2018 +0200
    
        netfilter: conntrack: pass nf_hook_state to packet and error handlers
    
        nf_hook_state contains all the hook meta-information: netns, protocol family,
        hook location, and so on.
    
        Instead of only passing selected information, pass a pointer to entire
        structure.
    
        This will allow to merge the error and the packet handlers and remove
        the ->new() function in followup patches.
    
        Signed-off-by: Florian Westphal <fw@strlen.de>
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    Signed-off-by: Florian Westphal <fwestpha@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- net/netfilter/nf_conntrack_proto.c
+++ net/netfilter/nf_conntrack_proto.c
@@ -452,7 +452,7 @@ static unsigned int ipv4_conntrack_in(void *priv,
 				      struct sk_buff *skb,
 				      const struct nf_hook_state *state)
 {
-	return nf_conntrack_in(state->net, PF_INET, state->hook, skb);
+	return nf_conntrack_in(skb, state);
 }
 
 static unsigned int ipv4_conntrack_local(void *priv,
@@ -474,7 +474,7 @@ static unsigned int ipv4_conntrack_local(void *priv,
 		return NF_ACCEPT;
 	}
 
-	return nf_conntrack_in(state->net, PF_INET, state->hook, skb);
+	return nf_conntrack_in(skb, state);
 }
 
 /* Connection tracking may drop packets, but never alters them, so
@@ -687,14 +687,14 @@ static unsigned int ipv6_conntrack_in(void *priv,
 				      struct sk_buff *skb,
 				      const struct nf_hook_state *state)
 {
-	return nf_conntrack_in(state->net, PF_INET6, state->hook, skb);
+	return nf_conntrack_in(skb, state);
 }
 
 static unsigned int ipv6_conntrack_local(void *priv,
 					 struct sk_buff *skb,
 					 const struct nf_hook_state *state)
 {
-	return nf_conntrack_in(state->net, PF_INET6, state->hook, skb);
+	return nf_conntrack_in(skb, state);
 }
 
 static unsigned int ipv6_helper(void *priv,
