dd0bbcf3a0d304bfce2af7ee87a250821eb54ded
David Arcari <darcari@redhat.com>
2020-04-23 20:34:24 -0400

    [cpuidle] cpuidle: Drop disabled field from struct cpuidle_state
    Message-id: <20200423203424.59061-1-darcari@redhat.com>
    Patchwork-id: 304727
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 BZ 1825353 11/18] cpuidle: Drop disabled field from struct cpuidle_state
    Bugzilla: 1825353
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1825353
    Conflicts: shmobile is not supported on RHEL
    	   KABI: deprecated disabled
    
    commit ba1e78a1dc0ca3e92f0be82279e6ba24177af7d6
    Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Date:   Thu Nov 21 19:41:51 2019 +0100
    
        cpuidle: Drop disabled field from struct cpuidle_state
    
        After recent cpuidle updates the "disabled" field in struct
        cpuidle_state is only used by two drivers (intel_idle and shmobile
        cpuidle) for marking unusable idle states, but that may as well be
        achieved with the help of a state flag, so define an "unusable" idle
        state flag, CPUIDLE_FLAG_UNUSABLE, make the drivers in question use
        it instead of the "disabled" field and make the core set
        CPUIDLE_STATE_DISABLED_BY_DRIVER for the idle states with that flag
        set.
    
        After the above changes, the "disabled" field in struct cpuidle_state
        is not used any more, so drop it.
    
        No intentional functional impact.
    
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Cc: Al Stone <ahs3@redhat.com>
    Cc: Prarit Bhargava <prarit@redhat.com>
    Cc: David Arcari <darcari@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -86,7 +86,7 @@ struct cpuidle_state {
 	unsigned int	exit_latency; /* in US */
 	int		power_usage; /* in mW */
 	unsigned int	target_residency; /* in US */
-	bool		disabled; /* disabled on all CPUs */
+	RH_KABI_DEPRECATE(bool,	disabled) /* disabled on all CPUs */
 
 	int (*enter)	(struct cpuidle_device *dev,
 			struct cpuidle_driver *drv,
@@ -111,6 +111,7 @@ struct cpuidle_state {
 #define CPUIDLE_FLAG_POLLING	BIT(0) /* polling state */
 #define CPUIDLE_FLAG_COUPLED	BIT(1) /* state applies to multiple cpus */
 #define CPUIDLE_FLAG_TIMER_STOP BIT(2) /* timer is stopped on this state */
+#define CPUIDLE_FLAG_UNUSABLE	BIT(3) /* avoid using this state */
 
 struct cpuidle_device_kobj;
 struct cpuidle_state_kobj;
