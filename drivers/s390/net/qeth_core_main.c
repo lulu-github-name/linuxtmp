45685731ba190ca192d9aae80177491a11392b29
Philipp Rudo <prudo@redhat.com>
2020-04-29 16:39:16 -0400

    [s390] s390/qeth: consolidate some duplicated HW cmd code
    Message-id: <20200429163937.10969-21-prudo@redhat.com>
    Patchwork-id: 305851
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 20/41] [s390] s390/qeth: consolidate some duplicated HW cmd code
    Bugzilla: 1780667
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1780667
    Upstream Status: kernel/git/torvalds/linux.git
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=28230994
    Tested: by IBM
    Conflicts: None
    
    commit 9897d583b01525c6fb5dcaaa87263e7f395c905c
    Author: Julian Wiedmann <jwi@linux.ibm.com>
    Date:   Thu Oct 31 13:42:18 2019 +0100
    
        s390/qeth: consolidate some duplicated HW cmd code
    
        When setting a device online, both subdrivers have the same code to
        program the HW trap and Isolation mode. Move that code into a single
        place.
    
        Signed-off-by: Julian Wiedmann <jwi@linux.ibm.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Philipp Rudo <prudo@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/s390/net/qeth_core_main.c
+++ drivers/s390/net/qeth_core_main.c
@@ -4325,7 +4325,6 @@ int qeth_set_access_ctrl_online(struct qeth_card *card, int fallback)
 	}
 	return rc;
 }
-EXPORT_SYMBOL_GPL(qeth_set_access_ctrl_online);
 
 void qeth_tx_timeout(struct net_device *dev, unsigned int txqueue)
 {
@@ -5043,6 +5042,15 @@ retriable:
 			goto out;
 		}
 	}
+
+	if (!qeth_is_diagass_supported(card, QETH_DIAGS_CMD_TRAP) ||
+	    (card->info.hwtrap && qeth_hw_trap(card, QETH_DIAGS_TRAP_ARM)))
+		card->info.hwtrap = 0;
+
+	rc = qeth_set_access_ctrl_online(card, 0);
+	if (rc)
+		goto out;
+
 	return 0;
 out:
 	dev_warn(&card->gdev->dev, "The qeth device driver failed to recover "
