c102a11649004d2c1be810c233aebdd12bfa6e8e
Ivan Vecera <ivecera@redhat.com>
2020-04-30 11:28:35 -0400

    [net] ipv6: Remove old route notifications and convert listeners
    Message-id: <20200430112841.2445049-21-ivecera@redhat.com>
    Patchwork-id: 305984
    Patchwork-instance: patchwork
    O-Subject: [RHEL-8.3 net PATCH 20/26] ipv6: Remove old route notifications and convert listeners
    Bugzilla: 1828994
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1828994
    
    Upstream commit(s):
    commit caafb2509fac1432849650826953dd88b7cbe374
    Author: Ido Schimmel <idosch@mellanox.com>
    Date:   Mon Dec 23 15:28:20 2019 +0200
    
        ipv6: Remove old route notifications and convert listeners
    
        Now that mlxsw is converted to use the new FIB notifications it is
        possible to delete the old ones and use the new replace / append /
        delete notifications.
    
        Signed-off-by: Ido Schimmel <idosch@mellanox.com>
        Reviewed-by: Jiri Pirko <jiri@mellanox.com>
        Reviewed-by: David Ahern <dsahern@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -3300,15 +3300,10 @@ static int __ip6_del_rt_siblings(struct fib6_info *rt, struct fib6_config *cfg)
 								  replace_rt);
 			else
 				call_fib6_multipath_entry_notifiers(net,
-						       FIB_EVENT_ENTRY_DEL_TMP,
+						       FIB_EVENT_ENTRY_DEL,
 						       rt, rt->fib6_nsiblings,
 						       NULL);
 		}
-		call_fib6_multipath_entry_notifiers(net,
-						    FIB_EVENT_ENTRY_DEL,
-						    rt,
-						    rt->fib6_nsiblings,
-						    NULL);
 		list_for_each_entry_safe(sibling, next_sibling,
 					 &rt->fib6_siblings,
 					 fib6_siblings) {
@@ -4454,7 +4449,6 @@ static int ip6_route_multipath_add(struct fib6_config *cfg,
 {
 	struct fib6_info *rt_notif = NULL, *rt_last = NULL;
 	struct nl_info *info = &cfg->fc_nlinfo;
-	enum fib_event_type event_type;
 	struct fib6_config r_cfg;
 	struct rtnexthop *rtnh;
 	struct fib6_info *rt;
@@ -4590,7 +4584,7 @@ static int ip6_route_multipath_add(struct fib6_config *cfg,
 		if (rt_notif->fib6_nsiblings != nhn - 1)
 			fib_event = FIB_EVENT_ENTRY_APPEND;
 		else
-			fib_event = FIB_EVENT_ENTRY_REPLACE_TMP;
+			fib_event = FIB_EVENT_ENTRY_REPLACE;
 
 		err = call_fib6_multipath_entry_notifiers(info->nl_net,
 							  fib_event, rt_notif,
@@ -4600,14 +4594,6 @@ static int ip6_route_multipath_add(struct fib6_config *cfg,
 			err_nh = NULL;
 			goto add_errout;
 		}
-	}
-	event_type = replace ? FIB_EVENT_ENTRY_REPLACE : FIB_EVENT_ENTRY_ADD;
-	err = call_fib6_multipath_entry_notifiers(info->nl_net, event_type,
-						  rt_notif, nhn - 1, extack);
-	if (err) {
-		/* Delete all the siblings that were just added */
-		err_nh = NULL;
-		goto add_errout;
 	}
 
 	/* success ... tell user about new route */
