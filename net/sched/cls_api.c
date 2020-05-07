torvalds/linux

commit 74522e7baae2561870ea8ddf09dc6a126458cd7b

net: sched: set the hw_stats_type in pedit loop
For a single pedit action, multiple offload entries may be used. Set the
hw_stats_type to all of them.

Fixes: 44f865801741 ("sched: act: allow user to specify type of HW stats for a filter")
Signed-off-by: Jiri Pirko <jiri@mellanox.com>
Signed-off-by: David S. Miller <davem@davemloft.net>

net/sched/cls_api.c
@@ -3614,6 +3614,7 @@ int tc_setup_flow_action(struct flow_action *flow_action,
 				entry->mangle.val = tcf_pedit_val(act, k);
 				entry->mangle.offset = tcf_pedit_offset(act, k);
 				entry = &flow_action->entries[++j];
+				entry->hw_stats_type = act->hw_stats_type;
 			}
 		} else if (is_tcf_csum(act)) {
 			entry->id = FLOW_ACTION_CSUM;

lulu-github-name/linuxtmp

commit 8b26e8fac929ca11d58d035221c2ccb8298824b7

[net] sched: Fix hw_stats_type setting in pedit loop
Message-id: <20200429132237.1383648-13-ivecera@redhat.com>
Patchwork-id: 305812
Patchwork-instance: patchwork
O-Subject: [RHEL-8.3 net PATCH 12/12] net: sched: Fix hw_stats_type setting in pedit loop
Bugzilla: 1817156
RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
RH-Acked-by: Petr Oros <poros@redhat.com>
RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1817156

Upstream commit(s):
commit 2c4b58dc75dd291c9ce6100bcb892322cd004614
Author: Petr Machata <petrm@mellanox.com>
Date:   Wed Mar 18 19:42:29 2020 +0200

    net: sched: Fix hw_stats_type setting in pedit loop

    In the commit referenced below, hw_stats_type of an entry is set for every
    entry that corresponds to a pedit action. However, the assignment is only
    done after the entry pointer is bumped, and therefore could overwrite
    memory outside of the entries array.

    The reason for this positioning may have been that the current entry's
    hw_stats_type is already set above, before the action-type dispatch.
    However, if there are no more actions, the assignment is wrong. And if
    there are, the next round of the for_each_action loop will make the
    assignment before the action-type dispatch anyway.

    Therefore fix this issue by simply reordering the two lines.

    Fixes: 74522e7baae2 ("net: sched: set the hw_stats_type in pedit loop")
    Signed-off-by: Petr Machata <petrm@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

Signed-off-by: Ivan Vecera <ivecera@redhat.com>
Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

net/sched/cls_api.c
@@ -3554,8 +3554,8 @@ int tc_setup_flow_action(struct flow_action *flow_action,
 				entry->mangle.mask = tcf_pedit_mask(act, k);
 				entry->mangle.val = tcf_pedit_val(act, k);
 				entry->mangle.offset = tcf_pedit_offset(act, k);
-				entry = &flow_action->entries[++j];
 				entry->hw_stats_type = act->hw_stats_type;
+				entry = &flow_action->entries[++j];
 			}
 		} else if (is_tcf_csum(act)) {
 			entry->id = FLOW_ACTION_CSUM;