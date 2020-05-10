d2f44647b15fc59d9c4c8a4dd1aefc91e42cbbb4
Alaa Hleihel <ahleihel@redhat.com>
2020-05-10 14:52:18 -0400

    [netdrv] net/mlx5e: Improve stateless offload capability check
    Message-id: <20200510145245.10054-56-ahleihel@redhat.com>
    Patchwork-id: 306596
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 BZ 1789378 v2 55/82] net/mlx5e: Improve stateless offload capability check
    Bugzilla: 1789378
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1789378
    Upstream: v5.4-rc1
    
    commit e3a53bc536fc279de2ace13b8d6d54b071afb722
    Author: Marina Varshaver <marinav@mellanox.com>
    Date:   Tue Aug 20 03:36:29 2019 +0300
    
        net/mlx5e: Improve stateless offload capability check
    
        Use generic function for checking tunnel stateless offload capability
        instead of separate macros.
    
        Signed-off-by: Marina Varshaver <marinav@mellanox.com>
        Reviewed-by: Aya Levin <ayal@mellanox.com>
        Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -4852,7 +4852,7 @@ static void mlx5e_build_nic_netdev(struct net_device *netdev)
 	netdev->hw_features      |= NETIF_F_HW_VLAN_STAG_TX;
 
 	if (mlx5_vxlan_allowed(mdev->vxlan) || mlx5_geneve_tx_allowed(mdev) ||
-	    MLX5_CAP_ETH(mdev, tunnel_stateless_gre)) {
+	    mlx5e_any_tunnel_proto_supported(mdev)) {
 		netdev->hw_enc_features |= NETIF_F_HW_CSUM;
 		netdev->hw_enc_features |= NETIF_F_TSO;
 		netdev->hw_enc_features |= NETIF_F_TSO6;
@@ -4869,7 +4869,7 @@ static void mlx5e_build_nic_netdev(struct net_device *netdev)
 					 NETIF_F_GSO_UDP_TUNNEL_CSUM;
 	}
 
-	if (MLX5_CAP_ETH(mdev, tunnel_stateless_gre)) {
+	if (mlx5e_tunnel_proto_supported(mdev, IPPROTO_GRE)) {
 		netdev->hw_features     |= NETIF_F_GSO_GRE |
 					   NETIF_F_GSO_GRE_CSUM;
 		netdev->hw_enc_features |= NETIF_F_GSO_GRE |
