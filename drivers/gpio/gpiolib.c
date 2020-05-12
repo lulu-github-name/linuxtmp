f5b273cf8dcf985e52774044d83e7bd412c24177
David Arcari <darcari@redhat.com>
2020-05-12 19:45:46 -0400

    [gpio] gliolib: set hooks in gpiochip_set_irq_hooks()
    Message-id: <20200512194917.141423-23-darcari@redhat.com>
    Patchwork-id: 307058
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 BZ 1829900 022/233] gliolib: set hooks in gpiochip_set_irq_hooks()
    Bugzilla: 1829900
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Brian Masney <bmasney@redhat.com>
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1829900
    
    commit ca620f2de153d690d6d6f50f32b8a14dd46107f4
    Author: Hans Verkuil <hans.verkuil@cisco.com>
    Date:   Sat Sep 8 11:23:15 2018 +0200
    
        gliolib: set hooks in gpiochip_set_irq_hooks()
    
        Centralize setting the irq_request/release_resources callbacks
        in one function since we'll be adding more callbacks to that.
    
        Also fix the removal of the callback overrides: this should
        only be done if we actually installed our own callback there.
    
        Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
        Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -1858,6 +1858,17 @@ static void gpiochip_irq_relres(struct irq_data *d)
 	gpiochip_relres_irq(chip, d->hwirq);
 }
 
+static void gpiochip_set_irq_hooks(struct gpio_chip *gpiochip)
+{
+	struct irq_chip *irqchip = gpiochip->irq.chip;
+
+	if (!irqchip->irq_request_resources &&
+	    !irqchip->irq_release_resources) {
+		irqchip->irq_request_resources = gpiochip_irq_reqres;
+		irqchip->irq_release_resources = gpiochip_irq_relres;
+	}
+}
+
 /**
  * gpiochip_add_irqchip() - adds an IRQ chip to a GPIO chip
  * @gpiochip: the GPIO chip to add the IRQ chip to
@@ -1916,16 +1927,6 @@ static int gpiochip_add_irqchip(struct gpio_chip *gpiochip,
 	if (!gpiochip->irq.domain)
 		return -EINVAL;
 
-	/*
-	 * It is possible for a driver to override this, but only if the
-	 * alternative functions are both implemented.
-	 */
-	if (!irqchip->irq_request_resources &&
-	    !irqchip->irq_release_resources) {
-		irqchip->irq_request_resources = gpiochip_irq_reqres;
-		irqchip->irq_release_resources = gpiochip_irq_relres;
-	}
-
 	if (gpiochip->irq.parent_handler) {
 		void *data = gpiochip->irq.parent_handler_data ?: gpiochip;
 
@@ -1941,6 +1942,8 @@ static int gpiochip_add_irqchip(struct gpio_chip *gpiochip,
 		}
 	}
 
+	gpiochip_set_irq_hooks(gpiochip);
+
 	acpi_gpiochip_request_interrupts(gpiochip);
 
 	return 0;
@@ -1954,11 +1957,12 @@ static int gpiochip_add_irqchip(struct gpio_chip *gpiochip,
  */
 static void gpiochip_irqchip_remove(struct gpio_chip *gpiochip)
 {
+	struct irq_chip *irqchip = gpiochip->irq.chip;
 	unsigned int offset;
 
 	acpi_gpiochip_free_interrupts(gpiochip);
 
-	if (gpiochip->irq.chip && gpiochip->irq.parent_handler) {
+	if (irqchip && gpiochip->irq.parent_handler) {
 		struct gpio_irq_chip *irq = &gpiochip->irq;
 		unsigned int i;
 
@@ -1982,11 +1986,12 @@ static void gpiochip_irqchip_remove(struct gpio_chip *gpiochip)
 		irq_domain_remove(gpiochip->irq.domain);
 	}
 
-	if (gpiochip->irq.chip) {
-		gpiochip->irq.chip->irq_request_resources = NULL;
-		gpiochip->irq.chip->irq_release_resources = NULL;
-		gpiochip->irq.chip = NULL;
+	if (irqchip &&
+	    irqchip->irq_request_resources == gpiochip_irq_reqres) {
+		irqchip->irq_request_resources = NULL;
+		irqchip->irq_release_resources = NULL;
 	}
+	gpiochip->irq.chip = NULL;
 
 	gpiochip_irqchip_free_valid_mask(gpiochip);
 }
@@ -2075,15 +2080,7 @@ int gpiochip_irqchip_add_key(struct gpio_chip *gpiochip,
 		return -EINVAL;
 	}
 
-	/*
-	 * It is possible for a driver to override this, but only if the
-	 * alternative functions are both implemented.
-	 */
-	if (!irqchip->irq_request_resources &&
-	    !irqchip->irq_release_resources) {
-		irqchip->irq_request_resources = gpiochip_irq_reqres;
-		irqchip->irq_release_resources = gpiochip_irq_relres;
-	}
+	gpiochip_set_irq_hooks(gpiochip);
 
 	acpi_gpiochip_request_interrupts(gpiochip);
 
