254916aafd247866c1e7a2d4928c1327d064c75d
Marcelo Leitner <mleitner@redhat.com>
2020-05-15 18:34:00 -0400

    [net] netfilter: nf_tables_offload: move indirect flow_block callback logic to core
    Message-id: <6a714d5524c08b4629ca0c73d148386d1cb1daaf.1589567677.git.mleitner@redhat.com>
    Patchwork-id: 309992
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 net 035/146] netfilter: nf_tables_offload: move indirect flow_block callback logic to core
    Bugzilla: 1811193
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Phil Sutter <psutter@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1811193
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=28605855
    Conflicts: due to
            dbad3408896c ("net: core: rename indirect block ingress cb function")
            being in already. Due to the lack of c1deb065cf3b ("netfilter:
            nf_tables: merge route type into core")
    
    commit 3474a2c62ff9694ee627bdb51cf9a60021d9e814
    Author: Pablo Neira Ayuso <pablo@netfilter.org>
    Date:   Mon Sep 2 23:11:31 2019 +0200
    
        netfilter: nf_tables_offload: move indirect flow_block callback logic to core
    
        Add nft_offload_init() and nft_offload_exit() function to deal with the
        init and the exit path of the offload infrastructure.
    
        Rename nft_indr_block_get_and_ing_cmd() to nft_indr_block_cb().
    
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    Signed-off-by: Marcelo Ricardo Leitner <mleitner@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -7757,11 +7757,6 @@ static struct pernet_operations nf_tables_net_ops = {
 	.exit	= nf_tables_exit_net,
 };
 
-static struct flow_indr_block_entry block_ing_entry = {
-	.cb = nft_indr_block_get_and_ing_cmd,
-	.list = LIST_HEAD_INIT(block_ing_entry.list),
-};
-
 static int __init nf_tables_module_init(void)
 {
 	int err;
@@ -7792,7 +7787,8 @@ static int __init nf_tables_module_init(void)
 	if (err < 0)
 		goto err5;
 
-	flow_indr_add_block_cb(&block_ing_entry);
+	nft_offload_init();
+
 	return err;
 err5:
 	rhltable_destroy(&nft_objname_ht);
@@ -7809,7 +7805,7 @@ err1:
 
 static void __exit nf_tables_module_exit(void)
 {
-	flow_indr_del_block_cb(&block_ing_entry);
+	nft_offload_exit();
 	nfnetlink_subsys_unregister(&nf_tables_subsys);
 	unregister_netdevice_notifier(&nf_tables_flowtable_notifier);
 	nft_chain_filter_fini();
