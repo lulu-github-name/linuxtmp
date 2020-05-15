ed6445dac3b6ee146bf93d096a5be6443973c3bf
Marcelo Leitner <mleitner@redhat.com>
2020-05-15 18:35:01 -0400

    [net] netfilter: nf_tables: unbind callbacks from flowtable destroy path
    Message-id: <94de167f662b0e588c000b1b856c19e63e6911bc.1589567677.git.mleitner@redhat.com>
    Patchwork-id: 310016
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 net 096/146] netfilter: nf_tables: unbind callbacks from flowtable destroy path
    Bugzilla: 1811193
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Phil Sutter <psutter@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1811193
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=28605855
    
    commit 5acab91458ceae0e4b15205fda5437631089f7ee
    Author: Pablo Neira Ayuso <pablo@netfilter.org>
    Date:   Fri Jan 3 14:36:07 2020 +0100
    
        netfilter: nf_tables: unbind callbacks from flowtable destroy path
    
        Callback unbinding needs to be done after nf_flow_table_free(),
        otherwise entries are not removed from the hardware.
    
        Update nft_unregister_flowtable_net_hooks() to call
        nf_unregister_net_hook() instead since the commit/abort paths do not
        deal with the callback unbinding anymore.
    
        Add a comment to nft_flowtable_event() to clarify that
        flow_offload_netdev_event() already removes the entries before the
        callback unbinding.
    
        Fixes: 8bb69f3b2918 ("netfilter: nf_tables: add flowtable offload control plane")
        Fixes ff4bf2f42a40 ("netfilter: nf_tables: add nft_unregister_flowtable_hook()")
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
        Acked-by: wenxu <wenxu@ucloud.cn>
    
    Signed-off-by: Marcelo Ricardo Leitner <mleitner@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -6045,6 +6045,7 @@ nft_flowtable_type_get(struct net *net, u8 family)
 	return ERR_PTR(-ENOENT);
 }
 
+/* Only called from error and netdev event paths. */
 static void nft_unregister_flowtable_hook(struct net *net,
 					  struct nft_flowtable *flowtable,
 					  struct nft_hook *hook)
@@ -6060,7 +6061,7 @@ static void nft_unregister_flowtable_net_hooks(struct net *net,
 	struct nft_hook *hook;
 
 	list_for_each_entry(hook, &flowtable->hook_list, list)
-		nft_unregister_flowtable_hook(net, flowtable, hook);
+		nf_unregister_net_hook(net, &hook->ops);
 }
 
 static int nft_register_flowtable_net_hooks(struct net *net,
@@ -6509,12 +6510,14 @@ static void nf_tables_flowtable_destroy(struct nft_flowtable *flowtable)
 {
 	struct nft_hook *hook, *next;
 
+	flowtable->data.type->free(&flowtable->data);
 	list_for_each_entry_safe(hook, next, &flowtable->hook_list, list) {
+		flowtable->data.type->setup(&flowtable->data, hook->ops.dev,
+					    FLOW_BLOCK_UNBIND);
 		list_del_rcu(&hook->list);
 		kfree(hook);
 	}
 	kfree(flowtable->name);
-	flowtable->data.type->free(&flowtable->data);
 	module_put(flowtable->data.type->owner);
 	kfree(flowtable);
 }
@@ -6558,6 +6561,7 @@ static void nft_flowtable_event(unsigned long event, struct net_device *dev,
 		if (hook->ops.dev != dev)
 			continue;
 
+		/* flow_offload_netdev_event() cleans up entries for us. */
 		nft_unregister_flowtable_hook(dev_net(dev), flowtable, hook);
 		list_del_rcu(&hook->list);
 		kfree_rcu(hook, rcu);
