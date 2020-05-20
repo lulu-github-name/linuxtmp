e78b918e30a1ace9d03e8528dcb7d21dc263207e
Waiman Long <longman@redhat.com>
2020-05-19 20:49:14 -0400

    [mm] mm: vmscan: memcontrol: remove mem_cgroup_select_victim_node()
    Message-id: <20200519204939.15390-43-longman@redhat.com>
    Patchwork-id: 312768
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 42/67] mm: vmscan: memcontrol: remove mem_cgroup_select_victim_node()
    Bugzilla: 1812073
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1812073
    Conflicts: There is a merge conflict in mm/vmscan.c due to missing the
    	   upstream commit 1732d2b0117c ("mm/vmscan.c:
    	   add checks for incorrect handling of
    	   current->reclaim_state"). RH_KABI_DEPRECATE() is
    	   used for deprecated fields in struct mem_cgroup and
    	   MEM_CGROUP_TARGET_NUMAINFO is retained to keep kABI,
    
    commit fa40d1ee9f156624658ca409a04a78882ca5b3c5
    Author: Shakeel Butt <shakeelb@google.com>
    Date:   Sat, 30 Nov 2019 17:50:16 -0800
    
        mm: vmscan: memcontrol: remove mem_cgroup_select_victim_node()
    
        Since commit 1ba6fc9af35b ("mm: vmscan: do not share cgroup iteration
        between reclaimers"), the memcg reclaim does not bail out earlier based
        on sc->nr_reclaimed and will traverse all the nodes.  All the
        reclaimable pages of the memcg on all the nodes will be scanned relative
        to the reclaim priority.  So, there is no need to maintain state
        regarding which node to start the memcg reclaim from.
    
        This patch effectively reverts the commit 889976dbcb12 ("memcg: reclaim
        memory from nodes in round-robin order") and commit 453a9bf347f1
        ("memcg: fix numa scan information update to be triggered by memory
        event").
    
        [shakeelb@google.com: v2]
          Link: http://lkml.kernel.org/r/20191030204232.139424-1-shakeelb@google.com
        Link: http://lkml.kernel.org/r/20191029234753.224143-1-shakeelb@google.com
        Signed-off-by: Shakeel Butt <shakeelb@google.com>
        Acked-by: Roman Gushchin <guro@fb.com>
        Acked-by: Michal Hocko <mhocko@suse.com>
        Acked-by: Johannes Weiner <hannes@cmpxchg.org>
        Cc: Greg Thelen <gthelen@google.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -90,7 +90,7 @@ struct mem_cgroup_id {
 enum mem_cgroup_events_target {
 	MEM_CGROUP_TARGET_THRESH,
 	MEM_CGROUP_TARGET_SOFTLIMIT,
-	MEM_CGROUP_TARGET_NUMAINFO,
+	__MEM_CGROUP_TARGET_NUMAINFO,	/* Deprecated */
 	MEM_CGROUP_NTARGETS,
 };
 
@@ -308,11 +308,11 @@ struct mem_cgroup {
 	struct list_head kmem_caches;
 #endif
 
-	int last_scanned_node;
+	RH_KABI_DEPRECATE(int, last_scanned_node)
 #if MAX_NUMNODES > 1
-	nodemask_t	scan_nodes;
-	atomic_t	numainfo_events;
-	atomic_t	numainfo_updating;
+	RH_KABI_DEPRECATE(nodemask_t, scan_nodes)
+	RH_KABI_DEPRECATE(atomic_t, numainfo_events)
+	RH_KABI_DEPRECATE(atomic_t, numainfo_updating)
 #endif
 
 #ifdef CONFIG_CGROUP_WRITEBACK
