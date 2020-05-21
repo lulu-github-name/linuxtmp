3f061479f6ca8f44a75dec12eb3658052711ee62
Torez Smith <torez@redhat.com>
2020-05-21 18:08:44 -0400

    [dma] dma-mapping: remove dma_release_declared_memory
    Message-id: <20200521180844.15700-360-torez@redhat.com>
    Patchwork-id: 313341
    Patchwork-instance: patchwork
    O-Subject: [RHEL8 PATCH 1825410, 1821835, 1825141, 1788005 359/359] dma-mapping: remove dma_release_declared_memory
    Bugzilla: 1825410 1821835
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/1825410
    Bugzilla: https://bugzilla.redhat.com/1821835
    Bugzilla: https://bugzilla.redhat.com/1825141
    Bugzilla: https://bugzilla.redhat.com/1788005
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=28738146
    Testing: on Dell Whitehaven Precision 7730, HP ZBook 15 G3/80D5 and
    	ThinkCentre M910z All-in-One
    
    Conflicts: line fuzz due to non applied patches.
    
    commit 1fa0682448acd5198f79c1d28ee1292a27ae406d
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Fri Aug 30 08:46:57 2019 +0200
    
        dma-mapping: remove dma_release_declared_memory
    
        This function is entirely unused given that declared memory is
        generally provided by platform setup code.
    
        Signed-off-by: Christoph Hellwig <hch@lst.de>
    
    Signed-off-by: Torez Smith <torez@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -762,7 +762,6 @@ static inline int dma_get_cache_alignment(void)
 #ifdef CONFIG_HAVE_GENERIC_DMA_COHERENT
 int dma_declare_coherent_memory(struct device *dev, phys_addr_t phys_addr,
 				dma_addr_t device_addr, size_t size);
-void dma_release_declared_memory(struct device *dev);
 void *dma_mark_declared_memory_occupied(struct device *dev,
 					dma_addr_t device_addr, size_t size);
 #else
@@ -771,11 +770,6 @@ dma_declare_coherent_memory(struct device *dev, phys_addr_t phys_addr,
 			    dma_addr_t device_addr, size_t size)
 {
 	return -ENOSYS;
-}
-
-static inline void
-dma_release_declared_memory(struct device *dev)
-{
 }
 
 static inline void *
