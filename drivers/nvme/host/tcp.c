974285c9c2380cca192eedc654587814ef8db7b7
Sabrina Dubroca <sdubroca@redhat.com>
2020-05-22 15:35:27 -0400

    [net] use skb_queue_empty_lockless() in busy poll contexts
    Message-id: <80f0e57602a2d7fe86d81f89a81636d06ddb59c6.1590149283.git.sdubroca@redhat.com>
    Patchwork-id: 313746
    Patchwork-instance: patchwork
    O-Subject: [PATCH RHEL8.3 net 04/19] net: use skb_queue_empty_lockless() in busy poll contexts
    Bugzilla: 1446392
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1446392
    Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=28753681
    Tested: basic encap tests with udp and tcp over ipv4 and ipv6
    
    Conflicts: different context in drivers/nvme/host/tcp.c, f86e5bf817a5
      already backported
    
    commit 3f926af3f4d688e2e11e7f8ed04e277a14d4d4a4
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Wed Oct 23 22:44:51 2019 -0700
    
        net: use skb_queue_empty_lockless() in busy poll contexts
    
        Busy polling usually runs without locks.
        Let's use skb_queue_empty_lockless() instead of skb_queue_empty()
    
        Also uses READ_ONCE() in __skb_try_recv_datagram() to address
        a similar potential problem.
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Sabrina Dubroca <sdubroca@redhat.com>
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/nvme/host/tcp.c
+++ drivers/nvme/host/tcp.c
@@ -2302,7 +2302,7 @@ static int nvme_tcp_poll(struct blk_mq_hw_ctx *hctx)
 	if (!test_bit(NVME_TCP_Q_LIVE, &queue->flags))
 		return 0;
 
-	if (sk_can_busy_loop(sk) && skb_queue_empty(&sk->sk_receive_queue))
+	if (sk_can_busy_loop(sk) && skb_queue_empty_lockless(&sk->sk_receive_queue))
 		sk_busy_loop(sk, true);
 	nvme_tcp_try_recv(queue);
 	return queue->nr_cqe;
