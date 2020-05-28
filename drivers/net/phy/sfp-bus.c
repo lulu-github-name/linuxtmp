4d6543d67c2feb85b8c6cd8589810d781a474bbc
Petr Oros <poros@redhat.com>
2020-05-28 14:28:55 -0400

    [netdrv] net: sfp: remove sfp-bus use of netdevs
    Message-id: <dea8cd4b366680f1ff7a8db8de561f79d57190d3.1590674882.git.poros@redhat.com>
    Patchwork-id: 314811
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 025/298] net: sfp: remove sfp-bus use of netdevs
    Bugzilla: 1816982
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1816982
    
    Upstream commit(s):
    commit 54f70b3ba364f19291dc8b9cb096b02a00fb4461
    Author: Russell King <rmk+kernel@armlinux.org.uk>
    Date:   Tue May 28 10:57:39 2019 +0100
    
        net: sfp: remove sfp-bus use of netdevs
    
        The sfp-bus code now no longer has any use for the network device
        structure, so remove its use.
    
        Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
        Reviewed-by: Andrew Lunn <andrew@lunn.ch>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Petr Oros <poros@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/net/phy/sfp-bus.c
+++ drivers/net/phy/sfp-bus.c
@@ -23,7 +23,6 @@ struct sfp_bus {
 
 	const struct sfp_upstream_ops *upstream_ops;
 	void *upstream;
-	struct net_device *netdev;
 	struct phy_device *phydev;
 
 	bool registered;
@@ -454,13 +453,11 @@ static void sfp_upstream_clear(struct sfp_bus *bus)
 {
 	bus->upstream_ops = NULL;
 	bus->upstream = NULL;
-	bus->netdev = NULL;
 }
 
 /**
  * sfp_register_upstream() - Register the neighbouring device
  * @fwnode: firmware node for the SFP bus
- * @ndev: network device associated with the interface
  * @upstream: the upstream private data
  * @ops: the upstream's &struct sfp_upstream_ops
  *
@@ -471,7 +468,7 @@ static void sfp_upstream_clear(struct sfp_bus *bus)
  * On error, returns %NULL.
  */
 struct sfp_bus *sfp_register_upstream(struct fwnode_handle *fwnode,
-				      struct net_device *ndev, void *upstream,
+				      void *upstream,
 				      const struct sfp_upstream_ops *ops)
 {
 	struct sfp_bus *bus = sfp_bus_get(fwnode);
@@ -481,7 +478,6 @@ struct sfp_bus *sfp_register_upstream(struct fwnode_handle *fwnode,
 		rtnl_lock();
 		bus->upstream_ops = ops;
 		bus->upstream = upstream;
-		bus->netdev = ndev;
 
 		if (bus->sfp) {
 			ret = sfp_register_bus(bus);
@@ -603,7 +599,7 @@ struct sfp_bus *sfp_register_socket(struct device *dev, struct sfp *sfp,
 		bus->sfp = sfp;
 		bus->socket_ops = ops;
 
-		if (bus->netdev) {
+		if (bus->upstream_ops) {
 			ret = sfp_register_bus(bus);
 			if (ret)
 				sfp_socket_clear(bus);
@@ -623,7 +619,7 @@ EXPORT_SYMBOL_GPL(sfp_register_socket);
 void sfp_unregister_socket(struct sfp_bus *bus)
 {
 	rtnl_lock();
-	if (bus->netdev)
+	if (bus->upstream_ops)
 		sfp_unregister_bus(bus);
 	sfp_socket_clear(bus);
 	rtnl_unlock();
