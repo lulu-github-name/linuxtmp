3b91c94588f46a001ea418530d0533709d812da6
Petr Oros <poros@redhat.com>
2020-05-28 14:30:02 -0400

    [netdrv] net: phy: force phy suspend when calling phy_stop
    Message-id: <b37ddd6dd9780b5ea8243515a792a7a522b87e9d.1590674883.git.poros@redhat.com>
    Patchwork-id: 314845
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 092/298] net: phy: force phy suspend when calling phy_stop
    Bugzilla: 1816982
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1816982
    
    Upstream commit(s):
    commit 95fb8bb3181bbe1ee87c95e91dff94f74f148c33
    Author: Jian Shen <shenjian15@huawei.com>
    Date:   Wed Aug 28 09:34:47 2019 +0800
    
        net: phy: force phy suspend when calling phy_stop
    
        Some ethernet drivers may call phy_start() and phy_stop() from
        ndo_open() and ndo_close() respectively.
    
        When network cable is unconnected, and operate like below:
        step 1: ifconfig ethX up -> ndo_open -> phy_start ->start
        autoneg, and phy is no link.
        step 2: ifconfig ethX down -> ndo_close -> phy_stop -> just stop
        phy state machine.
    
        This patch forces phy suspend even phydev->link is off.
    
        Signed-off-by: Jian Shen <shenjian15@huawei.com>
        Reviewed-by: Heiner Kallweit <hkallweit1@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Petr Oros <poros@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -916,8 +916,8 @@ void phy_state_machine(struct work_struct *work)
 		if (phydev->link) {
 			phydev->link = 0;
 			phy_link_down(phydev, true);
-			do_suspend = true;
 		}
+		do_suspend = true;
 		break;
 	}
 
