57f1502777cd24694c7ba009b43b2f82be9ab98c
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2020-05-29 06:21:10 -0400

    [net] atomics/treewide: Rename __atomic_add_unless() => atomic_fetch_add_unless()
    Message-id: <20200529062245.152030-140-yauheni.kaliuta@redhat.com>
    Patchwork-id: 315477
    Patchwork-instance: patchwork
    O-Subject: [PATCH RHEL8.3 140/235] atomics/treewide: Rename __atomic_add_unless() => atomic_fetch_add_unless()
    Bugzilla: 1813370
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1813370
    
    Conflicts: drivers/infiniband/core/rdma_core.c due to applied
      9867f5c6695f ("IB/uverbs: Convert 'bool exclusive' into an enum");
    
      drivers/firmware/efi/efi.c due to applied with modification
      80424b02d42b ("efi: Reduce the amount of memblock reservations for
      persistent allocations");
    
      drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c modified by
      driver update in RHEL 58bf5da818ae ("[wireless] Backport iwlwifi
      drivers from linux-5.2-rc5")
      drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c
    
    commit bfc18e389c7a09fbbbed6bf4032396685b14246e
    Author: Mark Rutland <mark.rutland@arm.com>
    Date:   Thu Jun 21 13:13:04 2018 +0100
    
        atomics/treewide: Rename __atomic_add_unless() => atomic_fetch_add_unless()
    
        While __atomic_add_unless() was originally intended as a building-block
        for atomic_add_unless(), it's now used in a number of places around the
        kernel. It's the only common atomic operation named __atomic*(), rather
        than atomic_*(), and for consistency it would be better named
        atomic_fetch_add_unless().
    
        This lack of consistency is slightly confusing, and gets in the way of
        scripting atomics. Given that, let's clean things up and promote it to
        an official part of the atomics API, in the form of
        atomic_fetch_add_unless().
    
        This patch converts definitions and invocations over to the new name,
        including the instrumented version, using the following script:
    
          ----
          git grep -w __atomic_add_unless | while read line; do
          sed -i '{s/\<__atomic_add_unless\>/atomic_fetch_add_unless/}' "${line%%:*}";
          done
          git grep -w __arch_atomic_add_unless | while read line; do
          sed -i '{s/\<__arch_atomic_add_unless\>/arch_atomic_fetch_add_unless/}' "${line%%:*}";
          done
          ----
    
        Note that we do not have atomic{64,_long}_fetch_add_unless(), which will
        be introduced by later patches.
    
        There should be no functional change as a result of this patch.
    
        Signed-off-by: Mark Rutland <mark.rutland@arm.com>
        Reviewed-by: Will Deacon <will.deacon@arm.com>
        Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
        Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Acked-by: Palmer Dabbelt <palmer@sifive.com>
        Cc: Boqun Feng <boqun.feng@gmail.com>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Link: https://lore.kernel.org/lkml/20180621121321.4761-2-mark.rutland@arm.com
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/firmware/efi/efi.c
+++ drivers/firmware/efi/efi.c
@@ -1040,7 +1040,7 @@ int __ref efi_mem_reserve_persistent(phys_addr_t addr, u64 size)
 	/* first try to find a slot in an existing linked list entry */
 	for (prsv = efi_memreserve_root->next; prsv; prsv = rsv->next) {
 		rsv = memremap(prsv, sizeof(*rsv), MEMREMAP_WB);
-		index = __atomic_add_unless(&rsv->count, 1, rsv->size);
+		index = atomic_fetch_add_unless(&rsv->count, 1, rsv->size);
 		if (index < rsv->size) {
 			rsv->entry[index].base = addr;
 			rsv->entry[index].size = size;
