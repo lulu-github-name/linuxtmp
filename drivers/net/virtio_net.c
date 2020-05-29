a3e6ef44baf2b90ada0d1e107b6f365adc294cd7
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2020-05-29 06:21:28 -0400

    [netdrv] bpf: Convert bpf_prog refcnt to atomic64_t
    Message-id: <20200529062245.152030-158-yauheni.kaliuta@redhat.com>
    Patchwork-id: 315441
    Patchwork-instance: patchwork
    O-Subject: [PATCH RHEL8.3 158/235] bpf: Convert bpf_prog refcnt to atomic64_t
    Bugzilla: 1813370
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1813370
    
    Conflicts: include/linux/bpf.h kABI extention;
    
      drivers/net/ethernet/freescale/dpaa2/dpaa2-eth.c is missing in
      RHEL;
    
      drivers/net/virtio_net.c fuzz due to missing
      03aa6d34868c ("virtio_net: Don't process redirected XDP frames
      when XDP is disabled")
    
    commit 85192dbf4de08795afe2b88e52a36fc6abfc3dba
    Author: Andrii Nakryiko <andriin@fb.com>
    Date:   Sun Nov 17 09:28:03 2019 -0800
    
        bpf: Convert bpf_prog refcnt to atomic64_t
    
        Similarly to bpf_map's refcnt/usercnt, convert bpf_prog's refcnt to atomic64
        and remove artificial 32k limit. This allows to make bpf_prog's refcounting
        non-failing, simplifying logic of users of bpf_prog_add/bpf_prog_inc.
    
        Validated compilation by running allyesconfig kernel build.
    
        Suggested-by: Daniel Borkmann <daniel@iogearbox.net>
        Signed-off-by: Andrii Nakryiko <andriin@fb.com>
        Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
        Link: https://lore.kernel.org/bpf/20191117172806.2195367-3-andriin@fb.com
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -2287,11 +2287,8 @@ static int virtnet_xdp_set(struct net_device *dev, struct bpf_prog *prog,
 		return -ENOMEM;
 	}
 
-	if (prog) {
-		prog = bpf_prog_add(prog, vi->max_queue_pairs - 1);
-		if (IS_ERR(prog))
-			return PTR_ERR(prog);
-	}
+	if (prog)
+		bpf_prog_add(prog, vi->max_queue_pairs - 1);
 
 	/* Make sure NAPI is not using any XDP TX queues for RX. */
 	if (netif_running(dev))
