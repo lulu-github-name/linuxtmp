8e847f4194eb7f50ac99ee14a327b5df9bd2ea25
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2020-05-29 06:22:22 -0400

    [kernel] ftrace: Add ftrace_find_direct_func()
    Message-id: <20200529062245.152030-212-yauheni.kaliuta@redhat.com>
    Patchwork-id: 315435
    Patchwork-instance: patchwork
    O-Subject: [PATCH RHEL8.3 212/235] ftrace: Add ftrace_find_direct_func()
    Bugzilla: 1813370
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1813370
    
    commit 013bf0da0474816f57739daa006c8564ad7396a3
    Author: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Date:   Fri Nov 8 13:11:27 2019 -0500
    
        ftrace: Add ftrace_find_direct_func()
    
        As function_graph tracer modifies the return address to insert a trampoline
        to trace the return of a function, it must be aware of a direct caller, as
        when it gets called, the function's return address may not be at on the
        stack where it expects. It may have to see if that return address points to
        the a direct caller and adjust if it is.
    
        Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -51,6 +51,7 @@ static inline void early_trace_init(void) { }
 
 struct module;
 struct ftrace_hash;
+struct ftrace_direct_func;
 
 #if defined(CONFIG_FUNCTION_TRACER) && defined(CONFIG_MODULES) && \
 	defined(CONFIG_DYNAMIC_FTRACE)
@@ -252,6 +253,7 @@ static inline void ftrace_free_mem(struct module *mod, void *start, void *end) {
 #ifdef CONFIG_DYNAMIC_FTRACE_WITH_DIRECT_CALLS
 int register_ftrace_direct(unsigned long ip, unsigned long addr);
 int unregister_ftrace_direct(unsigned long ip, unsigned long addr);
+struct ftrace_direct_func *ftrace_find_direct_func(unsigned long addr);
 #else
 static inline int register_ftrace_direct(unsigned long ip, unsigned long addr)
 {
@@ -260,6 +262,10 @@ static inline int register_ftrace_direct(unsigned long ip, unsigned long addr)
 static inline int unregister_ftrace_direct(unsigned long ip, unsigned long addr)
 {
 	return -ENODEV;
+}
+static inline struct ftrace_direct_func *ftrace_find_direct_func(unsigned long addr)
+{
+	return NULL;
 }
 #endif /* CONFIG_DYNAMIC_FTRACE_WITH_DIRECT_CALLS */
 
