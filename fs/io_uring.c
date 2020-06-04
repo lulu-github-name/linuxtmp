ebc92c7a7fd96f904a2af4cb8a5b7451461ac446
Jeff Moyer <jmoyer@redhat.com>
2020-06-04 17:36:43 -0400

    [fs] io_uring: clear req->result always before issuing a read/write request
    Message-id: <20200604174034.2713-211-jmoyer@redhat.com>
    Patchwork-id: 316575
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 210/441] io_uring: clear req->result always before issuing a read/write request
    Bugzilla: 1784478
    RH-Acked-by: Ming Lei <ming.lei@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Bijan Mottahedeh <bijan.mottahedeh@oracle.com>
    
    BZ: 1784478
    Brew: 28979848
    
    commit 797f3f535d59f05ad12c629338beef6cb801d19e
    Author: Bijan Mottahedeh <bijan.mottahedeh@oracle.com>
    Date:   Wed Jan 15 18:37:45 2020 -0800
    
        io_uring: clear req->result always before issuing a read/write request
    
        req->result is cleared when io_issue_sqe() calls io_read/write_pre()
        routines.  Those routines however are not called when the sqe
        argument is NULL, which is the case when io_issue_sqe() is called from
        io_wq_submit_work().  io_issue_sqe() may then examine a stale result if
        a polled request had previously failed with -EAGAIN:
    
                if (ctx->flags & IORING_SETUP_IOPOLL) {
                        if (req->result == -EAGAIN)
                                return -EAGAIN;
    
                        io_iopoll_req_issued(req);
                }
    
        and in turn cause a subsequently completed request to be re-issued in
        io_wq_submit_work().
    
        Signed-off-by: Bijan Mottahedeh <bijan.mottahedeh@oracle.com>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -1844,6 +1844,7 @@ static int io_read(struct io_kiocb *req, struct io_kiocb **nxt,
 	if (!force_nonblock)
 		req->rw.kiocb.ki_flags &= ~IOCB_NOWAIT;
 
+	req->result = 0;
 	io_size = ret;
 	if (req->flags & REQ_F_LINK)
 		req->result = io_size;
@@ -1931,6 +1932,7 @@ static int io_write(struct io_kiocb *req, struct io_kiocb **nxt,
 	if (!force_nonblock)
 		req->rw.kiocb.ki_flags &= ~IOCB_NOWAIT;
 
+	req->result = 0;
 	io_size = ret;
 	if (req->flags & REQ_F_LINK)
 		req->result = io_size;
