5130e329605748c5a5f7dda827f92d6dc38cc8cc
Jeff Moyer <jmoyer@redhat.com>
2020-06-04 17:37:34 -0400

    [fs] io_uring: honor IOSQE_ASYNC for linked reqs
    Message-id: <20200604174034.2713-262-jmoyer@redhat.com>
    Patchwork-id: 316597
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 261/441] io_uring: honor IOSQE_ASYNC for linked reqs
    Bugzilla: 1784478
    RH-Acked-by: Ming Lei <ming.lei@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Pavel Begunkov <asml.silence@gmail.com>
    
    BZ: 1784478
    Brew: 28979848
    
    commit 86a761f81ec87a96572214f5db606f60d36aaf08
    Author: Pavel Begunkov <asml.silence@gmail.com>
    Date:   Wed Jan 22 23:09:36 2020 +0300
    
        io_uring: honor IOSQE_ASYNC for linked reqs
    
        REQ_F_FORCE_ASYNC is checked only for the head of a link. Fix it.
    
        Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -4458,6 +4458,7 @@ again:
 	 */
 	if (ret == -EAGAIN && (!(req->flags & REQ_F_NOWAIT) ||
 	    (req->flags & REQ_F_MUST_PUNT))) {
+punt:
 		if (req->work.flags & IO_WQ_WORK_NEEDS_FILES) {
 			ret = io_grab_files(req);
 			if (ret)
@@ -4493,6 +4494,9 @@ done_req:
 	if (nxt) {
 		req = nxt;
 		nxt = NULL;
+
+		if (req->flags & REQ_F_FORCE_ASYNC)
+			goto punt;
 		goto again;
 	}
 }
