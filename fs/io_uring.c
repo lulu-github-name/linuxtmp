4392ab4839c9f7723b602c19ff604d4862510899
Jeff Moyer <jmoyer@redhat.com>
2020-06-04 17:38:06 -0400

    [fs] io_uring: statx/openat/openat2 don't support fixed files
    Message-id: <20200604174034.2713-294-jmoyer@redhat.com>
    Patchwork-id: 317144
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 293/441] io_uring: statx/openat/openat2 don't support fixed files
    Bugzilla: 1784478
    RH-Acked-by: Ming Lei <ming.lei@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Jens Axboe <axboe@kernel.dk>
    
    BZ: 1784478
    Brew: 28979848
    Conflicts: We don't have sys_openat2 support, so drop that hunk.  Minor
    context difference in io_openat due to missing struct open_how in RHEL.
    
    commit cf3040ca55f2085b0a372a620ee2cb93ae19b686
    Author: Jens Axboe <axboe@kernel.dk>
    Date:   Thu Feb 6 21:31:40 2020 -0700
    
        io_uring: statx/openat/openat2 don't support fixed files
    
        All of these opcodes take a directory file descriptor. We can't easily
        support fixed files for these operations, and the use case for that
        probably isn't all that clear (or sensible) anyway.
    
        Disable IOSQE_FIXED_FILE for these operations.
    
        Reported-by: Stefan Metzmacher <metze@samba.org>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2516,6 +2516,8 @@ static int io_openat_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe)
 
 	if (sqe->ioprio || sqe->buf_index)
 		return -EINVAL;
+	if (sqe->flags & IOSQE_FIXED_FILE)
+		return -EBADF;
 
 	req->open.dfd = READ_ONCE(sqe->fd);
 	req->open.mode = READ_ONCE(sqe->len);
@@ -2692,6 +2694,8 @@ static int io_statx_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe)
 
 	if (sqe->ioprio || sqe->buf_index)
 		return -EINVAL;
+	if (sqe->flags & IOSQE_FIXED_FILE)
+		return -EBADF;
 
 	req->open.dfd = READ_ONCE(sqe->fd);
 	req->open.mask = READ_ONCE(sqe->len);
@@ -2765,7 +2769,7 @@ static int io_close_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe)
 	    sqe->rw_flags || sqe->buf_index)
 		return -EINVAL;
 	if (sqe->flags & IOSQE_FIXED_FILE)
-		return -EINVAL;
+		return -EBADF;
 
 	req->close.fd = READ_ONCE(sqe->fd);
 	if (req->file->f_op == &io_uring_fops ||
