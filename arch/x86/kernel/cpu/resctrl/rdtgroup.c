d10ef1c1b8cbece98fd3d0b666f1988deb5a18be
Alexander Beregalov <aberegal@redhat.com>
2020-06-05 15:43:34 -0400

    [x86] x86/resctrl: Clean up unused function parameter in mkdir path
    Message-id: <20200605154335.22641-20-aberegal@redhat.com>
    Patchwork-id: 317593
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 BZ 1830948 19/20] x86/resctrl: Clean up unused function parameter in mkdir path
    Bugzilla: 1830948
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1830948
    
    commit 32ada3b9e04c6f6d4916967bd8bbe2450ad5bc5e
    Author: Xiaochen Shen <xiaochen.shen@intel.com>
    Date:   Thu Jan 9 00:28:06 2020 +0800
    
        x86/resctrl: Clean up unused function parameter in mkdir path
    
        Commit
    
          334b0f4e9b1b ("x86/resctrl: Fix a deadlock due to inaccurate reference")
    
        changed the argument to rdtgroup_kn_lock_live()/rdtgroup_kn_unlock()
        within mkdir_rdt_prepare(). That change resulted in an unused function
        parameter to mkdir_rdt_prepare().
    
        Clean up the unused function parameter in mkdir_rdt_prepare() and its
        callers rdtgroup_mkdir_mon() and rdtgroup_mkdir_ctrl_mon().
    
        Signed-off-by: Xiaochen Shen <xiaochen.shen@intel.com>
        Signed-off-by: Borislav Petkov <bp@suse.de>
        Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
        Reviewed-by: Tony Luck <tony.luck@intel.com>
        Acked-by: Thomas Gleixner <tglx@linutronix.de>
        Link: https://lkml.kernel.org/r/1578500886-21771-5-git-send-email-xiaochen.shen@intel.com
    
    Cc: David Arcari <darcari@redhat.com>
    Cc: Prarit Bhargava <prarit@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- arch/x86/kernel/cpu/resctrl/rdtgroup.c
+++ arch/x86/kernel/cpu/resctrl/rdtgroup.c
@@ -2696,7 +2696,6 @@ static int rdtgroup_init_alloc(struct rdtgroup *rdtgrp)
 }
 
 static int mkdir_rdt_prepare(struct kernfs_node *parent_kn,
-			     struct kernfs_node *prgrp_kn,
 			     const char *name, umode_t mode,
 			     enum rdt_group_type rtype, struct rdtgroup **r)
 {
@@ -2806,15 +2805,12 @@ static void mkdir_rdt_prepare_clean(struct rdtgroup *rgrp)
  * to monitor a subset of tasks and cpus in its parent ctrl_mon group.
  */
 static int rdtgroup_mkdir_mon(struct kernfs_node *parent_kn,
-			      struct kernfs_node *prgrp_kn,
-			      const char *name,
-			      umode_t mode)
+			      const char *name, umode_t mode)
 {
 	struct rdtgroup *rdtgrp, *prgrp;
 	int ret;
 
-	ret = mkdir_rdt_prepare(parent_kn, prgrp_kn, name, mode, RDTMON_GROUP,
-				&rdtgrp);
+	ret = mkdir_rdt_prepare(parent_kn, name, mode, RDTMON_GROUP, &rdtgrp);
 	if (ret)
 		return ret;
 
@@ -2836,7 +2832,6 @@ static int rdtgroup_mkdir_mon(struct kernfs_node *parent_kn,
  * to allocate and monitor resources.
  */
 static int rdtgroup_mkdir_ctrl_mon(struct kernfs_node *parent_kn,
-				   struct kernfs_node *prgrp_kn,
 				   const char *name, umode_t mode)
 {
 	struct rdtgroup *rdtgrp;
@@ -2844,8 +2839,7 @@ static int rdtgroup_mkdir_ctrl_mon(struct kernfs_node *parent_kn,
 	u32 closid;
 	int ret;
 
-	ret = mkdir_rdt_prepare(parent_kn, prgrp_kn, name, mode, RDTCTRL_GROUP,
-				&rdtgrp);
+	ret = mkdir_rdt_prepare(parent_kn, name, mode, RDTCTRL_GROUP, &rdtgrp);
 	if (ret)
 		return ret;
 
@@ -2919,14 +2913,14 @@ static int rdtgroup_mkdir(struct kernfs_node *parent_kn, const char *name,
 	 * subdirectory
 	 */
 	if (rdt_alloc_capable && parent_kn == rdtgroup_default.kn)
-		return rdtgroup_mkdir_ctrl_mon(parent_kn, parent_kn, name, mode);
+		return rdtgroup_mkdir_ctrl_mon(parent_kn, name, mode);
 
 	/*
 	 * If RDT monitoring is supported and the parent directory is a valid
 	 * "mon_groups" directory, add a monitoring subdirectory.
 	 */
 	if (rdt_mon_capable && is_mon_groups(parent_kn, name))
-		return rdtgroup_mkdir_mon(parent_kn, parent_kn->parent, name, mode);
+		return rdtgroup_mkdir_mon(parent_kn, name, mode);
 
 	return -EPERM;
 }
