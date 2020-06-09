6ba1f4b56f2368e35d7499f4bbdb2cf9fd3e9ecb
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2020-06-08 22:27:35 -0400

    [net] bpf: Add batch ops to all htab bpf map
    Message-id: <20200608222925.282045-147-yauheni.kaliuta@redhat.com>
    Patchwork-id: 318109
    Patchwork-instance: patchwork
    O-Subject: [PATCH RHEL8.3 147/257] bpf: Add batch ops to all htab bpf map
    Bugzilla: 1813372
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1813372
    
    Conflicts: due to kABI extention
    
    commit 057996380a42bb64ccc04383cfa9c0ace4ea11f0
    Author: Yonghong Song <yhs@fb.com>
    Date:   Wed Jan 15 10:43:04 2020 -0800
    
        bpf: Add batch ops to all htab bpf map
    
        htab can't use generic batch support due some problematic behaviours
        inherent to the data structre, i.e. while iterating the bpf map  a
        concurrent program might delete the next entry that batch was about to
        use, in that case there's no easy solution to retrieve the next entry,
        the issue has been discussed multiple times (see [1] and [2]).
    
        The only way hmap can be traversed without the problem previously
        exposed is by making sure that the map is traversing entire buckets.
        This commit implements those strict requirements for hmap, the
        implementation follows the same interaction that generic support with
        some exceptions:
    
         - If keys/values buffer are not big enough to traverse a bucket,
           ENOSPC will be returned.
         - out_batch contains the value of the next bucket in the iteration, not
           the next key, but this is transparent for the user since the user
           should never use out_batch for other than bpf batch syscalls.
    
        This commits implements BPF_MAP_LOOKUP_BATCH and adds support for new
        command BPF_MAP_LOOKUP_AND_DELETE_BATCH. Note that for update/delete
        batch ops it is possible to use the generic implementations.
    
        [1] https://lore.kernel.org/bpf/20190724165803.87470-1-brianvv@google.com/
        [2] https://lore.kernel.org/bpf/20190906225434.3635421-1-yhs@fb.com/
    
        Signed-off-by: Yonghong Song <yhs@fb.com>
        Signed-off-by: Brian Vazquez <brianvv@google.com>
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
        Link: https://lore.kernel.org/bpf/20200115184308.162644-6-brianvv@google.com
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -48,6 +48,7 @@ struct bpf_map_ops {
 	void (*map_release_uref)(struct bpf_map *map);
 	RH_KABI_BROKEN_INSERT(void *(*map_lookup_elem_sys_only)(struct bpf_map *map, void *key))
 	RH_KABI_BROKEN_INSERT(int (*map_lookup_batch)(struct bpf_map *map, const union bpf_attr *attr, union bpf_attr __user *uattr))
+	RH_KABI_BROKEN_INSERT(int (*map_lookup_and_delete_batch)(struct bpf_map *map, const union bpf_attr *attr, union bpf_attr __user *uattr))
 	RH_KABI_BROKEN_INSERT(int (*map_update_batch)(struct bpf_map *map, const union bpf_attr *attr, union bpf_attr __user *uattr))
 	RH_KABI_BROKEN_INSERT(int (*map_delete_batch)(struct bpf_map *map, const union bpf_attr *attr, union bpf_attr __user *uattr))
 
