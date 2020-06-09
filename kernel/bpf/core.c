740bc866bf0acbf9038ce71c9e2c7bf4ebcfbb3d
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2020-06-08 22:25:31 -0400

    [arm64] bpf, x86, arm64: Enable jit by default when not built as always-on
    Message-id: <20200608222925.282045-23-yauheni.kaliuta@redhat.com>
    Patchwork-id: 317986
    Patchwork-instance: patchwork
    O-Subject: [PATCH RHEL8.3 023/257] bpf, x86, arm64: Enable jit by default when not built as always-on
    Bugzilla: 1813372
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1813372
    
    Conflicts: kernel/bpf/core.c due to RH-only  fd8b4d2a025e ("[kernel]
      bpf: set default values for bpf_jit_harden and bpf_jit_kallsyms");
    
      arch/arm64/Kconfig due to missing 84c187afa210 ("arm64: don't
      trash config with compat symbol if COMPAT is disabled") and
      67f3977f805b ("arm64, mm: move generic mmap layout functions to
      mm")
    
    commit 81c22041d9f19df07b9cba95e3cd02e0f41bc1e1
    Author: Daniel Borkmann <daniel@iogearbox.net>
    Date:   Mon Dec 9 16:08:03 2019 +0100
    
        bpf, x86, arm64: Enable jit by default when not built as always-on
    
        After Spectre 2 fix via 290af86629b2 ("bpf: introduce BPF_JIT_ALWAYS_ON
        config") most major distros use BPF_JIT_ALWAYS_ON configuration these days
        which compiles out the BPF interpreter entirely and always enables the
        JIT. Also given recent fix in e1608f3fa857 ("bpf: Avoid setting bpf insns
        pages read-only when prog is jited"), we additionally avoid fragmenting
        the direct map for the BPF insns pages sitting in the general data heap
        since they are not used during execution. Latter is only needed when run
        through the interpreter.
    
        Since both x86 and arm64 JITs have seen a lot of exposure over the years,
        are generally most up to date and maintained, there is more downside in
        !BPF_JIT_ALWAYS_ON configurations to have the interpreter enabled by default
        rather than the JIT. Add a ARCH_WANT_DEFAULT_BPF_JIT config which archs can
        use to set the bpf_jit_{enable,kallsyms} to 1. Back in the days the
        bpf_jit_kallsyms knob was set to 0 by default since major distros still
        had /proc/kallsyms addresses exposed to unprivileged user space which is
        not the case anymore. Hence both knobs are set via BPF_JIT_DEFAULT_ON which
        is set to 'y' in case of BPF_JIT_ALWAYS_ON or ARCH_WANT_DEFAULT_BPF_JIT.
    
        Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
        Acked-by: Will Deacon <will@kernel.org>
        Acked-by: Martin KaFai Lau <kafai@fb.com>
        Link: https://lore.kernel.org/bpf/f78ad24795c2966efcc2ee19025fa3459f622185.1575903816.git.daniel@iogearbox.net
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -524,11 +524,10 @@ void bpf_prog_kallsyms_del_all(struct bpf_prog *fp)
 
 #ifdef CONFIG_BPF_JIT
 /* All BPF JIT sysctl knobs here. */
-int bpf_jit_enable   __read_mostly = IS_BUILTIN(CONFIG_BPF_JIT_ALWAYS_ON);
+int bpf_jit_enable   __read_mostly = IS_BUILTIN(CONFIG_BPF_JIT_DEFAULT_ON);
+int bpf_jit_kallsyms __read_mostly = IS_BUILTIN(CONFIG_BPF_JIT_DEFAULT_ON);;
 /* RHEL-only: set it to 1 by default */
 int bpf_jit_harden   __read_mostly = 1;
-/* RHEL-only: set it to 1 by default */
-int bpf_jit_kallsyms __read_mostly = 1;
 long bpf_jit_limit   __read_mostly;
 
 static __always_inline void
