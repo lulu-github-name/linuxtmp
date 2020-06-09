08ed6ce480b5307afc5fcb4b1e3441e27cfe8f13
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2020-06-08 22:26:01 -0400

    [tools] libbpf: Expose BPF program's function name
    Message-id: <20200608222925.282045-53-yauheni.kaliuta@redhat.com>
    Patchwork-id: 318016
    Patchwork-instance: patchwork
    O-Subject: [PATCH RHEL8.3 053/257] libbpf: Expose BPF program's function name
    Bugzilla: 1813372
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1813372
    
    Conlicts: due to RH version hack.
    
    commit 01af3bf06755dc5cda7050fe5d898998e5775e63
    Author: Andrii Nakryiko <andriin@fb.com>
    Date:   Fri Dec 13 17:43:32 2019 -0800
    
        libbpf: Expose BPF program's function name
    
        Add APIs to get BPF program function name, as opposed to bpf_program__title(),
        which returns BPF program function's section name. Function name has a benefit
        of being a valid C identifier and uniquely identifies a specific BPF program,
        while section name can be duplicated across multiple independent BPF programs.
    
        Add also bpf_object__find_program_by_name(), similar to
        bpf_object__find_program_by_title(), to facilitate looking up BPF programs by
        their C function names.
    
        Convert one of selftests to new API for look up.
    
        Signed-off-by: Andrii Nakryiko <andriin@fb.com>
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
        Acked-by: Martin KaFai Lau <kafai@fb.com>
        Link: https://lore.kernel.org/bpf/20191214014341.3442258-9-andriin@fb.com
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -212,8 +212,8 @@ static const char * const libbpf_type_to_btf_name[] = {
 };
 
 struct bpf_map {
-	int fd;
 	char *name;
+	int fd;
 	int sec_idx;
 	size_t sec_offset;
 	int map_ifindex;
@@ -1387,7 +1387,7 @@ static int bpf_object__init_user_btf_maps(struct bpf_object *obj, bool strict,
 }
 
 static int bpf_object__init_maps(struct bpf_object *obj,
-				 struct bpf_object_open_opts *opts)
+				 const struct bpf_object_open_opts *opts)
 {
 	const char *pin_root_path = OPTS_GET(opts, pin_root_path, NULL);
 	bool strict = !OPTS_GET(opts, relaxed_maps, false);
@@ -1751,6 +1751,19 @@ bpf_object__find_program_by_title(const struct bpf_object *obj,
 	bpf_object__for_each_program(pos, obj) {
 		if (pos->section_name && !strcmp(pos->section_name, title))
 			return pos;
+	}
+	return NULL;
+}
+
+struct bpf_program *
+bpf_object__find_program_by_name(const struct bpf_object *obj,
+				 const char *name)
+{
+	struct bpf_program *prog;
+
+	bpf_object__for_each_program(prog, obj) {
+		if (!strcmp(prog->name, name))
+			return prog;
 	}
 	return NULL;
 }
@@ -3969,7 +3982,7 @@ static int libbpf_find_attach_btf_id(const char *name,
 				     __u32 attach_prog_fd);
 static struct bpf_object *
 __bpf_object__open(const char *path, const void *obj_buf, size_t obj_buf_sz,
-		   struct bpf_object_open_opts *opts)
+		   const struct bpf_object_open_opts *opts)
 {
 	struct bpf_program *prog;
 	struct bpf_object *obj;
@@ -4079,7 +4092,7 @@ struct bpf_object *bpf_object__open(const char *path)
 
 struct bpf_object *
 bpf_object__open_file_v0_0_4(const char *path,
-			     struct bpf_object_open_opts *opts)
+			     const struct bpf_object_open_opts *opts)
 {
 	if (!path)
 		return ERR_PTR(-EINVAL);
@@ -4091,7 +4104,7 @@ bpf_object__open_file_v0_0_4(const char *path,
 
 struct bpf_object *
 bpf_object__open_mem_v0_0_4(const void *obj_buf, size_t obj_buf_sz,
-			    struct bpf_object_open_opts *opts)
+			    const struct bpf_object_open_opts *opts)
 {
 	if (!obj_buf || obj_buf_sz == 0)
 		return ERR_PTR(-EINVAL);
@@ -4894,6 +4907,11 @@ void *bpf_program__priv(const struct bpf_program *prog)
 void bpf_program__set_ifindex(struct bpf_program *prog, __u32 ifindex)
 {
 	prog->prog_ifindex = ifindex;
+}
+
+const char *bpf_program__name(const struct bpf_program *prog)
+{
+	return prog->name;
 }
 
 const char *bpf_program__title(const struct bpf_program *prog, bool needs_copy)
@@ -6835,11 +6853,11 @@ int libbpf_num_possible_cpus(void)
 /* RHEL-only, libbpf version workaround */
 extern struct bpf_object *
 bpf_object__open_file_v0_0_6(const char *path,
-			     struct bpf_object_open_opts *opts)
+			     const struct bpf_object_open_opts *opts)
 	__attribute__((alias("bpf_object__open_file_v0_0_4")));
 extern struct bpf_object *
 bpf_object__open_mem_v0_0_6(const void *obj_buf, size_t obj_buf_sz,
-			    struct bpf_object_open_opts *opts)
+			    const struct bpf_object_open_opts *opts)
 	__attribute__((alias("bpf_object__open_mem_v0_0_4")));
 extern enum bpf_attach_type
 bpf_program__get_expected_attach_type_v0_0_6(struct bpf_program *prog)
