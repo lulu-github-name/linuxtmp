6260dd9e9619adabf2f8826beb86958b5dce376e
Al Stone <ahs3@redhat.com>
2020-06-10 21:51:42 -0400

    [iommu] iommu/arm-smmu-v3: Parse PASID devicetree property of platform devices
    Message-id: <20200610215207.2697022-133-ahs3@redhat.com>
    Patchwork-id: 319757
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH v3 BZ1803969 132/157] iommu/arm-smmu-v3: Parse PASID devicetree property of platform devices
    Bugzilla: 1803969
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1803969
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=29241681
    Upstream Status: 89535821c04256964e266bf585cf224f65e08983
    Tested: Ran through the kernel CKI KT1 test suite multiple times to ensure the
     results were consistent, that we hit multiple platforms, and that they
     were successful.  Also successfully ran a few simple boot tests looking
     for ACPI error messages across multiple x86_64 and aarch64 machines.
    
    Conflicts:
        drivers/iommu/arm-smmu-v3.c -- prior patches were added in a different
        order than upstream, changing context slightly
    
        include/linux/iommu.h -- needed to extend struct iommu_fwspec
        again in a way acceptable to kABI, but had to use
        broken insert since realloc() is used on this struct
        and field ordering is assumed
    
    commit 89535821c04256964e266bf585cf224f65e08983
    Author: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Date:   Wed Jan 15 13:52:29 2020 +0100
    
        iommu/arm-smmu-v3: Parse PASID devicetree property of platform devices
    
        For platform devices that support SubstreamID (SSID), firmware provides
        the number of supported SSID bits. Restrict it to what the SMMU supports
        and cache it into master->ssid_bits, which will also be used for PCI
        PASID.
    
        Reviewed-by: Eric Auger <eric.auger@redhat.com>
        Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
        Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
        Signed-off-by: Will Deacon <will@kernel.org>
        (cherry picked from commit 89535821c04256964e266bf585cf224f65e08983)
    
    Signed-off-by: Al Stone <ahs3@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -598,6 +598,7 @@ extern struct iommu_group *generic_device_group(struct device *dev);
  * @ops: ops for this device's IOMMU
  * @iommu_fwnode: firmware handle for this device's IOMMU
  * @iommu_priv: IOMMU driver private data for this device
+ * @num_pasid_bits: number of PASID bits supported by this device
  * @num_ids: number of associated device IDs
  * @ids: IDs which this device may present to the IOMMU
  */
@@ -606,6 +607,7 @@ struct iommu_fwspec {
 	struct fwnode_handle	*iommu_fwnode;
 	RH_KABI_BROKEN_REMOVE(void *iommu_priv)
 	RH_KABI_BROKEN_INSERT(u32 flags)
+	RH_KABI_BROKEN_INSERT(u32 num_pasid_bits)
 	unsigned int		num_ids;
 	RH_KABI_BROKEN_REPLACE(u32 ids[1], u32 ids[])
 };
