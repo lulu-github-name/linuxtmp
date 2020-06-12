ed9a1e2ed3c50f9493fcc60fb08f25a12e0a29f0
Benjamin Coddington <bcodding@redhat.com>
2020-06-12 13:28:45 -0400

    [net] SUNRPC: Ensure to ratelimit the "server not responding" syslog messages
    Message-id: <6e39dbe00af5c5b4b3a8ae6b6a221f54ee0410ae.1591958517.git.bcodding@redhat.com>
    Patchwork-id: 320133
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 fs PATCH 12/52] SUNRPC: Ensure to ratelimit the "server not responding" syslog messages
    Bugzilla: 1831672
    RH-Acked-by: Scott Mayhew <smayhew@redhat.com>
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    
    From: Trond Myklebust <trondmy@gmail.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1831672
    
    commit 0729d995f2a2726598642d552ebe916b43aef73d
    Author: Trond Myklebust <trondmy@gmail.com>
    Date:   Sun Apr 7 13:58:57 2019 -0400
    
        SUNRPC: Ensure to ratelimit the "server not responding" syslog messages
    
        In particular, the timeout messages can be very noisy, so we ought to
        ratelimit them in order to avoid spamming the syslog.
    
        Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
        Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>
    
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- net/sunrpc/clnt.c
+++ net/sunrpc/clnt.c
@@ -2407,7 +2407,8 @@ rpc_check_timeout(struct rpc_task *task)
 			return;
 
 		if (clnt->cl_chatty) {
-			printk(KERN_NOTICE "%s: server %s not responding, timed out\n",
+			pr_notice_ratelimited(
+				"%s: server %s not responding, timed out\n",
 				clnt->cl_program->name,
 				task->tk_xprt->servername);
 		}
@@ -2421,9 +2422,10 @@ rpc_check_timeout(struct rpc_task *task)
 	if (!(task->tk_flags & RPC_CALL_MAJORSEEN)) {
 		task->tk_flags |= RPC_CALL_MAJORSEEN;
 		if (clnt->cl_chatty) {
-			printk(KERN_NOTICE "%s: server %s not responding, still trying\n",
-			clnt->cl_program->name,
-			task->tk_xprt->servername);
+			pr_notice_ratelimited(
+				"%s: server %s not responding, still trying\n",
+				clnt->cl_program->name,
+				task->tk_xprt->servername);
 		}
 	}
 	rpc_force_rebind(clnt);
@@ -2454,7 +2456,7 @@ call_decode(struct rpc_task *task)
 
 	if (task->tk_flags & RPC_CALL_MAJORSEEN) {
 		if (clnt->cl_chatty) {
-			printk(KERN_NOTICE "%s: server %s OK\n",
+			pr_notice_ratelimited("%s: server %s OK\n",
 				clnt->cl_program->name,
 				task->tk_xprt->servername);
 		}
