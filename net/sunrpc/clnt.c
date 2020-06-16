dd6878bb21209451be2a71845ace05fcb50429f7
Benjamin Coddington <bcodding@redhat.com>
2020-06-16 12:26:57 -0400

    [net] SUNRPC: Handle EADDRINUSE and ENOBUFS correctly
    Message-id: <216ea049197e760ea4044742d84de2a5b34652f6.1592309971.git.bcodding@redhat.com>
    Patchwork-id: 320328
    Patchwork-instance: patchwork
    O-Subject: [RHEL8 fs PATCH 48/55] SUNRPC: Handle EADDRINUSE and ENOBUFS correctly
    Bugzilla: 1846295
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Scott Mayhew <smayhew@redhat.com>
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    
    From: Trond Myklebust <trond.myklebust@hammerspace.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1846295
    
    commit 80f455da6cd0998a5be30a8af24ea2a22815c212
    Author: Trond Myklebust <trond.myklebust@hammerspace.com>
    Date:   Thu Aug 15 22:55:19 2019 -0400
    
        SUNRPC: Handle EADDRINUSE and ENOBUFS correctly
    
        If a connect or bind attempt returns EADDRINUSE, that means we want to
        retry with a different port. It is not a fatal connection error.
        Similarly, ENOBUFS is not fatal, but just indicates a memory allocation
        issue. Retry after a short delay.
    
        Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- net/sunrpc/clnt.c
+++ net/sunrpc/clnt.c
@@ -2007,6 +2007,9 @@ call_bind_status(struct rpc_task *task)
 		task->tk_rebind_retry--;
 		rpc_delay(task, 3*HZ);
 		goto retry_timeout;
+	case -ENOBUFS:
+		rpc_delay(task, HZ >> 2);
+		goto retry_timeout;
 	case -EAGAIN:
 		goto retry_timeout;
 	case -ETIMEDOUT:
@@ -2030,7 +2033,6 @@ call_bind_status(struct rpc_task *task)
 	case -ENETDOWN:
 	case -EHOSTUNREACH:
 	case -ENETUNREACH:
-	case -ENOBUFS:
 	case -EPIPE:
 		dprintk("RPC: %5u remote rpcbind unreachable: %d\n",
 				task->tk_pid, task->tk_status);
@@ -2133,8 +2135,6 @@ call_connect_status(struct rpc_task *task)
 	case -ENETDOWN:
 	case -ENETUNREACH:
 	case -EHOSTUNREACH:
-	case -EADDRINUSE:
-	case -ENOBUFS:
 	case -EPIPE:
 		xprt_conditional_disconnect(task->tk_rqstp->rq_xprt,
 					    task->tk_rqstp->rq_connect_cookie);
@@ -2143,9 +2143,13 @@ call_connect_status(struct rpc_task *task)
 		/* retry with existing socket, after a delay */
 		rpc_delay(task, 3*HZ);
 		/* fall through */
+	case -EADDRINUSE:
 	case -ENOTCONN:
 	case -EAGAIN:
 	case -ETIMEDOUT:
+		goto out_retry;
+	case -ENOBUFS:
+		rpc_delay(task, HZ >> 2);
 		goto out_retry;
 	}
 	rpc_call_rpcerror(task, status);
