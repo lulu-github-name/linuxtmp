3d44ac6a8586cad25a68adf64566386d84ed0048
Lyude Paul <lyude@redhat.com>
2020-06-19 20:22:02 -0400

    [video] fbdev: drop res_id parameter from remove_conflicting_pci_framebuffers
    Message-id: <20200619202542.4130248-75-lyude@redhat.com>
    Patchwork-id: 320850
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 drm PATCH v3 074/293] fbdev: drop res_id parameter from remove_conflicting_pci_framebuffers
    Bugzilla: 1814893
    RH-Acked-by: Dave Airlie <airlied@redhat.com>
    RH-Acked-by: Michel Dänzer <mdaenzer@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1814893
    Upstream: since v5.5-rc1
    
    Conflicts:
            include/linux/fb.h
    
    commit 0a8459693238a339de9705da4e26f6ffbb6a4ebc
    Author:     Gerd Hoffmann <kraxel@redhat.com>
    AuthorDate: Thu Aug 22 11:06:43 2019 +0200
    Commit:     Gerd Hoffmann <kraxel@redhat.com>
    CommitDate: Fri Aug 23 10:48:31 2019 +0200
    
        Since commit b0e999c95581 ("fbdev: list all pci memory bars as
        conflicting apertures") the parameter was used for some sanity checks
        only, to make sure we detect any issues with the new approach to just
        list all memory bars as apertures.
    
        No issues turned up so far, so continue to cleanup:  Drop the res_id
        parameter, drop the sanity checks.  Also downgrade the logging from
        "info" level to "debug" level and update documentation.
    
        Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
        Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
        Link: http://patchwork.freedesktop.org/patch/msgid/20190822090645.25410-2-kraxel@redhat.com
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -638,7 +638,7 @@ extern ssize_t fb_sys_write(struct fb_info *info, const char __user *buf,
 extern int register_framebuffer(struct fb_info *fb_info);
 extern int unregister_framebuffer(struct fb_info *fb_info);
 extern int unlink_framebuffer(struct fb_info *fb_info);
-extern int remove_conflicting_pci_framebuffers(struct pci_dev *pdev, int res_id,
+extern int remove_conflicting_pci_framebuffers(struct pci_dev *pdev,
 					       const char *name);
 extern int remove_conflicting_framebuffers(struct apertures_struct *a,
 					   const char *name, bool primary);
