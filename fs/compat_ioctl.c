5d5201ddf9f1470f84952db373f4983b01a2fb79
Lyude Paul <lyude@redhat.com>
2020-06-19 20:21:17 -0400

    [rtc] compat_ioctl: move rtc handling into drivers/rtc/dev.c
    Message-id: <20200619202542.4130248-30-lyude@redhat.com>
    Patchwork-id: 320836
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 drm PATCH v3 029/293] compat_ioctl: move rtc handling into drivers/rtc/dev.c
    Bugzilla: 1814893
    RH-Acked-by: Dave Airlie <airlied@redhat.com>
    RH-Acked-by: Michel Dänzer <mdaenzer@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1814893
    Upstream: since v5.5-rc1
    
    Conflicts:
            fs/compat_ioctl.c
    
    commit 076ff658628678f73d2349a699c3431877c1a075
    Author:     Arnd Bergmann <arnd@arndb.de>
    AuthorDate: Fri Aug 24 00:11:19 2018 +0200
    Commit:     Arnd Bergmann <arnd@arndb.de>
    CommitDate: Wed Oct 23 17:23:43 2019 +0200
    
        We no longer need the rtc compat handling to be in common code, now that
        all drivers are either moved to the rtc-class framework, or (rarely)
        exist in drivers/char for architectures without compat mode (m68k,
        alpha and ia64, respectively).
    
        I checked the list of ioctl commands in drivers, and the ones that are
        not already handled are all compatible, again with the one exception of
        m68k driver, which implements RTC_PLL_GET and RTC_PLL_SET, but has no
        compat mode.
    
        Unlike earlier versions of this patch, I'm now adding a separate
        compat_ioctl handler that takes care of RTC_IRQP_READ32/RTC_IRQP_SET32
        and treats all other commands as compatible, leaving the native
        behavior unchanged.
    
        The old conversion handler also deals with RTC_EPOCH_READ and
        RTC_EPOCH_SET, which are not handled in rtc-dev.c but only in a single
        device driver (rtc-vr41xx), so I'm adding the compat version in the same
        place. I don't expect other drivers to need those commands in the future.
    
        Acked-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
        Reviewed-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
        Signed-off-by: Arnd Bergmann <arnd@arndb.de>
        ---
        v4: handle RTC_EPOCH_SET32 in rtc_dev_compat_ioctl
        v3: handle RTC_IRQP_READ32/RTC_IRQP_SET32 in rtc_dev_compat_ioctl
        v2: merge compat handler into ioctl function to avoid the
            compat_alloc_user_space() roundtrip, based on feedback
            from Al Viro.
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- fs/compat_ioctl.c
+++ fs/compat_ioctl.c
@@ -46,7 +46,6 @@
 #include <linux/raw.h>
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
-#include <linux/rtc.h>
 #include <linux/pci.h>
 #include <linux/serial.h>
 #include <linux/if_tun.h>
@@ -634,37 +633,6 @@ static int serial_struct_ioctl(struct file *file,
         return err;
 }
 
-#define RTC_IRQP_READ32		_IOR('p', 0x0b, compat_ulong_t)
-#define RTC_IRQP_SET32		_IOW('p', 0x0c, compat_ulong_t)
-#define RTC_EPOCH_READ32	_IOR('p', 0x0d, compat_ulong_t)
-#define RTC_EPOCH_SET32		_IOW('p', 0x0e, compat_ulong_t)
-
-static int rtc_ioctl(struct file *file,
-		unsigned cmd, void __user *argp)
-{
-	unsigned long __user *valp = compat_alloc_user_space(sizeof(*valp));
-	int ret;
-
-	if (valp == NULL)
-		return -EFAULT;
-	switch (cmd) {
-	case RTC_IRQP_READ32:
-	case RTC_EPOCH_READ32:
-		ret = do_ioctl(file, (cmd == RTC_IRQP_READ32) ?
-					RTC_IRQP_READ : RTC_EPOCH_READ,
-					(unsigned long)valp);
-		if (ret)
-			return ret;
-		return convert_in_user(valp, (unsigned int __user *)argp);
-	case RTC_IRQP_SET32:
-		return do_ioctl(file, RTC_IRQP_SET, (unsigned long)argp);
-	case RTC_EPOCH_SET32:
-		return do_ioctl(file, RTC_EPOCH_SET, (unsigned long)argp);
-	}
-
-	return -ENOIOCTLCMD;
-}
-
 /*
  * simple reversible transform to make our table more evenly
  * distributed after sorting.
@@ -770,21 +738,6 @@ COMPATIBLE_IOCTL(SCSI_IOCTL_GET_PCI)
 /* Big V (don't complain on serial console) */
 IGNORE_IOCTL(VT_OPENQRY)
 IGNORE_IOCTL(VT_GETMODE)
-/* Little p (/dev/rtc, /dev/envctrl, etc.) */
-COMPATIBLE_IOCTL(RTC_AIE_ON)
-COMPATIBLE_IOCTL(RTC_AIE_OFF)
-COMPATIBLE_IOCTL(RTC_UIE_ON)
-COMPATIBLE_IOCTL(RTC_UIE_OFF)
-COMPATIBLE_IOCTL(RTC_PIE_ON)
-COMPATIBLE_IOCTL(RTC_PIE_OFF)
-COMPATIBLE_IOCTL(RTC_WIE_ON)
-COMPATIBLE_IOCTL(RTC_WIE_OFF)
-COMPATIBLE_IOCTL(RTC_ALM_SET)
-COMPATIBLE_IOCTL(RTC_ALM_READ)
-COMPATIBLE_IOCTL(RTC_RD_TIME)
-COMPATIBLE_IOCTL(RTC_SET_TIME)
-COMPATIBLE_IOCTL(RTC_WKALM_SET)
-COMPATIBLE_IOCTL(RTC_WKALM_RD)
 /*
  * These two are only for the sbus rtc driver, but
  * hwclock tries them on every rtc device first when
@@ -1256,12 +1209,6 @@ static long do_ioctl_trans(unsigned int cmd,
 	case TIOCGSERIAL:
 	case TIOCSSERIAL:
 		return serial_struct_ioctl(file, cmd, argp);
-	/* Not implemented in the native kernel */
-	case RTC_IRQP_READ32:
-	case RTC_IRQP_SET32:
-	case RTC_EPOCH_READ32:
-	case RTC_EPOCH_SET32:
-		return rtc_ioctl(file, cmd, argp);
 
 	/* dvb */
 	case VIDEO_GET_EVENT:
