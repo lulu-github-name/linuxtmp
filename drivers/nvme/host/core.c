a2981b8a7552c76207945683e722259cb01f662b
Ewan Milne <emilne@redhat.com>
2020-06-23 00:33:35 -0400

    [nvme] nvme-multipath: do not reset on unknown status
    Message-id: <20200623003338.7675-2-emilne@redhat.com>
    Patchwork-id: 321364
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 PATCH 1/4] nvme-multipath: do not reset on unknown status
    Bugzilla: 1847572
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    
    The nvme multipath error handling defaults to controller reset if the
    error is unknown. There are, however, no existing nvme status codes that
    indicate a reset should be used, and resetting causes unnecessary
    disruption to the rest of IO.
    
    Change nvme's error handling to first check if failover should happen.
    If not, let the normal error handling take over rather than reset the
    controller.
    
    Conflicts: differences due to earlier RHEL-specific commit 158eef2e63ed
               ("nvme: allow ANA support to be independent of native multipathing")
    
    Based-on-a-patch-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: John Meneghini <johnm@netapp.com>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    (cherry picked from commit 764e9332098c0e60251386a507fe46ac91276120)
    Signed-off-by: Ewan D. Milne <emilne@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -290,13 +290,11 @@ void nvme_complete_rq(struct request *req)
 		nvme_req(req)->ctrl->comp_seen = true;
 
 	if (unlikely(status != BLK_STS_OK && nvme_req_needs_retry(req))) {
-		if (blk_path_error(status)) {
-			if (req->cmd_flags & REQ_NVME_MPATH) {
-				nvme_failover_req(req);
-				return;
-			}
+		if (blk_path_error(status))
 			nvme_update_ana(req);
-		}
+
+		if ((req->cmd_flags & REQ_NVME_MPATH) && nvme_failover_req(req))
+			return;
 
 		if (!blk_queue_dying(req->q)) {
 			nvme_retry_req(req);
