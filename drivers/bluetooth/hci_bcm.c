419e6b5d816753f35866653c07b3bfc384882482
Gopal Tiwari <gtiwari@redhat.com>
2020-07-07 10:20:04 -0400

    [bluetooth] Bluetooth: hci_bcm: Disallow set_baudrate for BCM4354
    Message-id: <20200707102204.31859-46-gtiwari@redhat.com>
    Patchwork-id: 321887
    Patchwork-instance: patchwork
    O-Subject: [RHEL8 BZ 1838924 045/165] Bluetooth: hci_bcm: Disallow set_baudrate for BCM4354
    Bugzilla: 1838924
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1838924
    
    commit 5d6f391073d5c1c903ac12be72c66b96b2ae93f4
    Author: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Date:   Tue Nov 26 08:17:29 2019 +0100
    
        Bluetooth: hci_bcm: Disallow set_baudrate for BCM4354
    
        Without updating the patchram, the BCM4354 does not support a higher
        operating speed. The normal bcm_setup follows the correct order
        (init_speed, patchram and then oper_speed) but the serdev driver will
        set the operating speed before calling the hu->setup function. Thus,
        for the BCM4354, don't set the operating speed before patchram.
    
        Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
        Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
        Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Signed-off-by: Gopal Tiwari <gtiwari@redhat.com>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- drivers/bluetooth/hci_bcm.c
+++ drivers/bluetooth/hci_bcm.c
@@ -63,6 +63,14 @@
 #define BCM_NUM_SUPPLIES 2
 
 /**
+ * struct bcm_device_data - device specific data
+ * @no_early_set_baudrate: Disallow set baudrate before driver setup()
+ */
+struct bcm_device_data {
+	bool	no_early_set_baudrate;
+};
+
+/**
  * struct bcm_device - device driver resources
  * @serdev_hu: HCI UART controller struct
  * @list: bcm_device_list node
@@ -94,6 +102,7 @@
  * @hu: pointer to HCI UART controller struct,
  *	used to disable flow control during runtime suspend and system sleep
  * @is_suspended: whether flow control is currently disabled
+ * @no_early_set_baudrate: don't set_baudrate before setup()
  */
 struct bcm_device {
 	/* Must be the first member, hci_serdev.c expects this. */
@@ -127,6 +136,7 @@ struct bcm_device {
 	struct hci_uart		*hu;
 	bool			is_suspended;
 #endif
+	bool			no_early_set_baudrate;
 };
 
 /* generic bcm uart resources */
@@ -462,7 +472,13 @@ out:
 	if (bcm->dev) {
 		hci_uart_set_flow_control(hu, true);
 		hu->init_speed = bcm->dev->init_speed;
-		hu->oper_speed = bcm->dev->oper_speed;
+
+		/* If oper_speed is set, ldisc/serdev will set the baudrate
+		 * before calling setup()
+		 */
+		if (!bcm->dev->no_early_set_baudrate)
+			hu->oper_speed = bcm->dev->oper_speed;
+
 		err = bcm_gpio_set_power(bcm->dev, true);
 		hci_uart_set_flow_control(hu, false);
 		if (err)
@@ -580,6 +596,8 @@ static int bcm_setup(struct hci_uart *hu)
 	/* Operational speed if any */
 	if (hu->oper_speed)
 		speed = hu->oper_speed;
+	else if (bcm->dev && bcm->dev->oper_speed)
+		speed = bcm->dev->oper_speed;
 	else if (hu->proto->oper_speed)
 		speed = hu->proto->oper_speed;
 	else
@@ -1389,6 +1407,7 @@ static struct platform_driver bcm_driver = {
 static int bcm_serdev_probe(struct serdev_device *serdev)
 {
 	struct bcm_device *bcmdev;
+	const struct bcm_device_data *data;
 	int err;
 
 	bcmdev = devm_kzalloc(&serdev->dev, sizeof(*bcmdev), GFP_KERNEL);
@@ -1423,6 +1442,10 @@ static int bcm_serdev_probe(struct serdev_device *serdev)
 	if (err)
 		dev_err(&serdev->dev, "Failed to power down\n");
 
+	data = device_get_match_data(bcmdev->dev);
+	if (data)
+		bcmdev->no_early_set_baudrate = data->no_early_set_baudrate;
+
 	return hci_uart_register_device(&bcmdev->serdev_hu, &bcm_proto);
 }
 
@@ -1438,7 +1461,7 @@ static const struct of_device_id bcm_bluetooth_of_match[] = {
 	{ .compatible = "brcm,bcm4345c5" },
 	{ .compatible = "brcm,bcm4330-bt" },
 	{ .compatible = "brcm,bcm43438-bt" },
-	{ .compatible = "brcm,bcm43540-bt" },
+	{ .compatible = "brcm,bcm43540-bt", .data = &bcm4354_device_data },
 	{ .compatible = "brcm,bcm4335a0" },
 	{ },
 };
