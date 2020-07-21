c443e5eb8bba964c51d6f2a64584bddf6b86dc09
Lukas Czerner <lczerner@redhat.com>
2020-07-21 11:38:46 -0400

    [fs] ext4: save the error code which triggered an ext4_error() in the superblock
    Message-id: <20200721113914.1967-59-lczerner@redhat.com>
    Patchwork-id: 322337
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.3 fs PATCH 58/86] ext4: save the error code which triggered an ext4_error() in the superblock
    Bugzilla: 1856760
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    RH-Acked-by: Eric Sandeen <esandeen@redhat.com>
    
    From: Theodore Ts'o <tytso@mit.edu>
    
    BZ 1856760
    https://bugzilla.redhat.com/show_bug.cgi?id=1856760
    
    BREW 30205319
    https://brewweb.devel.redhat.com/taskinfo?taskID=30205319
    
    commit 878520ac45f9f698432d4276db3d9144b83931b6
    
    This allows the cause of an ext4_error() report to be categorized
    based on whether it was triggered due to an I/O error, or an memory
    allocation error, or other possible causes.  Most errors are caused by
    a detected file system inconsistency, so the default code stored in
    the superblock will be EXT4_ERR_EFSCORRUPTED.
    
    Link: https://lore.kernel.org/r/20191204032335.7683-1-tytso@mit.edu
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Frantisek Hrbata <fhrbata@redhat.com>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -142,6 +142,7 @@ static struct buffer_head *__ext4_read_dirblock(struct inode *inode,
 		if (ext4_dx_csum_verify(inode, dirent))
 			set_buffer_verified(bh);
 		else {
+			ext4_set_errno(inode->i_sb, EFSBADCRC);
 			ext4_error_inode(inode, func, line, block,
 					 "Directory index failed checksum");
 			brelse(bh);
@@ -152,6 +153,7 @@ static struct buffer_head *__ext4_read_dirblock(struct inode *inode,
 		if (ext4_dirent_csum_verify(inode, dirent))
 			set_buffer_verified(bh);
 		else {
+			ext4_set_errno(inode->i_sb, EFSBADCRC);
 			ext4_error_inode(inode, func, line, block,
 					 "Directory block failed checksum");
 			brelse(bh);
@@ -1439,6 +1441,7 @@ restart:
 			goto next;
 		wait_on_buffer(bh);
 		if (!buffer_uptodate(bh)) {
+			ext4_set_errno(sb, EIO);
 			EXT4_ERROR_INODE(dir, "reading directory lblock %lu",
 					 (unsigned long) block);
 			brelse(bh);
@@ -1450,6 +1453,7 @@ restart:
 					 (struct ext4_dir_entry *)bh->b_data) &&
 		    !ext4_dirent_csum_verify(dir,
 				(struct ext4_dir_entry *)bh->b_data)) {
+			ext4_set_errno(sb, EFSBADCRC);
 			EXT4_ERROR_INODE(dir, "checksumming directory "
 					 "block %lu", (unsigned long)block);
 			brelse(bh);
