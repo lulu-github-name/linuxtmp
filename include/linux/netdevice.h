e1197bc69edb2fd55990d0e4be7468ffd8f3e56b
Ivan Vecera <ivecera@redhat.com>
2020-08-11 15:23:42 -0400

    [net] change addr_list_lock back to static key
    Message-id: <20200811152343.2901287-4-ivecera@redhat.com>
    Patchwork-id: 323648
    Patchwork-instance: patchwork
    O-Subject: [RHEL-8.4 net PATCH 3/4] net: change addr_list_lock back to static key
    Bugzilla: 1867961
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1867961
    
    Conflicts: \
    net/dsa/master.c
    - hunk removed as it is not applicable at all and DSA is not enabled in
      RHEL
    include/linux/netdevice.h
    - removed also RHEL specific comment and KABI macro
    
    Upstream commit(s):
    commit 845e0ebb4408d4473cf60d21224a897037e9a77a
    Author: Cong Wang <xiyou.wangcong@gmail.com>
    Date:   Mon Jun 8 14:53:01 2020 -0700
    
        net: change addr_list_lock back to static key
    
        The dynamic key update for addr_list_lock still causes troubles,
        for example the following race condition still exists:
    
        CPU 0:                          CPU 1:
        (RCU read lock)                 (RTNL lock)
        dev_mc_seq_show()               netdev_update_lockdep_key()
                                          -> lockdep_unregister_key()
         -> netif_addr_lock_bh()
    
        because lockdep doesn't provide an API to update it atomically.
        Therefore, we have to move it back to static keys and use subclass
        for nest locking like before.
    
        In commit 1a33e10e4a95 ("net: partially revert dynamic lockdep key
        changes"), I already reverted most parts of commit ab92d68fc22f
        ("net: core: add generic lockdep keys").
    
        This patch reverts the rest and also part of commit f3b0a18bb6cb
        ("net: remove unnecessary variables and callback"). After this
        patch, addr_list_lock changes back to using static keys and
        subclasses to satisfy lockdep. Thanks to dev->lower_level, we do
        not have to change back to ->ndo_get_lock_subclass().
    
        And hopefully this reduces some syzbot lockdep noises too.
    
        Reported-by: syzbot+f3a0e80c34b3fc28ac5e@syzkaller.appspotmail.com
        Cc: Taehee Yoo <ap420073@gmail.com>
        Cc: Dmitry Vyukov <dvyukov@google.com>
        Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Andrea Claudi <aclaudi@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1904,8 +1904,6 @@ struct net_device_extended_rh {
  *			for hardware timestamping
  *	@sfp_bus:	attached &struct sfp_bus structure.
  *
- *	@addr_list_lock_key:	lockdep class annotating
- *				net_device->addr_list_lock spinlock
  *	@qdisc_tx_busylock: lockdep class annotating Qdisc->busylock spinlock
  *	@qdisc_running_key: lockdep class annotating Qdisc->running seqcount
  *
@@ -2212,13 +2210,6 @@ struct net_device {
 	struct sfp_bus		*sfp_bus;
 	struct lock_class_key	*qdisc_tx_busylock;
 	struct lock_class_key	*qdisc_running_key;
-	/* RHEL: The KABI breakage here is not real. We are inserting
-	 * a field of type struct lock_class_key but the size of this
-	 * structure is zero for non-debug kernel configs because
-	 * LOCKDEP is not enabled for them and for debug flavours
-	 * KABI is not preserved.
-	 */
-	RH_KABI_BROKEN_INSERT(struct lock_class_key addr_list_lock_key)
 	bool			proto_down;
 	RH_KABI_FILL_HOLE(unsigned	wol_enabled:1)
 
@@ -2339,10 +2330,13 @@ static inline void netdev_for_each_tx_queue(struct net_device *dev,
 	static struct lock_class_key qdisc_tx_busylock_key;	\
 	static struct lock_class_key qdisc_running_key;		\
 	static struct lock_class_key qdisc_xmit_lock_key;	\
+	static struct lock_class_key dev_addr_list_lock_key;	\
 	unsigned int i;						\
 								\
 	(dev)->qdisc_tx_busylock = &qdisc_tx_busylock_key;	\
 	(dev)->qdisc_running_key = &qdisc_running_key;		\
+	lockdep_set_class(&(dev)->addr_list_lock,		\
+			  &dev_addr_list_lock_key);		\
 	for (i = 0; i < (dev)->num_tx_queues; i++)		\
 		lockdep_set_class(&(dev)->_tx[i]._xmit_lock,	\
 				  &qdisc_xmit_lock_key);	\
@@ -3361,7 +3355,6 @@ static inline void netif_stop_queue(struct net_device *dev)
 }
 
 void netif_tx_stop_all_queues(struct net_device *dev);
-void netdev_update_lockdep_key(struct net_device *dev);
 
 static inline bool netif_tx_queue_stopped(const struct netdev_queue *dev_queue)
 {
@@ -4278,6 +4271,11 @@ static inline void netif_tx_disable(struct net_device *dev)
 static inline void netif_addr_lock(struct net_device *dev)
 {
 	spin_lock(&dev->addr_list_lock);
+}
+
+static inline void netif_addr_lock_nested(struct net_device *dev)
+{
+	spin_lock_nested(&dev->addr_list_lock, dev->lower_level);
 }
 
 static inline void netif_addr_lock_bh(struct net_device *dev)
