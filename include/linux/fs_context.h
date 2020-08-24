4d1b10dc85d3797efcdc57f3d3edbaf1bf74aaa3
Scott Mayhew <smayhew@redhat.com>
2020-08-24 16:25:16 -0400

    [include] prefix-handling analogues of errorf() and friends
    Message-id: <20200824162534.24396-202-smayhew@redhat.com>
    Patchwork-id: 325010
    Patchwork-instance: patchwork
    O-Subject: [RHEL 8.4 fs PATCH v3 201/219] prefix-handling analogues of errorf() and friends
    Bugzilla: 1622041
    RH-Acked-by: David Howells <dhowells@redhat.com>
    RH-Acked-by: Ian Kent <ikent@redhat.com>
    
    From: Al Viro <viro@zeniv.linux.org.uk>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1622041
    
    commit a3ff937b33d9dfd0923ac5279d87723048599057
    Author: Al Viro <viro@zeniv.linux.org.uk>
    Date:   Sat Dec 21 21:30:50 2019 -0500
    
        prefix-handling analogues of errorf() and friends
    
        called errorfc/infofc/warnfc/invalfc
    
        Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    Signed-off-by: Scott Mayhew <smayhew@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/linux/fs_context.h
+++ include/linux/fs_context.h
@@ -208,6 +208,7 @@ void logfc(struct fc_log *log, const char *prefix, char level, const char *fmt, 
  */
 #define infof(fc, fmt, ...) __logfc(fc, 'i', fmt, ## __VA_ARGS__)
 #define info_plog(p, fmt, ...) __plog(p, 'i', fmt, ## __VA_ARGS__)
+#define infofc(p, fmt, ...) __plog((&(fc)->log), 'i', fmt, ## __VA_ARGS__)
 
 /**
  * warnf - Store supplementary warning message
@@ -219,6 +220,7 @@ void logfc(struct fc_log *log, const char *prefix, char level, const char *fmt, 
  */
 #define warnf(fc, fmt, ...) __logfc(fc, 'w', fmt, ## __VA_ARGS__)
 #define warn_plog(p, fmt, ...) __plog(p, 'w', fmt, ## __VA_ARGS__)
+#define warnfc(fc, fmt, ...) __plog((&(fc)->log), 'w', fmt, ## __VA_ARGS__)
 
 /**
  * errorf - Store supplementary error message
@@ -230,6 +232,7 @@ void logfc(struct fc_log *log, const char *prefix, char level, const char *fmt, 
  */
 #define errorf(fc, fmt, ...) __logfc(fc, 'e', fmt, ## __VA_ARGS__)
 #define error_plog(p, fmt, ...) __plog(p, 'e', fmt, ## __VA_ARGS__)
+#define errorfc(fc, fmt, ...) __plog((&(fc)->log), 'e', fmt, ## __VA_ARGS__)
 
 /**
  * invalf - Store supplementary invalid argument error message
@@ -241,5 +244,6 @@ void logfc(struct fc_log *log, const char *prefix, char level, const char *fmt, 
  */
 #define invalf(fc, fmt, ...) (errorf(fc, fmt, ## __VA_ARGS__), -EINVAL)
 #define inval_plog(p, fmt, ...) (error_plog(p, fmt, ## __VA_ARGS__), -EINVAL)
+#define invalfc(fc, fmt, ...) (errorfc(fc, fmt, ## __VA_ARGS__), -EINVAL)
 
 #endif /* _LINUX_FS_CONTEXT_H */
