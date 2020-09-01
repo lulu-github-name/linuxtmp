f7c62418403cd2a70acdcc9469e26ea793cbefc7
Vladis Dronov <vdronov@redhat.com>
2020-09-01 19:31:05 -0400

    [include] cgroup: Fix sock_cgroup_data on big-endian
    Message-id: <20200901193105.1037783-4-vdronov@redhat.com>
    Patchwork-id: 325540
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 BZ1868659 v2 PATCH 3/3] cgroup: Fix sock_cgroup_data on big-endian.
    Bugzilla: 1868659
    CVE: CVE-2020-14356
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: Waiman Long <longman@redhat.com>
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1868659
    Upstream Status: merged into the linux.git
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=31075691
    CVE: CVE-2020-14356
    
    Conflicts:
    * embed RH_KABI_REPLACE_SPLIT() so the kernel builds if bisected at this
      commit.
    
    commit 14b032b8f8fce03a546dcf365454bec8c4a58d7d
    Author: Cong Wang <xiyou.wangcong@gmail.com>
    Date:   Thu Jul 9 16:28:44 2020 -0700
    
        cgroup: Fix sock_cgroup_data on big-endian.
    
        In order for no_refcnt and is_data to be the lowest order two
        bits in the 'val' we have to pad out the bitfield of the u8.
    
        Fixes: ad0f75e5f57c ("cgroup: fix cgroup_sk_alloc() for sk_clone_lock()")
        Reported-by: Guenter Roeck <linux@roeck-us.net>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Vladis Dronov <vdronov@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/linux/cgroup-defs.h
+++ include/linux/cgroup-defs.h
@@ -823,7 +823,7 @@ struct sock_cgroup_data {
 	union {
 #ifdef __LITTLE_ENDIAN
 		struct {
-			RH_KABI_REPLACE_SPLIT(u8 is_data, u8 is_data : 1, u8 no_refcnt : 1)
+			RH_KABI_REPLACE_SPLIT(u8 is_data, u8 is_data : 1, u8 no_refcnt : 1, u8 unused : 6)
 			u8	padding;
 			u16	prioidx;
 			u32	classid;
@@ -833,7 +833,7 @@ struct sock_cgroup_data {
 			u32	classid;
 			u16	prioidx;
 			u8	padding;
-			RH_KABI_REPLACE_SPLIT(u8 is_data, u8 no_refcnt : 1, u8 is_data : 1)
+			RH_KABI_REPLACE_SPLIT(u8 is_data, u8 unused : 6, u8 no_refcnt : 1, u8 is_data : 1)
 		} __packed;
 #endif
 		u64		val;
