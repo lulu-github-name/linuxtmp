e63696d21af02c6f6713d349513d212a35ed1663
David Arcari <darcari@redhat.com>
2020-09-21 17:21:12 -0400

    [idle] intel_idle: Move and clean up intel_idle_cpuidle_devices_uninit()
    Message-id: <20200921172127.125860-7-darcari@redhat.com>
    Patchwork-id: 327501
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 BZ 1857129 06/21] intel_idle: Move and clean up intel_idle_cpuidle_devices_uninit()
    Bugzilla: 1857129
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Vladis Dronov <vdronov@redhat.com>
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1857129
    
    commit 0755a9bd9963ec3d611129cf2cdbb3e041ccbd6a
    Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Date:   Fri Jan 10 11:49:58 2020 +0100
    
        intel_idle: Move and clean up intel_idle_cpuidle_devices_uninit()
    
        Move intel_idle_cpuidle_devices_uninit() closer to its caller,
        intel_idle_init(), add the __init modifier to its header, drop a
        redundant local variable from it and fix up its kerneldoc comment.
    
        No intentional functional impact.
    
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Signed-off-by: David Arcari <darcari@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/idle/intel_idle.c
+++ drivers/idle/intel_idle.c
@@ -1303,21 +1303,6 @@ static inline bool intel_idle_off_by_default(u32 mwait_hint) { return false; }
 #endif /* !CONFIG_ACPI_PROCESSOR_CSTATE */
 
 /*
- * intel_idle_cpuidle_devices_uninit()
- * Unregisters the cpuidle devices.
- */
-static void intel_idle_cpuidle_devices_uninit(void)
-{
-	int i;
-	struct cpuidle_device *dev;
-
-	for_each_online_cpu(i) {
-		dev = per_cpu_ptr(intel_idle_cpuidle_devices, i);
-		cpuidle_unregister_device(dev);
-	}
-}
-
-/*
  * ivt_idle_state_table_update(void)
  *
  * Tune IVT multi-socket targets
@@ -1570,6 +1555,17 @@ static int intel_idle_cpu_online(unsigned int cpu)
 		return intel_idle_cpu_init(cpu);
 
 	return 0;
+}
+
+/**
+ * intel_idle_cpuidle_devices_uninit - Unregister all cpuidle devices.
+ */
+static void __init intel_idle_cpuidle_devices_uninit(void)
+{
+	int i;
+
+	for_each_online_cpu(i)
+		cpuidle_unregister_device(per_cpu_ptr(intel_idle_cpuidle_devices, i));
 }
 
 static int __init intel_idle_init(void)
