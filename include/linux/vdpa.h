4bcf7b9ce552147fed5d7ae157204f27590af574
Jason Wang <jasowang@redhat.com>
2020-09-24 11:21:08 +0800

    vdpa: add the missing comment for nvqs in struct vdpa_device
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- include/linux/vdpa.h
+++ include/linux/vdpa.h
@@ -42,6 +42,7 @@ struct vdpa_vq_state {
  * @config: the configuration ops for this device.
  * @index: device index
  * @features_valid: were features initialized? for legacy guests
+ * @nvqs: the number of virtqueues
  */
 struct vdpa_device {
 	struct device dev;
