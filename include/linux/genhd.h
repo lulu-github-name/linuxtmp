e83f31ce615ae085bd0af126726432b09e6b3c4a
Ming Lei <ming.lei@redhat.com>
2020-09-29 12:16:53 -0400

    [block] block: remove rcu_read_lock() from part_stat_lock()
    Message-id: <20200929121836.3196117-177-ming.lei@redhat.com>
    Patchwork-id: 328845
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 V2 176/279] block: remove rcu_read_lock() from part_stat_lock()
    Bugzilla: 1881760
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Ming <ming.lei@redhat.com>
    
    BZ: 1881760
    
    commit 8ab1d40a646e753adb6814642432a093d93dbf47
    Author: Konstantin Khlebnikov <koct9i@gmail.com>
    Date:   Wed May 27 07:24:17 2020 +0200
    
        block: remove rcu_read_lock() from part_stat_lock()
    
        The RCU lock is required only in disk_map_sector_rcu() to lookup the
        partition.  After that request holds reference to related hd_struct.
    
        Replace get_cpu() with preempt_disable() - returned cpu index is unused.
    
        [hch: rebased]
    
        Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/linux/genhd.h
+++ include/linux/genhd.h
@@ -321,8 +321,8 @@ bool disk_has_partitions(struct gendisk *disk);
  * internal use only.
  */
 #ifdef	CONFIG_SMP
-#define part_stat_lock()	({ rcu_read_lock(); get_cpu(); })
-#define part_stat_unlock()	do { put_cpu(); rcu_read_unlock(); } while (0)
+#define part_stat_lock()	preempt_disable()
+#define part_stat_unlock()	preempt_enable()
 
 #define part_stat_get_cpu(part, field, cpu)					\
 	(per_cpu_ptr((part)->dkstats, (cpu))->field)
