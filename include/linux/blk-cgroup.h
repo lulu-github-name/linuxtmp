86ecea7f5a61c694bf6e42c2997774ece2a379f0
Ming Lei <ming.lei@redhat.com>
2020-09-29 12:15:27 -0400

    [block] blkcg: rename blkcg->cgwb_refcnt to ->online_pin and always use it
    Message-id: <20200929121836.3196117-91-ming.lei@redhat.com>
    Patchwork-id: 328685
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 V2 090/279] blkcg: rename blkcg->cgwb_refcnt to ->online_pin and always use it
    Bugzilla: 1881760
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Ming <ming.lei@redhat.com>
    
    BZ: 1881760
    Conflicts: don't remove blkcg->cgwb_refcnt for avoiding to break kabi
    
    commit d866dbf6178713e37d2fec2870af00b345684e1a
    Author: Tejun Heo <tj@kernel.org>
    Date:   Wed Jul 24 10:37:22 2019 -0700
    
        blkcg: rename blkcg->cgwb_refcnt to ->online_pin and always use it
    
        blkcg->cgwb_refcnt is used to delay blkcg offlining so that blkgs
        don't get offlined while there are active cgwbs on them.  However, it
        ends up making offlining unordered sometimes causing parents to be
        offlined before children.
    
        To fix it, we want child blkcgs to pin the parents' online states
        turning the refcnt into a more generic online pinning mechanism.
    
        In prepartion,
    
        * blkcg->cgwb_refcnt -> blkcg->online_pin
        * blkcg_cgwb_get/put() -> blkcg_pin/unpin_online()
        * Take them out of CONFIG_CGROUP_WRITEBACK
    
        Signed-off-by: Tejun Heo <tj@kernel.org>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/linux/blk-cgroup.h
+++ include/linux/blk-cgroup.h
@@ -87,7 +87,7 @@ struct blkcg {
 	struct list_head		cgwb_list;
 	refcount_t			cgwb_refcnt;
 #endif
-	RH_KABI_RESERVE(1)
+	RH_KABI_USE(1, refcount_t			online_pin)
 	RH_KABI_RESERVE(2)
 	RH_KABI_RESERVE(3)
 	RH_KABI_RESERVE(4)
@@ -452,46 +452,33 @@ static inline struct blkcg *cpd_to_blkcg(struct blkcg_policy_data *cpd)
 
 extern void blkcg_destroy_blkgs(struct blkcg *blkcg);
 
-#ifdef CONFIG_CGROUP_WRITEBACK
-
 /**
- * blkcg_cgwb_get - get a reference for blkcg->cgwb_list
+ * blkcg_pin_online - pin online state
  * @blkcg: blkcg of interest
  *
- * This is used to track the number of active wb's related to a blkcg.
+ * While pinned, a blkcg is kept online.  This is primarily used to
+ * impedance-match blkg and cgwb lifetimes so that blkg doesn't go offline
+ * while an associated cgwb is still active.
  */
-static inline void blkcg_cgwb_get(struct blkcg *blkcg)
+static inline void blkcg_pin_online(struct blkcg *blkcg)
 {
-	refcount_inc(&blkcg->cgwb_refcnt);
+	refcount_inc(&blkcg->online_pin);
 }
 
 /**
- * blkcg_cgwb_put - put a reference for @blkcg->cgwb_list
+ * blkcg_unpin_online - unpin online state
  * @blkcg: blkcg of interest
  *
- * This is used to track the number of active wb's related to a blkcg.
- * When this count goes to zero, all active wb has finished so the
+ * This is primarily used to impedance-match blkg and cgwb lifetimes so
+ * that blkg doesn't go offline while an associated cgwb is still active.
+ * When this count goes to zero, all active cgwbs have finished so the
  * blkcg can continue destruction by calling blkcg_destroy_blkgs().
- * This work may occur in cgwb_release_workfn() on the cgwb_release
- * workqueue.
  */
-static inline void blkcg_cgwb_put(struct blkcg *blkcg)
+static inline void blkcg_unpin_online(struct blkcg *blkcg)
 {
-	if (refcount_dec_and_test(&blkcg->cgwb_refcnt))
+	if (refcount_dec_and_test(&blkcg->online_pin))
 		blkcg_destroy_blkgs(blkcg);
 }
-
-#else
-
-static inline void blkcg_cgwb_get(struct blkcg *blkcg) { }
-
-static inline void blkcg_cgwb_put(struct blkcg *blkcg)
-{
-	/* wb isn't being accounted, so trigger destruction right away */
-	blkcg_destroy_blkgs(blkcg);
-}
-
-#endif
 
 /**
  * blkg_path - format cgroup path of blkg
