ab510018e378375ddab02f61c1903af2958e2f5a
Ming Lei <ming.lei@redhat.com>
2020-09-29 12:15:01 -0400

    [block] block/diskstats: replace time_in_queue with sum of request times
    Message-id: <20200929121836.3196117-65-ming.lei@redhat.com>
    Patchwork-id: 328659
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 V2 064/279] block/diskstats: replace time_in_queue with sum of request times
    Bugzilla: 1881760
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Ming <ming.lei@redhat.com>
    
    BZ: 1881760
    Conflicts: use rh helper to remove field from disk_stats; remove
    STAT_FLUSH because we don't port that.
    
    commit 8cd5b8fc00716fb71f6b32d594b38a8f286d6c20
    Author: Konstantin Khlebnikov <koct9i@gmail.com>
    Date:   Wed Mar 25 16:07:08 2020 +0300
    
        block/diskstats: replace time_in_queue with sum of request times
    
        Column "time_in_queue" in diskstats is supposed to show total waiting time
        of all requests. I.e. value should be equal to the sum of times from other
        columns. But this is not true, because column "time_in_queue" is counted
        separately in jiffies rather than in nanoseconds as other times.
    
        This patch removes redundant counter for "time_in_queue" and shows total
        time of read, write, discard and flush requests.
    
        Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- block/genhd.c
+++ block/genhd.c
@@ -110,7 +110,6 @@ static void part_stat_read_all(struct hd_struct *part, struct disk_stats *stat)
 		}
 
 		stat->io_ticks += ptr->io_ticks;
-		stat->time_in_queue += ptr->time_in_queue;
 	}
 }
 #else /* CONFIG_SMP */
@@ -1334,7 +1333,10 @@ ssize_t part_stat_show(struct device *dev,
 		(unsigned int)div_u64(stat.nsecs[STAT_WRITE], NSEC_PER_MSEC),
 		inflight,
 		jiffies_to_msecs(stat.io_ticks),
-		jiffies_to_msecs(stat.time_in_queue),
+		(unsigned int)div_u64(stat.nsecs[STAT_READ] +
+				      stat.nsecs[STAT_WRITE] +
+				      stat.nsecs[STAT_DISCARD],
+						NSEC_PER_MSEC),
 		stat.ios[STAT_DISCARD],
 		stat.merges[STAT_DISCARD],
 		(unsigned long long)stat.sectors[STAT_DISCARD],
@@ -1645,7 +1647,10 @@ static int diskstats_show(struct seq_file *seqf, void *v)
 							NSEC_PER_MSEC),
 			   inflight,
 			   jiffies_to_msecs(stat.io_ticks),
-			   jiffies_to_msecs(stat.time_in_queue),
+			   (unsigned int)div_u64(stat.nsecs[STAT_READ] +
+						 stat.nsecs[STAT_WRITE] +
+						 stat.nsecs[STAT_DISCARD],
+							NSEC_PER_MSEC),
 			   stat.ios[STAT_DISCARD],
 			   stat.merges[STAT_DISCARD],
 			   stat.sectors[STAT_DISCARD],
