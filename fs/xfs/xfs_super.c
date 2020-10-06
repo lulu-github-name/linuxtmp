b55db5319c78a37d0aa5913b6e4b16f5edd94fe6
Carlos Maiolino <cmaiolino@redhat.com>
2020-10-06 11:49:20 -0400

    [fs] xfs: rename the XFS_MOUNT_DFLT_IOSIZE option to
    Message-id: <20201006115225.398580-60-cmaiolino@redhat.com>
    Patchwork-id: 329826
    Patchwork-instance: patchwork
    O-Subject: [RHEL8 fs PATCH 059/244] xfs: rename the XFS_MOUNT_DFLT_IOSIZE option to
    Bugzilla: 1859160
    RH-Acked-by: Bill O'Donnell <billodo@redhat.com>
    RH-Acked-by: Eric Sandeen <esandeen@redhat.com>
    RH-Acked-by: Brian Foster <bfoster@redhat.com>
    
    Make the flag match the mount option and usage.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    (cherry picked from commit 3274d00801007cccab8aec7f2ac50f6bc10d1692)
    
     Conflicts:
    	fs/xfs/xfs_super.c
    
    	Small conflict caused by the modification in the previous patch
    	to keep allocsize=1 valid
    
    Signed-off-by: Bill O'Donnell <billodo@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -236,7 +236,7 @@ xfs_parseargs(
 			if (size == 1)
 				break;
 			mp->m_allocsize_log = ffs(size) - 1;
-			mp->m_flags |= XFS_MOUNT_DFLT_IOSIZE;
+			mp->m_flags |= XFS_MOUNT_ALLOCSIZE;
 			break;
 		case Opt_grpid:
 		case Opt_bsdgroups:
@@ -404,7 +404,7 @@ done:
 		return -EINVAL;
 	}
 
-	if ((mp->m_flags & XFS_MOUNT_DFLT_IOSIZE) &&
+	if ((mp->m_flags & XFS_MOUNT_ALLOCSIZE) &&
 	    (mp->m_allocsize_log > XFS_MAX_IO_LOG ||
 	     mp->m_allocsize_log < XFS_MIN_IO_LOG)) {
 		xfs_warn(mp, "invalid log iosize: %d [not %d-%d]",
@@ -458,7 +458,7 @@ xfs_showargs(
 			seq_puts(m, xfs_infop->str);
 	}
 
-	if (mp->m_flags & XFS_MOUNT_DFLT_IOSIZE)
+	if (mp->m_flags & XFS_MOUNT_ALLOCSIZE)
 		seq_printf(m, ",allocsize=%dk",
 				(int)(1 << mp->m_allocsize_log) >> 10);
 
