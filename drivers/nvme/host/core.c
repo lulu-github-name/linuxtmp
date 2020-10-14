553f1962c9d94ddf747bdf5d0af15b536d489477
Gopal Tiwari <gtiwari@redhat.com>
2020-10-14 11:59:57 -0400

    [nvme] nvme: introduce nvme_execute_passthru_rq to call nvme_passthru_()
    Message-id: <20201014120050.20873-195-gtiwari@redhat.com>
    Patchwork-id: 331378
    Patchwork-instance: patchwork
    O-Subject: [RHEL-8.4 V2 PATCH 194/247] nvme: introduce nvme_execute_passthru_rq to call nvme_passthru_[start|end]()
    Bugzilla: 1857052 1857051 1853231
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1793190
    Bugzilla: http://bugzilla.redhat.com/1857052
    Bugzilla: http://bugzilla.redhat.com/1853231
    Bugzilla: http://bugzilla.redhat.com/1857051
    
    commit 17365ae6975c7f7494a2d1cd0bf18b5ed238e072
    Author: Logan Gunthorpe <logang@deltatee.com>
    Date:   Fri Jul 24 11:25:14 2020 -0600
    
        nvme: introduce nvme_execute_passthru_rq to call nvme_passthru_[start|end]()
    
        Introduce a new nvme_execute_passthru_rq() helper which calls
        nvme_passthru_[start|end]() around blk_execute_rq(). This ensures
        all passthru calls (including nvme_submit_io()) will be wrapped
        appropriately.
    
        nvme_execute_passthru_rq() will also be useful for the nvmet passthru
        code and is exported in the NVME_TARGET_PASSTHRU namespace.
    
        Signed-off-by: Logan Gunthorpe <logang@deltatee.com>
        Reviewed-by: Keith Busch <kbusch@kernel.org>
        Signed-off-by: Christoph Hellwig <hch@lst.de>
    
    Conflicts : RHEL-8 doesn't support EXPORT_SYMBOL_NS_GPL symbols.
    Signed-off-by: Gopal Tiwari <gtiwari@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -1059,6 +1059,20 @@ static void nvme_passthru_end(struct nvme_ctrl *ctrl, u32 effects)
 	}
 }
 
+void nvme_execute_passthru_rq(struct request *rq)
+{
+	struct nvme_command *cmd = nvme_req(rq)->cmd;
+	struct nvme_ctrl *ctrl = nvme_req(rq)->ctrl;
+	struct nvme_ns *ns = rq->q->queuedata;
+	struct gendisk *disk = ns ? ns->disk : NULL;
+	u32 effects;
+
+	effects = nvme_passthru_start(ctrl, ns, cmd->common.opcode);
+	blk_execute_rq(rq->q, disk, rq, 0);
+	nvme_passthru_end(ctrl, effects);
+}
+EXPORT_SYMBOL_GPL(nvme_execute_passthru_rq);
+
 static int nvme_submit_user_cmd(struct request_queue *q,
 		struct nvme_command *cmd, void __user *ubuffer,
 		unsigned bufflen, void __user *meta_buffer, unsigned meta_len,
@@ -1097,7 +1111,7 @@ static int nvme_submit_user_cmd(struct request_queue *q,
 		}
 	}
 
-	blk_execute_rq(req->q, disk, req, 0);
+	nvme_execute_passthru_rq(req);
 	if (nvme_req(req)->flags & NVME_REQ_CANCELLED)
 		ret = -EINTR;
 	else
@@ -1522,7 +1536,6 @@ static int nvme_user_cmd(struct nvme_ctrl *ctrl, struct nvme_ns *ns,
 	struct nvme_passthru_cmd cmd;
 	struct nvme_command c;
 	unsigned timeout = 0;
-	u32 effects;
 	u64 result;
 	int status;
 
@@ -1549,12 +1562,10 @@ static int nvme_user_cmd(struct nvme_ctrl *ctrl, struct nvme_ns *ns,
 	if (cmd.timeout_ms)
 		timeout = msecs_to_jiffies(cmd.timeout_ms);
 
-	effects = nvme_passthru_start(ctrl, ns, cmd.opcode);
 	status = nvme_submit_user_cmd(ns ? ns->queue : ctrl->admin_q, &c,
 			nvme_to_user_ptr(cmd.addr), cmd.data_len,
 			nvme_to_user_ptr(cmd.metadata), cmd.metadata_len,
 			0, &result, timeout);
-	nvme_passthru_end(ctrl, effects);
 
 	if (status >= 0) {
 		if (put_user(result, &ucmd->result))
@@ -1570,7 +1581,6 @@ static int nvme_user_cmd64(struct nvme_ctrl *ctrl, struct nvme_ns *ns,
 	struct nvme_passthru_cmd64 cmd;
 	struct nvme_command c;
 	unsigned timeout = 0;
-	u32 effects;
 	int status;
 
 	if (!capable(CAP_SYS_ADMIN))
@@ -1596,12 +1606,10 @@ static int nvme_user_cmd64(struct nvme_ctrl *ctrl, struct nvme_ns *ns,
 	if (cmd.timeout_ms)
 		timeout = msecs_to_jiffies(cmd.timeout_ms);
 
-	effects = nvme_passthru_start(ctrl, ns, cmd.opcode);
 	status = nvme_submit_user_cmd(ns ? ns->queue : ctrl->admin_q, &c,
 			nvme_to_user_ptr(cmd.addr), cmd.data_len,
 			nvme_to_user_ptr(cmd.metadata), cmd.metadata_len,
 			0, &cmd.result, timeout);
-	nvme_passthru_end(ctrl, effects);
 
 	if (status >= 0) {
 		if (put_user(cmd.result, &ucmd->result))
