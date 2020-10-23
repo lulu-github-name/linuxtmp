687be360b9fa3289f6b35ca4b7e28fbfb5445a1d
Gustavo Duarte <gduarte@redhat.com>
2020-10-23 19:58:12 -0400

    [powerpc] powerpc/security: Fix link stack flush instruction
    Message-id: <20201023195812.3155763-9-gduarte@redhat.com>
    Patchwork-id: 335086
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 PATCH BZ 1862103 8/8] powerpc/security: Fix link stack flush instruction
    Bugzilla: 1862103
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    
    BZ 1862103
    Upstream Status: 792254a77201453d9a77479e63dc216ad90462d2
    
    commit 792254a77201453d9a77479e63dc216ad90462d2
    Author: Nicholas Piggin <npiggin@gmail.com>
    Date:   Wed Oct 7 18:06:05 2020 +1000
    
        powerpc/security: Fix link stack flush instruction
    
        The inline execution path for the hardware assisted branch flush
        instruction failed to set CTR to the correct value before bcctr,
        causing a crash when the feature is enabled.
    
        Fixes: 4d24e21cc694 ("powerpc/security: Allow for processors that flush the link stack using the special bcctr")
        Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
        Link: https://lore.kernel.org/r/20201007080605.64423-1-npiggin@gmail.com
    Signed-off-by: Gustavo Luiz Duarte <gduarte@redhat.com>
    
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- arch/powerpc/include/asm/asm-prototypes.h
+++ arch/powerpc/include/asm/asm-prototypes.h
@@ -174,7 +174,9 @@ long kvmppc_h_set_xdabr(struct kvm_vcpu *vcpu, unsigned long dabr,
 			unsigned long dabrx);
 
 /* Patch sites */
-extern s32 patch__call_flush_branch_caches;
+extern s32 patch__call_flush_branch_caches1;
+extern s32 patch__call_flush_branch_caches2;
+extern s32 patch__call_flush_branch_caches3;
 extern s32 patch__flush_count_cache_return;
 extern s32 patch__flush_link_stack_return;
 extern s32 patch__call_kvm_flush_link_stack;
