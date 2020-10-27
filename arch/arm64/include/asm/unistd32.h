1976ecc3cc97bfe9c8b198be55d53c9aa1966df4
Waiman Long <longman@redhat.com>
2020-10-27 00:29:15 -0400

    [arm64] y2038: add 64-bit time_t syscalls to all 32-bit architectures
    Message-id: <20201027003220.16553-29-longman@redhat.com>
    Patchwork-id: 335399
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 PATCH v2 028/213] y2038: add 64-bit time_t syscalls to all 32-bit architectures
    Bugzilla: 1548297
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1548297
    Conflicts: Only hunks in generic code and supported architectures
    	   are merged. There are also merge conflicts in the following
    	   files that require manual merge:
    	    - arch/arm64/include/asm/unistd32.h
    	    - arch/s390/kernel/syscalls/syscall.tbl
    	    - arch/x86/entry/syscalls/syscall_32.tbl
    	    - include/uapi/asm-generic/unistd.h
    	   The following compat syscalls are also dropped as they are
    	   not currently implemented in RHEL8.
    	    - compat_sys_pselect6_time64
    	    - compat_sys_ppoll_time64
    	    - compat_sys_recvmmsg_time64
    	    - compat_sys_rt_sigtimedwait_time64
    
    commit 48166e6ea47d23984f0b481ca199250e1ce0730a
    Author: Arnd Bergmann <arnd@arndb.de>
    Date:   Thu, 10 Jan 2019 12:45:11 +0100
    
        y2038: add 64-bit time_t syscalls to all 32-bit architectures
    
        This adds 21 new system calls on each ABI that has 32-bit time_t
        today. All of these have the exact same semantics as their existing
        counterparts, and the new ones all have macro names that end in 'time64'
        for clarification.
    
        This gets us to the point of being able to safely use a C library
        that has 64-bit time_t in user space. There are still a couple of
        loose ends to tie up in various areas of the code, but this is the
        big one, and should be entirely uncontroversial at this point.
    
        In particular, there are four system calls (getitimer, setitimer,
        waitid, and getrusage) that don't have a 64-bit counterpart yet,
        but these can all be safely implemented in the C library by wrapping
        around the existing system calls because the 32-bit time_t they
        pass only counts elapsed time, not time since the epoch. They
        will be dealt with later.
    
        Signed-off-by: Arnd Bergmann <arnd@arndb.de>
        Acked-by: Heiko Carstens <heiko.carstens@de.ibm.com>
        Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
        Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -819,6 +819,39 @@ __SYSCALL(__NR_pkey_free, sys_pkey_free)
 __SYSCALL(__NR_statx, sys_statx)
 #define __NR_rseq 398
 __SYSCALL(__NR_rseq, sys_rseq)
+/* 402 is unused */
+#define __NR_clock_gettime64 403
+__SYSCALL(__NR_clock_gettime64, sys_clock_gettime)
+#define __NR_clock_settime64 404
+__SYSCALL(__NR_clock_settime64, sys_clock_settime)
+#define __NR_clock_adjtime64 405
+__SYSCALL(__NR_clock_adjtime64, sys_clock_adjtime)
+#define __NR_clock_getres_time64 406
+__SYSCALL(__NR_clock_getres_time64, sys_clock_getres)
+#define __NR_clock_nanosleep_time64 407
+__SYSCALL(__NR_clock_nanosleep_time64, sys_clock_nanosleep)
+#define __NR_timer_gettime64 408
+__SYSCALL(__NR_timer_gettime64, sys_timer_gettime)
+#define __NR_timer_settime64 409
+__SYSCALL(__NR_timer_settime64, sys_timer_settime)
+#define __NR_timerfd_gettime64 410
+__SYSCALL(__NR_timerfd_gettime64, sys_timerfd_gettime)
+#define __NR_timerfd_settime64 411
+__SYSCALL(__NR_timerfd_settime64, sys_timerfd_settime)
+#define __NR_utimensat_time64 412
+__SYSCALL(__NR_utimensat_time64, sys_utimensat)
+#define __NR_io_pgetevents_time64 416
+__SYSCALL(__NR_io_pgetevents_time64, sys_io_pgetevents)
+#define __NR_mq_timedsend_time64 418
+__SYSCALL(__NR_mq_timedsend_time64, sys_mq_timedsend)
+#define __NR_mq_timedreceive_time64 419
+__SYSCALL(__NR_mq_timedreceive_time64, sys_mq_timedreceive)
+#define __NR_semtimedop_time64 420
+__SYSCALL(__NR_semtimedop_time64, sys_semtimedop)
+#define __NR_futex_time64 422
+__SYSCALL(__NR_futex_time64, sys_futex)
+#define __NR_sched_rr_get_interval_time64 423
+__SYSCALL(__NR_sched_rr_get_interval_time64, sys_sched_rr_get_interval)
 #define __NR_io_uring_setup 425
 __SYSCALL(__NR_io_uring_setup, sys_io_uring_setup)
 #define __NR_io_uring_enter 426
