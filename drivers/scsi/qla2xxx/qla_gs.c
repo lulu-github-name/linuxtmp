6396f739ae7d7cae8318b0e717a6569aec1f9aaf
Nilesh Javali <njavali@redhat.com>
2020-10-29 13:07:28 -0400

    [scsi] scsi: qla2xxx: Reduce duplicate code in reporting speed
    Message-id: <20201029130752.15040-90-njavali@redhat.com>
    Patchwork-id: 336085
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 v2 BZ 1887427 089/113] scsi: qla2xxx: Reduce duplicate code in reporting speed
    Bugzilla: 1887427
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1887427
    
    Tree: git://git.kernel.org/pub/scm/linux/kernel/git/mkp/scsi.git
    
    Note: Resolved conflict due to difference in code base from upstream
    
    commit d68930bae477e864e016aeb8d557f099b4636aac
    Author: Quinn Tran <qutran@marvell.com>
    Date:   Thu Sep 3 21:51:20 2020 -0700
    
        scsi: qla2xxx: Reduce duplicate code in reporting speed
    
        Indicate correct speed for 16G Mezz card.
    
        Link: https://lore.kernel.org/r/20200904045128.23631-6-njavali@marvell.com
        Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
        Signed-off-by: Quinn Tran <qutran@marvell.com>
        Signed-off-by: Nilesh Javali <njavali@marvell.com>
        Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Cc: Nilesh Javali <njavali@redhat.com>
    Cc: Arun Easi <aeasi@redhat.com>
    Cc: "Ewan D. Milne" <emilne@redhat.com>
    Signed-off-by: Nilesh Javali <njavali@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/scsi/qla2xxx/qla_gs.c
+++ drivers/scsi/qla2xxx/qla_gs.c
@@ -1502,7 +1502,7 @@ qla2x00_prep_ct_fdmi_req(struct ct_sns_pkt *p, uint16_t cmd,
 	return &p->p.req;
 }
 
-static uint
+uint
 qla25xx_fdmi_port_speed_capability(struct qla_hw_data *ha)
 {
 	if (IS_CNA_CAPABLE(ha))
@@ -1539,7 +1539,7 @@ qla25xx_fdmi_port_speed_capability(struct qla_hw_data *ha)
 	if (IS_QLA2031(ha))
 		return FDMI_PORT_SPEED_16GB|FDMI_PORT_SPEED_8GB|
 			FDMI_PORT_SPEED_4GB;
-	if (IS_QLA25XX(ha))
+	if (IS_QLA25XX(ha) || IS_QLAFX00(ha))
 		return FDMI_PORT_SPEED_8GB|FDMI_PORT_SPEED_4GB|
 			FDMI_PORT_SPEED_2GB|FDMI_PORT_SPEED_1GB;
 	if (IS_QLA24XX_TYPE(ha))
@@ -1549,7 +1549,8 @@ qla25xx_fdmi_port_speed_capability(struct qla_hw_data *ha)
 		return FDMI_PORT_SPEED_2GB|FDMI_PORT_SPEED_1GB;
 	return FDMI_PORT_SPEED_1GB;
 }
-static uint
+
+uint
 qla25xx_fdmi_port_speed_currently(struct qla_hw_data *ha)
 {
 	switch (ha->link_data_rate) {
