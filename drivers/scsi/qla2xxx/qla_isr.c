c6396de8f16ed32acae2ed74a7d80be7065a4b31
Nilesh Javali <njavali@redhat.com>
2020-10-29 13:07:20 -0400

    [scsi] scsi: qla2xxx: Simplify return value logic in qla2x00_get_sp_from_handle()
    Message-id: <20201029130752.15040-82-njavali@redhat.com>
    Patchwork-id: 336062
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 v2 BZ 1887427 081/113] scsi: qla2xxx: Simplify return value logic in qla2x00_get_sp_from_handle()
    Bugzilla: 1887427
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1887427
    
    Tree: git://git.kernel.org/pub/scm/linux/kernel/git/mkp/scsi.git
    
    commit 622299f16f33852baddf94b32bc80e2c1e24c39a
    Author: Daniel Wagner <dwagner@suse.de>
    Date:   Tue Sep 8 10:15:14 2020 +0200
    
        scsi: qla2xxx: Simplify return value logic in qla2x00_get_sp_from_handle()
    
        Refactor qla2x00_get_sp_from_handle() to avoid the unnecessary goto if
        early returns are used. With this we can also avoid preinitialzing the sp
        pointer.
    
        Link: https://lore.kernel.org/r/20200908081516.8561-3-dwagner@suse.de
        Reviewed-by: Martin Wilck <mwilck@suse.com>
        Reviewed-by: Arun Easi <aeasi@marvell.com>
        Signed-off-by: Daniel Wagner <dwagner@suse.de>
        Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Cc: Nilesh Javali <njavali@redhat.com>
    Cc: Arun Easi <aeasi@redhat.com>
    Cc: "Ewan D. Milne" <emilne@redhat.com>
    Signed-off-by: Nilesh Javali <njavali@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/scsi/qla2xxx/qla_isr.c
+++ drivers/scsi/qla2xxx/qla_isr.c
@@ -1715,7 +1715,7 @@ qla2x00_get_sp_from_handle(scsi_qla_host_t *vha, const char *func,
 {
 	struct qla_hw_data *ha = vha->hw;
 	sts_entry_t *pkt = iocb;
-	srb_t *sp = NULL;
+	srb_t *sp;
 	uint16_t index;
 
 	index = LSW(pkt->handle);
@@ -1727,13 +1727,13 @@ qla2x00_get_sp_from_handle(scsi_qla_host_t *vha, const char *func,
 			set_bit(FCOE_CTX_RESET_NEEDED, &vha->dpc_flags);
 		else
 			set_bit(ISP_ABORT_NEEDED, &vha->dpc_flags);
-		goto done;
+		return NULL;
 	}
 	sp = req->outstanding_cmds[index];
 	if (!sp) {
 		ql_log(ql_log_warn, vha, 0x5032,
 		    "Invalid completion handle (%x) -- timed-out.\n", index);
-		return sp;
+		return NULL;
 	}
 	if (sp->handle != index) {
 		ql_log(ql_log_warn, vha, 0x5033,
@@ -1742,8 +1742,6 @@ qla2x00_get_sp_from_handle(scsi_qla_host_t *vha, const char *func,
 	}
 
 	req->outstanding_cmds[index] = NULL;
-
-done:
 	return sp;
 }
 
