92499e76dd0aeb872070d2dcaedc8bda446fdcad
Eric Sandeen <sandeen@redhat.com>
2020-10-29 17:38:39 -0400

    [fs] vfs: create a generic checking function for FS_IOC_FSSETXATTR
    Message-id: <20201029173930.11735-4-sandeen@redhat.com>
    Patchwork-id: 336286
    Patchwork-instance: patchwork
    O-Subject: [RHEL8 PATCH V2 03/54] vfs: create a generic checking function for FS_IOC_FSSETXATTR
    Bugzilla: 1838876 1854346 1838344
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    
    From: "Darrick J. Wong" <darrick.wong@oracle.com>
    
    Create a generic checking function for the incoming FS_IOC_FSSETXATTR
    fsxattr values so that we can standardize some of the implementation
    behaviors.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    (cherry picked from commit 7b0e492e6b80d51db4156996b248522c7b50d467)
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3572,4 +3572,13 @@ static inline int inode_drain_writes(struct inode *inode)
 int vfs_ioc_setflags_prepare(struct inode *inode, unsigned int oldflags,
 			     unsigned int flags);
 
+int vfs_ioc_fssetxattr_check(struct inode *inode, const struct fsxattr *old_fa,
+			     struct fsxattr *fa);
+
+static inline void simple_fill_fsxattr(struct fsxattr *fa, __u32 xflags)
+{
+	memset(fa, 0, sizeof(*fa));
+	fa->fsx_xflags = xflags;
+}
+
 #endif /* _LINUX_FS_H */
