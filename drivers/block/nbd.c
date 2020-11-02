8f00287c7271c77540e6871a453a5c1ecacabe4d
Ming Lei <ming.lei@redhat.com>
2020-11-02 04:19:10 -0500

    [block] nbd: don't update block size after device is started
    Message-id: <20201102041910.2131756-1-ming.lei@redhat.com>
    Patchwork-id: 336429
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4] nbd: don't update block size after device is started
    Bugzilla: 1891363
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    BZ: 1891363
    BREW: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=32714122
    
    commit b40813ddcd6bf9f01d020804e4cb8febc480b9e4
    Author: Ming Lei <ming.lei@redhat.com>
    Date:   Wed Oct 28 15:24:34 2020 +0800
    
        nbd: don't update block size after device is started
    
        Mounted NBD device can be resized, one use case is rbd-nbd.
    
        Fix the issue by setting up default block size, then not touch it
        in nbd_size_update() any more. This kind of usage is aligned with loop
        which has same use case too.
    
        Cc: stable@vger.kernel.org
        Fixes: c8a83a6b54d0 ("nbd: Use set_blocksize() to set device blocksize")
        Reported-by: lining <lining2020x@163.com>
        Signed-off-by: Ming Lei <ming.lei@redhat.com>
        Cc: Josef Bacik <josef@toxicpanda.com>
        Cc: Jan Kara <jack@suse.cz>
        Tested-by: lining <lining2020x@163.com>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming <ming.lei@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/block/nbd.c
+++ drivers/block/nbd.c
@@ -294,7 +294,7 @@ static void nbd_size_clear(struct nbd_device *nbd)
 	}
 }
 
-static void nbd_size_update(struct nbd_device *nbd)
+static void nbd_size_update(struct nbd_device *nbd, bool start)
 {
 	struct nbd_config *config = nbd->config;
 	struct block_device *bdev = bdget_disk(nbd->disk, 0);
@@ -310,7 +310,8 @@ static void nbd_size_update(struct nbd_device *nbd)
 	if (bdev) {
 		if (bdev->bd_disk) {
 			bd_set_size(bdev, config->bytesize);
-			set_blocksize(bdev, config->blksize);
+			if (start)
+				set_blocksize(bdev, config->blksize);
 		} else
 			bdev->bd_invalidated = 1;
 		bdput(bdev);
@@ -325,7 +326,7 @@ static void nbd_size_set(struct nbd_device *nbd, loff_t blocksize,
 	config->blksize = blocksize;
 	config->bytesize = blocksize * nr_blocks;
 	if (nbd->task_recv != NULL)
-		nbd_size_update(nbd);
+		nbd_size_update(nbd, false);
 }
 
 static void nbd_complete_rq(struct request *req)
@@ -1320,7 +1321,7 @@ static int nbd_start_device(struct nbd_device *nbd)
 		args->index = i;
 		queue_work(nbd->recv_workq, &args->work);
 	}
-	nbd_size_update(nbd);
+	nbd_size_update(nbd, true);
 	return error;
 }
 
