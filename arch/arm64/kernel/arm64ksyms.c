1d8f6bbdc2d7ce3afdc545718f99973ae0dd7f73
Mark Salter <msalter@redhat.com>
2020-11-03 19:58:35 -0500

    [arm64] arm64: tishift: use asm EXPORT_SYMBOL()
    Message-id: <20201103195841.355634-11-msalter@redhat.com>
    Patchwork-id: 337321
    Patchwork-instance: patchwork
    O-Subject: [RHEL 8.4 BZ1883365 PATCH 10/16] arm64: tishift: use asm EXPORT_SYMBOL()
    Bugzilla: 1883365
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    commit abb77f3d96407cf30c2068f4a23a14b9540a1c1f
    From: Mark Rutland <mark.rutland@arm.com>
    
    For a while now it's been possible to use EXPORT_SYMBOL() in assembly
    files, which allows us to place exports immediately after assembly
    functions, as we do for C functions.
    
    As a step towards removing arm64ksyms.c, let's move the tishift exports
    to the assembly file the functions are defined in.
    
    There should be no functional change as a result of this patch.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Mark Salter <msalter@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>
    
    Conflicts:
    	arch/arm64/kernel/arm64ksyms.c
    	Context diff caused by previous backport. Move
    	export of kimage_vaddr to asm file where it
    	belongs.

--- arch/arm64/kernel/arm64ksyms.c
+++ arch/arm64/kernel/arm64ksyms.c
@@ -68,12 +68,3 @@ NOKPROBE_SYMBOL(_mcount);
 EXPORT_SYMBOL(__arm_smccc_smc);
 EXPORT_SYMBOL(__arm_smccc_hvc);
 
-	/* tishift.S */
-extern long long __ashlti3(long long a, int b);
-EXPORT_SYMBOL(__ashlti3);
-extern long long __ashrti3(long long a, int b);
-EXPORT_SYMBOL(__ashrti3);
-extern long long __lshrti3(long long a, int b);
-EXPORT_SYMBOL(__lshrti3);
-
-EXPORT_SYMBOL(kimage_vaddr);
