3a537bb869e2b3056fbe69594173981868cdb31c
Petr Oros <poros@redhat.com>
2020-11-24 12:01:29 -0500

    [netdrv] net: ena: use SHUTDOWN as reset reason when closing interface
    Message-id: <489d2c621e68ddb3ceb3db9f13c03f0e6a423bdd.1606203455.git.poros@redhat.com>
    Patchwork-id: 341601
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 PATCH 47/78] net: ena: use SHUTDOWN as reset reason when closing interface
    Bugzilla: 1861967
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1861967
    
    Upstream commit(s):
    commit c1c0e40b367efc92ad05bb802180817534a32343
    Author: Sameeh Jubran <sameehj@amazon.com>
    Date:   Sun May 3 09:52:19 2020 +0000
    
        net: ena: use SHUTDOWN as reset reason when closing interface
    
        The 'ENA_REGS_RESET_SHUTDOWN' enum indicates a normal driver
        shutdown / removal procedure.
    
        Also, a comment is added to one of the reset reason assignments for
        code clarity.
    
        Signed-off-by: Shay Agroskin <shayagr@amazon.com>
        Signed-off-by: Arthur Kiyanovski <akiyano@amazon.com>
        Signed-off-by: Sameeh Jubran <sameehj@amazon.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Petr Oros <poros@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/net/ethernet/amazon/ena/ena_netdev.c
+++ drivers/net/ethernet/amazon/ena/ena_netdev.c
@@ -3441,6 +3441,7 @@ static void ena_destroy_device(struct ena_adapter *adapter, bool graceful)
 
 	ena_com_mmio_reg_read_request_destroy(ena_dev);
 
+	/* return reset reason to default value */
 	adapter->reset_reason = ENA_REGS_RESET_NORMAL;
 
 	clear_bit(ENA_FLAG_TRIGGER_RESET, &adapter->flags);
@@ -4364,6 +4365,7 @@ static void __ena_shutoff(struct pci_dev *pdev, bool shutdown)
 	cancel_work_sync(&adapter->reset_task);
 
 	rtnl_lock(); /* lock released inside the below if-else block */
+	adapter->reset_reason = ENA_REGS_RESET_SHUTDOWN;
 	ena_destroy_device(adapter, true);
 	if (shutdown) {
 		netif_device_detach(netdev);
