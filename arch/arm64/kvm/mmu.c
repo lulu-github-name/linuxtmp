a527b799a626de47e01216faa93dd17cb5ce7d6f
Auger Eric <eric.auger@redhat.com>
2020-11-25 13:41:16 -0500

    [arm64] kvm: arm64: Drop @max param from mmu_topup_memory_cache()
    Message-id: <20201125134123.18590-85-eric.auger@redhat.com>
    Patchwork-id: 341798
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 virt BZ1882794 PATCH v2 84/91] KVM: arm64: Drop @max param from mmu_topup_memory_cache()
    Bugzilla: 1882794
    RH-Acked-by: Andrew Jones <drjones@redhat.com>
    RH-Acked-by: Gavin Shan <gshan@redhat.com>
    
    From: Sean Christopherson <sean.j.christopherson@intel.com>
    
    Replace the @max param in mmu_topup_memory_cache() and instead use
    ARRAY_SIZE() to terminate the loop to fill the cache.  This removes a
    BUG_ON() and sets the stage for moving arm64 to the common memory cache
    implementation.
    
    No functional change intended.
    
    Tested-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Message-Id: <20200703023545.8771-17-sean.j.christopherson@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 61ffb3a50c4402e17ead1257914ee98fd8c6f2f8)
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    
    Conflicts:
    	arch/arm64/kvm/mmu.c
    We don't have 50f11a8a4620  arm64: switch to generic version of pte allocation
    belonging to arm64: switch to generic version of pte allocation series
    And this latter cannot be applied cleanly as we also miss
    arm64: mm: use appropriate ctors for page tables
    
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- arch/arm64/kvm/mmu.c
+++ arch/arm64/kvm/mmu.c
@@ -136,15 +136,13 @@ static void stage2_dissolve_pud(struct kvm *kvm, phys_addr_t addr, pud_t *pudp)
 	put_page(virt_to_page(pudp));
 }
 
-static int mmu_topup_memory_cache(struct kvm_mmu_memory_cache *cache,
-				  int min, int max)
+static int mmu_topup_memory_cache(struct kvm_mmu_memory_cache *cache, int min)
 {
 	void *page;
 
-	BUG_ON(max > KVM_NR_MEM_OBJS);
 	if (cache->nobjs >= min)
 		return 0;
-	while (cache->nobjs < max) {
+	while (cache->nobjs < ARRAY_SIZE(cache->objects)) {
 		page = (void *)__get_free_page(PGALLOC_GFP);
 		if (!page)
 			return -ENOMEM;
@@ -1368,8 +1366,7 @@ int kvm_phys_addr_ioremap(struct kvm *kvm, phys_addr_t guest_ipa,
 			pte = kvm_s2pte_mkwrite(pte);
 
 		ret = mmu_topup_memory_cache(&cache,
-					     kvm_mmu_cache_min_pages(kvm),
-					     KVM_NR_MEM_OBJS);
+					     kvm_mmu_cache_min_pages(kvm));
 		if (ret)
 			goto out;
 		spin_lock(&kvm->mmu_lock);
@@ -1750,8 +1747,7 @@ static int user_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
 	up_read(&current->mm->mmap_sem);
 
 	/* We need minimum second+third level pages */
-	ret = mmu_topup_memory_cache(memcache, kvm_mmu_cache_min_pages(kvm),
-				     KVM_NR_MEM_OBJS);
+	ret = mmu_topup_memory_cache(memcache, kvm_mmu_cache_min_pages(kvm));
 	if (ret)
 		return ret;
 
