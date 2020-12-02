f7cfe86dc950864548e9a2ffca677c8ef8e3b633
Alaa Hleihel <ahleihel@redhat.com>
2020-12-02 09:13:50 -0500

    [netdrv] net/mlx5e: Optimize performance for IPv4/IPv6 ethertype
    Message-id: <20201202091426.24995-164-ahleihel@redhat.com>
    Patchwork-id: 342764
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 BZ 1858560 v2 163/199] net/mlx5e: Optimize performance for IPv4/IPv6 ethertype
    Bugzilla: 1858560
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1858560
    Upstream: v5.8-rc1
    Conflicts:
     - drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
       7e36feeb0467 ("net/mlx5e: CT: Don't offload tuple rewrites for established tuples")
       ---> We have new local variable modify_tuple.
    
    commit fca533041aac0426f5b5618a564aeb588fc125e9
    Author: Eli Britstein <elibr@mellanox.com>
    Date:   Tue May 19 05:55:59 2020 +0000
    
        net/mlx5e: Optimize performance for IPv4/IPv6 ethertype
    
        The HW is optimized for IPv4/IPv6. For such cases, pending capability,
        avoid matching on ethertype, and use ip_version field instead.
    
        Signed-off-by: Eli Britstein <elibr@mellanox.com>
        Reviewed-by: Roi Dayan <roid@mellanox.com>
        Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en/tc_ct.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en/tc_ct.c
@@ -279,7 +279,7 @@ mlx5_tc_ct_rule_to_tuple_nat(struct mlx5_ct_tuple *tuple,
 }
 
 static int
-mlx5_tc_ct_set_tuple_match(struct mlx5_flow_spec *spec,
+mlx5_tc_ct_set_tuple_match(struct mlx5e_priv *priv, struct mlx5_flow_spec *spec,
 			   struct flow_rule *rule)
 {
 	void *headers_c = MLX5_ADDR_OF(fte_match_param, spec->match_criteria,
@@ -294,7 +294,8 @@ mlx5_tc_ct_set_tuple_match(struct mlx5_flow_spec *spec,
 
 		flow_rule_match_basic(rule, &match);
 
-		mlx5e_tc_set_ethertype(headers_c, headers_v, &match);
+		mlx5e_tc_set_ethertype(priv->mdev, &match, true, headers_c,
+				       headers_v);
 		MLX5_SET(fte_match_set_lyr_2_4, headers_c, ip_protocol,
 			 match.mask->ip_proto);
 		MLX5_SET(fte_match_set_lyr_2_4, headers_v, ip_protocol,
@@ -678,7 +679,7 @@ mlx5_tc_ct_entry_add_rule(struct mlx5_tc_ct_priv *ct_priv,
 	attr->counter = entry->counter;
 	attr->flags |= MLX5_ESW_ATTR_FLAG_NO_IN_PORT;
 
-	mlx5_tc_ct_set_tuple_match(spec, flow_rule);
+	mlx5_tc_ct_set_tuple_match(netdev_priv(ct_priv->netdev), spec, flow_rule);
 	mlx5e_tc_match_to_reg_match(spec, ZONE_TO_REG,
 				    entry->tuple.zone & MLX5_CT_ZONE_MASK,
 				    MLX5_CT_ZONE_MASK);
