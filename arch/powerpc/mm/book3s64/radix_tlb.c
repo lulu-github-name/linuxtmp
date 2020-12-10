90ad26f0ca2873a609af103e1ab73e097e42a2e1
Steve Best <sbest@redhat.com>
2020-12-10 10:15:56 -0500

    [mm] powerpc/64s: Add cp_abort after tlbiel to invalidate copy-buffer address
    Message-id: <20201210101545.19247.2107.sendpatchset@sbest2>
    Patchwork-id: 343982
    Patchwork-instance: patchwork
    O-Subject: [PATCH RHEL8.4 BZ1905892 2/6] powerpc/64s: Add cp_abort after tlbiel to invalidate copy-buffer address
    Bugzilla: 1905892
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Vladis Dronov <vdronov@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1905892
    Upstream:
    commit 05504b42562066ae27ce3e7dcec37f81dea476cb
    
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=05504b42562066ae27ce3e7dcec37f81dea476cb
    
    Conflicts: upstream removes #include <asm/asm-const.h> we don't have in RHEL
    
    Description:
    The copy buffer is implemented as a real address in the nest which is
    translated from EA by copy, and used for memory access by paste. This
    requires that it be invalidated by TLB invalidation.
    
    TLBIE does invalidate the copy buffer, but TLBIEL does not. Add
    cp_abort to the tlbiel sequence.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    [mpe: Fixup whitespace and comment formatting]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200916030234.4110379-2-npiggin@gmail.com
    
    Signed-off-by: Steve Best <sbest@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- arch/powerpc/mm/book3s64/radix_tlb.c
+++ arch/powerpc/mm/book3s64/radix_tlb.c
@@ -69,7 +69,7 @@ static void tlbiel_all_isa300(unsigned int num_sets, unsigned int is)
 	for (set = 1; set < num_sets; set++)
 		tlbiel_radix_set_isa300(set, is, 0, RIC_FLUSH_TLB, 1);
 
-	asm volatile("ptesync": : :"memory");
+	ppc_after_tlbiel_barrier();
 }
 
 void radix__tlbiel_all(unsigned int action)
@@ -300,7 +300,7 @@ static inline void _tlbiel_pid(unsigned long pid, unsigned long ric)
 
 	/* For PWC, only one flush is needed */
 	if (ric == RIC_FLUSH_PWC) {
-		asm volatile("ptesync": : :"memory");
+		ppc_after_tlbiel_barrier();
 		return;
 	}
 
@@ -308,7 +308,7 @@ static inline void _tlbiel_pid(unsigned long pid, unsigned long ric)
 	for (set = 1; set < POWER9_TLB_SETS_RADIX ; set++)
 		__tlbiel_pid(pid, set, RIC_FLUSH_TLB);
 
-	asm volatile("ptesync": : :"memory");
+	ppc_after_tlbiel_barrier();
 	asm volatile(PPC_INVALIDATE_ERAT "; isync" : : :"memory");
 }
 
@@ -435,7 +435,7 @@ static inline void _tlbiel_va(unsigned long va, unsigned long pid,
 
 	asm volatile("ptesync": : :"memory");
 	__tlbiel_va(va, pid, ap, ric);
-	asm volatile("ptesync": : :"memory");
+	ppc_after_tlbiel_barrier();
 }
 
 static inline void _tlbiel_va_range(unsigned long start, unsigned long end,
@@ -446,7 +446,7 @@ static inline void _tlbiel_va_range(unsigned long start, unsigned long end,
 	if (also_pwc)
 		__tlbiel_pid(pid, 0, RIC_FLUSH_PWC);
 	__tlbiel_va_range(start, end, pid, page_size, psize);
-	asm volatile("ptesync": : :"memory");
+	ppc_after_tlbiel_barrier();
 }
 
 static inline void __tlbie_va_range(unsigned long start, unsigned long end,
@@ -960,7 +960,7 @@ is_local:
 			if (gflush)
 				__tlbiel_va_range(gstart, gend, pid,
 						PUD_SIZE, MMU_PAGE_1G);
-			asm volatile("ptesync": : :"memory");
+			ppc_after_tlbiel_barrier();
 		} else if (cputlb_use_tlbie()) {
 			asm volatile("ptesync": : :"memory");
 			__tlbie_va_range(start, end, pid, page_size, mmu_virtual_psize);
