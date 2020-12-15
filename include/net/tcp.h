916b9da933d509729e750e9e94bb0e8d4c654894
Jiri Benc <jbenc@redhat.com>
2020-12-15 13:21:38 -0500

    [net] ipv4: remove unused arg exact_dif in compute_score
    Message-id: <3d6d16cdc697a30b7f6c28036388d40ffe48b544.1608036136.git.jbenc@redhat.com>
    Patchwork-id: 344776
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 net 44/90] net: ipv4: remove unused arg exact_dif in compute_score
    Bugzilla: 1882709
    RH-Acked-by: Hangbin Liu <haliu@redhat.com>
    RH-Acked-by: Eelco Chaudron <echaudro@redhat.com>
    RH-Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1882709
    
    Conflicts: \
    - __inet_lookup_listener: need to remove exact_dif, too; that was done
      upstream in commit d9fbc7f6431f ("net: tcp: prefer listeners bound to an
      address"), which we don't have
    - [minor] compute_score: different context due to missing d9fbc7f6431f
      ("net: tcp: prefer listeners bound to an address")
    
    commit 34e1ec319e99322bfed02767d51f4998a961d205
    Author: Miaohe Lin <linmiaohe@huawei.com>
    Date:   Mon Aug 31 02:26:34 2020 -0400
    
        net: ipv4: remove unused arg exact_dif in compute_score
    
        The arg exact_dif is not used anymore, remove it. inet_exact_dif_match()
        is no longer needed after the above is removed, so remove it too.
    
        Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Andrea Claudi <aclaudi@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -943,16 +943,6 @@ extern const struct inet_connection_sock_af_ops ipv6_specific;
 
 #endif
 
-static inline bool inet_exact_dif_match(struct net *net, struct sk_buff *skb)
-{
-#if IS_ENABLED(CONFIG_NET_L3_MASTER_DEV)
-	if (!net->ipv4.sysctl_tcp_l3mdev_accept &&
-	    skb && ipv4_l3mdev_skb(IPCB(skb)->flags))
-		return true;
-#endif
-	return false;
-}
-
 /* TCP_SKB_CB reference means this can not be used from early demux */
 static inline int tcp_v4_sdif(struct sk_buff *skb)
 {
