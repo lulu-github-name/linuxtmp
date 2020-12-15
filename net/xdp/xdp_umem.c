d50b02d15cf91e6b887c0349ae8e6dee511ee838
Chris von Recklinghausen <crecklin@redhat.com>
2020-12-14 20:50:35 -0500

    [infiniband] mm, tree-wide: rename put_user_page*() to unpin_user_page*()
    Message-id: <20201214205107.20609-39-crecklin@redhat.com>
    Patchwork-id: 344619
    Patchwork-instance: patchwork
    O-Subject: [RHEL 8.4 bz1885412 v8 patch 38/70] mm, tree-wide: rename put_user_page*() to unpin_user_page*()
    Bugzilla: 1885412
    RH-Acked-by: Waiman Long <longman@redhat.com>
    RH-Acked-by: Peter Xu <peterx@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    
    From: John Hubbard <jhubbard@nvidia.com>
    
    Conflicts: drivers/infiniband/hw/mthca/mthca_memfree.c,
    	drivers/infiniband/hw/qib/qib_user_pages.c,
    	drivers/infiniband/hw/qib/qib_user_sdma.c,
    	drivers/platform/goldfish/goldfish_pipe.c - unsupported drivers
    	include/linux/mm.h - context diffs
    
    In order to provide a clearer, more symmetric API for pinning and
    unpinning DMA pages.  This way, pin_user_pages*() calls match up with
    unpin_user_pages*() calls, and the API is a lot closer to being
    self-explanatory.
    
    Link: http://lkml.kernel.org/r/20200107224558.2362728-23-jhubbard@nvidia.com
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Cc: Alex Williamson <alex.williamson@redhat.com>
    Cc: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Cc: Björn Töpel <bjorn.topel@intel.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Cc: Ira Weiny <ira.weiny@intel.com>
    Cc: Jason Gunthorpe <jgg@mellanox.com>
    Cc: Jason Gunthorpe <jgg@ziepe.ca>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Jerome Glisse <jglisse@redhat.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Kirill A. Shutemov <kirill@shutemov.name>
    Cc: Leon Romanovsky <leonro@mellanox.com>
    Cc: Mauro Carvalho Chehab <mchehab@kernel.org>
    Cc: Mike Rapoport <rppt@linux.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit f1f6a7dd9b53aafd81b696b9017036e7b08e57ea)
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- net/xdp/xdp_umem.c
+++ net/xdp/xdp_umem.c
@@ -181,7 +181,7 @@ void xdp_umem_clear_dev(struct xdp_umem *umem)
 
 static void xdp_umem_unpin_pages(struct xdp_umem *umem)
 {
-	put_user_pages_dirty_lock(umem->pgs, umem->npgs, true);
+	unpin_user_pages_dirty_lock(umem->pgs, umem->npgs, true);
 
 	kfree(umem->pgs);
 	umem->pgs = NULL;
