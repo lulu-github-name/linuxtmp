294c15cbb02df6f47eb66be426ff5767400a5766
Waiman Long <longman@redhat.com>
2020-12-16 00:14:27 -0500

    [mm] mm/khugepaged: collapse_shmem() remember to clear holes
    Message-id: <20201216001840.9820-20-longman@redhat.com>
    Patchwork-id: 344889
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 PATCH v2 019/272] mm/khugepaged: collapse_shmem() remember to clear holes
    Bugzilla: 1877019
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1877019
    
    commit 2af8ff291848cc4b1cce24b6c943394eb2c761e8
    Author: Hugh Dickins <hughd@google.com>
    Date:   Fri, 30 Nov 2018 14:10:35 -0800
    
        mm/khugepaged: collapse_shmem() remember to clear holes
    
        Huge tmpfs testing reminds us that there is no __GFP_ZERO in the gfp
        flags khugepaged uses to allocate a huge page - in all common cases it
        would just be a waste of effort - so collapse_shmem() must remember to
        clear out any holes that it instantiates.
    
        The obvious place to do so, where they are put into the page cache tree,
        is not a good choice: because interrupts are disabled there.  Leave it
        until further down, once success is assured, where the other pages are
        copied (before setting PageUptodate).
    
        Link: http://lkml.kernel.org/r/alpine.LSU.2.11.1811261525080.2275@eggly.anvils
        Fixes: f3f0e1d2150b2 ("khugepaged: add support of collapse for tmpfs/shmem pages")
        Signed-off-by: Hugh Dickins <hughd@google.com>
        Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
        Cc: Jerome Glisse <jglisse@redhat.com>
        Cc: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
        Cc: Matthew Wilcox <willy@infradead.org>
        Cc: <stable@vger.kernel.org>    [4.8+]
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1468,7 +1468,12 @@ xa_unlocked:
 		 * Replacing old pages with new one has succeeded, now we
 		 * need to copy the content and free the old pages.
 		 */
+		index = start;
 		list_for_each_entry_safe(page, tmp, &pagelist, lru) {
+			while (index < page->index) {
+				clear_highpage(new_page + (index % HPAGE_PMD_NR));
+				index++;
+			}
 			copy_highpage(new_page + (page->index % HPAGE_PMD_NR),
 					page);
 			list_del(&page->lru);
@@ -1478,6 +1483,11 @@ xa_unlocked:
 			ClearPageActive(page);
 			ClearPageUnevictable(page);
 			put_page(page);
+			index++;
+		}
+		while (index < end) {
+			clear_highpage(new_page + (index % HPAGE_PMD_NR));
+			index++;
 		}
 
 		local_irq_disable();
