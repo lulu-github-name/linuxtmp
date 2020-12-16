222023e7c7c3099e51901c53971f831109a2134d
Torez Smith <torez@redhat.com>
2020-12-16 15:38:29 -0500

    [base] driver core: Add device links from fwnode only for the primary device
    Message-id: <20201216155447.212357-82-torez@redhat.com>
    Patchwork-id: 345242
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 1900751 0081/1059] driver core: Add device links from fwnode only for the primary device
    Bugzilla: 1900751 1856683 1838295 1837563 1783493
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/1900751
    Bugzilla: https://bugzilla.redhat.com/1837563
    Bugzilla: https://bugzilla.redhat.com/1856683
    Bugzilla: https://bugzilla.redhat.com/1853229
    Bugzilla: https://bugzilla.redhat.com/1838295
    Bugzilla: https://bugzilla.redhat.com/1836895
    Bugzilla: https://bugzilla.redhat.com/1798825
    Bugzilla: https://bugzilla.redhat.com/1789257
    Bugzilla: https://bugzilla.redhat.com/1783493
    
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=33777895
    Upstream Status: 4dbe191c046e71d6ea1ba85365ecb33961b07c4f
    
    Tested: HP ZBook 15 G5/80D5 Laptop
     TGL-U(QS) Laptop
     Dell Customer: onsite team in China via TGL-U(QS) and RKL-S(ES2) CPU
    
    Sometimes, more than one (generally two) device can point to the same
    fwnode.  However, only one device is set as the fwnode's device
    (fwnode->dev) and can be looked up from the fwnode.
    
    Typically, only one of these devices actually have a driver and actually
    probe. If we create device links for all these devices, then the
    suppliers' of these devices (with the same fwnode) will never get a
    sync_state() call because one of their consumer devices will never probe
    (because they don't have a driver).
    
    So, create device links only for the device that is considered as the
    fwnode's device.
    
    One such example of this is the PCI bridge platform_device and the
    corresponding pci_bus device. Both these devices will have the same
    fwnode. It's the platform_device that is registered first and is set as
    the fwnode's device. Also the platform_device is the one that actually
    probes. Without this patch none of the suppliers of a PCI bridge
    platform_device would get a sync_state() callback.
    
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: linux-pci@vger.kernel.org
    Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Link: https://lore.kernel.org/r/20200321045448.15192-1-saravanak@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 4dbe191c046e71d6ea1ba85365ecb33961b07c4f)
    
    Signed-off-by: Al Stone <ahs3@redhat.com>
    Signed-off-by: Torez Smith <torez@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -2322,6 +2322,7 @@ int device_add(struct device *dev)
 	struct class_interface *class_intf;
 	int error = -EINVAL, fw_ret;
 	struct kobject *glue_dir = NULL;
+	bool is_fwnode_dev = false;
 
 	dev = get_device(dev);
 	if (!dev)
@@ -2419,8 +2420,10 @@ int device_add(struct device *dev)
 
 	kobject_uevent(&dev->kobj, KOBJ_ADD);
 
-	if (dev->fwnode && !dev->fwnode->dev)
+	if (dev->fwnode && !dev->fwnode->dev) {
 		dev->fwnode->dev = dev;
+		is_fwnode_dev = true;
+	}
 
 	/*
 	 * Check if any of the other devices (consumers) have been waiting for
@@ -2436,7 +2439,8 @@ int device_add(struct device *dev)
 	 */
 	device_link_add_missing_supplier_links();
 
-	if (fw_devlink_flags && fwnode_has_op(dev->fwnode, add_links)) {
+	if (fw_devlink_flags && is_fwnode_dev &&
+	    fwnode_has_op(dev->fwnode, add_links)) {
 		fw_ret = fwnode_call_int_op(dev->fwnode, add_links, dev);
 		if (fw_ret == -ENODEV && !fw_devlink_is_permissive())
 			device_link_wait_for_mandatory_supplier(dev);
