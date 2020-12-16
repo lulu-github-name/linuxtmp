2584c095cbb3b788de74df275b1ab765a9594a34
Waiman Long <longman@redhat.com>
2020-12-16 00:18:22 -0500

    [mm] mm: replace hpage_nr_pages with thp_nr_pages
    Message-id: <20201216001840.9820-255-longman@redhat.com>
    Patchwork-id: 345125
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 PATCH v2 254/272] mm: replace hpage_nr_pages with thp_nr_pages
    Bugzilla: 1877019
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1877019
    Conflicts:
     1) The 3 hunks in include/linux/pagemap.h are dropped due to missing
        the following upstream commits:
        - commit 042124cc64c33 ("mm: add new readahead_control API")
        - commit 4101196b19d7f ("mm: page cache: store only head pages in i_pages")
     2) The hunk in mm/compaction.c is dropped due to missing upstream commit
        1da2f328fa643 ("mm,thp,compaction,cma: allow THP migration for
        CMA allocations").
     3) The hunk in mm/migrate.c is dropped due to missing upstream commit
        1a5bae25e3cf9 ("mm/vmstat: add events for THP migration without split").
     4) The hunk in mm/swapfile.c is dropped due to missing upstream commit
        a3aea839e42ef ("mm, THP, swap: support to clear swap cache flag
        for THP swapped out").
     5) For backward compatibility, the hpage_nr_pages() inline function is
        retained in include/linux/huge_mm.h to call thp_nr_pages().
    
    commit 6c357848b44b4016ca422178aa368a7472245f6f
    Author: Matthew Wilcox (Oracle) <willy@infradead.org>
    Date:   Fri, 14 Aug 2020 17:30:37 -0700
    
        mm: replace hpage_nr_pages with thp_nr_pages
    
        The thp prefix is more frequently used than hpage and we should be
        consistent between the various functions.
    
        [akpm@linux-foundation.org: fix mm/migrate.c]
    
        Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Reviewed-by: William Kucharski <william.kucharski@oracle.com>
        Reviewed-by: Zi Yan <ziy@nvidia.com>
        Cc: Mike Kravetz <mike.kravetz@oracle.com>
        Cc: David Hildenbrand <david@redhat.com>
        Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
        Link: http://lkml.kernel.org/r/20200629151959.15779-6-willy@infradead.org
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -277,9 +277,14 @@ static inline unsigned int thp_order(struct page *page)
 	return 0;
 }
 
-static inline int hpage_nr_pages(struct page *page)
+/**
+ * thp_nr_pages - The number of regular pages in this huge page.
+ * @page: The head page of a huge page.
+ */
+static inline int thp_nr_pages(struct page *page)
 {
-	if (unlikely(PageTransHuge(page)))
+	VM_BUG_ON_PGFLAGS(PageTail(page), page);
+	if (PageHead(page))
 		return HPAGE_PMD_NR;
 	return 1;
 }
@@ -342,9 +347,9 @@ static inline unsigned int thp_order(struct page *page)
 	return 0;
 }
 
-static inline int hpage_nr_pages(struct page *page)
+static inline int thp_nr_pages(struct page *page)
 {
-	VM_BUG_ON_PAGE(PageTail(page), page);
+	VM_BUG_ON_PGFLAGS(PageTail(page), page);
 	return 1;
 }
 
@@ -479,4 +484,11 @@ static inline unsigned long thp_size(struct page *page)
 	return PAGE_SIZE << thp_order(page);
 }
 
+/*
+ * Backward compatibility
+ */
+static inline int hpage_nr_pages(struct page *page)
+{
+	return thp_nr_pages(page);
+}
 #endif /* _LINUX_HUGE_MM_H */
