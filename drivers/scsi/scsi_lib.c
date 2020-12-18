4e9f69115e50f2c8f4082af8055f22a4049a1edc
Ming Lei <ming.lei@redhat.com>
2020-12-18 04:01:01 -0500

    [scsi] scsi: core: Fix race between handling STS_RESOURCE and completion
    Message-id: <20201218040104.2355161-2-ming.lei@redhat.com>
    Patchwork-id: 346321
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 1/4] scsi: core: Fix race between handling STS_RESOURCE and completion
    Bugzilla: 1897404
    RH-Acked-by: Heinz Mauelshagen <heinzm@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    
    BZ: 1897404
    
    commit 673235f915318ced5d7ec4b2bfd8cb909e6a4a55
    Author: Ming Lei <ming.lei@redhat.com>
    Date:   Wed Dec 2 18:04:19 2020 +0800
    
        scsi: core: Fix race between handling STS_RESOURCE and completion
    
        When queuing I/O request to LLD, STS_RESOURCE may be returned because:
    
         - Host is in recovery or blocked
    
         - Target queue throttling or target is blocked
    
         - LLD rejection
    
        In these scenarios BLK_STS_DEV_RESOURCE is returned to the block layer to
        avoid an unnecessary re-run of the queue. However, all of the requests
        queued to this SCSI device may complete immediately after reading
        'sdev->device_busy' and BLK_STS_DEV_RESOURCE is returned to block layer. In
        that case the current I/O won't get a chance to get queued since it is
        invisible at that time for both scsi_run_queue_async() and blk-mq's
        RESTART.
    
        Fix the issue by not returning BLK_STS_DEV_RESOURCE in this situation.
    
        Link: https://lore.kernel.org/r/20201202100419.525144-1-ming.lei@redhat.com
        Fixes: 86ff7c2a80cd ("blk-mq: introduce BLK_STS_DEV_RESOURCE")
        Cc: Hannes Reinecke <hare@suse.com>
        Cc: Sumit Saxena <sumit.saxena@broadcom.com>
        Cc: Kashyap Desai <kashyap.desai@broadcom.com>
        Cc: Bart Van Assche <bvanassche@acm.org>
        Cc: Ewan Milne <emilne@redhat.com>
        Cc: Long Li <longli@microsoft.com>
        Reported-by: John Garry <john.garry@huawei.com>
        Tested-by: "chenxiang (M)" <chenxiang66@hisilicon.com>
        Signed-off-by: Ming Lei <ming.lei@redhat.com>
        Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -1717,8 +1717,7 @@ out_put_budget:
 		break;
 	case BLK_STS_RESOURCE:
 	case BLK_STS_ZONE_RESOURCE:
-		if (atomic_read(&sdev->device_busy) ||
-		    scsi_device_blocked(sdev))
+		if (scsi_device_blocked(sdev))
 			ret = BLK_STS_DEV_RESOURCE;
 		break;
 	case BLK_STS_AGAIN:
