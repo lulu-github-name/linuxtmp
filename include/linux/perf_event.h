29f00d35b09484be928b08a26f87479f7e9e6a53
Michael Petlan <mpetlan@redhat.com>
2021-01-08 00:12:40 -0500

    [x86] perf/x86/intel: Support per-thread RDPMC TopDown metrics
    Message-id: <20210108001544.23913-12-mpetlan@redhat.com>
    Patchwork-id: 347320
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 PATCH 011/197] perf/x86/intel: Support per-thread RDPMC TopDown metrics
    Bugzilla: 1884818 1908305
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: 1908305
    https://bugzilla.redhat.com/show_bug.cgi?id=1908305
    
    upstream
    ========
    commit 2cb5383b30d47c446ec7d884cd80f93ffcc31817
    Author: Kan Liang <kan.liang@linux.intel.com>
    Date: Thu Jul 23 10:11:14 2020 -0700
    
    description
    ===========
    Starts from Ice Lake, the TopDown metrics are directly available as
    fixed counters and do not require generic counters. Also, the TopDown
    metrics can be collected per thread. Extend the RDPMC usage to support
    per-thread TopDown metrics.
    
    The RDPMC index of the PERF_METRICS will be output if RDPMC users ask
    for the RDPMC index of the metrics events.
    
    To support per thread RDPMC TopDown, the metrics and slots counters have
    to be saved/restored during the context switching.
    
    The last_period and period_left are not used in the counting mode. Use
    the fields for saved_metric and saved_slots.
    
    KABI
    ====
    The perf related structures are incorporated into some key
    core structures (like 'struct perf_event *' pointer is used
    within 'struct thread_struct', 'struct perf_event_context*'
    pointer is in 'struct task_struct'). This breaks KABI any
    time we change perf related struct, while at the moment we
    don't have any perf related function on KABI list.
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -220,6 +220,28 @@ struct hw_perf_event {
 	 */
 	u64				sample_period;
 
+#ifndef __GENKSYMS__
+	union {
+		struct { /* Sampling */
+			/*
+			 * The period we started this sample with.
+			 */
+			u64				last_period;
+
+			/*
+			 * However much is left of the current period;
+			 * note that this is a full 64bit value and
+			 * allows for generation of periods longer
+			 * than hardware might allow.
+			 */
+			local64_t			period_left;
+		};
+		struct { /* Topdown events counting for context switch */
+			u64				saved_metric;
+			u64				saved_slots;
+		};
+	};
+#else
 	/*
 	 * The period we started this sample with.
 	 */
@@ -231,6 +253,7 @@ struct hw_perf_event {
 	 * than hardware might allow.
 	 */
 	local64_t			period_left;
+#endif /* __GENKSYMS__ */
 
 	/*
 	 * State for throttling the event, see __perf_event_overflow() and
