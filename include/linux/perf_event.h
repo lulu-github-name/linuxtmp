ec01511b39a11112dfa4cb254d4345eb4fa3d4e0
Michael Petlan <mpetlan@redhat.com>
2021-01-08 00:13:18 -0500

    [include] perf/arch: Remove perf_sample_data::regs_user_copy
    Message-id: <20210108001544.23913-50-mpetlan@redhat.com>
    Patchwork-id: 347358
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 PATCH 049/197] perf/arch: Remove perf_sample_data::regs_user_copy
    Bugzilla: 1884818 1908305
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: 1908305
    https://bugzilla.redhat.com/show_bug.cgi?id=1908305
    
    upstream
    ========
    commit 76a4efa80900fc40e0fdf243b42aec9fb8c35d24
    Author: Peter Zijlstra <peterz@infradead.org>
    Date: Fri Oct 30 12:14:21 2020 +0100
    
    description
    ===========
    struct perf_sample_data lives on-stack, we should be careful about it's
    size. Furthermore, the pt_regs copy in there is only because x86_64 is a
    trainwreck, solve it differently.
    
    Conflicts:
    ==========
    Skipping the hunks in archs we don't support.
    
    KABI
    ====
    The perf related structures are incorporated into some key
    core structures (like 'struct perf_event *' pointer is used
    within 'struct thread_struct', 'struct perf_event_context*'
    pointer is in 'struct task_struct'). This breaks KABI any
    time we change perf related struct, while at the moment we
    don't have any perf related function on KABI list.
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -1046,13 +1046,8 @@ struct perf_sample_data {
 	struct perf_callchain_entry	*callchain;
 	RH_KABI_BROKEN_INSERT(u64				aux_size)
 
-	/*
-	 * regs_user may point to task_pt_regs or to regs_user_copy, depending
-	 * on arch details.
-	 */
 	struct perf_regs		regs_user;
-	struct pt_regs			regs_user_copy;
-
+	RH_KABI_BROKEN_REMOVE(struct pt_regs          regs_user_copy)
 	struct perf_regs		regs_intr;
 	u64				stack_user_size;
 
