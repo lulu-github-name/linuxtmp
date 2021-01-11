a361f2a0a8f5661e4fc160c1bf512dd8711ccf56
Petr Oros <poros@redhat.com>
2021-01-11 16:39:32 -0500

    [netdrv] net: mdiobus: add clause 45 mdiobus accessors
    Message-id: <5c672175a39e1f43622928905e150d83beaf4c8d.1610380954.git.poros@redhat.com>
    Patchwork-id: 347877
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 PATCH v2 128/206] net: mdiobus: add clause 45 mdiobus accessors
    Bugzilla: 1878147
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1878147
    
    Upstream commit(s):
    commit 90ce665c6a40dc1be771bf5f86e624c0acf3a76f
    Author: Russell King <rmk+kernel@armlinux.org.uk>
    Date:   Tue May 26 16:29:36 2020 +0100
    
        net: mdiobus: add clause 45 mdiobus accessors
    
        There is a recurring pattern throughout some of the PHY code converting
        a devad and regnum to our packed clause 45 representation. Rather than
        having this scattered around the code, let's put a common translation
        function in mdio.h, and provide some register accessors.
    
        Convert the phylib core, phylink, bcm87xx and cortina to use these.
    
        Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
        Reviewed-by: Andrew Lunn <andrew@lunn.ch>
        Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Petr Oros <poros@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/net/phy/phylink.c
+++ drivers/net/phy/phylink.c
@@ -1630,7 +1630,7 @@ static int phylink_phy_read(struct phylink *pl, unsigned int phy_id,
 	if (mdio_phy_id_is_c45(phy_id)) {
 		prtad = mdio_phy_id_prtad(phy_id);
 		devad = mdio_phy_id_devad(phy_id);
-		devad = MII_ADDR_C45 | devad << 16 | reg;
+		devad = mdiobus_c45_addr(devad, reg);
 	} else if (phydev->is_c45) {
 		switch (reg) {
 		case MII_BMCR:
@@ -1653,7 +1653,7 @@ static int phylink_phy_read(struct phylink *pl, unsigned int phy_id,
 			return -EINVAL;
 		}
 		prtad = phy_id;
-		devad = MII_ADDR_C45 | devad << 16 | reg;
+		devad = mdiobus_c45_addr(devad, reg);
 	} else {
 		prtad = phy_id;
 		devad = reg;
@@ -1670,7 +1670,7 @@ static int phylink_phy_write(struct phylink *pl, unsigned int phy_id,
 	if (mdio_phy_id_is_c45(phy_id)) {
 		prtad = mdio_phy_id_prtad(phy_id);
 		devad = mdio_phy_id_devad(phy_id);
-		devad = MII_ADDR_C45 | devad << 16 | reg;
+		devad = mdiobus_c45_addr(devad, reg);
 	} else if (phydev->is_c45) {
 		switch (reg) {
 		case MII_BMCR:
@@ -1693,7 +1693,7 @@ static int phylink_phy_write(struct phylink *pl, unsigned int phy_id,
 			return -EINVAL;
 		}
 		prtad = phy_id;
-		devad = MII_ADDR_C45 | devad << 16 | reg;
+		devad = mdiobus_c45_addr(devad, reg);
 	} else {
 		prtad = phy_id;
 		devad = reg;
@@ -2291,7 +2291,6 @@ void phylink_mii_c22_pcs_an_restart(struct mdio_device *pcs)
 }
 EXPORT_SYMBOL_GPL(phylink_mii_c22_pcs_an_restart);
 
-#define C45_ADDR(d,a)	(MII_ADDR_C45 | (d) << 16 | (a))
 void phylink_mii_c45_pcs_get_state(struct mdio_device *pcs,
 				   struct phylink_link_state *state)
 {
@@ -2299,7 +2298,7 @@ void phylink_mii_c45_pcs_get_state(struct mdio_device *pcs,
 	int addr = pcs->addr;
 	int stat;
 
-	stat = mdiobus_read(bus, addr, C45_ADDR(MDIO_MMD_PCS, MDIO_STAT1));
+	stat = mdiobus_c45_read(bus, addr, MDIO_MMD_PCS, MDIO_STAT1);
 	if (stat < 0) {
 		state->link = false;
 		return;
