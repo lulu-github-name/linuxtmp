98cef7a12c7e2e79ec4c1e7fd4272cb34549438b
Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
2021-01-13 14:09:00 -0500

    [net] net: sk_msg: Simplify sk_psock initialization
    Message-id: <20210113140908.457588-283-yauheni.kaliuta@redhat.com>
    Patchwork-id: 348633
    Patchwork-instance: patchwork
    O-Subject: [PATCH RHEL8.4 v2 283/291] net: sk_msg: Simplify sk_psock initialization
    Bugzilla: 1874005
    RH-Acked-by: Jiri Olsa <jolsa@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1874005
    
    commit 7b219da43f94a3b4d5a8aa4cc52b75b34f0301ec
    Author: Lorenz Bauer <lmb@cloudflare.com>
    Date:   Fri Aug 21 11:29:43 2020 +0100
    
        net: sk_msg: Simplify sk_psock initialization
    
        Initializing psock->sk_proto and other saved callbacks is only
        done in sk_psock_update_proto, after sk_psock_init has returned.
        The logic for this is difficult to follow, and needlessly complex.
    
        Instead, initialize psock->sk_proto whenever we allocate a new
        psock. Additionally, assert the following invariants:
    
        * The SK has no ULP: ULP does it's own finagling of sk->sk_prot
        * sk_user_data is unused: we need it to store sk_psock
    
        Protect our access to sk_user_data with sk_callback_lock, which
        is what other users like reuseport arrays, etc. do.
    
        The result is that an sk_psock is always fully initialized, and
        that psock->sk_proto is always the "original" struct proto.
        The latter allows us to use psock->sk_proto when initializing
        IPv6 TCP / UDP callbacks for sockmap.
    
        Signed-off-by: Lorenz Bauer <lmb@cloudflare.com>
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
        Acked-by: John Fastabend <john.fastabend@gmail.com>
        Link: https://lore.kernel.org/bpf/20200821102948.21918-2-lmb@cloudflare.com
    
    Signed-off-by: Yauheni Kaliuta <yauheni.kaliuta@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- net/core/sock_map.c
+++ net/core/sock_map.c
@@ -188,8 +188,6 @@ static int sock_map_init_proto(struct sock *sk, struct sk_psock *psock)
 {
 	struct proto *prot;
 
-	sock_owned_by_me(sk);
-
 	switch (sk->sk_type) {
 	case SOCK_STREAM:
 		prot = tcp_bpf_get_proto(sk, psock);
@@ -276,8 +274,8 @@ static int sock_map_link(struct bpf_map *map, struct sk_psock_progs *progs,
 		}
 	} else {
 		psock = sk_psock_init(sk, map->numa_node);
-		if (!psock) {
-			ret = -ENOMEM;
+		if (IS_ERR(psock)) {
+			ret = PTR_ERR(psock);
 			goto out_progs;
 		}
 	}
@@ -326,8 +324,8 @@ static int sock_map_link_no_progs(struct bpf_map *map, struct sock *sk)
 
 	if (!psock) {
 		psock = sk_psock_init(sk, map->numa_node);
-		if (!psock)
-			return -ENOMEM;
+		if (IS_ERR(psock))
+			return PTR_ERR(psock);
 	}
 
 	ret = sock_map_init_proto(sk, psock);
@@ -482,8 +480,6 @@ static int sock_map_update_common(struct bpf_map *map, u32 idx,
 		return -EINVAL;
 	if (unlikely(idx >= map->max_entries))
 		return -E2BIG;
-	if (inet_csk_has_ulp(sk))
-		return -EINVAL;
 
 	link = sk_psock_init_link();
 	if (!link)
@@ -858,8 +854,6 @@ static int sock_hash_update_common(struct bpf_map *map, void *key,
 
 	WARN_ON_ONCE(!rcu_read_lock_held());
 	if (unlikely(flags > BPF_EXIST))
-		return -EINVAL;
-	if (inet_csk_has_ulp(sk))
 		return -EINVAL;
 
 	link = sk_psock_init_link();
