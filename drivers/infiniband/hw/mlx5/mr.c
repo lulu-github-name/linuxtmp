11490abcc59b1493f8690e43bf0a6b5d18399b59
Alaa Hleihel <ahleihel@redhat.com>
2021-01-13 15:55:19 -0500

    [infiniband] RDMA/mlx5: Assign dev to DM MR
    Message-id: <20210113155530.6322-3-ahleihel@redhat.com>
    Patchwork-id: 348649
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 BZ 1899457 02/13] RDMA/mlx5: Assign dev to DM MR
    Bugzilla: 1899457
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1899457
    Upstream: v5.11-rc1
    Conflicts:
     - drivers/infiniband/hw/mlx5/mr.c
       Drop two hunks that modify non-existing code due to missing commit:
       f1eaac37da20 ("RDMA/mlx5: Split mlx5_ib_update_xlt() into ODP and non-ODP cases")
     - drivers/infiniband/hw/mlx5/odp.c
       In function pagefault_real_mr(), replace mr->dev that was removed upstream in
       a v5.5 patch that wasn't backported to RHEL:
       f25a546e6529 ("RDMA/odp: Use mmu_interval_notifier_insert()")
    
    commit ca991a7d14d4835b302bcd182fdbf54470f45520
    Author: Maor Gottlieb <maorg@nvidia.com>
    Date:   Thu Dec 3 21:08:07 2020 +0200
    
        RDMA/mlx5: Assign dev to DM MR
    
        Currently, DM MR registration flow doesn't set the mlx5_ib_dev pointer and
        can cause a NULL pointer dereference if userspace dumps the MR via rdma
        tool.
    
        Assign the IB device together with the other fields and remove the
        redundant reference of mlx5_ib_dev from mlx5_ib_mr.
    
        Cc: stable@vger.kernel.org
        Fixes: 6c29f57ea475 ("IB/mlx5: Device memory mr registration support")
        Link: https://lore.kernel.org/r/20201203190807.127189-1-leon@kernel.org
        Signed-off-by: Maor Gottlieb <maorg@nvidia.com>
        Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
        Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/infiniband/hw/mlx5/mr.c
+++ drivers/infiniband/hw/mlx5/mr.c
@@ -134,8 +134,8 @@ static void create_mkey_callback(int status, struct mlx5_async_work *context)
 {
 	struct mlx5_ib_mr *mr =
 		container_of(context, struct mlx5_ib_mr, cb_work);
-	struct mlx5_ib_dev *dev = mr->dev;
 	struct mlx5_cache_ent *ent = mr->cache_ent;
+	struct mlx5_ib_dev *dev = ent->dev;
 	unsigned long flags;
 
 	if (status) {
@@ -174,7 +174,6 @@ static struct mlx5_ib_mr *alloc_cache_mr(struct mlx5_cache_ent *ent, void *mkc)
 		return NULL;
 	mr->order = ent->order;
 	mr->cache_ent = ent;
-	mr->dev = ent->dev;
 
 	set_mkc_access_pd_addr_fields(mkc, 0, 0, ent->dev->umrc.pd);
 	MLX5_SET(mkc, mkc, free, 1);
@@ -1015,7 +1014,7 @@ alloc_mr_from_cache(struct ib_pd *pd, struct ib_umem *umem, u64 virt_addr,
 int mlx5_ib_update_xlt(struct mlx5_ib_mr *mr, u64 idx, int npages,
 		       int page_shift, int flags)
 {
-	struct mlx5_ib_dev *dev = mr->dev;
+	struct mlx5_ib_dev *dev = mr_to_mdev(mr);
 	struct device *ddev = dev->ib_dev.dev.parent;
 	int size;
 	void *xlt;
@@ -1229,7 +1228,6 @@ static struct mlx5_ib_mr *reg_create(struct ib_mr *ibmr, struct ib_pd *pd,
 	}
 	mr->mmkey.type = MLX5_MKEY_MR;
 	mr->desc_size = sizeof(struct mlx5_mtt);
-	mr->dev = dev;
 	kvfree(in);
 
 	mlx5_ib_dbg(dev, "mkey = 0x%x\n", mr->mmkey.key);
@@ -1252,6 +1250,7 @@ static void set_mr_fields(struct mlx5_ib_dev *dev, struct mlx5_ib_mr *mr,
 	mr->ibmr.lkey = mr->mmkey.key;
 	mr->ibmr.rkey = mr->mmkey.key;
 	mr->ibmr.length = length;
+	mr->ibmr.device = &dev->ib_dev;
 	mr->access_flags = access_flags;
 }
 
@@ -1468,17 +1467,17 @@ int mlx5_mr_cache_invalidate(struct mlx5_ib_mr *mr)
 {
 	struct mlx5_umr_wr umrwr = {};
 
-	if (mr->dev->mdev->state == MLX5_DEVICE_STATE_INTERNAL_ERROR)
+	if (mr_to_mdev(mr)->mdev->state == MLX5_DEVICE_STATE_INTERNAL_ERROR)
 		return 0;
 
 	umrwr.wr.send_flags = MLX5_IB_SEND_UMR_DISABLE_MR |
 			      MLX5_IB_SEND_UMR_UPDATE_PD_ACCESS;
 	umrwr.wr.opcode = MLX5_IB_WR_UMR;
-	umrwr.pd = mr->dev->umrc.pd;
+	umrwr.pd = mr_to_mdev(mr)->umrc.pd;
 	umrwr.mkey = mr->mmkey.key;
 	umrwr.ignore_free_state = 1;
 
-	return mlx5_ib_post_send_wait(mr->dev, &umrwr);
+	return mlx5_ib_post_send_wait(mr_to_mdev(mr), &umrwr);
 }
 
 static int rereg_umr(struct ib_pd *pd, struct mlx5_ib_mr *mr,
