14beffcad8e803209dade2ab843961e1da8b6a3f
Desnes Augusto Nunes do Rosario <drosario@redhat.com>
2021-01-21 22:11:18 -0500

    [scsi] scsi: ibmvfc: Send Cancel MAD down each hw SCSI channel
    Message-id: <20210121221122.24687-36-drosario@redhat.com>
    Patchwork-id: 350719
    Patchwork-instance: patchwork
    O-Subject: [RHEL-8.4 BZ1868055 PATCH v2 35/39] scsi: ibmvfc: Send Cancel MAD down each hw SCSI channel
    Bugzilla: 1868055
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    BUGZILLA: https://bugzilla.redhat.com/show_bug.cgi?id=1868055
    UPSTREAM STATUS: https://git.kernel.org/pub/scm/linux/kernel/git/mkp/scsi.git/commit/?id=a835f386f9709504a99346be011da92b5ea905e5
    BUILD INFO: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=34346030
    
    DESCRIPTION
    commit a835f386f9709504a99346be011da92b5ea905e5
    Author: Tyrel Datwyler <tyreld@linux.ibm.com>
    Date: Thu, 14 Jan 2021 14:31:45 -0600
    
      In general the client needs to send Cancel MADs and task management
      commands down the same channel as the command(s) intended to cancel or
      abort. The client assigns cancel keys per LUN and thus must send a Cancel
      down each channel commands were submitted for that LUN. Further, the client
      then must wait for those cancel completions prior to submitting a LUN RESET
      or ABORT TASK SET.
    
      Add a cancel rsp iu syncronization field to the ibmvfc_queue struct such
      that the cancel routine can sync the cancel response to each queue that
      requires a cancel command. Build a list of each cancel event sent and wait
      for the completion of each submitted cancel.
    
      Link: https://lore.kernel.org/r/20210114203148.246656-19-tyreld@linux.ibm.com
      Reviewed-by: Brian King <brking@linux.vnet.ibm.com>
      Signed-off-by: Tyrel Datwyler <tyreld@linux.ibm.com>
      Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
      (cherry picked from commit a835f386f9709504a99346be011da92b5ea905e5)
    
    Signed-off-by: Desnes A. Nunes do Rosario <drosario@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/scsi/ibmvscsi/ibmvfc.h
+++ drivers/scsi/ibmvscsi/ibmvfc.h
@@ -751,6 +751,7 @@ struct ibmvfc_target {
 /* a unit of work for the hosting partition */
 struct ibmvfc_event {
 	struct list_head queue_list;
+	struct list_head cancel;
 	struct ibmvfc_host *vhost;
 	struct ibmvfc_queue *queue;
 	struct ibmvfc_target *tgt;
@@ -803,6 +804,8 @@ struct ibmvfc_queue {
 	struct list_head sent;
 	struct list_head free;
 	spinlock_t l_lock;
+
+	union ibmvfc_iu cancel_rsp;
 
 	/* Sub-CRQ fields */
 	struct ibmvfc_host *vhost;
