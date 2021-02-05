143d446fb788469170cbd2c508261885f2c8f16d
Pavel Reichl <preichl@redhat.com>
2021-02-05 18:08:32 -0500

    [tools] tests: close_range - Replace clone3 with clone
    Message-id: <20210205180834.140959-9-preichl@redhat.com>
    Patchwork-id: 352844
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 fs PATCH v4 08/10] tests: close_range - Replace clone3 with clone
    Bugzilla: 1900674
    RH-Acked-by: Ian Kent <ikent@redhat.com>
    RH-Acked-by: Bill O'Donnell <billodo@redhat.com>
    RH-Acked-by: Eric Sandeen <esandeen@redhat.com>
    
    Replace clone3(), which is not imlemented in RHEL yet, with clone().
    
    Sematics of clone3() return value is similar to fork()'s. So in case
    of clone3() the caller controls the program flow based on return value.
    However clone() behaves diferently and child behaviour is defined by
    function pointer passed to clone(). That's why part of the
    close_range_unshare and close_range_unshare_capped tests are moved
    into separate functions fn() resp. fn_capped(). Note that this
    introduces whitespace changes that make the patch look more complex
    than it really is.
    
    Remove the '#if 0' guards as the code should compile now.
    
    Upstream Status: RHEL only
    Signed-off-by: Pavel Reichl <preichl@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- tools/testing/selftests/core/close_range_test.c
+++ tools/testing/selftests/core/close_range_test.c
@@ -11,9 +11,10 @@
 #include <string.h>
 #include <syscall.h>
 #include <unistd.h>
+#include <sched.h>
+#include <signal.h>
 
 #include "../kselftest_harness.h"
-#include "../clone3/clone3_selftests.h"
 
 #ifndef __NR_close_range
 #define __NR_close_range -1
@@ -32,9 +33,6 @@ static inline int sys_close_range(unsigned int fd, unsigned int max_fd,
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 #endif
-
-/* Due to missing sys_clone3 these tests cannot compile now */
-#if 0
 
 TEST(close_range)
 {
@@ -95,17 +93,84 @@ TEST(close_range)
 	EXPECT_EQ(-1, fcntl(open_fds[100], F_GETFL));
 }
 
+struct arg {
+	int open_fds[101];
+};
+
+int fn(void *p)
+{
+	int i, ret;
+	struct arg *arg = (struct arg *)(p);
+	int *open_fds = arg->open_fds;
+
+	ret = sys_close_range(open_fds[0], open_fds[50],
+			      CLOSE_RANGE_UNSHARE);
+	if (ret)
+		exit(EXIT_FAILURE);
+
+	for (i = 0; i <= 50; i++)
+		if (fcntl(open_fds[i], F_GETFL) != -1)
+			exit(EXIT_FAILURE);
+
+	for (i = 51; i <= 100; i++)
+		if (fcntl(open_fds[i], F_GETFL) == -1)
+			exit(EXIT_FAILURE);
+
+	/* create a couple of gaps */
+	close(57);
+	close(78);
+	close(81);
+	close(82);
+	close(84);
+	close(90);
+
+	ret = sys_close_range(open_fds[51], open_fds[92],
+			      CLOSE_RANGE_UNSHARE);
+	if (ret)
+		exit(EXIT_FAILURE);
+
+	for (i = 51; i <= 92; i++)
+		if (fcntl(open_fds[i], F_GETFL) != -1)
+			exit(EXIT_FAILURE);
+
+	for (i = 93; i <= 100; i++)
+		if (fcntl(open_fds[i], F_GETFL) == -1)
+			exit(EXIT_FAILURE);
+
+	/* test that the kernel caps and still closes all fds */
+	ret = sys_close_range(open_fds[93], open_fds[99],
+			CLOSE_RANGE_UNSHARE);
+	if (ret)
+		exit(EXIT_FAILURE);
+
+	for (i = 93; i <= 99; i++)
+		if (fcntl(open_fds[i], F_GETFL) != -1)
+			exit(EXIT_FAILURE);
+
+	if (fcntl(open_fds[100], F_GETFL) == -1)
+		exit(EXIT_FAILURE);
+
+	ret = sys_close_range(open_fds[100], open_fds[100],
+			      CLOSE_RANGE_UNSHARE);
+	if (ret)
+		exit(EXIT_FAILURE);
+
+	if (fcntl(open_fds[100], F_GETFL) != -1)
+		exit(EXIT_FAILURE);
+
+	exit(EXIT_SUCCESS);
+}
+
+
 TEST(close_range_unshare)
 {
-	int i, ret, status;
+	int i, status;
 	pid_t pid;
-	int open_fds[101];
-	struct clone_args args = {
-		.flags = CLONE_FILES,
-		.exit_signal = SIGCHLD,
-	};
+	struct arg arg;
+	const unsigned int stack_size = 1024 * 1024;
+	void *stack;
 
-	for (i = 0; i < ARRAY_SIZE(open_fds); i++) {
+	for (i = 0; i < ARRAY_SIZE(arg.open_fds); i++) {
 		int fd;
 
 		fd = open("/dev/null", O_RDONLY | O_CLOEXEC);
@@ -114,87 +179,49 @@ TEST(close_range_unshare)
 				SKIP(return, "Skipping test since /dev/null does not exist");
 		}
 
-		open_fds[i] = fd;
+		arg.open_fds[i] = fd;
 	}
 
-	pid = sys_clone3(&args, sizeof(args));
+	stack = malloc(stack_size);
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+
+	pid = clone(fn, stack + stack_size - 1, CLONE_FILES | SIGCHLD, (void *)&arg);
 	ASSERT_GE(pid, 0);
-
-	if (pid == 0) {
-		ret = sys_close_range(open_fds[0], open_fds[50],
-				      CLOSE_RANGE_UNSHARE);
-		if (ret)
-			exit(EXIT_FAILURE);
-
-		for (i = 0; i <= 50; i++)
-			if (fcntl(open_fds[i], F_GETFL) != -1)
-				exit(EXIT_FAILURE);
-
-		for (i = 51; i <= 100; i++)
-			if (fcntl(open_fds[i], F_GETFL) == -1)
-				exit(EXIT_FAILURE);
-
-		/* create a couple of gaps */
-		close(57);
-		close(78);
-		close(81);
-		close(82);
-		close(84);
-		close(90);
-
-		ret = sys_close_range(open_fds[51], open_fds[92],
-				      CLOSE_RANGE_UNSHARE);
-		if (ret)
-			exit(EXIT_FAILURE);
-
-		for (i = 51; i <= 92; i++)
-			if (fcntl(open_fds[i], F_GETFL) != -1)
-				exit(EXIT_FAILURE);
-
-		for (i = 93; i <= 100; i++)
-			if (fcntl(open_fds[i], F_GETFL) == -1)
-				exit(EXIT_FAILURE);
-
-		/* test that the kernel caps and still closes all fds */
-		ret = sys_close_range(open_fds[93], open_fds[99],
-				      CLOSE_RANGE_UNSHARE);
-		if (ret)
-			exit(EXIT_FAILURE);
-
-		for (i = 93; i <= 99; i++)
-			if (fcntl(open_fds[i], F_GETFL) != -1)
-				exit(EXIT_FAILURE);
-
-		if (fcntl(open_fds[100], F_GETFL) == -1)
-			exit(EXIT_FAILURE);
-
-		ret = sys_close_range(open_fds[100], open_fds[100],
-				      CLOSE_RANGE_UNSHARE);
-		if (ret)
-			exit(EXIT_FAILURE);
-
-		if (fcntl(open_fds[100], F_GETFL) != -1)
-			exit(EXIT_FAILURE);
-
-		exit(EXIT_SUCCESS);
-	}
 
 	EXPECT_EQ(waitpid(pid, &status, 0), pid);
 	EXPECT_EQ(true, WIFEXITED(status));
 	EXPECT_EQ(0, WEXITSTATUS(status));
+	free(stack);
+}
+
+int fn_capped(void *p)
+{
+	int i, ret;
+	struct arg *arg = (struct arg *)(p);
+	int *open_fds = arg->open_fds;
+
+	ret = sys_close_range(open_fds[0], UINT_MAX,
+			CLOSE_RANGE_UNSHARE);
+	if (ret)
+		exit(EXIT_FAILURE);
+
+	for (i = 0; i <= 100; i++)
+		if (fcntl(open_fds[i], F_GETFL) != -1)
+			exit(EXIT_FAILURE);
+
+	exit(EXIT_SUCCESS);
 }
 
 TEST(close_range_unshare_capped)
 {
 	int i, ret, status;
 	pid_t pid;
-	int open_fds[101];
-	struct clone_args args = {
-		.flags = CLONE_FILES,
-		.exit_signal = SIGCHLD,
-	};
+	struct arg arg;
+	const unsigned int stack_size = 1024 * 1024;
+	void *stack;
 
-	for (i = 0; i < ARRAY_SIZE(open_fds); i++) {
+	for (i = 0; i < ARRAY_SIZE(arg.open_fds); i++) {
 		int fd;
 
 		fd = open("/dev/null", O_RDONLY | O_CLOEXEC);
@@ -203,30 +230,20 @@ TEST(close_range_unshare_capped)
 				SKIP(return, "Skipping test since /dev/null does not exist");
 		}
 
-		open_fds[i] = fd;
+		arg.open_fds[i] = fd;
 	}
 
-	pid = sys_clone3(&args, sizeof(args));
+	stack = malloc(stack_size);
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+
+	pid = clone(fn_capped, stack + stack_size - 1, CLONE_FILES | SIGCHLD, (void *)&arg);
 	ASSERT_GE(pid, 0);
-
-	if (pid == 0) {
-		ret = sys_close_range(open_fds[0], UINT_MAX,
-				      CLOSE_RANGE_UNSHARE);
-		if (ret)
-			exit(EXIT_FAILURE);
-
-		for (i = 0; i <= 100; i++)
-			if (fcntl(open_fds[i], F_GETFL) != -1)
-				exit(EXIT_FAILURE);
-
-		exit(EXIT_SUCCESS);
-	}
 
 	EXPECT_EQ(waitpid(pid, &status, 0), pid);
 	EXPECT_EQ(true, WIFEXITED(status));
 	EXPECT_EQ(0, WEXITSTATUS(status));
+	free(stack);
 }
-
-#endif /* Skip tests compilation because of missing sys_clone3 for now */
 
 TEST_HARNESS_MAIN
