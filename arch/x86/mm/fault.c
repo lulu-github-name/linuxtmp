96b35fe13068dcc854647963656a8f108e07ee42
Vladis Dronov <vdronov@redhat.com>
2021-02-05 11:20:20 -0500

    [mm] x86/traps: Attempt to fixup exceptions in vDSO before signaling
    Message-id: <20210205112040.9144-20-vdronov@redhat.com>
    Patchwork-id: 352779
    Patchwork-instance: patchwork
    O-Subject: [RHEL8.4 BZ1660337 v2 PATCH 19/39] x86/traps: Attempt to fixup exceptions in vDSO before signaling
    Bugzilla: 1660337
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Waiman Long <longman@redhat.com>
    RH-Acked-by: Chris von Recklinghausen <crecklin@redhat.com>
    RH-Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1660337
    Upstream Status: merged into the linux.git
    
    Conflicts:
    * missing 59c1dcbed5b5 ("x86/traps: Print address on #GP") so remove
      "#include <asm/insn.h>" and "#include <asm/insn-eval.h>" off the context
    * missing ("36209766cede x86/traps: Cleanup do_general_protection()") so
      adjust the patch context a bit
    * missing 3cf5d076fb4d ("signal: Remove task parameter from force_sig")
      so change "force_sig(SIGSEGV)" back to "force_sig(SIGSEGV, tsk)"
    * missing 2e1661d26736 ("signal: Remove the task parameter from force_sig_fault")
      so change "force_sig_fault(...)" back to "force_sig_fault(..., task)"
    * missing ca4c6a9858c2 ("x86/traps: Make interrupt enable/disable symmetric
      in C code") so change "goto exit" back to "return" in the patch context
    * missing 186525bd6b83 ("mm, x86/mm: Untangle address space layout definitions
      from basic pgtable type definitions") so remove "#include <asm/pgtable_areas.h>"
      from the patch context
    
    commit 334872a0919890a70cccd00b8e11931020a819be
    Author: Sean Christopherson <seanjc@google.com>
    Date:   Fri Nov 13 00:01:29 2020 +0200
    
        x86/traps: Attempt to fixup exceptions in vDSO before signaling
    
        vDSO functions can now leverage an exception fixup mechanism similar to
        kernel exception fixup.  For vDSO exception fixup, the initial user is
        Intel's Software Guard Extensions (SGX), which will wrap the low-level
        transitions to/from the enclave, i.e. EENTER and ERESUME instructions,
        in a vDSO function and leverage fixup to intercept exceptions that would
        otherwise generate a signal.  This allows the vDSO wrapper to return the
        fault information directly to its caller, obviating the need for SGX
        applications and libraries to juggle signal handlers.
    
        Attempt to fixup vDSO exceptions immediately prior to populating and
        sending signal information.  Except for the delivery mechanism, an
        exception in a vDSO function should be treated like any other exception
        in userspace, e.g. any fault that is successfully handled by the kernel
        should not be directly visible to userspace.
    
        Although it's debatable whether or not all exceptions are of interest to
        enclaves, defer to the vDSO fixup to decide whether to do fixup or
        generate a signal.  Future users of vDSO fixup, if there ever are any,
        will undoubtedly have different requirements than SGX enclaves, e.g. the
        fixup vs. signal logic can be made function specific if/when necessary.
    
        Suggested-by: Andy Lutomirski <luto@amacapital.net>
        Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
        Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>
        Signed-off-by: Borislav Petkov <bp@suse.de>
        Acked-by: Jethro Beekman <jethro@fortanix.com>
        Link: https://lkml.kernel.org/r/20201112220135.165028-19-jarkko@kernel.org
    
    Signed-off-by: Vladis Dronov <vdronov@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -30,6 +30,7 @@
 #include <asm/desc.h>			/* store_idt(), ...		*/
 #include <asm/cpu_entry_area.h>		/* exception stack		*/
 #include <asm/kvm_para.h>		/* kvm_handle_async_pf		*/
+#include <asm/vdso.h>			/* fixup_vdso_exception()	*/
 
 #define CREATE_TRACE_POINTS
 #include <asm/trace/exceptions.h>
@@ -862,6 +863,9 @@ __bad_area_nosemaphore(struct pt_regs *regs, unsigned long error_code,
 
 		sanitize_error_code(address, &error_code);
 
+		if (fixup_vdso_exception(regs, X86_TRAP_PF, error_code, address))
+			return;
+
 		if (likely(show_unhandled_signals))
 			show_signal_msg(regs, error_code, address, tsk);
 
@@ -980,6 +984,9 @@ do_sigbus(struct pt_regs *regs, unsigned long error_code, unsigned long address,
 		return;
 
 	sanitize_error_code(address, &error_code);
+
+	if (fixup_vdso_exception(regs, X86_TRAP_PF, error_code, address))
+		return;
 
 	set_signal_archinfo(address, error_code);
 
