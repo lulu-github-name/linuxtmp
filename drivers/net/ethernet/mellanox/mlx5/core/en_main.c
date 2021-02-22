0234f30a2b6c5849935ad46af08a73893e1251ec
Alaa Hleihel <ahleihel@redhat.com>
2021-02-22 10:33:50 -0500

    net/mlx5e: Fix CQ params of ICOSQ and async ICOSQ
    Bugzilla: http://bugzilla.redhat.com/1928671
    Bugzilla: http://bugzilla.redhat.com/1928706
    Upstream: git://git.kernel.org/pub/scm/linux/kernel/git/davem/net.git , branch: master
    Conflicts:
     - drivers/net/ethernet/mellanox/mlx5/core/en_main.c
       Context diff due to missing commit:
       2f6b379cca4c ("net/mlx5e: Fill mlx5e_create_cq_param in a function")
       ---> In function mlx5e_open_queues(), we don't have a call to
            mlx5e_build_create_cq_param().
       4d0b7ef909b6 ("net/mlx5e: Allow CQ outside of channel context")
       ---> We still have the older prototype of function mlx5e_open_cq().
    
    commit ebf79b6be67c0a77a9ab7cdf74c43fd7d9619f0c
    Author: Maxim Mikityanskiy <maximmi@mellanox.com>
    Date:   Wed Feb 10 09:25:28 2021 +0200
    
        net/mlx5e: Fix CQ params of ICOSQ and async ICOSQ
    
        The commit mentioned below has split the parameters of ICOSQ and async
        ICOSQ, but it contained a typo: the CQ parameters were swapped for ICOSQ
        and async ICOSQ. Async ICOSQ is longer than the normal ICOSQ, and the CQ
        size must be the same as the size of the corresponding SQ, but due to
        this bug, the CQ of async ICOSQ was much shorter than async ICOSQ
        itself. It led to overflows of the CQ with such messages in dmesg, in
        particular, when running multiple kTLS-offloaded streams:
    
        mlx5_core 0000:08:00.0: cq_err_event_notifier:529:(pid 9422): CQ error
        on CQN 0x406, syndrome 0x1
        mlx5_core 0000:08:00.0 eth2: mlx5e_cq_error_event: cqn=0x000406
        event=0x04
    
        This commit fixes the issue by using the corresponding parameters for
        ICOSQ and async ICOSQ.
    
        Fixes: c293ac927fbb ("net/mlx5e: Refactor build channel params")
        Signed-off-by: Maxim Mikityanskiy <maximmi@mellanox.com>
        Reviewed-by: Tariq Toukan <tariqt@nvidia.com>
        Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -1835,11 +1835,11 @@ static int mlx5e_open_queues(struct mlx5e_channel *c,
 	struct dim_cq_moder icocq_moder = {0, 0};
 	int err;
 
-	err = mlx5e_open_cq(c, icocq_moder, &cparam->icosq.cqp, &c->async_icosq.cq);
+	err = mlx5e_open_cq(c, icocq_moder, &cparam->async_icosq.cqp, &c->async_icosq.cq);
 	if (err)
 		return err;
 
-	err = mlx5e_open_cq(c, icocq_moder, &cparam->async_icosq.cqp, &c->icosq.cq);
+	err = mlx5e_open_cq(c, icocq_moder, &cparam->icosq.cqp, &c->icosq.cq);
 	if (err)
 		goto err_close_async_icosq_cq;
 
