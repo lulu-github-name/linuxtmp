02b767d510438ae8a16b9f83a721926c4f8cd459
Jarod Wilson <jarod@redhat.com>
2021-03-09 15:51:08 -0500

    mac80211: Inform AP when returning operating channel
    Bugzilla: http://bugzilla.redhat.com/1937053
    
    commit dba0491f00114b9f688bfff60545e90a270023e1
    Author: Loic Poulain <loic.poulain@linaro.org>
    Date:   Mon Jun 29 10:55:23 2020 +0200
    
        mac80211: Inform AP when returning operating channel
    
        Because we can miss AP wakeup (beacon) while scanning other channels,
        it's better go into wakeup state and inform the AP of that upon
        returning to the operating channel, rather than staying asleep and
        waiting for the next TIM indicating traffic for us.
    
        This saves precious time, especially when we only have 200ms inter-
        scan period for monitoring the active connection.
    
        Signed-off-by: Loic Poulain <loic.poulain@linaro.org>
        Link: https://lore.kernel.org/r/1593420923-26668-1-git-send-email-loic.poulain@linaro.org
        [rewrite commit message a bit]
        Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- net/mac80211/ieee80211_i.h
+++ net/mac80211/ieee80211_i.h
@@ -1375,7 +1375,6 @@ struct ieee80211_local {
 				*/
 
 	bool pspolling;
-	bool offchannel_ps_enabled;
 	/*
 	 * PS can only be enabled when we have exactly one managed
 	 * interface (and monitors) in PS, this then points there.
