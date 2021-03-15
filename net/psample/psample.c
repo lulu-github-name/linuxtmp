9f3e2d5a001c3e72b8baebc77eceffebcd4065f5
Ivan Vecera <ivecera@redhat.com>
2021-03-15 10:07:37 +0100

    genetlink: move to smaller ops wherever possible
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1938919
    
    Conflicts:
    - drivers/net/ieee802154/mac802154_hwsim.c
    - drivers/thermal/thermal_netlink.c
        Hunks for these files removed as they are not exist in RHEL kernel
        source
    - drivers/net/gtp.c
    - net/batman-adv/netlink.c
    - net/hsr/hsr_netlink.c
        Simple context conflicts resolved and hunks applied although they
        do not have any influence on RHEL kernel because corresponding
        config options are not enabled in RHEL kernel config
    - net/mptcp/pm_netlink.c
        Adjusted due to existing backport of 674d3ab949776 ("mptcp:
        Constify mptcp_pm_ops")
    
    commit 66a9b9287d2447a91cef2fafc648dee32186f708
    Author: Jakub Kicinski <kuba@kernel.org>
    Date:   Fri Oct 2 14:49:54 2020 -0700
    
        genetlink: move to smaller ops wherever possible
    
        Bulk of the genetlink users can use smaller ops, move them.
    
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
        Reviewed-by: Johannes Berg <johannes@sipsolutions.net>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- net/psample/psample.c
+++ net/psample/psample.c
@@ -97,7 +97,7 @@ static int psample_nl_cmd_get_group_dumpit(struct sk_buff *msg,
 	return msg->len;
 }
 
-static const struct genl_ops psample_nl_ops[] = {
+static const struct genl_small_ops psample_nl_ops[] = {
 	{
 		.cmd = PSAMPLE_CMD_GET_GROUP,
 		.validate = GENL_DONT_VALIDATE_STRICT | GENL_DONT_VALIDATE_DUMP,
@@ -113,8 +113,8 @@ static struct genl_family psample_nl_family __ro_after_init = {
 	.netnsok	= true,
 	.module		= THIS_MODULE,
 	.mcgrps		= psample_nl_mcgrps,
-	.ops		= psample_nl_ops,
-	.n_ops		= ARRAY_SIZE(psample_nl_ops),
+	.small_ops	= psample_nl_ops,
+	.n_small_ops	= ARRAY_SIZE(psample_nl_ops),
 	.n_mcgrps	= ARRAY_SIZE(psample_nl_mcgrps),
 };
 
