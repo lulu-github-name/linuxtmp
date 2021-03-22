030ce721bdabcbc0ecdf5e5176115c325e9aef33
Waiman Long <longman@redhat.com>
2021-03-22 15:05:39 -0400

    sysfs: add sysfs_change_owner()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1937825
    
    commit 2c4f9401ceb00167a3bfd322a28aa87b646a253f
    Author: Christian Brauner <christian.brauner@ubuntu.com>
    Date:   Thu, 27 Feb 2020 04:37:14 +0100
    
        sysfs: add sysfs_change_owner()
    
        Add a helper to change the owner of sysfs objects.
        This function will be used to correctly account for kobject ownership
        changes, e.g. when moving network devices between network namespaces.
    
        This mirrors how a kobject is added through driver core which in its guts is
        done via kobject_add_internal() which in summary creates the main directory via
        create_dir(), populates that directory with the groups associated with the
        ktype of the kobject (if any) and populates the directory with the basic
        attributes associated with the ktype of the kobject (if any). These are the
        basic steps that are associated with adding a kobject in sysfs.
        Any additional properties are added by the specific subsystem itself (not by
        driver core) after it has registered the device. So for the example of network
        devices, a network device will e.g. register a queue subdirectory under the
        basic sysfs directory for the network device and than further subdirectories
        within that queues subdirectory.  But that is all specific to network devices
        and they call the corresponding sysfs functions to do that directly when they
        create those queue objects. So anything that a subsystem adds outside of what
        driver core does must also be changed by it (That's already true for removal of
        files it created outside of driver core.) and it's the same for ownership
        changes.
    
        Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -323,6 +323,7 @@ static inline void sysfs_enable_ns(struct kernfs_node *kn)
 
 int sysfs_file_change_owner(struct kobject *kobj, const char *name, kuid_t kuid,
 			    kgid_t kgid);
+int sysfs_change_owner(struct kobject *kobj, kuid_t kuid, kgid_t kgid);
 int sysfs_link_change_owner(struct kobject *kobj, struct kobject *targ,
 			    const char *name, kuid_t kuid, kgid_t kgid);
 int sysfs_groups_change_owner(struct kobject *kobj,
@@ -563,6 +564,11 @@ static inline int sysfs_link_change_owner(struct kobject *kobj,
 					  struct kobject *targ,
 					  const char *name, kuid_t kuid,
 					  kgid_t kgid)
+{
+	return 0;
+}
+
+static inline int sysfs_change_owner(struct kobject *kobj, kuid_t kuid, kgid_t kgid)
 {
 	return 0;
 }
