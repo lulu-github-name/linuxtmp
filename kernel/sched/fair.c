04cd382f03f43f78831af4de071cb53648043c58
Phil Auld <pauld@redhat.com>
2021-04-06 17:14:18 -0400

    sched/fair: Dissociate wakeup decisions from SD flag value
    Bugzilla: http://bugzilla.redhat.com/1937103
    
    commit dc824eb898534cd8e34582874dae3bb7cf2fa008
    Author: Valentin Schneider <valentin.schneider@arm.com>
    Date:   Mon Nov 2 18:45:14 2020 +0000
    
        sched/fair: Dissociate wakeup decisions from SD flag value
    
        The CFS wakeup code will only ever go through EAS / its fast path on
        "regular" wakeups (i.e. not on forks or execs). These are currently gated
        by a check against 'sd_flag', which would be SD_BALANCE_WAKE at wakeup.
    
        However, we now have a flag that explicitly tells us whether a wakeup is a
        "regular" one, so hinge those conditions on that flag instead.
    
        Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Link: https://lkml.kernel.org/r/20201102184514.2733-4-valentin.schneider@arm.com
    
    Signed-off-by: Phil Auld <pauld@redhat.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6625,7 +6625,7 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int wake_flags)
 	/* SD_flags and WF_flags share the first nibble */
 	int sd_flag = wake_flags & 0x1c; /* RHEL adjust this from 0xf */
 
-	if (sd_flag & SD_BALANCE_WAKE) {
+	if (wake_flags & WF_TTWU) {
 		record_wakee(p);
 
 		if (static_branch_unlikely(&sched_energy_present)) {
@@ -6666,9 +6666,8 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int wake_flags)
 	if (unlikely(sd)) {
 		/* Slow path */
 		new_cpu = find_idlest_cpu(sd, p, cpu, prev_cpu, sd_flag);
-	} else if (sd_flag & SD_BALANCE_WAKE) { /* XXX always ? */
+	} else if (wake_flags & WF_TTWU) { /* XXX always ? */
 		/* Fast path */
-
 		new_cpu = select_idle_sibling(p, prev_cpu, new_cpu);
 
 		if (want_affine)
