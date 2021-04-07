7bc89e9440eec96020b89b50863cfce52e61df6e
Xin Long <lxin@redhat.com>
2021-04-07 13:53:24 -0400

    sctp: add encap_err_lookup for udp encap socks
    Brew Info:https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=36007654
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1946733
    Upstream Status: net.git commit 89ba49171fb2
    Tested: these testcases without iptables used in sctp-tests.git
    
    commit 89ba49171fb22c130be4786d5ada9dda33dd7801
    Author: Xin Long <lucien.xin@gmail.com>
    Date:   Thu Oct 29 15:05:00 2020 +0800
    
        sctp: add encap_err_lookup for udp encap socks
    
        As it says in rfc6951#section-5.5:
    
          "When receiving ICMP or ICMPv6 response packets, there might not be
           enough bytes in the payload to identify the SCTP association that the
           SCTP packet triggering the ICMP or ICMPv6 packet belongs to.  If a
           received ICMP or ICMPv6 packet cannot be related to a specific SCTP
           association or the verification tag cannot be verified, it MUST be
           discarded silently.  In particular, this means that the SCTP stack
           MUST NOT rely on receiving ICMP or ICMPv6 messages.  Implementation
           constraints could prevent processing received ICMP or ICMPv6
           messages."
    
        ICMP or ICMPv6 packets need to be handled, and this is implemented by
        udp encap sock .encap_err_lookup function.
    
        The .encap_err_lookup function is called in __udp(6)_lib_err_encap()
        to confirm this path does need to be updated. For sctp, what we can
        do here is check if the corresponding asoc and transport exist.
    
        Note that icmp packet process for sctp over udp is done by udp sock
        .encap_err_lookup(), and it means for now we can't do as much as
        sctp_v4/6_err() does. Also we can't do the two mappings mentioned
        in rfc6951#section-5.5.
    
        Signed-off-by: Xin Long <lucien.xin@gmail.com>
        Acked-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Xin Long <lxin@redhat.com>

--- net/sctp/protocol.c
+++ net/sctp/protocol.c
@@ -863,6 +863,23 @@ static int sctp_udp_rcv(struct sock *sk, struct sk_buff *skb)
 	return 0;
 }
 
+static int sctp_udp_err_lookup(struct sock *sk, struct sk_buff *skb)
+{
+	struct sctp_association *asoc;
+	struct sctp_transport *t;
+	int family;
+
+	skb->transport_header += sizeof(struct udphdr);
+	family = (ip_hdr(skb)->version == 4) ? AF_INET : AF_INET6;
+	sk = sctp_err_lookup(dev_net(skb->dev), family, skb, sctp_hdr(skb),
+			     &asoc, &t);
+	if (!sk)
+		return -ENOENT;
+
+	sctp_err_finish(sk, t);
+	return 0;
+}
+
 int sctp_udp_sock_start(struct net *net)
 {
 	struct udp_tunnel_sock_cfg tuncfg = {NULL};
@@ -881,6 +898,7 @@ int sctp_udp_sock_start(struct net *net)
 
 	tuncfg.encap_type = 1;
 	tuncfg.encap_rcv = sctp_udp_rcv;
+	tuncfg.encap_err_lookup = sctp_udp_err_lookup;
 	setup_udp_tunnel_sock(net, sock, &tuncfg);
 	net->sctp_udp4_sock = sock->sk;
 
@@ -902,6 +920,7 @@ int sctp_udp_sock_start(struct net *net)
 
 	tuncfg.encap_type = 1;
 	tuncfg.encap_rcv = sctp_udp_rcv;
+	tuncfg.encap_err_lookup = sctp_udp_err_lookup;
 	setup_udp_tunnel_sock(net, sock, &tuncfg);
 	net->sctp_udp6_sock = sock->sk;
 #endif
