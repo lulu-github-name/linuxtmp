1dd97ecb1128a7c43ba31125e1f42edaf86d5b0f
Xin Long <lxin@redhat.com>
2021-04-07 13:56:26 -0400

    sctp: add udphdr to overhead when udp_port is set
    Brew Info:https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=36007654
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1946733
    Upstream Status: net.git commit f1bfe8b54151
    Tested: these testcases without iptables used in sctp-tests.git
    
    commit f1bfe8b5415171b5e70c2a47d399c91bd7c2752e
    Author: Xin Long <lucien.xin@gmail.com>
    Date:   Thu Oct 29 15:05:04 2020 +0800
    
        sctp: add udphdr to overhead when udp_port is set
    
        sctp_mtu_payload() is for calculating the frag size before making
        chunks from a msg. So we should only add udphdr size to overhead
        when udp socks are listening, as only then sctp can handle the
        incoming sctp over udp packets and outgoing sctp over udp packets
        will be possible.
    
        Note that we can't do this according to transport->encap_port, as
        different transports may be set to different values, while the
        chunks were made before choosing the transport, we could not be
        able to meet all rfc6951#section-5.6 recommends.
    
        v1->v2:
          - Add udp_port for sctp_sock to avoid a potential race issue, it
            will be used in xmit path in the next patch.
    
        Signed-off-by: Xin Long <lucien.xin@gmail.com>
        Acked-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Xin Long <lxin@redhat.com>

--- net/sctp/socket.c
+++ net/sctp/socket.c
@@ -5183,6 +5183,7 @@ static int sctp_init_sock(struct sock *sk)
 	 * be modified via SCTP_PEER_ADDR_PARAMS
 	 */
 	sp->hbinterval  = net->sctp.hb_interval;
+	sp->udp_port    = htons(net->sctp_udp_port);
 	sp->encap_port  = htons(net->sctp_encap_port);
 	sp->pathmaxrxt  = net->sctp.max_retrans_path;
 	sp->pf_retrans  = net->sctp.pf_retrans;
