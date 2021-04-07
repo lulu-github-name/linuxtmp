01ecd8bf1d608ba9a027d37c7ef87c7e952deb8d
Xin Long <lxin@redhat.com>
2021-04-07 12:23:56 -0400

    net: Convert protocol error handlers from void to int
    Brew Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=36007654
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1946733
    Upstream Status: net.git commit 32bbd8793f24
    Tested: these testcases without iptables used in sctp-tests.git
    
    Conflicts:
      - context difference due to 76c6d988aeb3 already in RHEL-8.5.
      - context difference due to lack of d888f3966677 and 1042caa79e93 in RHEL-8.5.
      - tunnelmpls6_err() changed to reurn int as 74148a66ab21 is already in RHEL-8.5.
        before this backport.
    
    commit 32bbd8793f24b0d5beb1cdb33c45c75ad1140e4b
    Author: Stefano Brivio <sbrivio@redhat.com>
    Date:   Thu Nov 8 12:19:21 2018 +0100
    
        net: Convert protocol error handlers from void to int
    
        We'll need this to handle ICMP errors for tunnels without a sending socket
        (i.e. FoU and GUE). There, we might have to look up different types of IP
        tunnels, registered as network protocols, before we get a match, so we
        want this for the error handlers of IPPROTO_IPIP and IPPROTO_IPV6 in both
        inet_protos and inet6_protos. These error codes will be used in the next
        patch.
    
        For consistency, return sensible error codes in protocol error handlers
        whenever handlers can't handle errors because, even if valid, they don't
        match a protocol or any of its states.
    
        This has no effect on existing error handling paths.
    
        Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
        Reviewed-by: Sabrina Dubroca <sd@queasysnail.net>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Xin Long <lxin@redhat.com>

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -667,7 +667,7 @@ static struct sock *__udp4_lib_err_encap(struct net *net,
  * to find the appropriate port.
  */
 
-void __udp4_lib_err(struct sk_buff *skb, u32 info, struct udp_table *udptable)
+int __udp4_lib_err(struct sk_buff *skb, u32 info, struct udp_table *udptable)
 {
 	struct inet_sock *inet;
 	const struct iphdr *iph = (const struct iphdr *)skb->data;
@@ -690,7 +690,7 @@ void __udp4_lib_err(struct sk_buff *skb, u32 info, struct udp_table *udptable)
 
 		if (!sk) {
 			__ICMP_INC_STATS(net, ICMP_MIB_INERRORS);
-			return;
+			return -ENOENT;
 		}
 		tunnel = true;
 	}
@@ -748,12 +748,12 @@ void __udp4_lib_err(struct sk_buff *skb, u32 info, struct udp_table *udptable)
 	sk->sk_err = err;
 	sk->sk_error_report(sk);
 out:
-	return;
+	return 0;
 }
 
-void udp_err(struct sk_buff *skb, u32 info)
+int udp_err(struct sk_buff *skb, u32 info)
 {
-	__udp4_lib_err(skb, info, &udp_table);
+	return __udp4_lib_err(skb, info, &udp_table);
 }
 
 /*
