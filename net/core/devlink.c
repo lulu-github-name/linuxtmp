bba6f87621e3f5faff4b0d7044555c4778932cce
Petr Oros <poros@redhat.com>
2021-04-13 13:10:49 +0200

    devlink: move flash end and begin to core devlink
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1935671
    
    Upstream commit(s):
    commit 52cc5f3a166a33012ebca2cdefebf4c689110068
    Author: Jacob Keller <jacob.e.keller@intel.com>
    Date:   Wed Nov 18 11:06:36 2020 -0800
    
        devlink: move flash end and begin to core devlink
    
        When performing a flash update via devlink, device drivers may inform
        user space of status updates via
        devlink_flash_update_(begin|end|timeout|status)_notify functions.
    
        It is expected that drivers do not send any status notifications unless
        they send a begin and end message. If a driver sends a status
        notification without sending the appropriate end notification upon
        finishing (regardless of success or failure), the current implementation
        of the devlink userspace program can get stuck endlessly waiting for the
        end notification that will never come.
    
        The current ice driver implementation may send such a status message
        without the appropriate end notification in rare cases.
    
        Fixing the ice driver is relatively simple: we just need to send the
        begin_notify at the start of the function and always send an end_notify
        no matter how the function exits.
    
        Rather than assuming driver authors will always get this right in the
        future, lets just fix the API so that it is not possible to get wrong.
        Make devlink_flash_update_begin_notify and
        devlink_flash_update_end_notify static, and call them in devlink.c core
        code. Always send the begin_notify just before calling the driver's
        flash_update routine. Always send the end_notify just after the routine
        returns regardless of success or failure.
    
        Doing this makes the status notification easier to use from the driver,
        as it no longer needs to worry about catching failures and cleaning up
        by calling devlink_flash_update_end_notify. It is now no longer possible
        to do the wrong thing in this regard. We also save a couple of lines of
        code in each driver.
    
        Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
        Acked-by: Vasundhara Volam <vasundhara-v.volam@broadcom.com>
        Reviewed-by: Jiri Pirko <jiri@nvidia.com>
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Petr Oros <poros@redhat.com>

--- net/core/devlink.c
+++ net/core/devlink.c
@@ -3381,7 +3381,7 @@ out_free_msg:
 	nlmsg_free(msg);
 }
 
-void devlink_flash_update_begin_notify(struct devlink *devlink)
+static void devlink_flash_update_begin_notify(struct devlink *devlink)
 {
 	struct devlink_flash_notify params = { 0 };
 
@@ -3389,9 +3389,8 @@ void devlink_flash_update_begin_notify(struct devlink *devlink)
 				      DEVLINK_CMD_FLASH_UPDATE,
 				      &params);
 }
-EXPORT_SYMBOL_GPL(devlink_flash_update_begin_notify);
 
-void devlink_flash_update_end_notify(struct devlink *devlink)
+static void devlink_flash_update_end_notify(struct devlink *devlink)
 {
 	struct devlink_flash_notify params = { 0 };
 
@@ -3399,7 +3398,6 @@ void devlink_flash_update_end_notify(struct devlink *devlink)
 				      DEVLINK_CMD_FLASH_UPDATE_END,
 				      &params);
 }
-EXPORT_SYMBOL_GPL(devlink_flash_update_end_notify);
 
 void devlink_flash_update_status_notify(struct devlink *devlink,
 					const char *status_msg,
@@ -3486,7 +3484,9 @@ static int devlink_nl_cmd_flash_update(struct sk_buff *skb,
 		return ret;
 	}
 
+	devlink_flash_update_begin_notify(devlink);
 	ret = devlink->ops->flash_update(devlink, &params, info->extack);
+	devlink_flash_update_end_notify(devlink);
 
 	release_firmware(params.fw);
 
@@ -10062,7 +10062,9 @@ int devlink_compat_flash_update(struct net_device *dev, const char *file_name)
 		goto out;
 
 	mutex_lock(&devlink->lock);
+	devlink_flash_update_begin_notify(devlink);
 	ret = devlink->ops->flash_update(devlink, &params, NULL);
+	devlink_flash_update_end_notify(devlink);
 	mutex_unlock(&devlink->lock);
 
 	release_firmware(params.fw);
