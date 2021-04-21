60e5f003bf9c47fc658beeed874c089ea4b1e382
Prarit Bhargava <prarit@redhat.com>
2021-04-21 11:22:52 -0400

    x86/traps: Handle #DB for bus lock
    Bugzilla: http://bugzilla.redhat.com/1837350
    
    Conflicts: RHEL8 disables split_lock and bus_lock by default, so there are some
    small differences in the patch due to that.  As well, traps.c is different
    between upstream and RHEL8 so there some differences there as well.  I've kept
    the spirit of changes in sync with upstream.
    
    Upstream Status: git://git.kernel.org/pub/scm/linux/kernel/git/tip/tip.git
    
    commit ebb1064e7c2e90b56e4d40ab154ef9796060a1c3
    Author: Fenghua Yu <fenghua.yu@intel.com>
    Date:   Mon Mar 22 13:53:24 2021 +0000
    
        x86/traps: Handle #DB for bus lock
    
        Bus locks degrade performance for the whole system, not just for the CPU
        that requested the bus lock. Two CPU features "#AC for split lock" and
        "#DB for bus lock" provide hooks so that the operating system may choose
        one of several mitigation strategies.
    
        #AC for split lock is already implemented. Add code to use the #DB for
        bus lock feature to cover additional situations with new options to
        mitigate.
    
        split_lock_detect=
                        #AC for split lock              #DB for bus lock
    
        off             Do nothing                      Do nothing
    
        warn            Kernel OOPs                     Warn once per task and
                        Warn once per task and          and continues to run.
                        disable future checking
                        When both features are
                        supported, warn in #AC
    
        fatal           Kernel OOPs                     Send SIGBUS to user.
                        Send SIGBUS to user
                        When both features are
                        supported, fatal in #AC
    
        ratelimit:N     Do nothing                      Limit bus lock rate to
                                                        N per second in the
                                                        current non-root user.
    
        Default option is "warn".
    
        Hardware only generates #DB for bus lock detect when CPL>0 to avoid
        nested #DB from multiple bus locks while the first #DB is being handled.
        So no need to handle #DB for bus lock detected in the kernel.
    
        #DB for bus lock is enabled by bus lock detection bit 2 in DEBUGCTL MSR
        while #AC for split lock is enabled by split lock detection bit 29 in
        TEST_CTRL MSR.
    
        Both breakpoint and bus lock in the same instruction can trigger one #DB.
        The bus lock is handled before the breakpoint in the #DB handler.
    
        Delivery of #DB for bus lock in userspace clears DR6[11], which is set by
        the #DB handler right after reading DR6.
    
        Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Reviewed-by: Tony Luck <tony.luck@intel.com>
        Link: https://lore.kernel.org/r/20210322135325.682257-3-fenghua.yu@intel.com
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>

--- arch/x86/kernel/cpu/intel.c
+++ arch/x86/kernel/cpu/intel.c
@@ -44,9 +44,9 @@ enum split_lock_detect_state {
 };
 
 /*
- * Default to sld_off because most systems do not support split lock detection
- * split_lock_setup() will switch this to sld_warn on systems that support
- * split lock detect, unless there is a command line override.
+ * Default to sld_off because most systems do not support split lock detection.
+ * sld_state_setup() will switch this to sld_warn on systems that support
+ * split lock/bus lock detect, unless there is a command line override.
  */
 static enum split_lock_detect_state sld_state = sld_off;
 static u64 msr_test_ctrl_cache;
@@ -661,6 +661,7 @@ static void init_intel_misc_features(struct cpuinfo_x86 *c)
 }
 
 static void split_lock_init(void);
+static void bus_lock_init(void);
 
 static void init_intel(struct cpuinfo_x86 *c)
 {
@@ -780,6 +781,7 @@ static void init_intel(struct cpuinfo_x86 *c)
 		tsx_disable();
 
 	split_lock_init();
+	bus_lock_init();
 }
 
 #ifdef CONFIG_X86_32
@@ -1079,16 +1081,15 @@ static bool split_lock_verify_msr(bool on)
 	return ctrl == tmp;
 }
 
-static void __init split_lock_setup(void)
+static void __init sld_state_setup(void)
 {
 	enum split_lock_detect_state state = sld_off;
 	char arg[20];
 	int i, ret;
 
-	if (!split_lock_verify_msr(false)) {
-		pr_info("MSR access failed: Disabled\n");
+	if (!boot_cpu_has(X86_FEATURE_SPLIT_LOCK_DETECT) &&
+	    !boot_cpu_has(X86_FEATURE_BUS_LOCK_DETECT))
 		return;
-	}
 
 	ret = cmdline_find_option(boot_command_line, "split_lock_detect",
 				  arg, sizeof(arg));
@@ -1100,17 +1101,14 @@ static void __init split_lock_setup(void)
 			}
 		}
 	}
+	sld_state = state;
+}
 
-	switch (state) {
-	case sld_off:
-		pr_info("disabled\n");
-		break;
-	case sld_warn:
-		pr_info("warning about split_locks\n");
-		break;
-	case sld_fatal:
-		pr_info("sending SIGBUS on user-space split_locks, panic on kenrel split_locks\n");
-		break;
+static void __init __split_lock_setup(void)
+{
+	if (!split_lock_verify_msr(false)) {
+		pr_info("MSR access failed: Disabled\n");
+		return;
 	}
 
 	rdmsrl(MSR_TEST_CTRL, msr_test_ctrl_cache);
@@ -1120,7 +1118,9 @@ static void __init split_lock_setup(void)
 		return;
 	}
 
-	sld_state = state;
+	/* Restore the MSR to its cached value. */
+	wrmsrl(MSR_TEST_CTRL, msr_test_ctrl_cache);
+
 	setup_force_cpu_cap(X86_FEATURE_SPLIT_LOCK_DETECT);
 }
 
@@ -1238,6 +1238,29 @@ bool handle_guest_split_lock(unsigned long ip)
 }
 EXPORT_SYMBOL_GPL(handle_guest_split_lock);
 
+static void bus_lock_init(void)
+{
+	u64 val;
+
+	/*
+	 * Warn and fatal are handled by #AC for split lock if #AC for
+	 * split lock is supported.
+	 */
+	if (!boot_cpu_has(X86_FEATURE_BUS_LOCK_DETECT) ||
+	    (boot_cpu_has(X86_FEATURE_SPLIT_LOCK_DETECT) &&
+	    (sld_state == sld_warn || sld_state == sld_fatal)) ||
+	    sld_state == sld_off)
+		return;
+
+	/*
+	 * Enable #DB for bus lock. All bus locks are handled in #DB except
+	 * split locks are handled in #AC in the fatal case.
+	 */
+	rdmsrl(MSR_IA32_DEBUGCTLMSR, val);
+	val |= DEBUGCTLMSR_BUS_LOCK_DETECT;
+	wrmsrl(MSR_IA32_DEBUGCTLMSR, val);
+}
+
 bool handle_user_split_lock(struct pt_regs *regs, long error_code)
 {
 	if ((regs->flags & X86_EFLAGS_AC) || sld_state == sld_fatal)
@@ -1245,6 +1268,21 @@ bool handle_user_split_lock(struct pt_regs *regs, long error_code)
 	if (sld_state == sld_warn)
 		split_lock_warn(regs->ip);
 	return true;
+}
+
+void handle_bus_lock(struct pt_regs *regs)
+{
+	switch (sld_state) {
+	case sld_off:
+		break;
+	case sld_warn:
+		pr_warn_ratelimited("#DB: %s/%d took a bus_lock trap at address: 0x%lx\n",
+				    current->comm, current->pid, regs->ip);
+		break;
+	case sld_fatal:
+		force_sig_fault(SIGBUS, BUS_ADRALN, NULL, current);
+		break;
+	}
 }
 
 /*
@@ -1281,7 +1319,7 @@ static const struct x86_cpu_id split_lock_cpu_ids[] __initconst = {
 	{}
 };
 
-void __init cpu_set_core_cap_bits(struct cpuinfo_x86 *c)
+static void __init split_lock_setup(struct cpuinfo_x86 *c)
 {
 	const struct x86_cpu_id *m;
 	u64 ia32_core_caps;
@@ -1307,5 +1345,40 @@ void __init cpu_set_core_cap_bits(struct cpuinfo_x86 *c)
 		return;
 	}
 
-	split_lock_setup();
+	__split_lock_setup();
+}
+
+static void sld_state_show(void)
+{
+	if (!boot_cpu_has(X86_FEATURE_BUS_LOCK_DETECT) &&
+	    !boot_cpu_has(X86_FEATURE_SPLIT_LOCK_DETECT))
+		return;
+
+	switch (sld_state) {
+	case sld_off:
+		pr_info("disabled\n");
+		break;
+	case sld_warn:
+		if (boot_cpu_has(X86_FEATURE_SPLIT_LOCK_DETECT))
+			pr_info("#AC: crashing the kernel on kernel split_locks and warning on user-space split_locks\n");
+		else if (boot_cpu_has(X86_FEATURE_BUS_LOCK_DETECT))
+			pr_info("#DB: warning on user-space bus_locks\n");
+		break;
+	case sld_fatal:
+		if (boot_cpu_has(X86_FEATURE_SPLIT_LOCK_DETECT)) {
+			pr_info("#AC: crashing the kernel on kernel split_locks and sending SIGBUS on user-space split_locks\n");
+		} else if (boot_cpu_has(X86_FEATURE_BUS_LOCK_DETECT)) {
+			pr_info("#DB: sending SIGBUS on user-space bus_locks%s\n",
+				boot_cpu_has(X86_FEATURE_SPLIT_LOCK_DETECT) ?
+				" from non-WB" : "");
+		}
+		break;
+	}
+}
+
+void __init sld_setup(struct cpuinfo_x86 *c)
+{
+	split_lock_setup(c);
+	sld_state_setup();
+	sld_state_show();
 }
