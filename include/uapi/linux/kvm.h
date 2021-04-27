5dc9e9ae6a2932c90307549ddcd3aac03dc059b2
Paolo Bonzini <pbonzini@redhat.com>
2021-04-27 12:18:56 -0400

    KVM: VMX: Enable bus lock VM exit
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1945742
    
    Virtual Machine can exploit bus locks to degrade the performance of
    system. Bus lock can be caused by split locked access to writeback(WB)
    memory or by using locks on uncacheable(UC) memory. The bus lock is
    typically >1000 cycles slower than an atomic operation within a cache
    line. It also disrupts performance on other cores (which must wait for
    the bus lock to be released before their memory operations can
    complete).
    
    To address the threat, bus lock VM exit is introduced to notify the VMM
    when a bus lock was acquired, allowing it to enforce throttling or other
    policy based mitigations.
    
    A VMM can enable VM exit due to bus locks by setting a new "Bus Lock
    Detection" VM-execution control(bit 30 of Secondary Processor-based VM
    execution controls). If delivery of this VM exit was preempted by a
    higher priority VM exit (e.g. EPT misconfiguration, EPT violation, APIC
    access VM exit, APIC write VM exit, exception bitmap exiting), bit 26 of
    exit reason in vmcs field is set to 1.
    
    In current implementation, the KVM exposes this capability through
    KVM_CAP_X86_BUS_LOCK_EXIT. The user can get the supported mode bitmap
    (i.e. off and exit) and enable it explicitly (disabled by default). If
    bus locks in guest are detected by KVM, exit to user space even when
    current exit reason is handled by KVM internally. Set a new field
    KVM_RUN_BUS_LOCK in vcpu->run->flags to inform the user space that there
    is a bus lock detected in guest.
    
    Document for Bus Lock VM exit is now available at the latest "Intel
    Architecture Instruction Set Extensions Programming Reference".
    
    Document Link:
    https://software.intel.com/content/www/us/en/develop/download/intel-architecture-instruction-set-extensions-programming-reference.html
    
    Co-developed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>
    Message-Id: <20201106090315.18606-4-chenyi.qiang@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit fe6b6bc802b40081e8a7a1abe8d32b88d10a03e1)
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -252,6 +252,7 @@ struct kvm_hyperv_exit {
 #define KVM_EXIT_X86_WRMSR        30
 #define KVM_EXIT_DIRTY_RING_FULL  31
 #define KVM_EXIT_AP_RESET_HOLD    32
+#define KVM_EXIT_X86_BUS_LOCK     33
 
 /* For KVM_EXIT_INTERNAL_ERROR */
 /* Emulate instruction failed. */
@@ -1057,6 +1058,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_ENFORCE_PV_FEATURE_CPUID 190
 #define KVM_CAP_SYS_HYPERV_CPUID 191
 #define KVM_CAP_DIRTY_LOG_RING 192
+#define KVM_CAP_X86_BUS_LOCK_EXIT 193
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
@@ -1771,5 +1773,8 @@ struct kvm_dirty_gfn {
 	__u32 slot;
 	__u64 offset;
 };
+
+#define KVM_BUS_LOCK_DETECTION_OFF             (1 << 0)
+#define KVM_BUS_LOCK_DETECTION_EXIT            (1 << 1)
 
 #endif /* __LINUX_KVM_H */
