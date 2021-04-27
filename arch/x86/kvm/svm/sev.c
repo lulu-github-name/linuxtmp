0593a2c969c25126c597084e9a2ade8414c010c3
Paolo Bonzini <pbonzini@redhat.com>
2021-04-27 12:18:29 -0400

    KVM: SVM: Create trace events for VMGEXIT processing
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1945742
    
    Add trace events for entry to and exit from VMGEXIT processing. The vCPU
    id and the exit reason will be common for the trace events. The exit info
    fields will represent the input and output values for the entry and exit
    events, respectively.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Message-Id: <25357dca49a38372e8f483753fb0c1c2a70a6898.1607620209.git.thomas.lendacky@amd.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit d523ab6ba2753bd41b4447ae48024182cb4da94f)
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -15,10 +15,12 @@
 #include <linux/pagemap.h>
 #include <linux/swap.h>
 #include <linux/processor.h>
+#include <linux/trace_events.h>
 
 #include "x86.h"
 #include "svm.h"
 #include "cpuid.h"
+#include "trace.h"
 
 static u8 sev_enc_bit;
 static int sev_flush_asids(void);
@@ -1469,6 +1471,8 @@ static void pre_sev_es_run(struct vcpu_svm *svm)
 	if (!svm->ghcb)
 		return;
 
+	trace_kvm_vmgexit_exit(svm->vcpu.vcpu_id, svm->ghcb);
+
 	sev_es_sync_to_ghcb(svm);
 
 	kvm_vcpu_unmap(&svm->vcpu, &svm->ghcb_map, true);
@@ -1617,6 +1621,8 @@ int sev_handle_vmgexit(struct vcpu_svm *svm)
 
 	svm->ghcb = svm->ghcb_map.hva;
 	ghcb = svm->ghcb_map.hva;
+
+	trace_kvm_vmgexit_enter(svm->vcpu.vcpu_id, ghcb);
 
 	exit_code = ghcb_get_sw_exit_code(ghcb);
 
