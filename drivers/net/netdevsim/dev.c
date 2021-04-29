16e24ce17e949a6bd58c309762fe002f8cb363bc
Ivan Vecera <ivecera@redhat.com>
2021-04-29 17:02:22 +0200

    netdevsim: Add dummy psample implementation
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1955163
    
    commit a8700c3dd0a48f379d269ac6ddcf8bd857042771
    Author: Ido Schimmel <idosch@nvidia.com>
    Date:   Sun Mar 14 14:19:32 2021 +0200
    
        netdevsim: Add dummy psample implementation
    
        Allow netdevsim to report "sampled" packets to the psample module by
        periodically generating packets from a work queue. The behavior can be
        enabled / disabled (default) and the various meta data attributes can be
        controlled via debugfs knobs.
    
        This implementation enables both testing of the psample module with all
        the optional attributes as well as development of user space
        applications on top of psample such as hsflowd and a Wireshark dissector
        for psample generic netlink packets.
    
        Signed-off-by: Ido Schimmel <idosch@nvidia.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/net/netdevsim/dev.c
+++ drivers/net/netdevsim/dev.c
@@ -1010,9 +1010,13 @@ static int nsim_dev_reload_create(struct nsim_dev *nsim_dev,
 	if (err)
 		goto err_traps_exit;
 
-	err = nsim_dev_port_add_all(nsim_dev, nsim_bus_dev->port_count);
+	err = nsim_dev_psample_init(nsim_dev);
 	if (err)
 		goto err_health_exit;
+
+	err = nsim_dev_port_add_all(nsim_dev, nsim_bus_dev->port_count);
+	if (err)
+		goto err_psample_exit;
 
 	nsim_dev->take_snapshot = debugfs_create_file("take_snapshot",
 						      0200,
@@ -1021,6 +1025,8 @@ static int nsim_dev_reload_create(struct nsim_dev *nsim_dev,
 						&nsim_dev_take_snapshot_fops);
 	return 0;
 
+err_psample_exit:
+	nsim_dev_psample_exit(nsim_dev);
 err_health_exit:
 	nsim_dev_health_exit(nsim_dev);
 err_traps_exit:
@@ -1095,14 +1101,20 @@ int nsim_dev_probe(struct nsim_bus_dev *nsim_bus_dev)
 	if (err)
 		goto err_health_exit;
 
-	err = nsim_dev_port_add_all(nsim_dev, nsim_bus_dev->port_count);
+	err = nsim_dev_psample_init(nsim_dev);
 	if (err)
 		goto err_bpf_dev_exit;
+
+	err = nsim_dev_port_add_all(nsim_dev, nsim_bus_dev->port_count);
+	if (err)
+		goto err_psample_exit;
 
 	devlink_params_publish(devlink);
 	devlink_reload_enable(devlink);
 	return 0;
 
+err_psample_exit:
+	nsim_dev_psample_exit(nsim_dev);
 err_bpf_dev_exit:
 	nsim_bpf_dev_exit(nsim_dev);
 err_health_exit:
@@ -1135,6 +1147,7 @@ static void nsim_dev_reload_destroy(struct nsim_dev *nsim_dev)
 		return;
 	debugfs_remove(nsim_dev->take_snapshot);
 	nsim_dev_port_del_all(nsim_dev);
+	nsim_dev_psample_exit(nsim_dev);
 	nsim_dev_health_exit(nsim_dev);
 	nsim_dev_traps_exit(devlink);
 	nsim_dev_dummy_region_exit(nsim_dev);
