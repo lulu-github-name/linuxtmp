f678f84fa7d44a49f8a538d1fb5692f194435da6
Myron Stowe <mstowe@redhat.com>
2021-05-04 11:50:18 -0600

    PCI/ERR: Rename reset_link() to reset_subordinates()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1895942
    Upstream Status: 8f1bbfbc3596d401b60d1562b27ec28c2724f60d
    
    Conflict(s):
      Checking file drivers/pci/pci.h: Hunk #1 FAILED at 572, 1 out of 1 hunk
      FAILED
      Checking file drivers/pci/pcie/err.c: Hunk #1 FAILED at 147, 1 out of 2
      hunks FAILED
    
      'enum pci_channel_state' conflicts are due to RHEL having not taken
      upstream commit 16d79cd4e23b ("PCI: Use 'pci_channel_state_t' instead of
      'enum pci_channel_state'") as that change caused a false kABI failure.
    
    
    commit 8f1bbfbc3596d401b60d1562b27ec28c2724f60d
    Author: Sean V Kelley <sean.v.kelley@intel.com>
    Date:   Fri Nov 20 16:10:25 2020 -0800
    
        PCI/ERR: Rename reset_link() to reset_subordinates()
    
        reset_link() appears to be misnamed.  The point is to reset any devices
        below a given bridge, so rename it to reset_subordinates() to make it clear
        that we are passing a bridge with the intent to reset the devices below it.
    
        Link: https://lore.kernel.org/r/20201121001036.8560-5-sean.v.kelley@intel.com
        Tested-by: Jonathan Cameron <Jonathan.Cameron@huawei.com> # non-native/no RCEC
        Signed-off-by: Sean V Kelley <sean.v.kelley@intel.com>
        Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
        Reviewed-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
        Acked-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    
    Signed-off-by: Myron Stowe <mstowe@redhat.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -581,8 +581,8 @@ static inline int pci_dev_specific_disable_acs_redir(struct pci_dev *dev)
 
 /* PCI error reporting and recovery */
 pci_ers_result_t pcie_do_recovery(struct pci_dev *dev,
-			enum pci_channel_state state,
-			pci_ers_result_t (*reset_link)(struct pci_dev *pdev));
+		enum pci_channel_state state,
+		pci_ers_result_t (*reset_subordinates)(struct pci_dev *pdev));
 
 bool pcie_wait_for_link(struct pci_dev *pdev, bool active);
 #ifdef CONFIG_PCIEASPM
