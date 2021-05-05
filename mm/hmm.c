036391a3802b0360eda2a9cde48d36dcf14f4f91
Chris von Recklinghausen <crecklin@redhat.com>
2021-05-05 15:47:59 -0400

    mm/hmm: add missing pfns set to hmm_vma_walk_pmd()
    Bugzilla: http://bugzilla.redhat.com/1945442
    Upstream status: linux
    
    commit 7d082987e5e562c07a208503a607a733d50553ba
    Author: Jason Gunthorpe <jgg@ziepe.ca>
    Date:   Wed Mar 4 16:25:56 2020 -0400
    
        mm/hmm: add missing pfns set to hmm_vma_walk_pmd()
    
        All success exit paths from the walker functions must set the pfns array.
    
        A migration entry with no required fault is a HMM_PFN_NONE return, just
        like the pte case.
    
        Fixes: d08faca018c4 ("mm/hmm: properly handle migration pmd")
        Reviewed-by: Ralph Campbell <rcampbell@nvidia.com>
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- mm/hmm.c
+++ mm/hmm.c
@@ -416,7 +416,7 @@ again:
 			pmd_migration_entry_wait(walk->mm, pmdp);
 			return -EBUSY;
 		}
-		return 0;
+		return hmm_pfns_fill(start, end, range, HMM_PFN_NONE);
 	} else if (!pmd_present(pmd))
 		return hmm_pfns_fill(start, end, range, HMM_PFN_ERROR);
 
