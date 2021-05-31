95b5258b94d50838d3374be7fb6e2c714a3053fb
Jiri Benc <jbenc@redhat.com>
2021-05-31 16:09:00 +0200

    xsk: i40e: ice: ixgbe: mlx5: Pass buffer pool to driver instead of umem
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1882710
    
    Conflicts:
    - [minor] i40e_xmit_zc: differenct context due to out of order backport of
      f320460b9489 ("i40e: Remove unnecessary sw_ring access from xsk Tx")
    - [minor] mlx5e_xsk_tx_post_err: different context due to out of order
      backport of b39fe61edc50 ("net/mlx5e: Rename xmit-related structs to
      generalize them")
    - netdevice.h: struct netdev_queue and struct netdev_rx_queue: the umem
      field is replaced inside the kABI workarounds; that's okay, we don't
      promise stable kABI for XDP
    - [minor] ethnl_set_channels: context difference due to out of order
      backport of ef72cd3c5ce1 ("ethtool: fix error paths in
      ethnl_set_channels()")
    
    commit 1742b3d528690ae7773cf7bf2f01a90ee1de2fe0
    Author: Magnus Karlsson <magnus.karlsson@intel.com>
    Date:   Fri Aug 28 10:26:15 2020 +0200
    
        xsk: i40e: ice: ixgbe: mlx5: Pass buffer pool to driver instead of umem
    
        Replace the explicit umem reference passed to the driver in AF_XDP
        zero-copy mode with the buffer pool instead. This in preparation for
        extending the functionality of the zero-copy mode so that umems can be
        shared between queues on the same netdev and also between netdevs. In
        this commit, only an umem reference has been added to the buffer pool
        struct. But later commits will add other entities to it. These are
        going to be entities that are different between different queue ids
        and netdevs even though the umem is shared between them.
    
        Signed-off-by: Magnus Karlsson <magnus.karlsson@intel.com>
        Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
        Acked-by: Björn Töpel <bjorn.topel@intel.com>
        Link: https://lore.kernel.org/bpf/1598603189-32145-2-git-send-email-magnus.karlsson@intel.com
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -636,7 +636,7 @@ struct netdev_queue {
 	struct dql		dql;
 #endif
 
-	RH_KABI_USE(1, struct xdp_umem	*umem)
+	RH_KABI_USE(1, struct xsk_buff_pool	*pool)
 	RH_KABI_RESERVE(2)
 	RH_KABI_RESERVE(3)
 	RH_KABI_RESERVE(4)
@@ -759,7 +759,7 @@ struct netdev_rx_queue {
 	struct net_device		*dev;
 	RH_KABI_EXCLUDE(struct xdp_rxq_info	xdp_rxq)
 
-	RH_KABI_USE(1, struct xdp_umem	*umem)
+	RH_KABI_USE(1, struct xsk_buff_pool	*pool)
 	RH_KABI_RESERVE(2)
 	RH_KABI_RESERVE(3)
 	RH_KABI_RESERVE(4)
@@ -893,7 +893,7 @@ enum bpf_netdev_command {
 	/* BPF program for offload callbacks, invoked at program load time. */
 	BPF_OFFLOAD_MAP_ALLOC,
 	BPF_OFFLOAD_MAP_FREE,
-	XDP_SETUP_XSK_UMEM,
+	XDP_SETUP_XSK_POOL,
 };
 
 struct bpf_prog_offload_ops;
@@ -927,9 +927,9 @@ struct netdev_bpf {
 		struct {
 			struct bpf_offloaded_map *offmap;
 		};
-		/* XDP_SETUP_XSK_UMEM */
+		/* XDP_SETUP_XSK_POOL */
 		struct {
-			struct xdp_umem *umem;
+			struct xsk_buff_pool *pool;
 			u16 queue_id;
 		} xsk;
 	};
