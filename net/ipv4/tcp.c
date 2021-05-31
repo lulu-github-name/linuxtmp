f4492bfe9b39370f8c7e512254b23d586abb211a
Jiri Benc <jbenc@redhat.com>
2021-05-31 16:08:56 +0200

    tcp: move mdev_us init to tcp_disconnect()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1882710
    
    Conflicts:
    - [minor] tcp_disconnect: different context due to missing
      3f6c65d6255a ("tcp: ignore rcv_rtt sample with old ts ecr value") and due
      to out of order backport of 0f31746452e6 ("tcp: annotate tp->write_seq
      lockless reads")
    
    commit b9e2e689aab293c3da0ceac0921449a07b692f1f
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Thu Jan 17 11:23:36 2019 -0800
    
        tcp: move mdev_us init to tcp_disconnect()
    
        If we make sure a listener always has its mdev_us
        field set to TCP_TIMEOUT_INIT, we do not need to rewrite
        this field after a new clone is created.
    
        tcp_disconnect() is very seldom used in real applications.
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2617,6 +2617,7 @@ int tcp_disconnect(struct sock *sk, int flags)
 	sk->sk_shutdown = 0;
 	sock_reset_flag(sk, SOCK_DONE);
 	tp->srtt_us = 0;
+	tp->mdev_us = jiffies_to_usecs(TCP_TIMEOUT_INIT);
 
 	seq = tp->write_seq + tp->max_window + 2;
 	if (!seq)
