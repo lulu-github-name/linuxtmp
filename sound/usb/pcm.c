25aaa050f59e6c531617c3e6ce89ecf7ffa66de5
Jaroslav Kysela <jkysela@redhat.com>
2021-06-08 09:44:25 +0200

    ALSA: usb-audio: Always set up the parameters after resume
    The commit 92adc96f8eec ("ALSA: usb-audio: set the interface format
    after resume on Dell WD19") introduced the workaround for the broken
    setup after the resume specifically on a Dell dock model.  However,
    the full setup should have been performed after the resume on all
    devices, as we can't guarantee the same state.  So this patch removes
    the conditional check and applies the workaround always.
    
        Tested-by: Keith Milner <kamilner@superlative.org>
        Tested-by: Dylan Robinson <dylan_robinson@motu.com>
        Link: https://lore.kernel.org/r/20201123085347.19667-24-tiwai@suse.de
        Signed-off-by: Takashi Iwai <tiwai@suse.de>
    
    Author: Takashi Iwai <tiwai@suse.de>
    Date: Mon Nov 23 09:53:29 2020 +0100
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>
    (cherry picked from commit 75c16b5147ee42270b18b5f32bc3f17f8b74b5eb)
    Bugzilla: https://bugzilla.redhat.com/1917388

--- sound/usb/pcm.c
+++ sound/usb/pcm.c
@@ -1814,6 +1814,9 @@ static int snd_usb_substream_playback_trigger(struct snd_pcm_substream *substrea
 					      subs);
 		subs->running = 1;
 		return 0;
+	case SNDRV_PCM_TRIGGER_SUSPEND:
+		subs->need_setup_fmt = true;
+		fallthrough;
 	case SNDRV_PCM_TRIGGER_STOP:
 		stop_endpoints(subs);
 		snd_usb_endpoint_set_callback(subs->data_endpoint,
@@ -1828,13 +1831,6 @@ static int snd_usb_substream_playback_trigger(struct snd_pcm_substream *substrea
 					      subs);
 		subs->running = 0;
 		return 0;
-	case SNDRV_PCM_TRIGGER_SUSPEND:
-		if (subs->stream->chip->setup_fmt_after_resume_quirk) {
-			stop_endpoints(subs);
-			subs->need_setup_fmt = true;
-			return 0;
-		}
-		break;
 	}
 
 	return -EINVAL;
@@ -1858,6 +1854,9 @@ static int snd_usb_substream_capture_trigger(struct snd_pcm_substream *substream
 					      subs);
 		subs->running = 1;
 		return 0;
+	case SNDRV_PCM_TRIGGER_SUSPEND:
+		subs->need_setup_fmt = true;
+		fallthrough;
 	case SNDRV_PCM_TRIGGER_STOP:
 		stop_endpoints(subs);
 		fallthrough;
@@ -1866,13 +1865,6 @@ static int snd_usb_substream_capture_trigger(struct snd_pcm_substream *substream
 					      NULL, NULL, NULL);
 		subs->running = 0;
 		return 0;
-	case SNDRV_PCM_TRIGGER_SUSPEND:
-		if (subs->stream->chip->setup_fmt_after_resume_quirk) {
-			stop_endpoints(subs);
-			subs->need_setup_fmt = true;
-			return 0;
-		}
-		break;
 	}
 
 	return -EINVAL;
